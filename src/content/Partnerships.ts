1efff485eee4772acf286dfc392af8fb3c02cbc0
cherik <pourcheriki@gmail.com>
2022-01-18 21:55:44 +0330

    add content

---
+++ src/content/Partnerships.ts
@@ -0,0 +1,95 @@
+import Discourse from '../../public/images/partnerships/Discourse.svg';
+import Alibre from '../../public/images/partnerships/Alibre.svg';
+import Maker_DAO from '../../public/images/partnerships/Maker_DAO.svg';
+import district0x from '../../public/images/partnerships/district0x.svg';
+import MyCrypto from '../../public/images/partnerships/MyCrypto.svg';
+import SALT_lending from '../../public/images/partnerships/SALT_lending.svg';
+import CanYa from '../../public/images/partnerships/CanYa.svg';
+import Alice from '../../public/images/partnerships/Alice.svg';
+import Aragon from '../../public/images/partnerships/Aragon.svg';
+import MyEtherWallet from '../../public/images/partnerships/MyEtherWallet.svg';
+import Status_im from '../../public/images/partnerships/Status.im.svg';
+import Swarm_City from '../../public/images/partnerships/Swarm_City.svg';
+import The_Commons_Stack from '../../public/images/partnerships/The_Commons_Stack.svg';
+import PrimeDAO from '../../public/images/partnerships/PrimeDAO.svg';
+
+const Partnerships = [
+	{
+		icon: Discourse,
+		title: 'Discourse',
+		description:
+			'A protocol that guards personal data, provides safe storage and enables ethical data exchange.',
+	},
+	{
+		icon: Alibre,
+		title: 'Alibre',
+		description: 'Financial Services for All',
+	},
+	{
+		icon: Maker_DAO,
+		title: 'Maker DAO',
+		description: 'Transparent and sustainable finance',
+	},
+	{
+		icon: district0x,
+		title: 'district0x',
+		description: 'A network of decentralized markets and communities',
+	},
+	{
+		icon: MyCrypto,
+		title: 'MyCrypto',
+		description:
+			'A free, open-source interface for interacting with the blockchain',
+	},
+	{
+		icon: SALT_lending,
+		title: 'SALT lending',
+		description: 'FinTech Solutions for Blockchain Assets',
+	},
+	{
+		icon: CanYa,
+		title: 'CanYa',
+		description: 'The decentralised serviceplace for the world',
+	},
+	{
+		icon: Alice,
+		title: 'Alice',
+		description:
+			'A platform that brings transparency to social funding through blockchain technology',
+	},
+	{
+		icon: Aragon,
+		title: 'Aragon',
+		description: 'Build unstoppable organizations on Ethereum',
+	},
+	{
+		icon: MyEtherWallet,
+		title: 'MyEtherWallet',
+		description:
+			'A free, open-source, client-side interface for generating Ethereum wallets & more.',
+	},
+	{
+		icon: Status_im,
+		title: 'Status.im',
+		description:
+			'A platform that brings transparency to social funding through blockchain technology',
+	},
+	{
+		icon: Swarm_City,
+		title: 'Swarm City',
+		description:
+			'Blockchain based marketplace with a built in reputation system',
+	},
+	{
+		icon: The_Commons_Stack,
+		title: 'The Commons Stack',
+		description: 'Realigning incentives around public goods',
+	},
+	{
+		icon: PrimeDAO,
+		title: 'PrimeDAO',
+		description: 'An Adoption Engine for Open Finance',
+	},
+];
+
+export default Partnerships;
