c1975d1d103963784d8f1bdcc42dd06679df3899
cherik <pourcheriki@gmail.com>
2022-01-18 16:07:25 +0330

    add header wrapper

---
+++ src/components/Header/HeaderWrapper.tsx
@@ -0,0 +1,7 @@
+import { useGeneral } from '@/context/general.context';
+import Header from './Header';
+
+export const HeaderWrapper = () => {
+	const { showHeader } = useGeneral();
+	return showHeader ? <Header /> : null;
+};
