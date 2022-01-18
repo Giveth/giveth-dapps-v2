113295bd2d625f7597e80a1ea032eee2118f5410
cherik <pourcheriki@gmail.com>
2022-01-18 16:08:14 +0330

    hide header in claim route

--- pages/claim.tsx
+++ pages/claim.tsx
@@ -2,8 +2,19 @@ import Head from 'next/head';
 import ClaimView from '@/components/views/claim/Claim.view';
 import { UserProvider } from '@/context/user.context';
 import { Toaster } from 'react-hot-toast';
+import { useGeneral } from '@/context/general.context';
+import { useEffect } from 'react';
 
 export default function GIVdropRoute() {
+	const { setShowHeader } = useGeneral();
+
+	useEffect(() => {
+		setShowHeader(false);
+		return () => {
+			setShowHeader(true);
+		};
+	}, [setShowHeader]);
+
 	return (
 		<>
 			<Head>
--- src/components/views/claim/Claim.view.tsx
+++ src/components/views/claim/Claim.view.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext, useState } from 'react';
+import React, { FC, useContext } from 'react';
 import config from '@/configuration';
 import styled from 'styled-components';
 import { CongratulationsCard } from '../../cards/Congratulations';
