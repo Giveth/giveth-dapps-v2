e4bff85a0ff3cb4d50469306d36180fef19683b5
cherik <pourcheriki@gmail.com>
2022-01-19 14:31:55 +0330

    add io about view

---
+++ src/components/views/about/AboutTeam.tsx
@@ -0,0 +1,43 @@
+import TeamCard from '@/components/TeamCard';
+import GivethTeam from '@/content/GivethTeam.json';
+import { Lead, brandColors, H2 } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+
+const AboutTeam = () => {
+	return (
+		<Wrapper>
+			<Title>Meet our team</Title>
+			<Caption>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus
+				urna orci vel commodo.
+			</Caption>
+			<TeamCards>
+				{GivethTeam.map(i => (
+					<TeamCard key={i.name} member={i} />
+				))}
+			</TeamCards>
+		</Wrapper>
+	);
+};
+
+const TeamCards = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 24px;
+`;
+
+const Caption = styled(Lead)`
+	color: ${brandColors.giv[800]};
+	margin-bottom: 76px;
+`;
+
+const Title = styled(H2)`
+	color: ${brandColors.giv[700]};
+	margin-bottom: 24px;
+`;
+
+const Wrapper = styled.div`
+	text-align: center;
+`;
+
+export default AboutTeam;
