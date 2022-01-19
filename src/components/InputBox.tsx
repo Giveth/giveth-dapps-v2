15b846fe43f5498b51aa842c9ddf3124bbd741a5
cherik <pourcheriki@gmail.com>
2022-01-19 14:34:58 +0330

    add io InputBox

---
+++ src/components/InputBox.tsx
@@ -0,0 +1,51 @@
+import { neutralColors, B } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+
+type OnChangeFunction = (e: any) => void;
+
+const InputBox = (props: {
+	onChange: OnChangeFunction;
+	placeholder?: string;
+}) => {
+	const { onChange, placeholder } = props;
+	return (
+		<Wrapper>
+			<B className='w-100 mr-2' color={neutralColors.gray[900]}>
+				<Input
+					onChange={(e: any) => onChange(e.target.value)}
+					placeholder={placeholder || 'Search Projects...'}
+				/>
+			</B>
+		</Wrapper>
+	);
+};
+
+const Input = styled.input`
+	border-width: 0;
+	height: 100%;
+	width: 100%;
+	font-weight: inherit;
+	background: inherit;
+	font-family: inherit;
+
+	&:focus {
+		outline: none;
+	}
+
+	&::placeholder {
+		color: ${neutralColors.gray[500]};
+	}
+`;
+
+const Wrapper = styled.div`
+	min-width: 343px;
+	max-width: 610px;
+	height: 54px;
+	padding: 5px 16px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin: 0 auto;
+`;
+
+export default InputBox;
