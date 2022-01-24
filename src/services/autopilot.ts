cb08ec75077d33e390a43d5a8f449502ab8e355d
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-01-24 20:14:09 -0300

    hotfix: eslint error

--- src/services/autopilot.ts
+++ src/services/autopilot.ts
@@ -1,9 +1,10 @@
+/* eslint-disable prettier/prettier */
 import axios from 'axios';
 
 export const autopilotClient = axios.create({
   baseURL: 'https://api2.autopilothq.com/v1/',
   headers: {
     'Content-Type': 'application/json',
-    autopilotapikey: process.env.NEXT_PUBLIC_AUTOPILOT_ID as string,
+    'autopilotapikey': process.env.NEXT_PUBLIC_AUTOPILOT_ID as string,
   },
 });
