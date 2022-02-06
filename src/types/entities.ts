71b30a92baa07f740b614d15edb5dffc34c6e20c
cherik <pourcheriki@gmail.com>
2022-02-06 12:19:44 +0330

    add user entity type

---
+++ src/types/entities.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+	avatar: string;
+	email: string;
+	firstName: string;
+	id: string;
+	lastName: string;
+	location: string;
+	name: string;
+	url: string;
+	walletAddress: string;
+}
