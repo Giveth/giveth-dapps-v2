4288d036c16327b83b28d465fc823741b7ba9f2c
cherik <pourcheriki@gmail.com>
2022-02-07 13:12:36 +0330

    add user contribute cards

--- src/components/views/UserPublicProfile.view.tsx
+++ src/components/views/UserPublicProfile.view.tsx
@@ -3,10 +3,13 @@ import {
 	brandColors,
 	Container,
 	GLink,
+	H2,
 	H3,
+	H5,
 	IconCopy,
 	IconExternalLink,
 	neutralColors,
+	Subline,
 } from '@giveth/ui-design-system';
 import { FC } from 'react';
 import styled from 'styled-components';
@@ -53,6 +56,35 @@ const UserPublicProfileView: FC<UserPublicProfileView> = ({ user }) => {
 					</UserInfoWithAvatarRow>
 				</Container>
 			</PubliCProfileHeader>
+			<UserContributeInfo>
+				<Container>
+					<UserContributeTitle
+						weight={700}
+					>{`${user.name}’s donations & projects`}</UserContributeTitle>
+					<ContributeCardContainer>
+						<ContributeCard>
+							<ContributeCardTitles>
+								donations
+							</ContributeCardTitles>
+							<ContributeCardTitles>
+								Total amount donated
+							</ContributeCardTitles>
+							<H2>2</H2>
+							<H5>$50.32</H5>
+						</ContributeCard>
+						<ContributeCard>
+							<ContributeCardTitles>
+								Projects
+							</ContributeCardTitles>
+							<ContributeCardTitles>
+								Donation received
+							</ContributeCardTitles>
+							<H2>1</H2>
+							<H5>$780.18</H5>
+						</ContributeCard>
+					</ContributeCardContainer>
+				</Container>
+			</UserContributeInfo>
 		</>
 	);
 };
@@ -86,4 +118,30 @@ const WalletContainer = styled(Row)`
 const WalletIconsContainer = styled.div`
 	color: ${brandColors.pinky[500]};
 	cursor: pointer;
+`;
+
+const UserContributeInfo = styled.div`
+	padding: 40px 0 60px;
+`;
+
+const UserContributeTitle = styled(H5)`
+	margin-bottom: 16px;
+`;
+
+const ContributeCardContainer = styled(Row)`
+	gap: 16px;
+`;
+
+const ContributeCard = styled.div`
+	background: ${brandColors.giv['000']};
+	box-shadow: 0px 3px 20px rgba(212, 218, 238, 0.4);
+	border-radius: 12px;
+	display: grid;
+	padding: 24px;
+	width: 556px;
+	grid-template-columns: 1fr 1fr;
+`;
+
+const ContributeCardTitles = styled(Subline)`
+	text-transform: uppercase;
 `;
