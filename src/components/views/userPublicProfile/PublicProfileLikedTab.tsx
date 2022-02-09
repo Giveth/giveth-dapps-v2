b77dc6690a1adec6b010d43a4c2da52557e9b2af
cherik <pourcheriki@gmail.com>
2022-02-09 13:33:42 +0330

    add liked project tab pagination

--- src/components/views/userPublicProfile/PublicProfileLikedTab.tsx
+++ src/components/views/userPublicProfile/PublicProfileLikedTab.tsx
@@ -1,25 +1,21 @@
 import { client } from '@/apollo/apolloClient';
 import { FETCH_USER_LIKED_PROJECTS } from '@/apollo/gql/gqlProjects';
-import { FETCH_USER_PROJECTS } from '@/apollo/gql/gqlUser';
 import { IUserLikedProjects } from '@/apollo/types/gqlTypes';
 import { IProject } from '@/apollo/types/types';
+import Pagination from '@/components/Pagination';
 import ProjectCard from '@/components/project-card/ProjectCard';
-import { mediaQueries } from '@/lib/helpers';
-import {
-	brandColors,
-	Container,
-	neutralColors,
-	P,
-} from '@giveth/ui-design-system';
 import { FC, useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { Row } from '../../styled-components/Grid';
 import { ProjectsContainer } from './PublicProfileProjectsTab';
 import { IUserPublicProfileView } from './UserPublicProfile.view';
+
+const itemPerPage = 6;
 
 const PublicProfileLikedTab: FC<IUserPublicProfileView> = ({ user }) => {
 	const [loading, setLoading] = useState(false);
 	const [projects, setProjects] = useState<IProject[]>([]);
+	const [totalCount, setTotalCount] = useState<number>(0);
+	const [page, setPage] = useState(0);
+
 	useEffect(() => {
 		if (!user) return;
 		const fetchUserProjects = async () => {
@@ -28,23 +24,20 @@ const PublicProfileLikedTab: FC<IUserPublicProfileView> = ({ user }) => {
 				query: FETCH_USER_LIKED_PROJECTS,
 				variables: {
 					userId: parseFloat(user.id) || -1,
-					take: 100,
-					skip: 0,
+					take: itemPerPage,
+					skip: page * itemPerPage,
 				},
-				fetchPolicy: 'network-only',
 			});
 			setLoading(false);
-			console.log('userLikedProjects', userLikedProjects);
 			if (userLikedProjects?.likedProjectsByUserId) {
 				const likedProjectsByUserId: IUserLikedProjects =
 					userLikedProjects.likedProjectsByUserId;
 				setProjects(likedProjectsByUserId.projects);
+				setTotalCount(likedProjectsByUserId.totalCount);
 			}
 		};
 		fetchUserProjects();
-	}, [user]);
-
-	console.log('projects', projects);
+	}, [page, user]);
 
 	return (
 		<>
@@ -54,6 +47,12 @@ const PublicProfileLikedTab: FC<IUserPublicProfileView> = ({ user }) => {
 					<ProjectCard key={project.id} project={project} />
 				))}
 			</ProjectsContainer>
+			<Pagination
+				currentPage={page}
+				totalCount={totalCount}
+				setPage={setPage}
+				itemPerPage={itemPerPage}
+			/>
 		</>
 	);
 };
