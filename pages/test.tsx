4488405b7a072e74c36095a677bb252a60061fa0
Cherik <Pourcheriki@gmail.com>
2022-02-14 18:52:46 +0330

    update test page

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,14 +2,15 @@ import Head from 'next/head';
 import toast from 'react-hot-toast';
 import styled from 'styled-components';
 import { Toaster } from 'react-hot-toast';
-import { gToast, ToastDirection, ToastType } from '@/components/toasts/success';
+import { gToast, ToastDirection, ToastType } from '@/components/toasts';
 
 const TestRoute = () => {
 	const notify = () =>
 		gToast('Testeeee', {
 			type: ToastType.SUCCESS,
-			direction: ToastDirection.RIGHT,
+			// direction: ToastDirection.RIGHT,
 			title: 'test',
+			dismissLabel: 'OK :D',
 		});
 	return (
 		<>
