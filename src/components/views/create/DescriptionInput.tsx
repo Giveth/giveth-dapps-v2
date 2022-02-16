c619c3bdc7ed5c65b344ec413618bbdc7559a75d
Ramin <r.ramazanpour@aut.ac.ir>
2022-02-17 01:44:17 +0330

    add error to description field

--- src/components/views/create/DescriptionInput.tsx
+++ src/components/views/create/DescriptionInput.tsx
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 import { H5, Caption, brandColors } from '@giveth/ui-design-system';
 import dynamic from 'next/dynamic';
 import styled from 'styled-components';
-import { InputContainer, Label } from './Create.sc';
+import { InputContainer, InputErrorMessage, Label } from './Create.sc';
 import { GoodProjectDescription } from '@/components/modals/GoodProjectDescription';
+import { ECreateErrFields } from '@/components/views/create/CreateIndex';
 
 const RichTextInput = dynamic(() => import('@/components/RichTextInput'), {
 	ssr: false,
 });
 
-const DescriptionInput = (props: { setValue: (e: string) => void }) => {
+const DescriptionInput = (props: {
+	setValue: (e: string) => void;
+	error: string;
+}) => {
 	const [showModal, setShowModal] = useState(false);
-	const { setValue } = props;
+	const { setValue, error } = props;
 	return (
 		<>
 			{showModal && (
@@ -21,7 +25,9 @@ const DescriptionInput = (props: { setValue: (e: string) => void }) => {
 				/>
 			)}
 
-			<H5>Tell us about your project...</H5>
+			<H5 id={ECreateErrFields.DESCRIPTION}>
+				Tell us about your project...
+			</H5>
 			<div>
 				<CaptionContainer>
 					Aim for 200-500 words.{' '}
@@ -30,7 +36,7 @@ const DescriptionInput = (props: { setValue: (e: string) => void }) => {
 					</span>
 				</CaptionContainer>
 			</div>
-			<InputContainer>
+			<InputContainerStyled error={error}>
 				<Label>Project story</Label>
 				<RichTextInput
 					style={TextInputStyle}
@@ -38,10 +44,27 @@ const DescriptionInput = (props: { setValue: (e: string) => void }) => {
 					autoFocus
 					onChange={setValue}
 				/>
-			</InputContainer>
+			</InputContainerStyled>
+			<ErrorStyled>{error || null}</ErrorStyled>
 		</>
 	);
 };
+
+const InputContainerStyled = styled(InputContainer)<{ error: string }>`
+	.ql-container.ql-snow {
+		border: ${props =>
+			props.error && `2px solid ${brandColors.pinky[500]}`};
+	}
+	.ql-toolbar.ql-snow {
+		border: ${props =>
+			props.error && `2px solid ${brandColors.pinky[500]}`};
+	}
+`;
+
+const ErrorStyled = styled(InputErrorMessage)`
+	margin-top: -10px;
+	margin-bottom: 20px;
+`;
 
 const CaptionContainer = styled(Caption)`
 	height: 18px;
