3971f063638bef2275b004a738da751a735c5871
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-02-17 09:51:41 -0300

    hotfix: update text and slider functionality

--- src/components/views/about/AboutHistory.tsx
+++ src/components/views/about/AboutHistory.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import { FlexCenter } from '../../styled-components/Grid';
 import styled from 'styled-components';
-import { brandColors, H3, H4, Lead } from '@giveth/ui-design-system';
+import {
+	brandColors,
+	ButtonLink,
+	H3,
+	H4,
+	Lead,
+	neutralColors,
+} from '@giveth/ui-design-system';
 
 const AboutHistory = () => {
+	const [sliderSection, setSliderSection] = useState<number>(0);
+
 	return (
 		<>
 			<Upper>
 				<Rect>
-					<span>The Giveth</span> Decentralized Altruistic Community
-					(DAC) is an eclectic mix of people from the Ethereum and
-					nonprofit worlds.
+					<span>Giveth</span> was founded in 2016 and provided value
+					to the Blockchain4Good ecosystem for 5 years, funded solely
+					by donations.
 				</Rect>
 				<UpperText>
 					They are dedicated to building the tools that will make it
@@ -20,41 +30,59 @@ const AboutHistory = () => {
 				</UpperText>
 			</Upper>
 			<End>
-				<Title>Some title goes here</Title>
-				<Lead>
-					We are Altruistic, we are entirely open-source and love to
-					reach out proactively to similar initiatives, which we see
-					as potential collaborators as opposed to competitors. The
-					Giveth DAC works with volunteers, and genius developers who
-					are working for a fraction of what they could be earning at
-					for-profit Ethereum projects. To help us in building Giveth,
-					please visit the contributors guide.
-				</Lead>
+				{sliderContent.map((elem, index) => (
+					<SliderTextSection
+						key={`slider-content-${index}`}
+						active={sliderSection === index}
+					>
+						<Title>{elem.title}</Title>
+						<Lead>{elem.body}</Lead>
+					</SliderTextSection>
+				))}
+
 				<SliderButtons>
-					<SliderButton className='active' />
-					<SliderButton />
-					<SliderButton />
+					{sliderContent.map((elem, index) => (
+						<SliderButton
+							key={`slider-button-${index}`}
+							active={sliderSection === index}
+							onClick={() => setSliderSection(index)}
+						/>
+					))}
 				</SliderButtons>
 			</End>
 		</>
 	);
 };
 
-const SliderButton = styled.div`
+const SliderButton = styled.div<{ active: boolean }>`
 	border-radius: 50%;
 	width: 23px;
 	height: 23px;
 	cursor: pointer;
 	border: 2px solid ${brandColors.giv[500]};
-
-	&.active {
-		background: ${brandColors.giv[500]};
-	}
+	background: ${props =>
+		props.active ? brandColors.giv[500] : 'transparent'};
 `;
 
 const SliderButtons = styled(FlexCenter)`
 	gap: 13px;
 	margin: 66px auto 0 auto;
+`;
+
+const SliderTextSection = styled.div<{ active: boolean }>`
+	display: ${props => (props.active ? 'flex' : 'none')};
+	flex-direction: column;
+	place-items: center;
+	min-height: 170px;
+`;
+
+const ReadMoreButton = styled(ButtonLink)`
+	background-color: ${brandColors.pinky[500]};
+	margin-top: 32px;
+
+	&:hover {
+		background-color: ${brandColors.pinky[500]}80;
+	}
 `;
 
 const Title = styled(H3)`
@@ -71,6 +99,7 @@ const UpperText = styled(Lead)`
 	color: ${brandColors.giv[900]};
 	max-width: 430px;
 	margin-top: 115px;
+	position: relative;
 `;
 
 const Rect = styled(H4)`
@@ -90,5 +119,30 @@ const Upper = styled.div`
 	flex-wrap: wrap;
 	gap: 0 50px;
 `;
+
+const sliderContent = [
+	{
+		title: 'Rising out of the ashes of TheDAO',
+		body: 'The Giveth founding team was a group of altruistic whitehat hackers who set out to build systems to turn non-profits into DAOs of their own, starting with platforms for P2P giving on the blockchain.',
+	},
+	{
+		title: 'Starting with traceable donations',
+		body: 'After writing and open-sourcing innovative smart contracts like the MiniMe token, the team launched the first Giveth DApp, now called Giveth TRACE, to enable a traceable & accountable donations to for-good Campaigns.',
+	},
+	{
+		title: 'And now... the GIVeconomy',
+		body: 'Building on that foundation and heaven-bent on using blockchain to fund public goods regeneratively, we created the GIVeconomy. Fueled by GIV, the GIVeconomy is rewarding & empowering all who give.',
+	},
+	{
+		title: 'Take a dive into the History of Giveth',
+		body: (
+			<ReadMoreButton
+				href='https://docs.giveth.io/whatisgiveth/history/'
+				target='_blank'
+				label='READ MORE'
+			/>
+		),
+	},
+];
 
 export default AboutHistory;
