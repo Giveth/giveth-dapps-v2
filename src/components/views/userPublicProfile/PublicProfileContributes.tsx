38ef998f15ceecbafe24782115b9309c9a7b4fc3
Mateo Daza <mateodaza@gmail.com>
2022-02-23 20:14:38 -0500

    Update PublicProfileContributes.tsx

--- src/components/views/userPublicProfile/PublicProfileContributes.tsx
+++ src/components/views/userPublicProfile/PublicProfileContributes.tsx
@@ -20,6 +20,10 @@ enum EPublicProfile {
 	LIKED,
 }
 
+interface ITab {
+	active: boolean;
+}
+
 const PublicProfileContributes: FC<IUserPublicProfileView> = ({
 	user,
 	myAccount,
@@ -50,6 +54,11 @@ const PublicProfileContributes: FC<IUserPublicProfileView> = ({
 						onClick={() => setTab(EPublicProfile.DONATIONS)}
 					>
 						{`${myAccount ? 'My ' : ''}Donations`}
+						{myAccount && (
+							<Count active={tab === EPublicProfile.DONATIONS}>
+								{user?.donationsCount}
+							</Count>
+						)}
 					</PubliCProfileTab>
 					<PubliCProfileTab
 						active={tab === EPublicProfile.LIKED}
@@ -93,14 +102,14 @@ const PubliCProfileTabsContainer = styled(Row)`
 	gap: 16px;
 `;
 
-interface ITab {
-	active?: boolean;
-}
-
 const PubliCProfileTab = styled(P)<ITab>`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 	padding: 9px 16px;
-	color: ${brandColors.pinky[500]};
 	cursor: pointer;
+	color: ${(props: ITab) =>
+		props.active ? brandColors.deep[600] : brandColors.pinky[500]};
 	${props =>
 		props.active &&
 		`
@@ -108,4 +117,14 @@ const PubliCProfileTab = styled(P)<ITab>`
 		box-shadow: 0px 3px 20px rgba(212, 218, 238, 0.4);
 		border-radius: 50px;
 	`}
+`;
+
+const Count = styled.div`
+	background-color: ${(props: ITab) =>
+		props.active ? neutralColors.gray[500] : brandColors.pinky[500]};
+	color: white;
+	border-radius: 50%;
+	padding: 2px 9px;
+	font-size: 12px;
+	margin-left: 4px;
 `;
