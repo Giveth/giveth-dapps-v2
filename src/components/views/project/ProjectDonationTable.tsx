5f6c03cd4dc367cbe04d725f48ebd105d936bedf
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-02-25 09:42:04 -0300

    feat: added new table with pagination and sort functionallity

--- src/components/views/project/ProjectDonationTable.tsx
+++ src/components/views/project/ProjectDonationTable.tsx
@@ -1,58 +1,114 @@
-import { useEffect, useRef, useState } from 'react';
-import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import {
-	neutralColors,
+	B,
 	brandColors,
 	H6,
-	Subline,
+	IconArrowBottom,
+	IconArrowTop,
+	IconExternalLink,
+	IconSort16,
+	neutralColors,
+	P,
+	SublineBold,
 } from '@giveth/ui-design-system';
-import styled from 'styled-components';
-
-import SearchBox from '@/components/SearchBox';
-import { formatDateFromString, formatTxLink } from '@/lib/helpers';
-import linkIcon from '/public/images/external_link.svg';
-import donorProfileIcon from '/public/images/default_donor.svg';
-import ExternalLink from '@/components/ExternalLink';
-import { initializeApollo } from '@/apollo/apolloClient';
+import { client } from '@/apollo/apolloClient';
 import { FETCH_PROJECT_DONATIONS } from '@/apollo/gql/gqlDonations';
-import { IDonationsByProjectId } from '@/apollo/types/gqlTypes';
+import { IDonation } from '@/apollo/types/types';
+import SearchBox from '@/components/SearchBox';
+import { Row } from '@/components/styled-components/Grid';
 import Pagination from '@/components/Pagination';
+import { networksParams } from '@/helpers/blockchain';
+import { smallFormatDate } from '@/lib/helpers';
 
-const ProjectDonationTable = (props: {
-	donations: IDonationsByProjectId;
-	projectId: string;
-}) => {
-	const { donations, totalCount } = props.donations;
-	const [activeTab, setActiveTab] = useState(0);
-	const [currentPage, setCurrentPage] = useState(0);
-	const [nDonations, setNDonations] = useState(donations);
+const itemPerPage = 10;
 
-	const firstRender = useRef(true);
+enum EOrderBy {
+	TokenAmount = 'TokenAmount',
+	UsdAmount = 'UsdAmount',
+	CreationDate = 'CreationDate',
+}
+
+enum EDirection {
+	DESC = 'DESC',
+	ASC = 'ASC',
+}
+
+interface IOrder {
+	by: EOrderBy;
+	direction: EDirection;
+}
+
+const injectSortIcon = (order: IOrder, title: EOrderBy) => {
+	return order.by === title ? (
+		order.direction === EDirection.DESC ? (
+			<IconArrowBottom size={16} />
+		) : (
+			<IconArrowTop size={16} />
+		)
+	) : (
+		<IconSort16 />
+	);
+};
+
+interface IProjectDonationTable {
+	donations: IDonation[];
+	id?: string;
+	showTrace: boolean;
+	totalDonations?: number;
+}
+
+const ProjectDonationTable = ({
+	donations,
+	id,
+	showTrace,
+	totalDonations,
+}: IProjectDonationTable) => {
+	const [pageDonations, setPageDonations] = useState<IDonation[]>(donations);
+	const [page, setPage] = useState<number>(0);
+	const [order, setOrder] = useState<IOrder>({
+		by: EOrderBy.CreationDate,
+		direction: EDirection.DESC,
+	});
+	const [activeTab, setActiveTab] = useState<number>(0);
+
+	const orderChangeHandler = (orderby: EOrderBy) => {
+		if (orderby === order.by) {
+			setOrder({
+				by: orderby,
+				direction:
+					order.direction === EDirection.ASC
+						? EDirection.DESC
+						: EDirection.ASC,
+			});
+		} else {
+			setOrder({
+				by: orderby,
+				direction: EDirection.DESC,
+			});
+		}
+	};
 
 	useEffect(() => {
-		const fetchDonations = () => {
-			initializeApollo()
-				.query({
-					query: FETCH_PROJECT_DONATIONS,
-					variables: {
-						projectId: parseInt(props.projectId),
-						take: donations.length,
-						skip: currentPage * donations.length,
-					},
-				})
-				.then(
-					(res: {
-						data: { donationsByProjectId: IDonationsByProjectId };
-					}) => {
-						setNDonations(res.data.donationsByProjectId.donations);
-					},
-				)
-				.catch(console.log);
+		if (!id) return;
+		const fetchProjectDonations = async () => {
+			const { data: projectDonations } = await client.query({
+				query: FETCH_PROJECT_DONATIONS,
+				variables: {
+					projectId: parseInt(id),
+					take: itemPerPage,
+					skip: page * itemPerPage,
+					orderBy: order.by,
+					direction: order.direction,
+				},
+			});
+			const { donationsByProjectId } = projectDonations;
+			if (!!donationsByProjectId?.donations) {
+				setPageDonations(donationsByProjectId.donations);
+			}
 		};
-
-		if (firstRender.current) firstRender.current = false;
-		else fetchDonations();
-	}, [currentPage, donations.length, props.projectId]);
+		fetchProjectDonations();
+	}, [page, order.by, order.direction, id]);
 
 	return (
 		<Wrapper>
@@ -64,132 +120,115 @@ const ProjectDonationTable = (props: {
 					>
 						Donations
 					</Tab>
-					<Tab
-						onClick={() => setActiveTab(1)}
-						className={activeTab === 1 ? 'active' : ''}
-					>
-						Traces
-					</Tab>
+					{showTrace && (
+						<Tab
+							onClick={() => setActiveTab(1)}
+							className={activeTab === 1 ? 'active' : ''}
+						>
+							Traces
+						</Tab>
+					)}
 				</Tabs>
-				{/*TODO implement search func*/}
 				<SearchBox onChange={() => {}} value='' reset={() => {}} />
 			</UpperSection>
-
 			{activeTab === 0 && (
-				<DonationSection>
-					<Table>
-						<thead>
-							<TableRow>
-								<TableHead>DATE</TableHead>
-								<TableHead>DONOR</TableHead>
-								<TableHead>AMOUNT</TableHead>
-								<TableHead>TX</TableHead>
-							</TableRow>
-						</thead>
-						<tbody>
-							{(nDonations || donations).map(i => {
-								return (
-									<TableRow key={i.id}>
-										<TableData>
-											{formatDateFromString(i.createdAt)}
-										</TableData>
-										<TableDonor>
-											<Image
-												src={donorProfileIcon}
-												alt='user image'
-											/>
-											<div>
-												{i.user?.name ||
-													i.fromWalletAddress}
-											</div>
-										</TableDonor>
-										<TableData>
-											<div>
-												{i.amount + ' ' + i.currency}
-											</div>
-											<UsdValue>
-												{i.valueUsd &&
-													i.valueUsd + ' USD'}
-											</UsdValue>
-										</TableData>
-										<td>
-											<ExternalLink
-												href={formatTxLink(
-													i.transactionNetworkId,
-													i.transactionId,
-												)}
-											>
-												<Image
-													src={linkIcon}
-													alt='link icon'
-												/>
-											</ExternalLink>
-										</td>
-									</TableRow>
-								);
-							})}
-						</tbody>
-					</Table>
-					<Pagination
-						currentPage={currentPage}
-						totalCount={totalCount}
-						setPage={setCurrentPage}
-						itemPerPage={donations.length}
-					/>
-				</DonationSection>
+				<DonationTableContainer>
+					<TableHeader
+						onClick={() =>
+							orderChangeHandler(EOrderBy.CreationDate)
+						}
+					>
+						<B>Donated at</B>
+						{injectSortIcon(order, EOrderBy.CreationDate)}
+					</TableHeader>
+					<TableHeader>
+						<B>Donor</B>
+					</TableHeader>
+					<TableHeader>
+						<B>Currency</B>
+					</TableHeader>
+					<TableHeader
+						onClick={() => orderChangeHandler(EOrderBy.TokenAmount)}
+					>
+						<B>Amount</B>
+						{injectSortIcon(order, EOrderBy.TokenAmount)}
+					</TableHeader>
+					<TableHeader
+						onClick={() => orderChangeHandler(EOrderBy.UsdAmount)}
+					>
+						<B>USD Value</B>
+						{injectSortIcon(order, EOrderBy.UsdAmount)}
+					</TableHeader>
+					{pageDonations.map((donation, idx) => (
+						<RowWrapper key={idx}>
+							<TableCell>
+								<P>
+									{smallFormatDate(
+										new Date(donation.createdAt),
+									)}
+								</P>
+							</TableCell>
+							<TableCell>
+								<P>
+									{donation.user?.name ||
+										donation.user?.firstName}
+								</P>
+							</TableCell>
+							<TableCell>
+								<CurrencyBadge>
+									{donation.currency}
+								</CurrencyBadge>
+							</TableCell>
+							<TableCell>
+								<P>{donation.amount}</P>
+								<TransactionLink
+									href={
+										networksParams[
+											donation.transactionNetworkId
+										]
+											? `${
+													networksParams[
+														donation
+															.transactionNetworkId
+													].blockExplorerUrls[0]
+											  }/tx/${donation.transactionId}`
+											: ''
+									}
+									target='_blank'
+								>
+									<IconExternalLink size={16} />
+								</TransactionLink>
+							</TableCell>
+							<TableCell>
+								<P>{donation.valueUsd?.toFixed(2)}$</P>
+							</TableCell>
+						</RowWrapper>
+					))}
+				</DonationTableContainer>
 			)}
+			<Pagination
+				currentPage={page}
+				totalCount={totalDonations || 0}
+				setPage={setPage}
+				itemPerPage={itemPerPage}
+			/>
 		</Wrapper>
 	);
 };
 
-const Table = styled.table`
-	margin-bottom: 32px;
-	border-collapse: collapse;
-`;
-
-const UsdValue = styled(Subline)`
-	color: ${neutralColors.gray[500]};
-`;
-
-const TableRow = styled.tr`
-	border-bottom: 1px solid ${neutralColors.gray[300]};
-	height: 58px;
-`;
-
-const TableData = styled.td`
-	height: 58px;
-	color: ${brandColors.deep[800]};
-	font-size: 14px;
-	line-height: 150%;
-	padding-right: 35px;
-`;
-
-const TableDonor = styled(TableData)`
-	color: ${brandColors.pinky[500]};
+const Wrapper = styled.div`
+	margin: 50px 0px 32px;
 	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	max-width: 750px;
+`;
+
+const UpperSection = styled.div`
+	display: flex;
+	gap: 30px;
 	align-items: center;
-	gap: 8px;
-`;
-
-const TableHead = styled.th`
-	font-weight: 500;
-	color: ${brandColors.giv[800]};
-	font-size: 10px;
-	line-height: 13px;
-	text-align: left;
-`;
-
-const DonationSection = styled.div`
-	margin-top: 30px;
-`;
-
-const Tab = styled(H6)`
-	font-weight: 400;
-	cursor: pointer;
-
-	&.active {
-		font-weight: 700;
-	}
+	max-width: 750px;
 `;
 
 const Tabs = styled.div`
@@ -202,16 +241,62 @@ const Tabs = styled.div`
 	}
 `;
 
-const UpperSection = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	gap: 30px;
-	align-items: center;
+const Tab = styled(H6)`
+	font-weight: 400;
+	cursor: pointer;
+
+	&.active {
+		font-weight: 700;
+	}
+`;
+
+const DonationTableContainer = styled.div`
+	margin-top: 12px;
+	display: grid;
+	grid-template-columns: 1.25fr 2fr 1fr 1fr 1fr;
+	width: 100%;
 	max-width: 750px;
 `;
 
-const Wrapper = styled.div`
-	margin-top: 50px;
+const TableHeader = styled(Row)`
+	height: 40px;
+	border-bottom: 1px solid ${neutralColors.gray[400]};
+	align-items: center;
+	${props =>
+		props.onClick &&
+		`cursor: pointer;
+	gap: 8px;
+	align-items: center;`}
+`;
+
+const RowWrapper = styled.div`
+	display: contents;
+	&:hover > div {
+		background-color: ${neutralColors.gray[300]};
+		color: ${brandColors.pinky[500]};
+	}
+	& > div:first-child {
+		padding-left: 4px;
+	}
+`;
+
+const TableCell = styled(Row)`
+	height: 60px;
+	border-bottom: 1px solid ${neutralColors.gray[300]};
+	align-items: center;
+	gap: 8px;
+`;
+
+const CurrencyBadge = styled(SublineBold)`
+	padding: 2px 8px;
+	border: 2px solid ${neutralColors.gray[400]};
+	border-radius: 50px;
+	color: ${neutralColors.gray[700]};
+`;
+
+const TransactionLink = styled.a`
+	cursor: pointer;
+	color: ${brandColors.pinky[500]};
 `;
 
 export default ProjectDonationTable;
