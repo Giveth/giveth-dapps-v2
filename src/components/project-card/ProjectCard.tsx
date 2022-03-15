bfecd0fbc43382922da3ba55f0c1ec2e1617a289
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-03-15 17:09:15 -0300

    feat: added org badge

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
 import {
 	GLink,
@@ -11,6 +12,7 @@ import {
 	OutlineLinkButton,
 } from '@giveth/ui-design-system';
 import ProjectCardBadges from './ProjectCardBadges';
+import ProjectCardOrgBadge from './ProjectCardOrgBadge';
 import { IProject } from '@/apollo/types/types';
 import { calcBiggestUnitDiffernceTime, htmlToText } from '@/lib/helpers';
 import ProjectCardImage from './ProjectCardImage';
@@ -60,6 +62,10 @@ const ProjectCard = (props: IProjectCard) => {
 					projectHref={slug}
 					projectDescription={description}
 					projectId={id}
+				/>
+				<ProjectCardOrgBadge
+					image={'/images/thegivingblock.svg'}
+					isHover={isHover}
 				/>
 				<ProjectCardImage image={image} />
 			</ImagePlaceholder>
@@ -135,7 +141,7 @@ const CardBody = styled.div`
 		props.isHover ? '136px' : '200px'};
 	background-color: ${neutralColors.gray[100]};
 	transition: top 0.3s ease;
-	border-radius: 12px;
+	border-radius: 0px 12px 12px 12px;
 `;
 
 const Author = styled(GLink)`
---
+++ src/components/project-card/ProjectCardOrgBadge.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+import { neutralColors, Overline } from '@giveth/ui-design-system';
+
+interface IProjectCardOrgBadge {
+	image: string;
+	isHover: boolean;
+}
+
+const ProjectCardOrgBadge = ({ image, isHover }: IProjectCardOrgBadge) => {
+	return (
+		<GivingBadgeContainer isHover={isHover}>
+			<GivingBlocksText>PROJECT BY:</GivingBlocksText>
+			<Image
+				src={image}
+				alt='The Giving Block icon.'
+				height={36}
+				width={78}
+			/>
+		</GivingBadgeContainer>
+	);
+};
+
+const GivingBadgeContainer = styled.div<{ isHover: boolean }>`
+	height: 36px;
+	width: 200px;
+	background-color: ${neutralColors.gray[200]};
+	display: flex;
+	align-items: center;
+	gap: 8px;
+	padding: 0 24px;
+	position: absolute;
+	bottom: ${props => (props.isHover ? '88px' : '24px')};
+	transition: bottom 0.3s ease;
+	border-radius: 0px 12px 0px 0px;
+`;
+
+const GivingBlocksText = styled(Overline)`
+	color: ${neutralColors.gray[600]};
+	font-size: 10px;
+`;
+
+export default ProjectCardOrgBadge;
