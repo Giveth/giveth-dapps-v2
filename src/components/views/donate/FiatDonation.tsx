3768f5e4cccce78139c9368a9cd736030c26d6c4
Ramin <r.ramazanpour@aut.ac.ir>
2022-03-22 03:38:25 +0430

    H #312 add coming soon for transak

--- src/components/views/donate/FiatDonation.tsx
+++ src/components/views/donate/FiatDonation.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { IProject } from '@/apollo/types/types';
-import { Button } from '@giveth/ui-design-system';
+import { Button, H4 } from '@giveth/ui-design-system';
 import styled from 'styled-components';
 
 type SuccessFunction = (param: boolean) => void;
@@ -13,17 +13,20 @@ const FiatDonation = (props: {
 
 	return (
 		<FiatContainer>
+			<H4>Coming soon ...</H4>
 			<ButtonContainer>
 				<Button
 					label='CONTINUE WITH TRANSAK'
 					onClick={() => setSuccessDonation(true)}
-				></Button>
+					disabled
+				/>
 			</ButtonContainer>
 			<ImageContainer>
 				<Image
 					src='/images/powered_by_transak.svg'
 					width='165px'
 					height='24px'
+					alt={'Powered by Transak'}
 				/>
 			</ImageContainer>
 		</FiatContainer>
@@ -34,6 +37,10 @@ const FiatContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+
+	> h4 {
+		margin: 90px 0;
+	}
 `;
 const ImageContainer = styled.div`
 	width: 100%;
@@ -43,4 +50,5 @@ const ImageContainer = styled.div`
 const ButtonContainer = styled.div`
 	padding: 32px 0 0 0;
 `;
+
 export default FiatDonation;
