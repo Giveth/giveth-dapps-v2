83b624424f14497335ff46c7d87b7fa66fc8dbf8
Mateo Daza <mateodaza@gmail.com>
2022-04-04 20:03:11 -0500

    adds bundle analyzer on build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({
 	reactStrictMode: true,
 	images: {
 		domains: [
@@ -7,4 +10,4 @@ module.exports = {
 			'static.tgbwidget.com',
 		],
 	},
-};
+});
