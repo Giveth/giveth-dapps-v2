a0f0d3a577dc02b84822bdce41ae3bdb1b6039da
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-04-13 15:32:04 -0300

    fix: config rpc urls

--- src/lib/wallet/walletTypes.ts
+++ src/lib/wallet/walletTypes.ts
@@ -11,15 +11,23 @@ import torusIcon from '/public//images/wallets/torus.svg';
 // import authereumIcon from '/public//images/wallets/authereum.svg';
 import { Web3ReactContextInterface } from '@web3-react/core/dist/types';
 import { TorusConnector } from '@/lib/wallet/torus-connector';
+import { networksParams } from '@/utils/constants';
 
 const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 
 const supportedChainIds = [1, 3, 4, 5, 42, 100];
+const RPC_URLS: { [chainId: number]: string } = {
+	1: networksParams[1].rpcUrls[0],
+	3: networksParams[3].rpcUrls[0],
+	42: networksParams[42].rpcUrls[0],
+	100: networksParams[100].rpcUrls[0],
+};
 
 export const injectedConnector = new InjectedConnector({});
 export const walletconnectConnector = new WalletConnectConnector({
 	infuraId: INFURA_API_KEY,
-	supportedChainIds,
+	supportedChainIds: supportedChainIds,
+	rpc: RPC_URLS,
 	qrcode: true,
 });
 // export const portisConnector = new PortisConnector({
