f26b2402399338c4017dccb8b4fac32cf288f5d9
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-04-19 15:29:40 -0300

    feat: added getBalance to provider interface

--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -30,6 +30,7 @@ interface IUserContext {
 		reFetchUser: () => void;
 		incrementLikedProjectsCount: () => void;
 		decrementLikedProjectsCount: () => void;
+		getBalance: () => void;
 	};
 }
 
@@ -45,6 +46,7 @@ const UserContext = createContext<IUserContext>({
 		reFetchUser: () => {},
 		incrementLikedProjectsCount: () => {},
 		decrementLikedProjectsCount: () => {},
+		getBalance: () => {},
 	},
 });
 
@@ -179,6 +181,7 @@ export const UserProvider = (props: { children: ReactNode }) => {
 					reFetchUser,
 					incrementLikedProjectsCount,
 					decrementLikedProjectsCount,
+					getBalance,
 				},
 			}}
 		>
