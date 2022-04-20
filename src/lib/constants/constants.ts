1a6b00219f27794659997ce9675204e33554e40d
ascode94 <ascode94@gmail.com>
2022-04-20 12:17:13 +0430

    Fixed GivFarm zIndex issue and created constant for some zIndexes

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -90,3 +90,16 @@ export const givEconomySupportedNetworks = [
 	config.MAINNET_NETWORK_NUMBER,
 	config.XDAI_NETWORK_NUMBER,
 ];
+
+export const zIndex = {
+	HEADER: 100,
+	DROPDOWN: 1000,
+	STICKY: 1020,
+	FIXED: 1030,
+	MODALBACKDROP: 1040,
+	OFFCANVAS: 1050,
+	MODAL: 1060,
+	POPOVER: 1070,
+	TOOLTIP: 1080,
+	NAVBAR: 1100,
+};
