a309ab0ea6bce08ae43a1122616c9bb5bca47ffd
ascode94 <ascode94@gmail.com>
2022-04-21 14:01:32 +0430

    Fixed linting issue

--- src/hooks/useNewsletterSubscription.ts
+++ src/hooks/useNewsletterSubscription.ts
@@ -2,9 +2,9 @@ import { autopilotClient } from '@/services/autopilot';
 import { useState } from 'react';
 
 function validateEmail(email: string): boolean {
-    const re =
+    const reg =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email.toLowerCase());
+    return reg.test(email.toLowerCase());
 }
 
 const useNewsletterSubscription = () => {
