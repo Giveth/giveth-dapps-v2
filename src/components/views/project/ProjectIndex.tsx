d0bef390615471484ece96aef0daf623ae70adad
Ramin <r.ramazanpour@aut.ac.ir>
2022-04-25 22:36:33 +0430

    add scroll header when reaching similar projects section

--- src/components/views/project/ProjectIndex.tsx
+++ src/components/views/project/ProjectIndex.tsx
@@ -137,75 +137,79 @@ const ProjectIndex = (props: { project?: IProject }) => {
 	}
 
 	return (
-		<Wrapper>
-			<Head>
-				<title>{title && `${title} |`} Giveth</title>
-				<ProjectMeta project={project} preTitle='Check out' />
-			</Head>
+		<>
+			<Wrapper>
+				<Head>
+					<title>{title && `${title} |`} Giveth</title>
+					<ProjectMeta project={project} preTitle='Check out' />
+				</Head>
 
-			<ProjectHeader project={project} />
-			{isDraft && (
-				<DraftIndicator>
-					<InfoBadge />
-					<Caption medium>This is a preview of your project.</Caption>
-				</DraftIndicator>
-			)}
-			<BodyWrapper>
-				<ContentWrapper>
-					{project && !isDraft && (
-						<ProjectTabs
-							activeTab={activeTab}
-							setActiveTab={setActiveTab}
-							project={project}
-							totalDonations={totalDonations}
-						/>
-					)}
-					{!isActive && !isDraft && (
-						<InlineToast message='This project is not active.' />
-					)}
-					{activeTab === 0 && (
-						<RichTextViewer content={description} />
-					)}
-					{activeTab === 1 && (
-						<ProjectUpdates
-							project={project}
-							fetchProject={fetchProject}
-						/>
-					)}
-					{activeTab === 2 && (
-						<ProjectDonations
-							donationsByProjectId={{
-								donations,
-								totalCount: totalDonations,
-							}}
-							project={project!}
-							isActive={isActive}
-							isDraft={isDraft}
-						/>
-					)}
-				</ContentWrapper>
-				{project && (
-					<ProjectDonateCard
-						isDraft={isDraft}
-						project={project!}
-						isMobile={isMobile}
-						isActive={isActive}
-						setIsActive={setIsActive}
-						setIsDraft={setIsDraft}
-						setCreationSuccessful={setCreationSuccessful}
-					/>
+				<ProjectHeader project={project} />
+				{isDraft && (
+					<DraftIndicator>
+						<InfoBadge />
+						<Caption medium>
+							This is a preview of your project.
+						</Caption>
+					</DraftIndicator>
 				)}
-			</BodyWrapper>
+				<BodyWrapper>
+					<ContentWrapper>
+						{project && !isDraft && (
+							<ProjectTabs
+								activeTab={activeTab}
+								setActiveTab={setActiveTab}
+								project={project}
+								totalDonations={totalDonations}
+							/>
+						)}
+						{!isActive && !isDraft && (
+							<InlineToast message='This project is not active.' />
+						)}
+						{activeTab === 0 && (
+							<RichTextViewer content={description} />
+						)}
+						{activeTab === 1 && (
+							<ProjectUpdates
+								project={project}
+								fetchProject={fetchProject}
+							/>
+						)}
+						{activeTab === 2 && (
+							<ProjectDonations
+								donationsByProjectId={{
+									donations,
+									totalCount: totalDonations,
+								}}
+								project={project!}
+								isActive={isActive}
+								isDraft={isDraft}
+							/>
+						)}
+					</ContentWrapper>
+					{project && (
+						<ProjectDonateCard
+							isDraft={isDraft}
+							project={project!}
+							isMobile={isMobile}
+							isActive={isActive}
+							setIsActive={setIsActive}
+							setIsDraft={setIsDraft}
+							setCreationSuccessful={setCreationSuccessful}
+						/>
+					)}
+				</BodyWrapper>
+			</Wrapper>
 			<SimilarProjectsContainer>
 				<H5 weight={700}>Similar projects</H5>
 				<SimilarProjects slug={slug} />
 			</SimilarProjectsContainer>
-		</Wrapper>
+		</>
 	);
 };
 
 const SimilarProjectsContainer = styled(Container)`
-	margin-top: 38px;
+	margin-top: 60px;
 `;
 
 const DraftIndicator = styled.div`
@@ -235,4 +239,5 @@ const ContentWrapper = styled.div`
 		padding: 0 24px 0 0;
 	}
 `;
+
 export default ProjectIndex;
