0c8752ff083cafb912084de8ccb41301cf31a393
ascode94 <ascode94@gmail.com>
2022-04-26 11:00:29 +0430

    Created Custom 404 Page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import ErrorsIndex from '@/components/views/Errors/ErrorsIndex';
+import React from 'react';
+
+const Custom404 = () => {
+	return <ErrorsIndex statusCode='404' />;
+};
+
+export default Custom404;
