cdb5f902b80dfef3ff5c3169c285b99b2e34b733
Cherik <Pourcheriki@gmail.com>
2022-05-18 17:00:45 +0430

    optimize useGIVTokenDistroHelper

--- src/hooks/useGIVTokenDistroHelper.tsx
+++ src/hooks/useGIVTokenDistroHelper.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { shallowEqual } from 'react-redux';
 import { useAppSelector } from '@/features/hooks';
 import { TokenDistroHelper } from '@/lib/contractHelper/TokenDistroHelper';
 import { defaultTokenDistroHelper } from '@/context/tokenDistro.context';
@@ -6,8 +7,9 @@ import { defaultTokenDistroHelper } from '@/context/tokenDistro.context';
 const useGIVTokenDistroHelper = () => {
 	const [givTokenDistroHelper, setGIVTokenDistroHelper] =
 		useState<TokenDistroHelper>(defaultTokenDistroHelper);
-	const { tokenDistroInfo } = useAppSelector(
-		state => state.subgraph.currentValues,
+	const tokenDistroInfo = useAppSelector(
+		state => state.subgraph.currentValues.tokenDistroInfo,
+		shallowEqual,
 	);
 	useEffect(() => {
 		if (!tokenDistroInfo) return;
