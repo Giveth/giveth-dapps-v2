dbb2da54ffcef2773b8c0a153e6edb55b3d4de18
Cherik <Pourcheriki@gmail.com>
2022-05-22 12:08:28 +0430

    add modal ModalController

---
+++ src/components/controller/modal.ctrl.tsx
@@ -0,0 +1,62 @@
+import WalletModal from '@/components/modals/WalletModal';
+import WelcomeModal from '@/components/modals/WelcomeModal';
+import { FirstWelcomeModal } from '@/components/modals/FirstWelcomeModal';
+import { SignWithWalletModal } from '@/components/modals/SignWithWalletModal';
+import { CompleteProfileModal } from '@/components/modals/CompleteProfileModal';
+import { useAppDispatch, useAppSelector } from '@/features/hooks';
+import {
+	setShowCompleteProfile,
+	setShowWelcomeModal,
+	setShowFirstWelcomeModal,
+	setShowWalletModal,
+	setShowSignWithWallet,
+} from '@/features/modal/modal.sclie';
+
+const ModalController = () => {
+	const {
+		showCompleteProfile,
+		showFirstWelcomeModal,
+		showSignWithWallet,
+		showWalletModal,
+		showWelcomeModal,
+	} = useAppSelector(state => state.modal);
+	const dispatch = useAppDispatch();
+
+	return (
+		<>
+			{showWalletModal && (
+				<WalletModal
+					setShowModal={state => dispatch(setShowWalletModal(state))}
+				/>
+			)}
+			{showSignWithWallet && (
+				<SignWithWalletModal
+					setShowModal={state =>
+						dispatch(setShowSignWithWallet(state))
+					}
+				/>
+			)}
+			{showCompleteProfile && (
+				<CompleteProfileModal
+					setShowModal={state =>
+						dispatch(setShowCompleteProfile(state))
+					}
+				/>
+			)}
+			{showWelcomeModal && (
+				<WelcomeModal
+					setShowModal={state => dispatch(setShowWelcomeModal(state))}
+				/>
+			)}
+			{showFirstWelcomeModal && (
+				<FirstWelcomeModal
+					setShowModal={state =>
+						dispatch(setShowFirstWelcomeModal(state))
+					}
+				/>
+			)}
+		</>
+	);
+};
+
+export default ModalController;
