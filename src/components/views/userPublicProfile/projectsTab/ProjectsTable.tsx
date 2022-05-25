8d051137e2aed2ced9b458287fd21c695b95b999
Ramin <r.ramazanpour@aut.ac.ir>
2022-05-25 20:37:32 +0430

    unify bullet components and refactoring their code

--- src/components/views/userPublicProfile/projectsTab/ProjectsTable.tsx
+++ src/components/views/userPublicProfile/projectsTab/ProjectsTable.tsx
@@ -101,7 +101,7 @@ const ProjectsTable: FC<IProjectsTable> = ({
 
 const Container = styled.div`
 	display: grid;
-	grid-template-columns: 1.5fr 1fr 3fr 1.1fr 1.5fr 1.75fr 1fr;
+	grid-template-columns: 1.5fr 1.1fr 3fr 1.1fr 1.5fr 1.75fr 1fr;
 	overflow: auto;
 	min-width: 900px;
 
