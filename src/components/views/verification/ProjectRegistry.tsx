8335569a1e2dac1d79779bd0fce7e4e716115c76
alireza-sharifpour <ascode94@gmail.com>
2022-05-25 17:49:15 +0430

    Implemented Project registery non-profit organization

--- src/components/views/verification/ProjectRegistry.tsx
+++ src/components/views/verification/ProjectRegistry.tsx
@@ -1,9 +1,34 @@
 import { H6, Lead, neutralColors, P } from '@giveth/ui-design-system';
 import styled from 'styled-components';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
+import Select, { StylesConfig } from 'react-select';
 import RadioTitle from '../donate/RadioTitle';
+import Input from '@/components/Input';
+import { Label } from '../create/Create.sc';
+
+const options = [
+	{ value: 'new york', label: 'New York' },
+	{ value: 'chicago', label: 'Chicago' },
+	{ value: 'san francisco', label: 'San Francisco' },
+];
+
+const selectCustomStyles: StylesConfig = {
+	control: styles => ({
+		...styles,
+		borderColor: neutralColors.gray[300],
+		borderWidth: '2px',
+		borderRadius: '8px',
+		boxShadow: 'none',
+		padding: '8px',
+		width: '70%',
+
+		maxWidth: '520px',
+	}),
+};
+
 export default function ProjectRegistry() {
 	const [isNonProfit, setIsNonProfit] = useState(true);
+
 	return (
 		<>
 			<H6 weight={700}>Project registry</H6>
@@ -31,6 +56,34 @@ export default function ProjectRegistry() {
 					/>
 				</RadioContainer>
 			</RadioSectionContainer>
+			<br />
+			<ProjectRegistryNonProfit />
+		</>
+	);
+}
+
+function ProjectRegistryNonProfit() {
+	const [link, setLink] = useState('');
+	return (
+		<>
+			<Lead>In which country are you registered?</Lead>
+			<br />
+			<Select options={options} styles={selectCustomStyles} />
+			<br />
+			<Lead>
+				Please provide a link to your country's government registry
+				where the team can look up and confirm your status.
+			</Lead>
+			<br />
+			<Label>Please enter full link</Label>
+			<Input
+				value={link}
+				name='link'
+				placeholder='https://'
+				onChange={(e: ChangeEvent<HTMLInputElement>) => {
+					setLink(e.target.value);
+				}}
+			/>
 		</>
 	);
 }
