0971e45d2e76fb29e34fd7f7d28af2f72d6d3d0c
Ramin <r.ramazanpour@aut.ac.ir>
2022-05-26 14:56:29 +0430

    change verification input to Input.tsx

--- src/components/views/verification/PersonalInfo.tsx
+++ src/components/views/verification/PersonalInfo.tsx
@@ -1,30 +1,31 @@
 import { brandColors, H6 } from '@giveth/ui-design-system';
 import styled from 'styled-components';
-import InputBox from '@/components/InputBox';
 import { Flex } from '@/components/styled-components/Flex';
 import { ButtonStyled } from './common.styled';
+import Input from '@/components/Input';
 
 const PersonalInfo = () => {
 	return (
 		<>
 			<H6 weight={700}>Personal info</H6>
 			<br />
-			<InputBox
+			<Input
 				label='What is your full name?'
 				value={'Lauren Luz'}
 				disabled
+				name='name'
 			/>
-			<br />
-			<InputBox
+			<Input
 				label='Your wallet address'
 				value={'0x6d97d65adff6771b31671443a6b9512104312d3d'}
 				disabled
+				name='walletAddress'
 			/>
-			<br />
 			<EmailSection>
-				<InputBox
+				<Input
 					label='What is your email address?'
 					value={'lauren@giveth.io'}
+					name='email'
 				/>
 				<ButtonStyled
 					color={brandColors.giv[500]}
@@ -38,7 +39,7 @@ const PersonalInfo = () => {
 
 const EmailSection = styled(Flex)`
 	gap: 24px;
-	align-items: flex-end;
+	align-items: center;
 	> :first-child {
 		width: 100%;
 	}
