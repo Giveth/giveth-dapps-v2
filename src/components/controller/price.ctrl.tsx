65ead62843131554bb86c06bcbc5e78449c5d62c
Cherik <Pourcheriki@gmail.com>
2022-05-30 01:28:55 +0430

    add price controller

---
+++ src/components/controller/price.ctrl.tsx
@@ -0,0 +1,25 @@
+import { useWeb3React } from '@web3-react/core';
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/features/hooks';
+import {
+	fetchEthPriceAsync,
+	fetchGIVPriceAsync,
+	fetchGnosisThirdPartyTokensPriceAsync,
+	fetchMainnetThirdPartyTokensPriceAsync,
+} from '@/features/price/price.thunks';
+
+const PriceController = () => {
+	const dispatch = useAppDispatch();
+	const { chainId } = useWeb3React();
+
+	useEffect(() => {
+		const _chainId = chainId ?? 0;
+		dispatch(fetchEthPriceAsync());
+		dispatch(fetchGIVPriceAsync(_chainId));
+		dispatch(fetchMainnetThirdPartyTokensPriceAsync());
+		dispatch(fetchGnosisThirdPartyTokensPriceAsync());
+	}, [chainId]);
+	return null;
+};
+
+export default PriceController;
