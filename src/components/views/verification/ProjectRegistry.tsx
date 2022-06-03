e1f7e46cae88de7cb7544a435049e3d4cabf185d
Ramin <r.ramazanpour@aut.ac.ir>
2022-06-03 15:43:14 +0430

    move radio button to components folder

--- src/components/views/verification/ProjectRegistry.tsx
+++ src/components/views/verification/ProjectRegistry.tsx
@@ -8,7 +8,7 @@ import {
 import styled from 'styled-components';
 import { ChangeEvent, useState } from 'react';
 import Select, { StylesConfig } from 'react-select';
-import RadioTitle from '../donate/RadioTitle';
+import RadioButton from '../../RadioButton';
 import Input from '@/components/Input';
 import { Label } from '../create/Create.sc';
 import { Shadow } from '@/components/styled-components/Shadow';
@@ -46,14 +46,14 @@ export default function ProjectRegistry() {
 				</RadioSectionSubTitle>
 				<br />
 				<RadioContainer>
-					<RadioTitle
+					<RadioButton
 						title='Yes'
 						toggleRadio={() =>
 							setIsNonProfit(ProjectRegistryStates.YES)
 						}
 						isSelected={isNonProfit === ProjectRegistryStates.YES}
 					/>
-					<RadioTitle
+					<RadioButton
 						title='No'
 						toggleRadio={() =>
 							setIsNonProfit(ProjectRegistryStates.NO)
