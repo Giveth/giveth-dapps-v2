c196e563c9838ba5bab341c8b516b6a71eb62c17
Cherik <Pourcheriki@gmail.com>
2022-06-16 16:58:43 +0430

    add loading state

--- src/components/views/verification/Loading.tsx
+++ src/components/views/verification/Loading.tsx
@@ -1,11 +1,16 @@
+import styled from 'styled-components';
 import LottieControl from '@/components/animations/lottieControl';
 import { FlexCenter } from '@/components/styled-components/Flex';
 import LoadingAnimation from '@/animations/loading_giv.json';
 
 export default function LoadingVerification() {
 	return (
-		<FlexCenter>
+		<Container>
 			<LottieControl animationData={LoadingAnimation} size={150} />
-		</FlexCenter>
+		</Container>
 	);
 }
+
+const Container = styled(FlexCenter)`
+	min-height: 700px;
+`;
