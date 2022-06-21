76dd12be083c14e17cccd0308fe818a0e7f24ce4
Cherik <Pourcheriki@gmail.com>
2022-06-21 14:52:11 +0430

    remove logs

--- src/components/modals/GIVdropHarvestModal.tsx
+++ src/components/modals/GIVdropHarvestModal.tsx
@@ -109,8 +109,6 @@ export const GIVdropHarvestModal: FC<IGIVdropHarvestModal> = ({
 		setGivDropStream(
 			givTokenDistroHelper.getStreamPartTokenPerWeek(givdropAmount),
 		);
-		console.log('givdropAmount', givdropAmount);
-		console.log('givTokenDistroHelper', givTokenDistroHelper);
 		const amount = new BigNumber(givdropAmount.mul(9).div(10).toString());
 		const percent = new BigNumber(givTokenDistroHelper.percent / 100);
 		const givDropAcc = amount
