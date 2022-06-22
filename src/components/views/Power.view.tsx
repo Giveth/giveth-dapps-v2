d520a14aa3b5e52991f50e7977f3d12022bc4737
Mateo Daza <mateodaza@gmail.com>
2022-06-21 21:04:29 -0500

    fixes naming issue part 1

--- src/components/views/Power.view.tsx
+++ src/components/views/Power.view.tsx
@@ -1,4 +1,4 @@
-import { TabPowerTop, TabPowerBottom } from '../homeTabs/GIVPower';
+import { TabPowerTop, TabPowerBottom } from '../homeTabs/_GIVPower';
 import Tabs from '../Tabs';
 
 export default function GIVpowerView() {
