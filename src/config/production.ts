5a20d691bdc97a4a5dd9b9baef8905e3d1257803
Amin Latifi <a.latifi.al@gmail.com>
2022-06-23 09:22:44 +0430

    Fixed subgraph endpoint wrong value Removed unused `uniswapV2EthGivPair` from query

--- src/config/production.ts
+++ src/config/production.ts
@@ -160,7 +160,7 @@ const config: EnvConfig = {
 		blockExplorerName: ['Blockscout'],
 		blockExplorerUrls: ['https://blockscout.com/xdai/mainnet'],
 		subgraphAddress:
-			'https://thegraph.com/explorer/subgraph/giveth/giveth-economy-xdai',
+			'https://api.thegraph.com/subgraphs/name/giveth/giveth-economy-xdai',
 
 		TOKEN_ADDRESS: '0x4f4F9b8D5B4d0Dc10506e5551B0513B61fD59e75',
 		tokenAddressOnUniswapV2: '0x4f4F9b8D5B4d0Dc10506e5551B0513B61fD59e75',
