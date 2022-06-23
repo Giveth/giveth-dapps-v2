a58d5ec0750f597dfc4e43b5e317cd17e19e8968
Cherik <Pourcheriki@gmail.com>
2022-06-23 12:24:07 +0430

    fix buil issue

--- pages/givpower.tsx
+++ pages/givpower.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import GIVpowerView from '@/components/views/Power.view';
 import { GeneralMetatags } from '@/components/Metatag';
-import { setDarkTheme, setLightTheme } from '@/features/general/general.sclie';
+import { setDarkTheme, setLightTheme } from '@/features/general/general.slice';
 import { useAppDispatch } from '@/features/hooks';
 import { givpowerMetatags } from '@/content/metatags';
 
