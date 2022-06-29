d9cc4c65d55888a98cf0754d29f260665ad7f57b
Cherik <Pourcheriki@gmail.com>
2022-06-29 14:13:51 +0430

    add GIVpower helpers

---
+++ src/helpers/givpower.ts
@@ -0,0 +1,19 @@
+import { IGIVpowerInfo } from '@/types/subgraph';
+import { getNowUnixMS } from './time';
+
+export const getGIVpowerRoundsInfo = (
+	initialDate: number,
+	roundDuration: number,
+) => {
+	const currentRound = Math.floor(
+		(getNowUnixMS() / 1000 - initialDate) / roundDuration,
+	);
+	const nextRoundDate =
+		(initialDate + roundDuration * (currentRound + 1)) * 1000;
+	return { currentRound, nextRoundDate };
+};
+
+export const getUnlockDate = (givpowerInfo: IGIVpowerInfo, rounds: number) => {
+	const { nextRoundDate, roundDuration } = givpowerInfo;
+	return nextRoundDate + rounds * roundDuration * 1000;
+};
