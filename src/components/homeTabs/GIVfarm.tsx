84b52c50b6e1df01642bf3b218597d3aeddb3d05
Cherik <Pourcheriki@gmail.com>
2022-07-05 22:12:26 +0430

    remove temp button

--- src/components/homeTabs/GIVfarm.tsx
+++ src/components/homeTabs/GIVfarm.tsx
@@ -4,7 +4,6 @@ import {
 	IconExternalLink,
 	GLink,
 	IconCopy,
-	Button,
 } from '@giveth/ui-design-system';
 import { BigNumber } from 'bignumber.js';
 import { constants } from 'ethers';
@@ -42,7 +41,6 @@ import {
 	DaoCardQuote,
 	DaoCardButton,
 } from '../GIVfrens.sc';
-import { addNetwork } from '@/lib/metamask';
 
 const renderPools = (
 	pools: BasicNetworkConfig['pools'],
@@ -146,10 +144,6 @@ export const TabGIVfarmBottom = () => {
 			<Container>
 				<Flex alignItems='center' gap='24px' wrap={1}>
 					<NetworkSelector />
-					<Button
-						label='addNetwork'
-						onClick={() => addNetwork(100)}
-					/>
 					<ExtLinkRow alignItems='center'>
 						<GLink
 							size='Big'
