5982151951b37c9e69d534369953f17b01019603
Cherik <Pourcheriki@gmail.com>
2022-07-24 12:03:26 +0430

    add size to checkbox and do some improvements

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,10 +1,11 @@
 import {
 	brandColors,
+	GLink,
 	IconCheck,
 	neutralColors,
 } from '@giveth/ui-design-system';
-import styled from 'styled-components';
-import { FlexCenter } from '@/components/styled-components/Flex';
+import styled, { css } from 'styled-components';
+import { Flex, FlexCenter } from '@/components/styled-components/Flex';
 import type { FC } from 'react';
 
 interface ICheckBox {
@@ -12,28 +13,42 @@ interface ICheckBox {
 	label: string;
 	checked?: boolean;
 	disabled?: boolean;
+	size?: 16 | 32 | 24;
+	labelSize?: 'Tiny' | 'Small' | 'Medium' | 'Big';
 }
 
-const CheckBox: FC<ICheckBox> = ({ onChange, checked, label, disabled }) => {
+const CheckBox: FC<ICheckBox> = ({
+	onChange,
+	checked,
+	label,
+	disabled,
+	size = 24,
+	labelSize = 'Big',
+}) => {
 	return (
 		<Wrapper
 			onClick={() => !disabled && onChange(!checked)}
 			disabled={disabled}
 			checked={checked}
+			size={size}
+			alignItems='center'
 		>
 			<FlexCenter>
-				{checked && <IconCheck size={24} color='white' />}
+				{checked && <IconCheck size={size} color='white' />}
 			</FlexCenter>
-			<div>{label}</div>
+			<GLink as='span' size={labelSize}>
+				{label}
+			</GLink>
 		</Wrapper>
 	);
 };
 
-const Wrapper = styled.div<{ disabled?: boolean; checked?: boolean }>`
+const Wrapper = styled(Flex)<{
+	size: number;
+	disabled?: boolean;
+	checked?: boolean;
+}>`
 	cursor: pointer;
-	display: flex;
-	align-items: center;
-	gap: 12px;
 	color: ${props =>
 		props.disabled ? neutralColors.gray[600] : neutralColors.gray[900]};
 	> div:first-child {
@@ -42,13 +57,41 @@ const Wrapper = styled.div<{ disabled?: boolean; checked?: boolean }>`
 				props.disabled
 					? neutralColors.gray[400]
 					: neutralColors.gray[900]};
-		border-radius: 4px;
-		width: 20px;
-		height: 20px;
 		flex-shrink: 0;
-		background: ${props =>
+		border-radius: 4px;
+		width: ${props => `${props.size}px`};
+		height: ${props => `${props.size}px`};
+		background-color: ${props =>
 			props.checked ? brandColors.deep[900] : 'white'};
+		transition: background-color 0.3s ease;
 	}
+	${props => {
+		switch (props.size) {
+			case 16:
+				return css`
+					gap: 10px;
+					> div:first-child {
+						border-width: 1px;
+					}
+				`;
+			case 24:
+				return css`
+					gap: 12px;
+					> div:first-child {
+						border-width: 2px;
+					}
+				`;
+			case 32:
+				return css`
+					gap: 12px;
+					> div:first-child {
+						border-width: 2px;
+					}
+				`;
+			default:
+				break;
+		}
+	}};
 `;
 
 export default CheckBox;
