7d66dbd344412d13e4f4b6e305cdde99f8d6b360
Cherik <Pourcheriki@gmail.com>
2022-08-01 11:39:02 +0430

    show tooltip instead of questionmark icon in angel vault card

--- src/components/cards/BaseStakingCard.tsx
+++ src/components/cards/BaseStakingCard.tsx
@@ -38,6 +38,7 @@ import {
 	IconHelpWraper,
 	IntroIcon,
 	LiquidityButton,
+	OutOfRangeTooltip,
 	SPTitle,
 	StakeAmount,
 	StakeButton,
@@ -329,23 +330,70 @@ const BaseStakingCard: FC<IBaseStakingCardProps> = ({
 																.mustard[500]
 														}
 													/>
-													<DetailValue>
-														{apr &&
-															formatEthHelper(
-																apr.effectiveAPR,
-																2,
-															)}
-														%
-													</DetailValue>
-													<IconContainer
-														onClick={() =>
-															setShowAPRModal(
-																true,
-															)
-														}
-													>
-														<IconHelp size={16} />
-													</IconContainer>
+													{type ===
+													StakingType.ICHI_GIV_ONEGIV ? (
+														<IconWithTooltip
+															direction='right'
+															icon={
+																<DetailValue>
+																	{apr &&
+																		formatEthHelper(
+																			apr.effectiveAPR,
+																			2,
+																		)}
+																	%
+																</DetailValue>
+															}
+														>
+															<OutOfRangeTooltip>
+																Your cumulative
+																APR including
+																both rewards
+																earned as fees &
+																added
+																automatically to
+																your position (
+																{apr?.vaultIRR &&
+																	formatEthHelper(
+																		apr.vaultIRR,
+																		2,
+																	)}
+																% IRR), and
+																rewards earned
+																in GIV from
+																staking your LP
+																(
+																{apr &&
+																	formatEthHelper(
+																		apr.effectiveAPR,
+																		2,
+																	)}
+																% APR).
+															</OutOfRangeTooltip>
+														</IconWithTooltip>
+													) : (
+														<>
+															<DetailValue>
+																{apr &&
+																	formatEthHelper(
+																		apr.effectiveAPR,
+																		2,
+																	)}
+																%
+															</DetailValue>
+															<IconContainer
+																onClick={() =>
+																	setShowAPRModal(
+																		true,
+																	)
+																}
+															>
+																<IconHelp
+																	size={16}
+																/>
+															</IconContainer>
+														</>
+													)}
 												</>
 											) : (
 												<div>N/A %</div>
