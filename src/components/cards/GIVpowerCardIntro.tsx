74c75fe9f0dce22ecb6459062d776375fa81c19d
Cherik <Pourcheriki@gmail.com>
2022-08-03 19:52:11 +0430

    add LockupDetailsModal to GIVpowerCardIntro

--- src/components/cards/GIVpowerCardIntro.tsx
+++ src/components/cards/GIVpowerCardIntro.tsx
@@ -9,52 +9,82 @@ import {
 	Button,
 	brandColors,
 } from '@giveth/ui-design-system';
-import { Dispatch, FC, SetStateAction } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 // import Routes from '@/lib/constants/Routes';
 import links from '@/lib/constants/links';
 import TotalGIVpowerBox from '../modals/StakeLock/TotalGIVpowerBox';
 import { Flex } from '../styled-components/Flex';
 import { StakeCardState } from './BaseStakingCard';
+import { LockupDetailsModal } from '../modals/LockupDetailsModal';
+import { useGIVpower } from '@/context/givpower.context';
+import { useAppSelector } from '@/features/hooks';
+import { SubgraphDataHelper } from '@/lib/subgraph/subgraphDataHelper';
+import type { Dispatch, FC, SetStateAction } from 'react';
 
 interface IGIVpowerCardIntro {
 	setState: Dispatch<SetStateAction<StakeCardState>>;
 }
 
 const GIVpowerCardIntro: FC<IGIVpowerCardIntro> = ({ setState }) => {
+	const [showLockDetailModal, setShowLockDetailModal] = useState(false);
+	const { stakedAmount } = useGIVpower();
+	const currentValues = useAppSelector(state => state.subgraph.currentValues);
+
+	const sdh = new SubgraphDataHelper(currentValues);
+	const userGIVLocked = sdh.getUserGIVLockedBalance();
+
 	return (
-		<GIVpowerCardIntroContainer>
-			<HeaderRow>
-				<IconRocketInSpace32 />
-				<H6 weight={700}>GIVpower</H6>
-				<div style={{ flex: 1 }}></div>
-				<CloseButton onClick={() => setState(StakeCardState.NORMAL)}>
-					<IconX size={24} />
-				</CloseButton>
-			</HeaderRow>
-			<TotalGIVpowerBox />
-			<Desc>
-				You get GIVpower when you stake &amp; lock GIV. GIVpower will
-				allow you to boost projects to influence their ranking on
-				Giveth.
-			</Desc>
-			<Desc>
-				It will allow you to support the projects you believe in,
-				without donating.
-			</Desc>
-			{/* <ButtonLink
+		<>
+			<GIVpowerCardIntroContainer>
+				<HeaderRow>
+					<IconRocketInSpace32 />
+					<H6 weight={700}>GIVpower</H6>
+					<div style={{ flex: 1 }}></div>
+					<CloseButton
+						onClick={() => setState(StakeCardState.NORMAL)}
+					>
+						<IconX size={24} />
+					</CloseButton>
+				</HeaderRow>
+				<TotalGIVpowerBox />
+				<Desc>
+					You get GIVpower when you stake &amp; lock GIV. GIVpower
+					will allow you to boost projects to influence their ranking
+					on Giveth.
+				</Desc>
+				<Desc>
+					It will allow you to support the projects you believe in,
+					without donating.
+				</Desc>
+				{/* <ButtonLink
 				label='Boost projects with GIVpower'
 				href={Routes.Projects}
 			/> */}
-			<LearnMoreButton
-				label='Learn More'
-				onClick={() => window.open(links.GIVPOWER_DOC)}
-				buttonType='texty'
-				icon={
-					<IconExternalLink size={16} color={brandColors.deep[100]} />
-				}
-			/>
-		</GIVpowerCardIntroContainer>
+				<LearnMoreButton
+					label='locked GIV details'
+					buttonType='texty'
+					onClick={() => setShowLockDetailModal(true)}
+				/>
+				<LearnMoreButton
+					label='Learn More'
+					onClick={() => window.open(links.GIVPOWER_DOC)}
+					buttonType='texty'
+					icon={
+						<IconExternalLink
+							size={16}
+							color={brandColors.deep[100]}
+						/>
+					}
+				/>
+			</GIVpowerCardIntroContainer>
+			{showLockDetailModal && (
+				<LockupDetailsModal
+					setShowModal={setShowLockDetailModal}
+					unstakeable={stakedAmount.sub(userGIVLocked.balance)}
+				/>
+			)}
+		</>
 	);
 };
 
