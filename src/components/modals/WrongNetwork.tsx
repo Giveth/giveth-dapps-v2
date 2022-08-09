be5e2568a113e736a6039280635782ca41fa1480
Cherik <Pourcheriki@gmail.com>
2022-08-09 19:36:04 +0430

    add connect wallet functionality and seprate WrongNetworkInnerModal

--- src/components/modals/WrongNetwork.tsx
+++ src/components/modals/WrongNetwork.tsx
@@ -1,72 +1,11 @@
-import { P, brandColors } from '@giveth/ui-design-system';
 import styled from 'styled-components';
-import { FC } from 'react';
-import { Button } from '@giveth/ui-design-system';
-import { useWeb3React } from '@web3-react/core';
-import { InjectedConnector } from '@web3-react/injected-connector';
-import { switchNetwork } from '@/lib/wallet';
-
-import { chainName } from '@/lib/constants/constants';
 import { Modal } from './Modal';
-import { IModal } from '@/types/common';
-
-interface IWrongNetworkInnerModal {
-	text?: string;
-	targetNetworks: number[];
-}
-
-export const WrongNetworkInnerModal: FC<IWrongNetworkInnerModal> = ({
-	text,
-	targetNetworks,
-}) => {
-	const { account, activate } = useWeb3React();
-
-	const checkWalletAndSwitchNetwork = async (network: number) => {
-		if (!account) {
-			await activate(new InjectedConnector({}));
-		}
-		if (account) {
-			await switchNetwork(network);
-		}
-	};
-
-	return (
-		<WrongNetworkInnerModalContainer>
-			<Description>
-				<P>{text}</P>
-				<P>Please switch the network.</P>
-			</Description>
-			<ButtonsContainer>
-				{targetNetworks.map(network => (
-					<Button
-						label={`SWITCH TO ${chainName(network).toUpperCase()}`}
-						onClick={() => checkWalletAndSwitchNetwork(network)}
-						buttonType='primary'
-						key={network}
-					/>
-				))}
-			</ButtonsContainer>
-		</WrongNetworkInnerModalContainer>
-	);
-};
-
-const WrongNetworkInnerModalContainer = styled.div`
-	max-width: 450px;
-	padding: 6px 24px;
-`;
-
-const Description = styled.div`
-	padding: 12px;
-	margin-bottom: 12px;
-	text-align: center;
-	color: ${brandColors.deep[100]};
-`;
-
-const ButtonsContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 8px;
-`;
+import {
+	IWrongNetworkInnerModal,
+	WrongNetworkInnerModal,
+} from './WrongNetworkInnerModal';
+import type { IModal } from '@/types/common';
+import type { FC } from 'react';
 
 interface IWrongNetworkModal extends IModal, IWrongNetworkInnerModal {}
 
---
+++ src/components/modals/WrongNetworkInnerModal.tsx
@@ -0,0 +1,93 @@
+import { P, brandColors } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+import { FC } from 'react';
+import { Button } from '@giveth/ui-design-system';
+import { useWeb3React } from '@web3-react/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { switchNetwork } from '@/lib/wallet';
+
+import { chainName } from '@/lib/constants/constants';
+import { useAppDispatch } from '@/features/hooks';
+import { setShowWalletModal } from '@/features/modal/modal.slice';
+
+export interface IWrongNetworkInnerModal {
+	text?: string;
+	targetNetworks: number[];
+}
+
+export const WrongNetworkInnerModal: FC<IWrongNetworkInnerModal> = ({
+	text,
+	targetNetworks,
+}) => {
+	const { account, activate } = useWeb3React();
+	const dispatch = useAppDispatch();
+
+	const connectWallet = () => {
+		dispatch(setShowWalletModal(true));
+	};
+
+	const checkWalletAndSwitchNetwork = async (network: number) => {
+		if (!account) {
+			await activate(new InjectedConnector({}));
+		}
+		if (account) {
+			await switchNetwork(network);
+		}
+	};
+
+	return (
+		<WrongNetworkInnerModalContainer>
+			{account ? (
+				<>
+					<Description>
+						<P>{text}</P>
+						<P>Please switch the network.</P>
+					</Description>
+					<ButtonsContainer>
+						{targetNetworks.map(network => (
+							<Button
+								label={`SWITCH TO ${chainName(network)}`}
+								onClick={() =>
+									checkWalletAndSwitchNetwork(network)
+								}
+								buttonType='primary'
+								key={network}
+							/>
+						))}
+					</ButtonsContainer>
+				</>
+			) : (
+				<>
+					<Description>
+						<P>{text}</P>
+					</Description>
+					<ButtonsContainer>
+						<Button
+							label='Connect Wallet'
+							onClick={connectWallet}
+							buttonType='primary'
+						/>
+					</ButtonsContainer>
+				</>
+			)}
+		</WrongNetworkInnerModalContainer>
+	);
+};
+
+const WrongNetworkInnerModalContainer = styled.div`
+	max-width: 450px;
+	padding: 6px 24px;
+`;
+
+const Description = styled.div`
+	padding: 12px;
+	margin-bottom: 12px;
+	text-align: center;
+	color: ${brandColors.deep[100]};
+`;
+
+const ButtonsContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+`;
