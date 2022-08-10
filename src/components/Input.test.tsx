b8029578fc307d35f9c6f74aa6fc8d0b7c3e3365
alireza-sharifpour <ascode94@gmail.com>
2022-08-10 12:26:22 +0430

    changed the usage of render method

--- src/components/Input.test.tsx
+++ src/components/Input.test.tsx
@@ -1,18 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import Input from '@/components/Input';
 import '@testing-library/jest-dom';
 
 test('the input is rendering', () => {
-	const { getByRole } = render(<Input />);
-	const input = getByRole('textbox');
+	render(<Input />);
+	const input = screen.getByRole('textbox');
 	expect(input).toBeInTheDocument();
 	expect(input).toHaveValue('');
 });
 
 test('the input is accessible', async () => {
-	const { getByRole } = render(<Input label='test' />);
-	const input = getByRole('textbox');
+	render(<Input label='test' />);
+	const input = screen.getByRole('textbox');
 	const accessibilityResults = await axe(input);
 	expect(accessibilityResults).toHaveNoViolations();
 });
