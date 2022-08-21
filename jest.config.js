d33f1f25487d1914765448794caaedebb20cb1a1
alireza-sharifpour <ascode94@gmail.com>
2022-08-21 13:17:36 +0430

    ignored cypress path from jest

--- jest.config.js
+++ jest.config.js
@@ -23,6 +23,7 @@ const customJestConfig = {
 		'jest-watch-typeahead/testname',
 	],
 	setupFiles: ['jest-canvas-mock'],
+	testPathIgnorePatterns: ['<rootDir>/cypress/'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
