fa9be3bd7a4027bb0389ceb71bb7b6b0ead69af2
Cherik <Pourcheriki@gmail.com>
2022-08-22 14:57:49 +0430

    add notification route

---
+++ pages/notification.tsx
@@ -0,0 +1,20 @@
+import { GeneralMetatags } from '@/components/Metatag';
+import NotificationView from '@/components/views/verification/notification/notification.view';
+
+const NotificationRoute = () => {
+	return (
+		<>
+			<GeneralMetatags
+				info={{
+					title: 'Notification Center',
+					desc: 'See tour notifications',
+					image: 'https://i.ibb.co/HTbdCdd/Thumbnail.png',
+					url: `https://giveth.io/notification`,
+				}}
+			/>
+			<NotificationView />
+		</>
+	);
+};
+
+export default NotificationRoute;
