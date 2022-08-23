7d5043127a85090ceb8b2215b808e513c584cf05
Cherik <Pourcheriki@gmail.com>
2022-08-23 10:53:26 +0430

    add templateToHTML function

--- src/components/views/verification/notification/notification.view.tsx
+++ src/components/views/verification/notification/notification.view.tsx
@@ -1,6 +1,39 @@
-import { H5, IconNotificationOutline32, Lead } from '@giveth/ui-design-system';
+import {
+	H5,
+	IconNotificationOutline32,
+	Lead,
+	P,
+	B,
+	GLink,
+} from '@giveth/ui-design-system';
+import { ReactNode } from 'react';
 import { Flex } from '@/components/styled-components/Flex';
 import { NotificationContainer } from './notification.sc';
+
+const templateToHTML = (template: any) => {
+	const { content, metaData } = template;
+	const res: ReactNode[] = [];
+	content.forEach((cont: any) => {
+		const c: string = cont.content;
+		const temp = c.startsWith('$') ? metaData[c] : c;
+		switch (cont.type) {
+			case 'p':
+				res.push(<P as='span'>{temp}</P>);
+				break;
+			case 'b':
+				res.push(<B as='span'>{temp}</B>);
+				break;
+			case 'a':
+				const c: string = cont.content;
+				res.push(<GLink href={metaData[cont.href]}>{temp}</GLink>);
+				break;
+			default:
+				break;
+		}
+		res.push(' ');
+	});
+	return res;
+};
 
 function NotificationView() {
 	return (
@@ -15,9 +48,75 @@ function NotificationView() {
 						</Lead>
 					</div>
 				</Flex>
+				<div>
+					{notifs.map((notif, idx) => (
+						<div key={idx}>{templateToHTML(notif)}</div>
+					))}
+				</div>
 			</NotificationContainer>
 		</>
 	);
 }
 
 export default NotificationView;
+
+const notifs = [
+	{
+		icon: '',
+		content: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			$amount: '400.2',
+			$farm: 'givfarm',
+			$href1: '/givfarm',
+		},
+		time: '193543213645',
+		qutoe: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		content: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			$amount: '400.2',
+			$farm: 'givfarm',
+			$href1: '/givfarm',
+		},
+		time: '193543213645',
+		qutoe: 'hey bro, how are you?',
+	},
+];
