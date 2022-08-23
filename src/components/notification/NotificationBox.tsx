f7bf30871cef82491fa2f2a1a0b39f4e12ceadd6
Cherik <Pourcheriki@gmail.com>
2022-08-23 16:37:56 +0430

    add NotificationBox

--- src/components/notification/NotificationBox.tsx
+++ src/components/notification/NotificationBox.tsx
@@ -1,9 +1,49 @@
 import styled from 'styled-components';
+import { IconHeartOutline24, neutralColors } from '@giveth/ui-design-system';
+import { Flex } from '../styled-components/Flex';
+import { convertRawDataToHTML, INotificationData } from '@/helpers/html';
+import type { FC } from 'react';
 
-export const NotificationBox = () => {
-	return <NotificationBoxContainer></NotificationBoxContainer>;
+interface INotificationBox {
+	notification: INotificationData;
+}
+
+export const NotificationBox: FC<INotificationBox> = ({ notification }) => {
+	return (
+		<NotificationBoxContainer gap='16px'>
+			<IconContainer>
+				<IconHeartOutline24 />
+			</IconContainer>
+			<NotificationContent>
+				<div>{convertRawDataToHTML(notification)}</div>
+				{notification.quote && (
+					<NotificationQuote>{notification.quote}</NotificationQuote>
+				)}
+				<NotificationTime>
+					{/* {new Date(notification.time)} */}
+				</NotificationTime>
+			</NotificationContent>
+		</NotificationBoxContainer>
+	);
 };
 
-const NotificationBoxContainer = styled.div`
+const IconContainer = styled.div`
+	padding-top: 4px;
+`;
+
+const NotificationBoxContainer = styled(Flex)`
 	padding: 24px;
+`;
+
+const NotificationContent = styled.div``;
+
+const NotificationQuote = styled.div`
+	border-left: 2px solid ${neutralColors.gray[400]};
+	font-weight: bold;
+	padding-left: 16px;
+	margin-top: 16px;
+`;
+
+const NotificationTime = styled.div`
+	margin-top: 16px;
 `;
