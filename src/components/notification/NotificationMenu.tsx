5e1c346a26a3ec1cc57220d2ea4663576539d2a2
alireza-sharifpour <ascode94@gmail.com>
2022-08-24 15:03:29 +0430

    created notificationmenu items and mocked data

--- src/components/notification/NotificationMenu.tsx
+++ src/components/notification/NotificationMenu.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import {
+	brandColors,
+	GLink,
+	neutralColors,
+	Overline,
+} from '@giveth/ui-design-system';
 import { useAppSelector } from '@/features/hooks';
 import { MenuContainer } from '../menu/Menu.sc';
+import { INotificationData } from '@/helpers/html';
+import { NotificationBox } from './NotificationBox';
 
 const NotificationMenu = () => {
 	const [isMounted, setIsMounted] = useState(false);
@@ -13,16 +21,28 @@ const NotificationMenu = () => {
 
 	return (
 		<NotifsMenuContainer isMounted={isMounted} theme={theme}>
-			<div>Hello</div>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
-			<h6>Hello</h6>
+			<NotificationsTitle styleType='Small'>
+				NOTIFICATIONS
+			</NotificationsTitle>
+			<br />
+			<br />
+
+			{notifications
+				.map(notification => (
+					<NotificationBox
+						key={notification.time}
+						short={true}
+						notification={notification}
+					/>
+				))
+				.slice(0, 5)}
+			<br />
+			<AllNotificationsLink
+				color={brandColors.pinky[500]}
+				href='/notification'
+			>
+				All notifications
+			</AllNotificationsLink>
 		</NotifsMenuContainer>
 	);
 };
@@ -30,5 +50,385 @@ const NotificationMenu = () => {
 export default NotificationMenu;
 
 const NotifsMenuContainer = styled(MenuContainer)`
-	max-height: 470px;
+	height: unset;
+	overflow-y: auto;
 `;
+
+const NotificationsTitle = styled(Overline)`
+	color: ${neutralColors.gray[700]};
+`;
+
+const AllNotificationsLink = styled(GLink)`
+	display: flex;
+	justify-content: center;
+	color: ${brandColors.pinky[500]};
+`;
+
+const notifications: INotificationData[] = [
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'b',
+				content: 'GIV tokens',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href',
+			},
+			{
+				type: 'p',
+				content: 'for',
+			},
+			{
+				type: 'b',
+				content: '$apr',
+			},
+			{
+				type: 'b',
+				content: 'APR',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href: '/givfarm',
+			apr: '18.2%',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?\ngood, an you?',
+	},
+
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+	{
+		icon: '',
+		template: [
+			{
+				type: 'p',
+				content: 'you staked',
+			},
+			{
+				type: 'b',
+				content: '$amount',
+			},
+			{
+				type: 'p',
+				content: 'on',
+			},
+			{
+				type: 'a',
+				content: '$farm',
+				href: '$href1',
+			},
+		],
+		metaData: {
+			amount: '400.2',
+			farm: 'givfarm',
+			href1: '/givfarm',
+		},
+		time: '1661256071107',
+		quote: 'hey bro, how are you?',
+	},
+];
