7f8983dd3887fd2bd8388c20aeeddc29582329d7
alireza-sharifpour <ascode94@gmail.com>
2022-08-29 13:31:56 +0430

    updated notificationCount api url

--- src/features/notification/notification.services.ts
+++ src/features/notification/notification.services.ts
@@ -1,18 +1,23 @@
 import { captureException } from '@sentry/nextjs';
 import { getRequest } from '@/helpers/requests';
 import { defaultNotificationValues } from './notification.slice';
-import type { INotificationState } from './notification.types';
+import config from '@/configuration';
+import type { INotificationCountResult } from './notification.types';
 
-export const fetchNotificationInfo = async (): Promise<INotificationState> => {
-	try {
-		return await getRequest('/api/notif');
-	} catch (e) {
-		console.error('Error on fetchNotificationInfo:', e);
-		captureException(e, {
-			tags: {
-				section: 'fetchNotificationInfo',
-			},
-		});
-		return defaultNotificationValues;
-	}
-};
+export const fetchNotificationCount =
+	async (): Promise<INotificationCountResult> => {
+		try {
+			return await getRequest(
+				`${config.MICROSERVICES.notification}/countUnread`,
+				true,
+			);
+		} catch (e) {
+			console.error('Error on fetchNotificationCount:', e);
+			captureException(e, {
+				tags: {
+					section: 'fetchNotificationCount',
+				},
+			});
+			return defaultNotificationValues;
+		}
+	};
