b83bb448b3d1805b5cd8f36228715083fe27eff3
Cherik <Pourcheriki@gmail.com>
2022-09-01 10:22:36 +0430

    fix links and styles

--- src/components/modals/Boost/BoostedInnerModal.tsx
+++ src/components/modals/Boost/BoostedInnerModal.tsx
@@ -1,14 +1,24 @@
 import { Lead } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+import Link from 'next/link';
 import useDetectDevice from '@/hooks/useDetectDevice';
 import { ConfettiContainer, BoostedTitle } from './BoostModal.sc';
 import ConfettiAnimation from '@/components/animations/confetti';
+import Routes from '@/lib/constants/Routes';
+import { StakingType } from '@/types/config';
+import { GetButton, CustomButton, CustomButtonLink } from './ZeroGivpowerModal';
+import { mediaQueries } from '@/lib/constants/constants';
 import type { FC } from 'react';
 
 interface IBoostedModalProps {
 	percentage: number;
+	closeModal: () => void;
 }
 
-const BoostedInnerModal: FC<IBoostedModalProps> = ({ percentage }) => {
+const BoostedInnerModal: FC<IBoostedModalProps> = ({
+	percentage,
+	closeModal,
+}) => {
 	const { isMobile } = useDetectDevice();
 
 	return (
@@ -18,13 +28,45 @@ const BoostedInnerModal: FC<IBoostedModalProps> = ({ percentage }) => {
 					<ConfettiAnimation size={isMobile ? 200 : 600} />
 				</ConfettiContainer>
 				<BoostedTitle>Project boosted!</BoostedTitle>
-				<Lead>
+				<Desc>
 					You boosted this project with {percentage}% of your
 					GIVpower.
-				</Lead>
+				</Desc>
+				<ActionsSection>
+					<GetButton
+						label='Get GIVpower'
+						size='small'
+						href={`${Routes.GIVfarm}/?open=${StakingType.GIV_LM}&chain=gnosis`}
+					/>
+					<Link href={Routes.MyBoostedProjects} passHref>
+						<CustomButtonLink
+							linkType='texty'
+							size='small'
+							label='see your boosted projects'
+						/>
+					</Link>
+					<CustomButton
+						buttonType='texty'
+						size='small'
+						label='DISMISS'
+						onClick={closeModal}
+					/>
+				</ActionsSection>
 			</>
 		</div>
 	);
 };
+
+const ActionsSection = styled.div`
+	margin-top: 68px;
+`;
+
+const Desc = styled(Lead)`
+	width: 100%;
+	margin: auto;
+	${mediaQueries.tablet} {
+		width: 398px;
+	}
+`;
 
 export default BoostedInnerModal;
