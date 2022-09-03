519e75704c93e0253cbb4ad5d89dc673fb262570
Cherik <Pourcheriki@gmail.com>
2022-09-03 19:39:14 +0430

    add boosted tab contribute card

--- src/components/views/userProfile/boostedTab/ProfileBoostedTab.tsx
+++ src/components/views/userProfile/boostedTab/ProfileBoostedTab.tsx
@@ -1,7 +1,34 @@
-import { UserProfileTab } from '../common.sc';
+import { H5, mediaQueries } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+import {
+	ContributeCard,
+	ContributeCardTitles,
+	UserProfileTab,
+} from '../common.sc';
 import { IUserProfileView } from '../UserProfile.view';
+import { formatWeiHelper } from '@/helpers/number';
 import type { FC } from 'react';
 
 export const ProfileBoostedTab: FC<IUserProfileView> = () => {
-	return <UserProfileTab>ProfileBoostedTab</UserProfileTab>;
+	const TotalAmountofGIVpower = '7989240000000000000000';
+
+	return (
+		<UserProfileTab>
+			<CustomContributeCard>
+				<ContributeCardTitles>
+					~total Amount of GIVpower
+				</ContributeCardTitles>
+				<ContributeCardTitles>Project boosted</ContributeCardTitles>
+				<H5>{formatWeiHelper(TotalAmountofGIVpower)}</H5>
+				<H5>8</H5>
+			</CustomContributeCard>
+		</UserProfileTab>
+	);
 };
+
+const CustomContributeCard = styled(ContributeCard)`
+	width: 100%;
+	${mediaQueries.tablet} {
+		width: 614px;
+	}
+`;
