0e05a4a76a4f837a93988f0c9f8893e095fa647b
Cherik <Pourcheriki@gmail.com>
2022-09-04 13:07:01 +0430

    update names

--- src/components/views/userProfile/boostedTab/BoostsTable.tsx
+++ src/components/views/userProfile/boostedTab/BoostsTable.tsx
@@ -6,7 +6,6 @@ import {
 import { FC } from 'react';
 import styled from 'styled-components';
 
-import { Flex } from '@/components/styled-components/Flex';
 import { mediaQueries } from '@/lib/constants/constants';
 import {
 	RowWrapper,
@@ -44,24 +43,24 @@ const BoostsTable: FC<IBoostsTable> = ({
 			<TableHeader></TableHeader>
 			{boosts?.map(boost => {
 				return (
-					<ProjectsRowWrapper key={boost.project.id}>
-						<ProjectTableCell bold>
+					<BoostsRowWrapper key={boost.project.id}>
+						<BoostsTableCell bold>
 							{boost.project.title}
-						</ProjectTableCell>
-						<ProjectTableCell>
+						</BoostsTableCell>
+						<BoostsTableCell>
 							{formatWeiHelper(
 								_totalAmountOfGIVpower
 									.mul(boost.percentage)
 									.div(100),
 							)}
-						</ProjectTableCell>
-						<ProjectTableCell bold>
+						</BoostsTableCell>
+						<BoostsTableCell bold>
 							{boost.percentage}%
-						</ProjectTableCell>
-						<ProjectTableCell>
+						</BoostsTableCell>
+						<BoostsTableCell>
 							<IconTrash size={24} />
-						</ProjectTableCell>
-					</ProjectsRowWrapper>
+						</BoostsTableCell>
+					</BoostsRowWrapper>
 				);
 			})}
 		</Table>
@@ -79,36 +78,18 @@ const Table = styled.div`
 	}
 `;
 
-const ProjectTableCell = styled(TableCell)<{ bold?: boolean }>`
+const BoostsTableCell = styled(TableCell)<{ bold?: boolean }>`
 	width: 100%;
 	height: 60px;
 	border-bottom: 1px solid ${neutralColors.gray[300]};
 	font-weight: ${props => (props.bold ? 500 : 400)};
 `;
 
-const ProjectsRowWrapper = styled(RowWrapper)`
+const BoostsRowWrapper = styled(RowWrapper)`
 	&:hover > div {
 		background-color: ${neutralColors.gray[300]};
 		color: ${brandColors.pinky[500]};
 	}
-`;
-
-const Actions = styled(Flex)<{ isCancelled: boolean }>`
-	gap: 10px;
-	> * {
-		color: ${props =>
-			props.isCancelled
-				? brandColors.pinky[200]
-				: brandColors.pinky[500]};
-		cursor: ${props => (props.isCancelled ? 'default' : 'pointer')};
-		font-size: 14px;
-	}
-`;
-
-const ProjectTitle = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	gap: 0 10px;
 `;
 
 export default BoostsTable;
