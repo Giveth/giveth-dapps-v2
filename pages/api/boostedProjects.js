387aded61d0d6c941a1eaa8eead0c31af4c09f6e
Cherik <Pourcheriki@gmail.com>
2022-09-04 15:34:52 +0430

    update mock api

--- pages/api/boostedProjects.js
+++ pages/api/boostedProjects.js
@@ -4,25 +4,28 @@ const handler = (req, res) => {
 		res.status(200).json({
 			boostedProjects: [
 				{
-					project: { title: 'The Giveth Community of Makers' },
+					project: { title: 'The Giveth Community of Makers', id: 1 },
 					percentage: 30,
 				},
 				{
-					project: { title: 'Gitcoin Grants Matching Pool' },
+					project: { title: 'Gitcoin Grants Matching Pool', id: 2 },
 					percentage: 5,
 				},
-				{ project: { title: 'Unchain Fund' }, percentage: 10 },
+				{ project: { title: 'Unchain Fund', id: 3 }, percentage: 10 },
 				{
-					project: { title: 'Bridging Digital Communities' },
+					project: { title: 'Bridging Digital Communities', id: 4 },
 					percentage: 10,
 				},
-				{ project: { title: 'Free The Food' }, percentage: 10 },
+				{ project: { title: 'Free The Food', id: 5 }, percentage: 10 },
 				{
-					project: { title: 'Diamante Bridge Collective' },
+					project: { title: 'Diamante Bridge Collective', id: 6 },
 					percentage: 10,
 				},
-				{ project: { title: 'Red Armonia' }, percentage: 10 },
-				{ project: { title: 'Giveth Matching Pool' }, percentage: 15 },
+				{ project: { title: 'Red Armonia', id: 7 }, percentage: 10 },
+				{
+					project: { title: 'Giveth Matching Pool', id: 8 },
+					percentage: 15,
+				},
 			],
 		});
 	} catch (error) {
