1b93eb53e5b2b3a1a2299adcfd26bb5a7ee8a0f4
Cherik <Pourcheriki@gmail.com>
2022-09-04 11:50:58 +0430

    add send request useeffect

--- src/components/views/userProfile/boostedTab/ProfileBoostedTab.tsx
+++ src/components/views/userProfile/boostedTab/ProfileBoostedTab.tsx
@@ -1,16 +1,44 @@
 import { H5, mediaQueries } from '@giveth/ui-design-system';
 import styled from 'styled-components';
+import { FC, useEffect, useState } from 'react';
 import {
 	ContributeCard,
 	ContributeCardTitles,
 	UserProfileTab,
 } from '../common.sc';
-import { IUserProfileView } from '../UserProfile.view';
+import { EOrderBy, IOrder, IUserProfileView } from '../UserProfile.view';
 import { formatWeiHelper } from '@/helpers/number';
-import type { FC } from 'react';
+import { EDirection } from '@/apollo/types/gqlEnums';
 
-export const ProfileBoostedTab: FC<IUserProfileView> = () => {
-	const TotalAmountofGIVpower = '7989240000000000000000';
+export const ProfileBoostedTab: FC<IUserProfileView> = ({ user }) => {
+	const [loading, setLoading] = useState(false);
+	const [order, setOrder] = useState<IOrder>({
+		by: EOrderBy.CreationDate,
+		direction: EDirection.DESC,
+	});
+
+	const TotalAmountOfGIVpower = '7989240000000000000000';
+
+	const changeOrder = (orderBy: EOrderBy) => {
+		if (orderBy === order.by) {
+			setOrder({
+				by: orderBy,
+				direction:
+					order.direction === EDirection.ASC
+						? EDirection.DESC
+						: EDirection.ASC,
+			});
+		} else {
+			setOrder({
+				by: orderBy,
+				direction: EDirection.DESC,
+			});
+		}
+	};
+
+	useEffect(() => {
+		if (!user) return;
+	}, [user, order.by, order.direction]);
 
 	return (
 		<UserProfileTab>
@@ -19,7 +47,7 @@ export const ProfileBoostedTab: FC<IUserProfileView> = () => {
 					~total Amount of GIVpower
 				</ContributeCardTitles>
 				<ContributeCardTitles>Project boosted</ContributeCardTitles>
-				<H5>{formatWeiHelper(TotalAmountofGIVpower)}</H5>
+				<H5>{formatWeiHelper(TotalAmountOfGIVpower)}</H5>
 				<H5>8</H5>
 			</CustomContributeCard>
 		</UserProfileTab>
