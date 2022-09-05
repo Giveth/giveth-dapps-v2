1c606e41a0c238b8ae8c3e730240b4203bfc0fea
Ramin <r.ramazanpour@aut.ac.ir>
2022-09-05 16:10:23 +0430

    remove sortby from filters

--- src/components/menu/FilterMenu.tsx
+++ src/components/menu/FilterMenu.tsx
@@ -2,19 +2,13 @@ import {
 	B,
 	brandColors,
 	ButtonText,
-	GLink,
-	IconArrowBottom,
-	IconArrowTop,
-	IconDonation16,
-	IconHeartOutline16,
 	IconX,
 	neutralColors,
 } from '@giveth/ui-design-system';
 import styled from 'styled-components';
 import { forwardRef } from 'react';
 import { mediaQueries } from '@/lib/constants/constants';
-import { Flex, FlexCenter } from '../styled-components/Flex';
-import { ESortbyAllProjects } from '@/apollo/types/gqlEnums';
+import { FlexCenter } from '../styled-components/Flex';
 import CheckBox from '../Checkbox';
 import { useProjectsContext } from '@/context/projects.context';
 import { zIndex } from '@/lib/constants/constants';
@@ -29,15 +23,14 @@ export const FilterMenu = forwardRef<HTMLDivElement, IFilterMenuProps>(
 		const { setVariables, variables } = useProjectsContext();
 		const filtersCount = variables?.filters?.length ?? 0;
 		const handleSelectFilter = (e: boolean, filter: string) => {
-			if (e === true) {
+			if (e) {
 				setVariables({
 					...variables,
 					filters: !variables.filters?.includes(filter)
 						? [...(variables.filters || []), filter]
 						: variables.filters,
 				});
-			}
-			if (e === false) {
+			} else {
 				setVariables({
 					...variables,
 					filters: variables.filters?.filter(
@@ -53,7 +46,7 @@ export const FilterMenu = forwardRef<HTMLDivElement, IFilterMenuProps>(
 						<IconX size={24} />
 					</CloseContainer>
 					<FlexCenter gap='8px'>
-						<Title size='medium'>Filters</Title>
+						<ButtonText size='medium'>Filters</ButtonText>
 						{filtersCount !== 0 && (
 							<PinkyColoredNumber size='medium'>
 								{filtersCount}
@@ -61,26 +54,6 @@ export const FilterMenu = forwardRef<HTMLDivElement, IFilterMenuProps>(
 						)}
 					</FlexCenter>
 				</Header>
-				<Section>
-					<B>Sort by</B>
-					{sortByOptions.map((sortByOption, idx) => (
-						<SortItem
-							isSortSelected={
-								variables.sortingBy === sortByOption.value
-							}
-							key={idx}
-							onClick={e => {
-								setVariables({
-									...variables,
-									sortingBy: sortByOption.value,
-								});
-							}}
-						>
-							<GLink>{sortByOption.label}</GLink>
-							<IconContainer>{sortByOption.icon}</IconContainer>
-						</SortItem>
-					))}
-				</Section>
 				<Section>
 					<B>Project features</B>
 					{projectFeatures.map((projectFeature, idx) => (
@@ -95,7 +68,7 @@ export const FilterMenu = forwardRef<HTMLDivElement, IFilterMenuProps>(
 										projectFeature.value,
 									) ?? false
 								}
-								size={16}
+								size={14}
 							/>
 						</FeatureItem>
 					))}
@@ -106,30 +79,6 @@ export const FilterMenu = forwardRef<HTMLDivElement, IFilterMenuProps>(
 );
 
 FilterMenu.displayName = 'FilterMenu';
-
-// should merge with sortByOptions in projectsIndex at the end
-const sortByOptions = [
-	{
-		label: 'Newest',
-		value: ESortbyAllProjects.NEWEST,
-		icon: <IconArrowTop size={16} />,
-	},
-	{
-		label: 'Oldest',
-		value: ESortbyAllProjects.OLDEST,
-		icon: <IconArrowBottom size={16} />,
-	},
-	{
-		label: 'Most liked',
-		value: ESortbyAllProjects.MOSTLIKED,
-		icon: <IconHeartOutline16 />,
-	},
-	{
-		label: 'Most funded',
-		value: ESortbyAllProjects.MOSTFUNDED,
-		icon: <IconDonation16 />,
-	},
-];
 
 const projectFeatures = [
 	{ label: 'Accept GIV token', value: 'AcceptGiv' },
@@ -175,31 +124,8 @@ const CloseContainer = styled.div`
 	cursor: pointer;
 `;
 
-const Title = styled(ButtonText)``;
-
 const Section = styled.section`
 	margin: 24px 0;
-`;
-
-const SortItem = styled(Flex)<{ isSortSelected?: boolean }>`
-	margin: 16px 0;
-
-	background-color: ${props =>
-		props.isSortSelected
-			? neutralColors.gray[900]
-			: neutralColors.gray[300]};
-	color: ${props => props.isSortSelected && neutralColors.gray[100]};
-	border-radius: 50px;
-	padding: 10px 16px;
-	gap: 10px;
-	width: fit-content;
-	cursor: pointer;
-`;
-
-const IconContainer = styled.div`
-	width: 16px;
-	height: 22px;
-	padding: 2px 0;
 `;
 
 const FeatureItem = styled.div`
@@ -212,7 +138,7 @@ const FeatureItem = styled.div`
 	}
 `;
 
-export const PinkyColoredNumber = styled(Title)`
+export const PinkyColoredNumber = styled(ButtonText)`
 	background-color: ${brandColors.pinky[500]};
 	width: 18px;
 	height: 18px;
