1c576d23c656b2aff3c7127e6a04ab4dca46018f
Ramin <r.ramazanpour@aut.ac.ir>
2022-09-28 03:05:48 +0330

    #722 GIVpower Staking Card Tooltip update

--- src/components/cards/BaseStakingCard.tsx
+++ src/components/cards/BaseStakingCard.tsx
@@ -345,9 +345,10 @@ const BaseStakingCard: FC<IBaseStakingCardProps> = ({
 										}
 									>
 										<GIVgardenTooltip>
-											While staking GIV in this pool, you
-											are also granted voting power (gGIV)
-											in the GIVgarden.
+											Staking GIV in this pool allows you
+											to support projects with GIVpower &
+											grants you voting power in Giveth
+											DAO (gGIV), in addition to the APR.
 										</GIVgardenTooltip>
 									</IconWithTooltip>
 								)}
