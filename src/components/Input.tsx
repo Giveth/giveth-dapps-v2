3193eee3a3651901c6b2785e8399f96c94459da9
Cherik <Pourcheriki@gmail.com>
2022-10-04 14:13:17 +0330

    remove wrong import

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,7 +14,6 @@ import React, {
 	useRef,
 } from 'react';
 import styled, { css } from 'styled-components';
-import { IIconProps } from '@giveth/ui-design-system/lib/esm/components/icons/giv-economy/type';
 import { EInputValidation, IInputValidation } from '@/types/inputValidation';
 import InputStyled from './styled-components/Input';
 import LottieControl from '@/components/animations/lottieControl';
