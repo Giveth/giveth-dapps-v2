f4ccd3ef1fd5e9b88bbd3f2071e8a74dae6a9adb
Cherik <Pourcheriki@gmail.com>
2022-10-12 14:18:31 +0330

    init boost modal test

---
+++ src/components/modals/Boost/BoostModa.test.tsx
@@ -0,0 +1,51 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import BoostModal from './BoostModal';
+import config from '@/configuration';
+import { render } from '@/tests/utils';
+
+// We use msw to intercept the network request during the test,
+// and return the response 'John Smith' after 150ms
+// when receiving a get request to the `/api/user` endpoint
+export const handlers = [
+	rest.get(config.XDAI_CONFIG.subgraphAddress, (req, res, ctx) => {
+		return res(
+			ctx.json({
+				givpowerInfo: {
+					id: '0xdaea66adc97833781139373df5b3bced3fdda5b1',
+					initialDate: '1640617800',
+					locksCreated: 18,
+					roundDuration: 1209600,
+					totalGIVLocked: '201271330350605663881960',
+				},
+				userGIVLocked: {
+					givLocked: '1000000000000000000',
+				},
+			}),
+			ctx.delay(150),
+		);
+	}),
+	// rest.get(config.MAINNET_CONFIG.subgraphAddress, (req, res, ctx) => {
+	// 	return res(ctx.json('John Smith'), ctx.delay(150));
+	// }),
+];
+
+const server = setupServer(...handlers);
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
+
+test('the GIVpower balance is zero', () => {
+	const setStateMock = jest.fn();
+	const useStateMock: any = (useState: any) => [useState, setStateMock];
+	jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+	render(<BoostModal projectId='0' setShowModal={setStateMock} />);
+});
