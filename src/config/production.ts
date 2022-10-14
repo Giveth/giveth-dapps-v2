92be1678f24b10428bd564c9cff75df242aa1e1f
Amin Latifi <a.latifi.al@gmail.com>
2022-10-14 19:53:09 +0330

    Changed fox/dai pool provide liquidity link

--- src/config/production.ts
+++ src/config/production.ts
@@ -312,7 +312,7 @@ const config: EnvConfig = {
 						title: 'FOX / xDAI',
 						description: '50% FOX, 50% xDAI',
 						provideLiquidityLink:
-							'https://app.honeyswap.org/#/add/0x21a42669643f45bc0e086b8fc2ed70c23d67509d/0xe91d153e0b41518a2ce8dd3d7944fa863463a97d?chainId=100',
+							'https://app.honeyswap.org/#/add/0x21a42669643f45bc0e086b8fc2ed70c23d67509d/XDAI?chainId=100',
 						unit: 'LP',
 						regenStreamType: StreamType.FOX,
 						regenFarmType: RegenFarmType.FOX_XDAI,
