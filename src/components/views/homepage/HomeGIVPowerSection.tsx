cef807542170cd2e266607518a7a633a61f76255
Cherik <Pourcheriki@gmail.com>
2022-10-18 18:55:35 +0330

    fix eslint issue

--- src/components/views/homepage/HomeGIVPowerSection.tsx
+++ src/components/views/homepage/HomeGIVPowerSection.tsx
@@ -1,18 +1,22 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import Image from 'next/image';
 import { HomeContainer } from '@/components/views/homepage/Home.sc';
 import { mediaQueries } from '@/lib/constants/constants';
 import { Shadow } from '@/components/styled-components/Shadow';
-import Image from 'next/image';
-import givpowerIsHere from '../../../../public/images/backgrounds/givpowerishere.png'
+import givpowerIsHere from '../../../../public/images/backgrounds/givpowerishere.png';
 
 const HomePurpleSection = () => {
 	return (
 		<Link href='/givpower'>
 			<a>
-			<Wrapper>
-				<Image src={givpowerIsHere} layout='fill' objectFit='cover' />
-			</Wrapper>
+				<Wrapper>
+					<Image
+						src={givpowerIsHere}
+						layout='fill'
+						objectFit='cover'
+					/>
+				</Wrapper>
 			</a>
 		</Link>
 	);
@@ -29,7 +33,6 @@ const Wrapper = styled(HomeContainer)`
 	z-index: 2;
 	overflow: hidden;
 	box-shadow: ${Shadow.Neutral[400]};
-
 
 	${mediaQueries.tablet} {
 		min-height: 550px;
