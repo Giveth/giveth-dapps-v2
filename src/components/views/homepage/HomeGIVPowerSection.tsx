095c337bd34db9cc4143e6dc8488b9e207aaa60a
Cherik <Pourcheriki@gmail.com>
2022-10-18 18:47:16 +0330

    fix image ratio

--- src/components/views/homepage/HomeGIVPowerSection.tsx
+++ src/components/views/homepage/HomeGIVPowerSection.tsx
@@ -4,13 +4,14 @@ import { HomeContainer } from '@/components/views/homepage/Home.sc';
 import { mediaQueries } from '@/lib/constants/constants';
 import { Shadow } from '@/components/styled-components/Shadow';
 import Image from 'next/image';
+import givpowerIsHere from '../../../../public/images/backgrounds/givpowerishere.png'
 
 const HomePurpleSection = () => {
 	return (
 		<Link href='/givpower'>
 			<a>
 			<Wrapper>
-				<Image src='/images/backgrounds/givpowerishere.png' layout='responsive' width='2880px' height='1080px' />
+				<Image src={givpowerIsHere} layout='fill' objectFit='cover' />
 			</Wrapper>
 			</a>
 		</Link>
@@ -28,26 +29,10 @@ const Wrapper = styled(HomeContainer)`
 	z-index: 2;
 	overflow: hidden;
 	box-shadow: ${Shadow.Neutral[400]};
-	top: -50px;
-	// ::after {
-	// 	background-image: url('/images/backgrounds/givpowerishere.png');
-	// 	background-position: center;
-	// 	content: '';
-	// 	top: 0;
-	// 	left: 0;
-	// 	bottom: 0;
-	// 	right: 0;
-	// 	position: absolute;
-	// 	z-index: -1;
-	// 	width: 100%;
-	// 	background-size: cover;
-	// }
+
 
 	${mediaQueries.tablet} {
 		min-height: 550px;
-		// ::after {
-		// 	background-size: cover;
-		// }
 	}
 `;
 
