167a628de025898d4ad3f491aa24218c4c64f834
Cherik <Pourcheriki@gmail.com>
2022-10-18 18:17:28 +0330

    try to use Image compnent instead of background

--- src/components/views/homepage/HomeGIVPowerSection.tsx
+++ src/components/views/homepage/HomeGIVPowerSection.tsx
@@ -3,11 +3,16 @@ import styled from 'styled-components';
 import { HomeContainer } from '@/components/views/homepage/Home.sc';
 import { mediaQueries } from '@/lib/constants/constants';
 import { Shadow } from '@/components/styled-components/Shadow';
+import Image from 'next/image';
 
 const HomePurpleSection = () => {
 	return (
 		<Link href='/givpower'>
-			<Wrapper />
+			<a>
+			<Wrapper>
+				<Image src='/images/backgrounds/givpowerishere.png' layout='responsive' width='2880px' height='1080px' />
+			</Wrapper>
+			</a>
 		</Link>
 	);
 };
@@ -24,25 +29,25 @@ const Wrapper = styled(HomeContainer)`
 	overflow: hidden;
 	box-shadow: ${Shadow.Neutral[400]};
 	top: -50px;
-	::after {
-		background-image: url('/images/backgrounds/GIVPowerIsComing.png');
-		background-position: center;
-		content: '';
-		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
-		position: absolute;
-		z-index: -1;
-		width: 100%;
-		background-size: cover;
-	}
+	// ::after {
+	// 	background-image: url('/images/backgrounds/givpowerishere.png');
+	// 	background-position: center;
+	// 	content: '';
+	// 	top: 0;
+	// 	left: 0;
+	// 	bottom: 0;
+	// 	right: 0;
+	// 	position: absolute;
+	// 	z-index: -1;
+	// 	width: 100%;
+	// 	background-size: cover;
+	// }
 
 	${mediaQueries.tablet} {
-		min-height: 450px;
-		::after {
-			background-size: cover;
-		}
+		min-height: 550px;
+		// ::after {
+		// 	background-size: cover;
+		// }
 	}
 `;
 
