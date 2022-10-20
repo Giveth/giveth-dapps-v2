61c41c0d63e891b7821d6800ce582553e51081e3
Ramin <r.ramazanpour@aut.ac.ir>
2022-10-20 15:57:03 +0330

    add giveth project wallet address GQL

--- src/apollo/gql/gqlProjects.ts
+++ src/apollo/gql/gqlProjects.ts
@@ -356,6 +356,18 @@ export const FETCH_PROJECT_BY_ID = gql`
 	}
 `;
 
+export const FETCH_GIVETH_PROJECT_BY_ID = gql`
+	query ProjectById($id: Float!) {
+		projectById(id: $id) {
+			addresses {
+				address
+				isRecipient
+				networkId
+			}
+		}
+	}
+`;
+
 export const FETCH_PROJECT_REACTION_BY_ID = gql`
 	query ProjectById($id: Float!, $connectedWalletUserId: Int) {
 		projectById(id: $id, connectedWalletUserId: $connectedWalletUserId) {
