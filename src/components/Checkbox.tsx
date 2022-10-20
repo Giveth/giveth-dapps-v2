33d4821ea0440dc99eab629140c92ef31a391649
Ramin <r.ramazanpour@aut.ac.ir>
2022-10-20 15:53:35 +0330

    fix border radius for checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -52,13 +52,10 @@ const Wrapper = styled(Flex)<{
 	color: ${props =>
 		props.disabled ? neutralColors.gray[600] : neutralColors.gray[800]};
 	> div:first-child {
-		border: 2px solid
-			${props =>
-				props.disabled
-					? neutralColors.gray[400]
-					: neutralColors.gray[900]};
+		border-color: ${props =>
+			props.disabled ? neutralColors.gray[400] : neutralColors.gray[900]};
+		border-style: solid;
 		flex-shrink: 0;
-		border-radius: 4px;
 		width: ${props => `${props.size}px`};
 		height: ${props => `${props.size}px`};
 		background-color: ${props =>
@@ -72,6 +69,7 @@ const Wrapper = styled(Flex)<{
 					gap: 10px;
 					> div:first-child {
 						border-width: 1px;
+						border-radius: 2px;
 					}
 				`;
 			case 16:
@@ -79,6 +77,7 @@ const Wrapper = styled(Flex)<{
 					gap: 10px;
 					> div:first-child {
 						border-width: 1px;
+						border-radius: 3px;
 					}
 				`;
 			case 20:
@@ -86,6 +85,7 @@ const Wrapper = styled(Flex)<{
 					gap: 10px;
 					> div:first-child {
 						border-width: 2px;
+						border-radius: 4px;
 					}
 				`;
 			case 24:
@@ -93,6 +93,7 @@ const Wrapper = styled(Flex)<{
 					gap: 12px;
 					> div:first-child {
 						border-width: 2px;
+						border-radius: 4px;
 					}
 				`;
 			case 32:
@@ -100,6 +101,7 @@ const Wrapper = styled(Flex)<{
 					gap: 12px;
 					> div:first-child {
 						border-width: 2px;
+						border-radius: 4px;
 					}
 				`;
 			default:
