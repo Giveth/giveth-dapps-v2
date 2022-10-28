27153211a7eac7c0575fce982974b7892edfa26b
Cherik <Pourcheriki@gmail.com>
2022-10-28 12:28:09 +0330

    check use has givpower or not

--- src/components/views/userProfile/boostedTab/EmptyPowerBoosting.tsx
+++ src/components/views/userProfile/boostedTab/EmptyPowerBoosting.tsx
@@ -3,19 +3,31 @@ import { ButtonLink, neutralColors, QuoteText } from '@giveth/ui-design-system';
 import Link from 'next/link';
 import { FlexCenter } from '@/components/styled-components/Flex';
 import Routes from '@/lib/constants/Routes';
+import { useAppSelector } from '@/features/hooks';
+import { SubgraphDataHelper } from '@/lib/subgraph/subgraphDataHelper';
 
 export const EmptyPowerBoosting = () => {
+	const sdh = new SubgraphDataHelper(
+		useAppSelector(state => state.subgraph.xDaiValues),
+	);
+	const givPower = sdh.getUserGIVPowerBalance();
 	return (
 		<EmptyPowerBoostingContainer direction='column'>
-			<Title size='small'>
-				You didn’t boost any project before!
-				<br />
-				Go to the projects and find a good project to boost
-			</Title>
-			<br />
-			<Link href={Routes.Projects} passHref>
-				<ButtonLink label='Go to projects' size='small' />
-			</Link>
+			{givPower.balance === '0' ? (
+				<>
+					<Title size='small'>
+						You didn’t boost any project before!
+						<br />
+						Go to the projects and find a good project to boost
+					</Title>
+					<br />
+					<Link href={Routes.Projects} passHref>
+						<ButtonLink label='Go to projects' size='small' />
+					</Link>
+				</>
+			) : (
+				<></>
+			)}
 		</EmptyPowerBoostingContainer>
 	);
 };
