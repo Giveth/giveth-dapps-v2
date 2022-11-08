642e9113373e6ac0410d4307b1cad037b31769ac
Cherik <Pourcheriki@gmail.com>
2022-11-08 13:29:58 +0300

    add RankBox

--- src/components/views/project/projectGIVPower/GIVPowerHeader.tsx
+++ src/components/views/project/projectGIVPower/GIVPowerHeader.tsx
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
 import {
 	brandColors,
+	Caption,
 	GLink,
+	H3,
 	H5,
+	IconAlertCircle16,
 	neutralColors,
 	P,
+	Subline,
 } from '@giveth/ui-design-system';
 import { FC } from 'react';
 import { IProjectPower } from '@/apollo/types/types';
+import { Col, Row } from '@/components/Grid';
+import { Flex } from '@/components/styled-components/Flex';
 
 interface IGIVPowerHeader {
 	projectPower?: IProjectPower;
@@ -39,6 +45,38 @@ const GIVPowerHeader: FC<IGIVPowerHeader> = ({
 					&nbsp;Learn more.
 				</LearnMoreLink>
 			</Desc>
+			<RanksRow>
+				<Col xs={12} md={6}>
+					<RankBox>
+						<RankTitle>Current Rank</RankTitle>
+						<CurrentRank weight={700}>
+							#{projectPower?.powerRank}
+						</CurrentRank>
+						<RankDescContainer gap='6px'>
+							<IconAlertCircle16 />
+							<Caption>
+								The rank will update at the start of the next
+								GIVbacks round.
+							</Caption>
+						</RankDescContainer>
+					</RankBox>
+				</Col>
+				<Col xs={12} md={6}>
+					<RankBox>
+						<RankTitle>Current Rank</RankTitle>
+						<CurrentRank weight={700}>
+							#{projectPower?.powerRank}
+						</CurrentRank>
+						<RankDescContainer gap='6px'>
+							<IconAlertCircle16 />
+							<Caption>
+								The rank will update at the start of the next
+								GIVbacks round.
+							</Caption>
+						</RankDescContainer>
+					</RankBox>
+				</Col>
+			</RanksRow>
 		</Container>
 	);
 };
@@ -55,6 +93,21 @@ const Desc = styled(P)`
 
 const LearnMoreLink = styled(GLink)`
 	color: ${brandColors.pinky[500]};
+`;
+
+const RanksRow = styled(Row)``;
+
+const RankBox = styled.div`
+	padding: 0 60px 32px 0;
+`;
+
+const RankTitle = styled(Subline)``;
+
+const CurrentRank = styled(H3)`
+	height: 54px;
+`;
+const RankDescContainer = styled(Flex)`
+	padding-top: 6px;
 `;
 
 export default GIVPowerHeader;
