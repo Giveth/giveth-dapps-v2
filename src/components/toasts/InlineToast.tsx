899de587dddeb0744903e6b30a7739f830c9e60a
Ramin <r.ramazanpour@aut.ac.ir>
2022-11-08 15:19:54 +0330

    Remove FixedToast.tsx

--- src/components/toasts/InlineToast.tsx
+++ src/components/toasts/InlineToast.tsx
@@ -1,68 +1,98 @@
 import {
+	brandColors,
 	Caption,
+	IconAlertCircle,
 	IconAlertTriangleFilled,
 	IconCheckCircleFilled,
-	IconHelpFilled,
+	IconHelpFilled16,
 	semanticColors,
 } from '@giveth/ui-design-system';
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import ExternalLink from '@/components/ExternalLink';
 
 export enum EToastType {
 	Error = 'error',
 	Warning = 'warning',
 	Success = 'success',
 	Info = 'info',
+	Hint = 'hint',
 }
 
 interface IProps {
-	message: string;
+	message: string | JSX.Element;
 	type: EToastType;
 	isHidden?: boolean;
+	link?: string;
+	noIcon?: boolean;
 }
 
-const InlineToast: FC<IProps> = ({ message, type, isHidden }) => {
-	const colorType: keyof typeof semanticColors =
-		type === EToastType.Error
-			? 'punch'
-			: type === EToastType.Warning
-			? 'golden'
-			: type === EToastType.Info
-			? 'blueSky'
-			: 'jade';
+interface IColorType {
+	color?: string;
+	backgroundColor?: string;
+	icon?: JSX.Element;
+}
+
+const InlineToast: FC<IProps> = props => {
+	const { message, type, isHidden, noIcon, link } = props;
+	const colorType: IColorType = {};
+	if (type === EToastType.Error) {
+		colorType.color = semanticColors.punch[700];
+		colorType.backgroundColor = semanticColors.punch[100];
+		colorType.icon = <IconAlertTriangleFilled />;
+	} else if (type === EToastType.Warning) {
+		colorType.color = semanticColors.golden[700];
+		colorType.backgroundColor = semanticColors.golden[200];
+		colorType.icon = <IconAlertTriangleFilled />;
+	} else if (type === EToastType.Info) {
+		colorType.color = semanticColors.blueSky[700];
+		colorType.backgroundColor = semanticColors.blueSky[100];
+		colorType.icon = <IconAlertCircle size={19} />;
+	} else if (type === EToastType.Hint) {
+		colorType.color = brandColors.giv[300];
+		colorType.backgroundColor = brandColors.giv[50];
+		colorType.icon = <IconHelpFilled16 />;
+	} else {
+		// Success
+		colorType.color = semanticColors.jade[700];
+		colorType.backgroundColor = semanticColors.jade[100];
+		colorType.icon = <IconCheckCircleFilled />;
+	}
 
 	return (
 		<Container
 			className={isHidden ? 'fadeOut' : 'fadeIn'}
 			colorType={colorType}
 		>
-			{type === EToastType.Success ? (
-				<IconCheckCircleFilled />
-			) : type === EToastType.Info ? (
-				<IconHelpFilled size={16} color={semanticColors.blueSky[700]} />
-			) : (
-				<IconAlertTriangleFilled />
-			)}
+			{!noIcon && colorType.icon}
 			<Caption>{message}</Caption>
+			{link && (
+				<ExternalLink color={colorType.color} href={link}>
+					<Caption medium>Learn more</Caption>
+				</ExternalLink>
+			)}
 		</Container>
 	);
 };
 
-const Container = styled.div<{ colorType: keyof typeof semanticColors }>`
+const Container = styled.div<{ colorType: IColorType }>`
 	display: flex;
 	align-items: center;
 	gap: 16px;
 	padding: 16px;
-	background: ${({ colorType }) =>
-		semanticColors[colorType][colorType === 'golden' ? 200 : 100]};
+	background: ${({ colorType }) => colorType.backgroundColor};
 	border-radius: 8px;
-	border: 1px solid ${({ colorType }) => semanticColors[colorType][700]};
+	border: 1px solid ${({ colorType }) => colorType.color};
 	margin: 24px 0;
 	max-width: 750px;
-	color: ${({ colorType }) => semanticColors[colorType][700]};
+	color: ${({ colorType }) => colorType.color};
 	word-break: break-word;
-	> *:first-child {
+	> svg:first-child,
+	a {
 		flex-shrink: 0;
+	}
+	a {
+		font-weight: 500;
 	}
 `;
 
