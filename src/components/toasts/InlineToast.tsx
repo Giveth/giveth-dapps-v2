5261ad39c34dd1c5b292c0ef5eff8ea30446dffe
Ramin <r.ramazanpour@aut.ac.ir>
2022-11-08 14:07:53 +0330

    Revert "remove duplicate FixedToast.tsx"
    This reverts commit 924e9a88de3176462642069124426846733e5daa.

--- src/components/toasts/InlineToast.tsx
+++ src/components/toasts/InlineToast.tsx
@@ -1,98 +1,68 @@
 import {
-	brandColors,
 	Caption,
-	IconAlertCricle,
 	IconAlertTriangle,
 	IconCheckmarkCircle,
-	IconHelp,
+	IconInfo,
 	semanticColors,
 } from '@giveth/ui-design-system';
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import ExternalLink from '@/components/ExternalLink';
 
 export enum EToastType {
 	Error = 'error',
 	Warning = 'warning',
 	Success = 'success',
 	Info = 'info',
-	Hint = 'hint',
 }
 
 interface IProps {
-	message: string | JSX.Element;
+	message: string;
 	type: EToastType;
 	isHidden?: boolean;
-	link?: string;
-	noIcon?: boolean;
 }
 
-interface IColorType {
-	color?: string;
-	backgroundColor?: string;
-	icon?: JSX.Element;
-}
-
-const InlineToast: FC<IProps> = props => {
-	const { message, type, isHidden, noIcon, link } = props;
-	const colorType: IColorType = {};
-	if (type === EToastType.Error) {
-		colorType.color = semanticColors.punch[700];
-		colorType.backgroundColor = semanticColors.punch[100];
-		colorType.icon = <IconAlertTriangle />;
-	} else if (type === EToastType.Warning) {
-		colorType.color = semanticColors.golden[700];
-		colorType.backgroundColor = semanticColors.golden[200];
-		colorType.icon = <IconAlertTriangle />;
-	} else if (type === EToastType.Info) {
-		colorType.color = semanticColors.blueSky[700];
-		colorType.backgroundColor = semanticColors.blueSky[100];
-		colorType.icon = <IconAlertCricle size={19} />;
-	} else if (type === EToastType.Hint) {
-		colorType.color = brandColors.giv[300];
-		colorType.backgroundColor = brandColors.giv[50];
-		colorType.icon = <IconHelp />;
-	} else {
-		// Success
-		colorType.color = semanticColors.jade[700];
-		colorType.backgroundColor = semanticColors.jade[100];
-		colorType.icon = <IconCheckmarkCircle />;
-	}
+const InlineToast: FC<IProps> = ({ message, type, isHidden }) => {
+	const colorType: keyof typeof semanticColors =
+		type === EToastType.Error
+			? 'punch'
+			: type === EToastType.Warning
+			? 'golden'
+			: type === EToastType.Info
+			? 'blueSky'
+			: 'jade';
 
 	return (
 		<Container
 			className={isHidden ? 'fadeOut' : 'fadeIn'}
 			colorType={colorType}
 		>
-			{!noIcon && colorType.icon}
-			<Caption>{message}</Caption>
-			{link && (
-				<ExternalLink color={colorType.color} href={link}>
-					<Caption medium>Learn more</Caption>
-				</ExternalLink>
+			{type === EToastType.Success ? (
+				<IconCheckmarkCircle />
+			) : type === EToastType.Info ? (
+				<IconInfo size={16} color={semanticColors.blueSky[700]} />
+			) : (
+				<IconAlertTriangle />
 			)}
+			<Caption>{message}</Caption>
 		</Container>
 	);
 };
 
-const Container = styled.div<{ colorType: IColorType }>`
+const Container = styled.div<{ colorType: keyof typeof semanticColors }>`
 	display: flex;
 	align-items: center;
 	gap: 16px;
 	padding: 16px;
-	background: ${({ colorType }) => colorType.backgroundColor};
+	background: ${({ colorType }) =>
+		semanticColors[colorType][colorType === 'golden' ? 200 : 100]};
 	border-radius: 8px;
-	border: 1px solid ${({ colorType }) => colorType.color};
+	border: 1px solid ${({ colorType }) => semanticColors[colorType][700]};
 	margin: 24px 0;
 	max-width: 750px;
-	color: ${({ colorType }) => colorType.color};
+	color: ${({ colorType }) => semanticColors[colorType][700]};
 	word-break: break-word;
-	> svg:first-child,
-	a {
+	> *:first-child {
 		flex-shrink: 0;
-	}
-	a {
-		font-weight: 500;
 	}
 `;
 
