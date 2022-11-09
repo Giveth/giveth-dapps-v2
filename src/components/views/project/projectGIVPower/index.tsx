e0849c8f29d84d8ceea4133d19f9740ced1d2541
Cherik <Pourcheriki@gmail.com>
2022-11-09 16:08:25 +0300

    change request

--- src/components/views/project/projectGIVPower/index.tsx
+++ src/components/views/project/projectGIVPower/index.tsx
@@ -1,13 +1,14 @@
-import { captureException } from '@sentry/nextjs';
 import { useEffect, useState } from 'react';
+import { captureException } from '@sentry/nextjs';
 import GIVPowerHeader from './GIVPowerHeader';
 import GIVPowerTable from './GIVPowerTable';
 import NoBoost from '@/components/views/project/projectGIVPower/NoBoost';
-import { client } from '@/apollo/apolloClient';
-import { FETCH_PROJECT_BOOSTINGS } from '@/apollo/gql/gqlPowerBoosting';
 import { IProjectPower, IUserProjectPowers } from '@/apollo/types/types';
 import Pagination from '@/components/Pagination';
 import { Flex } from '@/components/styled-components/Flex';
+import { client } from '@/apollo/apolloClient';
+import { FETCH_PROJECT_BOOSTINGS } from '@/apollo/gql/gqlPowerBoosting';
+import { showToastError } from '@/lib/helpers';
 
 interface ProjectGIVPowerIndexProps {
 	userId?: string;
@@ -26,41 +27,67 @@ const ProjectGIVPowerIndex = ({
 	isAdmin,
 }: ProjectGIVPowerIndexProps) => {
 	const [boostingsData, setBoostingsData] = useState<IUserProjectPowers>();
+	const [loading, setLoading] = useState(true);
 	const [page, setPage] = useState(0);
 	const hasGivPower = boostingsData ? boostingsData.totalCount > 0 : false;
 	const totalCount = boostingsData?.totalCount ?? 0;
-	console.log('projectFuturePower', projectFuturePower);
-	const fetchProjectBoostings = async () => {
-		if (projectId) {
-			client
-				.query({
-					query: FETCH_PROJECT_BOOSTINGS,
-					variables: {
-						projectId: +projectId,
-						take: itemPerPage,
-						skip: page * itemPerPage,
-					},
-					fetchPolicy: 'network-only',
-				})
-				.then(
-					(res: {
-						data: { userProjectPowers: IUserProjectPowers };
-					}) => {
-						console.log('Res', res?.data);
-						setBoostingsData(res?.data?.userProjectPowers ?? []);
-					},
-				)
-				.catch((error: unknown) => {
-					captureException(error, {
-						tags: {
-							section: 'fetchProjectBoostings',
-						},
-					});
-				});
-		}
-	};
 
 	useEffect(() => {
+		const fetchProjectBoostings = async () => {
+			setLoading(true);
+			if (projectId) {
+				try {
+					const { data } = await client.query({
+						query: FETCH_PROJECT_BOOSTINGS,
+						variables: {
+							projectId: +projectId,
+							take: itemPerPage,
+							skip: page * itemPerPage,
+						},
+						fetchPolicy: 'network-only',
+					});
+					const { userProjectPowers } = data;
+					setBoostingsData(
+						(userProjectPowers as IUserProjectPowers) ?? [],
+					);
+					console.log('userProjectPowers', userProjectPowers);
+				} catch (err) {
+					showToastError(err);
+					captureException(err, {
+						tags: { section: 'createProjectFetchCategories' },
+					});
+				}
+				// const { data, loading } = await client.query({
+				// 	query: FETCH_PROJECT_BOOSTINGS,
+				// 	variables: {
+				// 		projectId: +projectId,
+				// 		take: itemPerPage,
+				// 		skip: page * itemPerPage,
+				// 	},
+				// 	fetchPolicy: 'network-only',
+				// });
+				// console.log('res', res);
+				// .then(
+				// 	(res: {
+				// 		data: { userProjectPowers: IUserProjectPowers };
+				// 	}) => {
+				// 		console.log('Res', res?.data);
+				// 		setBoostingsData(
+				// 			res?.data?.userProjectPowers ?? [],
+				// 		);
+				// 	},
+				// )
+				// .catch((error: unknown) => {
+				// 	captureException(error, {
+				// 		tags: {
+				// 			section: 'fetchProjectBoostings',
+				// 		},
+				// 	});
+				// });
+			}
+			setLoading(false);
+		};
+
 		fetchProjectBoostings();
 	}, [page]);
 
