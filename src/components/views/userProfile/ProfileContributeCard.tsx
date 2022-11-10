2e7bcd217ae31b00a15a6d5e9c17b6b04d08b19b
Cherik <Pourcheriki@gmail.com>
2022-11-10 13:52:12 +0300

    use row and col for ProfileContributeCard

--- src/components/views/userProfile/ProfileContributeCard.tsx
+++ src/components/views/userProfile/ProfileContributeCard.tsx
@@ -2,29 +2,34 @@ import { H2, H5 } from '@giveth/ui-design-system';
 import { FC } from 'react';
 import styled from 'styled-components';
 
-import { mediaQueries } from '@/lib/constants/constants';
-import { Flex } from '../../styled-components/Flex';
 import { IUserProfileView } from '@/components/views/userProfile/UserProfile.view';
 import { formatUSD } from '@/lib/helpers';
 import { ContributeCard, ContributeCardTitles } from './common.sc';
+import { Col, Row } from '@/components/Grid';
 
 const ProfileContributeCard: FC<IUserProfileView> = ({ user }) => {
 	return (
 		<ContributeCardContainer>
-			<ContributeCard>
-				<ContributeCardTitles>donations</ContributeCardTitles>
-				<ContributeCardTitles>
-					Total amount donated
-				</ContributeCardTitles>
-				<H2>{user.donationsCount || 0}</H2>
-				<H5>${formatUSD(user.totalDonated)}</H5>
-			</ContributeCard>
-			<ContributeCard>
-				<ContributeCardTitles>Projects</ContributeCardTitles>
-				<ContributeCardTitles>Donation received</ContributeCardTitles>
-				<H2>{user.projectsCount || 0}</H2>
-				<H5>${formatUSD(user.totalReceived)}</H5>
-			</ContributeCard>
+			<Col lg={6}>
+				<ContributeCard>
+					<ContributeCardTitles>donations</ContributeCardTitles>
+					<ContributeCardTitles>
+						Total amount donated
+					</ContributeCardTitles>
+					<H2>{user.donationsCount || 0}</H2>
+					<H5>${formatUSD(user.totalDonated)}</H5>
+				</ContributeCard>
+			</Col>
+			<Col lg={6}>
+				<ContributeCard>
+					<ContributeCardTitles>Projects</ContributeCardTitles>
+					<ContributeCardTitles>
+						Donation received
+					</ContributeCardTitles>
+					<H2>{user.projectsCount || 0}</H2>
+					<H5>${formatUSD(user.totalReceived)}</H5>
+				</ContributeCard>
+			</Col>
 		</ContributeCardContainer>
 	);
 };
@@ -34,14 +39,6 @@ export const UserContributeTitle = styled(H5)`
 	margin-top: 40px;
 `;
 
-const ContributeCardContainer = styled(Flex)`
-	gap: 32px;
-	justify-content: space-between;
-	flex-direction: column;
-	align-items: center;
-	${mediaQueries.tablet} {
-		flex-direction: row;
-	}
-`;
+const ContributeCardContainer = styled(Row)``;
 
 export default ProfileContributeCard;
