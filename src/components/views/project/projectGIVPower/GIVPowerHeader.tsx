d89f0d95e92520ed397d6cd27ff8cf379b6ca6cd
Cherik <Pourcheriki@gmail.com>
2022-11-14 14:40:17 +0300

    remove arrow for same rank

--- src/components/views/project/projectGIVPower/GIVPowerHeader.tsx
+++ src/components/views/project/projectGIVPower/GIVPowerHeader.tsx
@@ -71,15 +71,15 @@ const GIVPowerHeader: FC<IGIVPowerHeader> = ({
 						<RankTitle>Projected Rank</RankTitle>
 						<NextRank state={goingUp}>
 							<Flex alignItems='baseline' gap='4px'>
-								{goingUp > 0 ? (
+								{goingUp === 0 ? (
+									''
+								) : goingUp > 0 ? (
 									<IconArrowDown16 />
 								) : (
 									<IconArrowUp16 />
 								)}
 								<H6 weight={700}>
-									#
-									{projectFuturePower?.powerRank ||
-										projectPower?.powerRank}
+									#{projectFuturePower?.powerRank || '--'}
 								</H6>
 							</Flex>
 						</NextRank>
