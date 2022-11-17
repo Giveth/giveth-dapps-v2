3b2b07d1dc28d45c2047e9cfbb58c11170af5fb0
Cherik <Pourcheriki@gmail.com>
2022-11-17 13:25:36 +0300

    get data and merge them together

--- src/components/views/project/projectGIVPower/index.tsx
+++ src/components/views/project/projectGIVPower/index.tsx
@@ -3,7 +3,7 @@ import { captureException } from '@sentry/nextjs';
 import GIVPowerHeader from './GIVPowerHeader';
 import GIVPowerTable from './GIVPowerTable';
 import NoBoost from '@/components/views/project/projectGIVPower/NoBoost';
-import { IProjectPower, IUserProjectPowers } from '@/apollo/types/types';
+import { IPowerBoosting, IProjectPower } from '@/apollo/types/types';
 import Pagination from '@/components/Pagination';
 import { Flex } from '@/components/styled-components/Flex';
 import { client } from '@/apollo/apolloClient';
@@ -11,6 +11,22 @@ import { showToastError } from '@/lib/helpers';
 import LoadingAnimation from '@/animations/loading_giv.json';
 import LottieControl from '@/components/animations/lottieControl';
 import { FETCH_PROJECT_BOOSTERS } from '@/apollo/gql/gqlPowerBoosting';
+import { gqlRequest } from '@/helpers/requests';
+import config from '@/configuration';
+import { FETCH_USERS_GIVPOWER_BY_ADDRESS } from '@/apollo/gql/gqlUser';
+
+interface IPowerBoostingWithUserGIVpower extends Omit<IPowerBoosting, 'user'> {
+	user: {
+		name: string;
+		walletAddress: string;
+		totalGIVpowerBalance: string;
+	};
+}
+interface IBoostersData {
+	powerBoostings: IPowerBoostingWithUserGIVpower[];
+	totalPowerBoosting: string;
+	totalCount: number;
+}
 
 interface ProjectGIVPowerIndexProps {
 	userId?: string;
@@ -28,18 +44,19 @@ const ProjectGIVPowerIndex = ({
 	projectFuturePower,
 	isAdmin,
 }: ProjectGIVPowerIndexProps) => {
-	const [boostingsData, setBoostingsData] = useState<IUserProjectPowers>();
+	const [boostersData, setBoostersData] = useState<IBoostersData>();
 	const [loading, setLoading] = useState(true);
 	const [page, setPage] = useState(0);
-	const hasGivPower = boostingsData ? boostingsData.totalCount > 0 : false;
-	const totalCount = boostingsData?.totalCount ?? 0;
+	const hasGivPower = boostersData ? boostersData.totalCount > 0 : false;
+	const totalCount = boostersData?.totalCount ?? 0;
 
 	useEffect(() => {
 		const fetchProjectBoosters = async () => {
 			setLoading(true);
 			if (projectId) {
 				try {
-					const { data } = await client.query({
+					//get users with percentage
+					const boostingResp = await client.query({
 						query: FETCH_PROJECT_BOOSTERS,
 						variables: {
 							take: itemPerPage,
@@ -47,14 +64,66 @@ const ProjectGIVPowerIndex = ({
 							projectId: +projectId,
 						},
 					});
-					const { userProjectPowers } = data;
-					setBoostingsData(
-						(userProjectPowers as IUserProjectPowers) ?? [],
+
+					if (!boostingResp) {
+						setLoading(false);
+						return;
+					}
+
+					const _users =
+						boostingResp.data.getPowerBoosting.powerBoostings.map(
+							(boosting: IPowerBoosting) =>
+								boosting.user.walletAddress?.toLocaleLowerCase(),
+						);
+
+					if (!_users || _users.length === 0) {
+						setLoading(false);
+						return;
+					}
+
+					//get users balance
+					const balancesResp = await gqlRequest(
+						config.XDAI_CONFIG.subgraphAddress,
+						false,
+						FETCH_USERS_GIVPOWER_BY_ADDRESS,
+						{
+							addresses: _users,
+							contract:
+								config.XDAI_CONFIG.GIV.LM_ADDRESS.toLowerCase(),
+							length: _users.length,
+						},
 					);
+
+					const unipoolBalances = balancesResp.data.unipoolBalances;
+
+					const unipoolBalancesObj: { [key: string]: string } = {};
+
+					for (let i = 0; i < unipoolBalances.length; i++) {
+						const unipoolBalance = unipoolBalances[i];
+						unipoolBalancesObj[unipoolBalance.user.id] =
+							unipoolBalance.balance;
+					}
+
+					const _boostersData: IBoostersData = structuredClone(
+						boostingResp.data.getPowerBoosting,
+					);
+
+					for (
+						let i = 0;
+						i < _boostersData.powerBoostings.length;
+						i++
+					) {
+						const powerBoosting = _boostersData.powerBoostings[i];
+						powerBoosting.user.totalGIVpowerBalance =
+							unipoolBalancesObj[
+								powerBoosting.user.walletAddress
+							];
+					}
+					setBoostersData(_boostersData);
 				} catch (err) {
 					showToastError(err);
 					captureException(err, {
-						tags: { section: 'fetchProjectBoostings' },
+						tags: { section: 'fetchProjectBoosters' },
 					});
 				}
 			}
@@ -76,7 +145,7 @@ const ProjectGIVPowerIndex = ({
 			{hasGivPower ? (
 				<>
 					<GIVPowerTable
-						boostingsData={boostingsData?.userProjectPowers ?? []}
+						boostingsData={[]}
 						projectPower={projectPower}
 					/>
 					<Flex justifyContent='flex-end'>
