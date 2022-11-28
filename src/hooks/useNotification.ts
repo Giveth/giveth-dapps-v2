830b199358ad67ac58dd844da2ebd446fa53573a
Cherik <Pourcheriki@gmail.com>
2022-11-28 16:01:47 +0300

    update useNotification

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { INotification } from '@/features/notification/notification.types';
 
 export const useNotification = () => {
 	const [notifications, setNotifications] = useState<INotification[]>([]);
-	const markOneNotificationRead = (notificationId: number) => {
-		console.log('notificationId', notificationId);
-		setNotifications(oldNotifications =>
-			oldNotifications.map(oldNotif =>
-				oldNotif.id === notificationId
-					? { ...oldNotif, isRead: true }
-					: oldNotif,
+	const markOneNotificationRead = useCallback(
+		(notificationId: number) =>
+			setNotifications(oldNotifications =>
+				oldNotifications.map(oldNotif =>
+					oldNotif.id === notificationId
+						? { ...oldNotif, isRead: true }
+						: oldNotif,
+				),
 			),
-		);
-	};
+		[],
+	);
+
 	return { notifications, setNotifications, markOneNotificationRead };
 };
