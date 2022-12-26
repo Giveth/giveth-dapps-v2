6e6862792f7cf9629c60b9cdcbca962b27aaeedc
Cherik <Pourcheriki@gmail.com>
2022-12-26 11:15:24 +0330

    make useModalCallback general

--- src/hooks/useModalCallback.tsx
+++ src/hooks/useModalCallback.tsx
@@ -3,24 +3,35 @@ import { useAppDispatch, useAppSelector } from '@/features/hooks';
 import { setShowSignWithWallet } from '@/features/modal/modal.slice';
 import { useEvent } from './useEvent';
 
-export const useModalCallback = (callback: () => void) => {
+export enum EModalEvents {
+	SIGNEDIN = 'signedin',
+	CONNECTED = 'conneced',
+}
+
+const obj: Record<EModalEvents, 'showSignWithWallet' | 'showWalletModal'> = {
+	[EModalEvents.SIGNEDIN]: 'showSignWithWallet',
+	[EModalEvents.CONNECTED]: 'showWalletModal',
+};
+
+export const useModalCallback = (
+	callback: () => void,
+	event: EModalEvents = EModalEvents.SIGNEDIN,
+) => {
 	const dispatch = useAppDispatch();
 	const cbRef = useEvent(callback);
-	const showSignWithWallet = useAppSelector(
-		state => state.modal.showSignWithWallet,
-	);
+	const showModal = useAppSelector(state => state.modal[obj[event]]);
 	const signInThenDoSomething = () => {
 		if (typeof window === 'undefined') return;
-		window.addEventListener('signin', cbRef, {
+		window.addEventListener(event, cbRef, {
 			once: true,
 		});
 		dispatch(setShowSignWithWallet(true));
 	};
 
 	useEffect(() => {
-		if (showSignWithWallet === false) {
-			window.removeEventListener('signin', cbRef);
+		if (showModal === false) {
+			window.removeEventListener(event, cbRef);
 		}
-	}, [showSignWithWallet]);
+	}, [showModal]);
 	return { signInThenDoSomething };
 };
