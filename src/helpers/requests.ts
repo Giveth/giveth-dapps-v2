1ffa7453c11951039ea6db790ee55ed40bc50c66
alireza-sharifpour <ascode94@gmail.com>
2022-12-31 10:53:44 +0330

    added log in requests

--- src/helpers/requests.ts
+++ src/helpers/requests.ts
@@ -12,6 +12,7 @@ export function sendRequest(
 	additionalOptions: RequestInit = {},
 ) {
 	const token = !isSSRMode ? localStorage.getItem(StorageLabel.TOKEN) : null;
+	console.log('local currT requests', token);
 	const Authorization =
 		authorization && token ? `Bearer ${token}` : undefined;
 	const defaultHeaders = {
