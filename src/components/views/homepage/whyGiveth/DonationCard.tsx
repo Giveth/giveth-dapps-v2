6815f206428fa2bb30867cb47b749373cf27d8d1
Ramin <r.ramazanpour@aut.ac.ir>
2023-01-10 15:13:18 +0330

    implement donation card

---
+++ src/components/views/homepage/whyGiveth/DonationCard.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import { neutralColors } from '@giveth/ui-design-system';
+import { Flex } from '@/components/styled-components/Flex';
+
+interface IDonationCard {
+	address: string;
+	amount: number;
+	projectTitle: string;
+}
+
+const DonationCard: FC<IDonationCard> = props => {
+	const { address, amount, projectTitle } = props;
+	return (
+		<CardContainer>
+			<div>{address}</div>
+			<div>{amount}</div>
+			<div>{projectTitle}</div>
+		</CardContainer>
+	);
+};
+
+const CardContainer = styled(Flex)`
+	padding: 8px 24px;
+	border-radius: 100px;
+	border: 1px solid ${neutralColors.gray[300]};
+`;
+
+export default DonationCard;
