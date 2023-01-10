5bc4539e990a4cc0ca521abe169cacce8ca39bb5
Ramin <r.ramazanpour@aut.ac.ir>
2023-01-10 15:31:36 +0330

    remove home old sections

--- src/components/views/homepage/HomeIndex.tsx
+++ src/components/views/homepage/HomeIndex.tsx
@@ -1,40 +1,16 @@
 import { FC } from 'react';
-import HomeHeader from './HomeHeader';
-import HomeExploreProjects from './HomeExploreProjects';
-import HomePurpleSection from './HomePurpleSection';
-import HomeNiceSection from './HomeNiceSection';
-import HomeGIVPowerSection from './HomeGIVPowerSection';
 import HomeFromBlog from './HomeFromBlog';
 import HomeGetUpdates from './HomeGetUpdates';
-import HomeChangeMakers from './HomeChangeMakers';
 import { IProject } from '@/apollo/types/types';
-import { BigArc } from '@/components/styled-components/Arc';
 
 interface IHomeView {
 	projects: IProject[];
 	totalCount: number;
 }
 
-const projectsSlice = 6;
-
 const HomeIndex: FC<IHomeView> = props => {
-	const { projects, totalCount } = props;
 	return (
 		<>
-			<BigArc />
-			<HomeHeader />
-			<HomeExploreProjects
-				totalCount={totalCount}
-				projects={projects.slice(0, projectsSlice)}
-			/>
-			<HomeGIVPowerSection />
-			<HomeNiceSection />
-			<HomePurpleSection />
-			<HomeExploreProjects
-				projects={projects.slice(projectsSlice)}
-				noTitle
-			/>
-			<HomeChangeMakers />
 			<HomeFromBlog />
 			<HomeGetUpdates />
 		</>
