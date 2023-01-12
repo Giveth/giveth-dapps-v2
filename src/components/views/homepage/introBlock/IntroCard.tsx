ef1ce618ff90e5d0337531ac6df2dd5decd3dcdf
alireza-sharifpour <ascode94@gmail.com>
2023-01-12 15:08:45 +0330

    Fix IntroCard Responsiveness

--- src/components/views/homepage/introBlock/IntroCard.tsx
+++ src/components/views/homepage/introBlock/IntroCard.tsx
@@ -2,6 +2,8 @@ import { H5, neutralColors, P } from '@giveth/ui-design-system';
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 import { Flex } from '@/components/styled-components/Flex';
+import useDetectDevice from '@/hooks/useDetectDevice';
+import { mediaQueries } from '@/lib/constants/constants';
 
 interface IntroCardProps {
 	Icon: ReactElement;
@@ -16,25 +18,33 @@ const IntroCard = ({
 	title,
 	description,
 }: IntroCardProps) => {
+	const { isDesktop } = useDetectDevice();
 	return (
-		<IntroCardContainer>
-			<Flex flexDirection='column' gap='16px'>
-				<Flex gap='16px'>
-					{Icon}
-					<H5 weight={700}>{title}</H5>
-				</Flex>
-				<P>{description}</P>
-				{LinkComponent}
+		<IntroCardContainer flexDirection='column' gap='16px'>
+			<Flex
+				gap='16px'
+				flexDirection={isDesktop ? 'row' : 'column'}
+				alignItems='center'
+			>
+				{Icon}
+				<H5 weight={700}>{title}</H5>
 			</Flex>
+			<P>{description}</P>
+			{LinkComponent}
 		</IntroCardContainer>
 	);
 };
 
-const IntroCardContainer = styled.div`
+const IntroCardContainer = styled(Flex)`
 	border: 1px solid ${neutralColors.gray[300]};
-	padding: 16px 40px;
+	padding: 16px;
 	border-radius: 16px;
 	max-width: 360px;
+	flex: 1;
+	justify-content: space-between;
+	${mediaQueries.desktop} {
+		padding: 16px 40px;
+	}
 `;
 
 export default IntroCard;
