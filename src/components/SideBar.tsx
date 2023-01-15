ddd7af780a70cbd4f8193de8e5e155b70c2a626e
Cherik <Pourcheriki@gmail.com>
2023-01-15 10:23:49 +0330

    set color of sidebar by theme

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,11 @@
-import { brandColors } from '@giveth/ui-design-system';
+import { brandColors, neutralColors } from '@giveth/ui-design-system';
 import React, { FC, ReactNode, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import styled from 'styled-components';
 import { zIndex } from '@/lib/constants/constants';
 import { FlexCenter } from './styled-components/Flex';
 import { useAppSelector } from '@/features/hooks';
+import { ETheme } from '@/features/general/general.slice';
 
 export enum ESideBarDirection {
 	Left,
@@ -51,7 +52,7 @@ export const SideBar: FC<ISideBar> = ({ close, isAnimating, children }) => {
 				close();
 			}}
 		>
-			<SidebarContainer isAnimating={isAnimating}>
+			<SidebarContainer isAnimating={isAnimating} theme={theme}>
 				{children}
 			</SidebarContainer>
 		</Background>,
@@ -75,6 +76,7 @@ const Background = styled(FlexCenter)<{ isAnimating: boolean }>`
 const SidebarContainer = styled(FlexCenter)<{ isAnimating: boolean }>`
 	width: 353px;
 	height: 100%;
+	overflow-y: auto;
 	background: ${brandColors.giv[900]};
 	position: fixed;
 	top: 0;
@@ -82,4 +84,12 @@ const SidebarContainer = styled(FlexCenter)<{ isAnimating: boolean }>`
 	left: 0;
 	left: ${props => (props.isAnimating ? 0 : '-353px')};
 	transition: left 0.3s ease;
+	background-color: ${props =>
+		props.theme === ETheme.Dark
+			? brandColors.giv[600]
+			: neutralColors.gray[100]};
+	color: ${props =>
+		props.theme === ETheme.Dark
+			? neutralColors.gray[100]
+			: neutralColors.gray[900]};
 `;
