faaa0c20c8e7eec93471c31495129b5d42471102
alireza-sharifpour <ascode94@gmail.com>
2023-01-15 14:47:50 +0330

    Added background image to IntroBlock

--- src/components/views/homepage/introBlock/index.tsx
+++ src/components/views/homepage/introBlock/index.tsx
@@ -18,80 +18,101 @@ const IntroBlock = () => {
 	const { isMobile } = useDetectDevice();
 
 	return (
-		<Container>
-			<IntroContainer>
-				<Flex
-					justifyContent='space-around'
-					flexDirection={isMobile ? 'column' : 'row'}
-					alignItems={isMobile ? 'center' : 'stretch'}
-				>
-					<IntroTitle>
-						<H3 weight={700}>
-							Giveth is <UnderlinedText>rewarding</UnderlinedText>{' '}
-							and empowering those who give to projects, to
-							society, and to the world!
-						</H3>
-						<ButtonsContainer gap='16px'>
-							<ButtonLink
-								label='Explore projects'
-								icon={<IconChevronRight24 />}
-							/>
-							<ButtonLink
-								linkType='texty-secondary'
-								label='Our mission'
-								icon={<IconChevronRight24 />}
-							/>
-						</ButtonsContainer>
-					</IntroTitle>
-					<div>Image</div>
-				</Flex>
-				<IntroCards
-					justifyContent='space-between'
-					gap='24px'
-					flexDirection={isMobile ? 'column' : 'row'}
-					alignItems={isMobile ? 'center' : 'stretch'}
-				>
-					<IntroCard
-						Icon={<IconDonation32 />}
-						LinkComponent={
-							<ButtonLink
-								linkType='texty-secondary'
-								label='HOW IT WORKS	'
-								icon={<IconChevronRight24 />}
-							/>
-						}
-						title='Zero fees'
-						description='Create a project or donate directly to for-good projects with zero fees.'
-					/>
-					<IntroCard
-						Icon={<IconBulbOutline32 />}
-						LinkComponent={
-							<ButtonLink
-								linkType='texty-secondary'
-								label='HOW IT WORKS	'
-								icon={<IconChevronRight24 />}
-							/>
-						}
-						title='Earn rewards'
-						description='By donating crypto to verified projects, you get rewarded!'
-					/>
-					<IntroCard
-						Icon={<IconSpark32 />}
-						LinkComponent={
-							<ButtonLink
-								linkType='texty-secondary'
-								label='HOW IT WORKS	'
-								icon={<IconChevronRight24 />}
-							/>
-						}
-						title='Easy onboarding'
-						description='New to crypto? No Problem Start right here'
-					/>
-				</IntroCards>
-			</IntroContainer>
-		</Container>
+		<SectionContainer>
+			<Container>
+				<IntroContainer>
+					<Flex
+						justifyContent='space-around'
+						flexDirection={isMobile ? 'column' : 'row'}
+						alignItems={isMobile ? 'center' : 'stretch'}
+					>
+						<IntroTitle>
+							<H3 weight={700}>
+								Giveth is{' '}
+								<UnderlinedText>rewarding</UnderlinedText> and
+								empowering those who give to projects, to
+								society, and to the world!
+							</H3>
+							<ButtonsContainer gap='16px'>
+								<ButtonLink
+									label='Explore projects'
+									icon={<IconChevronRight24 />}
+								/>
+								<ButtonLink
+									linkType='texty-secondary'
+									label='Our mission'
+									icon={<IconChevronRight24 />}
+								/>
+							</ButtonsContainer>
+						</IntroTitle>
+						<div>Image</div>
+					</Flex>
+					<IntroCards
+						justifyContent='space-between'
+						gap='24px'
+						flexDirection={isMobile ? 'column' : 'row'}
+						alignItems={isMobile ? 'center' : 'stretch'}
+					>
+						<IntroCard
+							Icon={<IconDonation32 />}
+							LinkComponent={
+								<ButtonLink
+									linkType='texty-secondary'
+									label='HOW IT WORKS	'
+									icon={<IconChevronRight24 />}
+								/>
+							}
+							title='Zero fees'
+							description='Create a project or donate directly to for-good projects with zero fees.'
+						/>
+						<IntroCard
+							Icon={<IconBulbOutline32 />}
+							LinkComponent={
+								<ButtonLink
+									linkType='texty-secondary'
+									label='LEARN MORE'
+									icon={<IconChevronRight24 />}
+								/>
+							}
+							title='Earn rewards'
+							description='By donating crypto to verified projects, you get rewarded!'
+						/>
+						<IntroCard
+							Icon={<IconSpark32 />}
+							LinkComponent={
+								<ButtonLink
+									linkType='texty-primary'
+									label='GET STARTED'
+									icon={<IconChevronRight24 />}
+								/>
+							}
+							title='Easy onboarding'
+							description='New to crypto? No Problem Start right here'
+						/>
+					</IntroCards>
+				</IntroContainer>
+			</Container>
+		</SectionContainer>
 	);
 };
+
+const SectionContainer = styled.div`
+	position: relative;
+	background-color: ${neutralColors.gray[100]};
+	::before {
+		content: '';
+		background-image: url('/images/backgrounds/giv-outlined-bright-opacity.png');
+		background-repeat: repeat;
+		position: absolute;
+		height: 100%;
+		opacity: 0.5;
+		top: 0;
+		left: 0;
+		right: 0;
+		width: 100%;
+		z-index: 0;
+	}
+`;
 
 const IntroContainer = styled.div`
 	margin-top: 200px;
