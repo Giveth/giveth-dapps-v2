41f18f7156d4c0c17619f429e3300ec575713575
Cherik <Pourcheriki@gmail.com>
2023-01-16 10:51:20 +0330

    update HomeSidebar

--- src/components/sidebar/HomeSidebar.tsx
+++ src/components/sidebar/HomeSidebar.tsx
@@ -1,10 +1,12 @@
 import { B, Caption, GLink } from '@giveth/ui-design-system';
 import React from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
 import { useAppSelector } from '@/features/hooks';
 import { Flex } from '../styled-components/Flex';
 import { projectsMenuItem } from '../menu/ProjectsMenu';
 import { SidebarItem } from './SidebarItem';
+import { HighlightSection } from '../menu/common';
 
 export const HomeSidebar = () => {
 	const theme = useAppSelector(state => state.general.theme);
@@ -13,16 +15,26 @@ export const HomeSidebar = () => {
 		<HomeSidebarContainer>
 			<SidebarItem item={{ title: 'Home', href: '/' }} />
 			<SidebarItem item={{ title: 'Projects', href: '/projects' }}>
-				<Caption medium>Explore by</Caption>
-				{projectsMenuItem.explore.map((explore, idx) => (
-					<B key={idx}>{explore.name}</B>
-				))}
-				<Caption medium>BY CATEGORY</Caption>
-				{projectsMenuItem.categories.map((explore, idx) => (
-					<GLink size='Big' key={idx}>
-						{explore.name}
-					</GLink>
-				))}
+				<SidebarHighlightSection theme={theme}>
+					<ChildTitle>
+						<Caption medium>Explore by</Caption>
+					</ChildTitle>
+					{projectsMenuItem.explore.map((explore, idx) => (
+						<Link href={explore.url} key={idx}>
+							<ChildItemBold>
+								<B>{explore.name}</B>
+							</ChildItemBold>
+						</Link>
+					))}
+				</SidebarHighlightSection>
+				<SibarChildSection>
+					<ChildTitle medium>BY CATEGORY</ChildTitle>
+					{projectsMenuItem.categories.map((explore, idx) => (
+						<ChildItem size='Big' key={idx}>
+							{explore.name}
+						</ChildItem>
+					))}
+				</SibarChildSection>
 			</SidebarItem>
 		</HomeSidebarContainer>
 	);
@@ -32,5 +44,29 @@ const HomeSidebarContainer = styled(Flex)`
 	padding: 16px;
 	gap: 16px;
 	justify-content: stretch;
+	flex-direction: column;
+`;
+
+const ChildTitle = styled(Caption)`
+	padding: 10px 16px;
+`;
+
+const ChildItem = styled(GLink)`
+	padding: 4px 16px;
+	display: block;
+`;
+
+const ChildItemBold = styled(B)`
+	padding: 4px 16px;
+`;
+
+const SidebarHighlightSection = styled(HighlightSection)`
+	gap: 8px;
+	padding: 8px;
+`;
+
+const SibarChildSection = styled(Flex)`
+	margin-top: 16px;
+	gap: 8px;
 	flex-direction: column;
 `;
