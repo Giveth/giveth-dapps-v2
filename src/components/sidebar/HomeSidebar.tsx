9a38089cac129cdb380802ff904273e5f91309ad
Cherik <Pourcheriki@gmail.com>
2023-01-16 10:18:04 +0330

    add HomeSidebar

--- src/components/sidebar/HomeSidebar.tsx
+++ src/components/sidebar/HomeSidebar.tsx
@@ -1,18 +1,29 @@
-import { B } from '@giveth/ui-design-system';
+import { B, Caption, GLink } from '@giveth/ui-design-system';
 import React from 'react';
 import styled from 'styled-components';
 import { useAppSelector } from '@/features/hooks';
 import { Flex } from '../styled-components/Flex';
 import { projectsMenuItem } from '../menu/ProjectsMenu';
+import { SidebarItem } from './SidebarItem';
 
 export const HomeSidebar = () => {
 	const theme = useAppSelector(state => state.general.theme);
 
 	return (
 		<HomeSidebarContainer>
-			{projectsMenuItem.explore.map((explore, idx) => (
-				<B key={idx}>{explore.name}</B>
-			))}
+			<SidebarItem item={{ title: 'Home', href: '/' }} />
+			<SidebarItem item={{ title: 'Projects', href: '/projects' }}>
+				<Caption medium>Explore by</Caption>
+				{projectsMenuItem.explore.map((explore, idx) => (
+					<B key={idx}>{explore.name}</B>
+				))}
+				<Caption medium>BY CATEGORY</Caption>
+				{projectsMenuItem.categories.map((explore, idx) => (
+					<GLink size='Big' key={idx}>
+						{explore.name}
+					</GLink>
+				))}
+			</SidebarItem>
 		</HomeSidebarContainer>
 	);
 };
@@ -21,20 +32,5 @@ const HomeSidebarContainer = styled(Flex)`
 	padding: 16px;
 	gap: 16px;
 	justify-content: stretch;
+	flex-direction: column;
 `;
-
-// const HighlightSection
-
-// interface ISidebarItem {
-// 	title: string;
-// 	href: string;
-// 	subItems: ISidebarItemProps[];
-// }
-
-// interface ISidebarItemProps {
-// 	items: ISidebarItem[];
-// }
-
-// const SidebarItem: FC<ISidebarItemProps> = () => {
-// 	return <div></div>;
-// };
