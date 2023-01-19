daff2f360f4fd285f58b7ca17b5b02238b310056
Cherik <Pourcheriki@gmail.com>
2023-01-19 14:34:19 +0330

    add SwiperPagination

---
+++ src/components/SwiperPagination.tsx
@@ -0,0 +1,55 @@
+import { H5, neutralColors } from '@giveth/ui-design-system';
+import React, { FC, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Swiper as SwiperType } from 'swiper/types';
+import { Flex } from './styled-components/Flex';
+
+export interface ISwiperPaginationItem {
+	label: string;
+	page: number;
+}
+
+interface ISwiperPagination {
+	swiper?: SwiperType;
+	items: ISwiperPaginationItem[];
+	hasPrevButton?: boolean;
+	hasNextButton?: boolean;
+}
+
+export const SwiperPagination: FC<ISwiperPagination> = ({ swiper, items }) => {
+	const [realIndex, setRealIndex] = useState(items[0].page);
+	useEffect(() => {
+		function realIndexChangeHandler(_swiper: SwiperType) {
+			console.log('realIndexChange', _swiper.realIndex);
+			setRealIndex(_swiper.realIndex);
+		}
+		swiper && swiper.on('realIndexChange', realIndexChangeHandler);
+		return () => {
+			swiper && swiper.off('realIndexChange', realIndexChangeHandler);
+		};
+	}, [swiper]);
+
+	return (
+		<PaginationContainer>
+			{items.map((item, idx) => (
+				<PaginationItem isActive={realIndex === item.page} key={idx}>
+					{item.label}
+				</PaginationItem>
+			))}
+		</PaginationContainer>
+	);
+};
+
+const PaginationContainer = styled(Flex)`
+	gap: 24px;
+	align-items: center;
+`;
+
+const PaginationItem = styled(H5)<{ isActive: boolean }>`
+	border-radius: 50%;
+	cursor: pointer;
+	user-select: none;
+	color: ${({ isActive }) =>
+		isActive ? neutralColors.gray[900] : neutralColors.gray[700]};
+	font-weight: ${({ isActive }) => (isActive ? 700 : 500)};
+`;
--- src/components/views/homepage/projectAtivityBlock/ProjectUpdatesBlock.tsx
+++ src/components/views/homepage/projectAtivityBlock/ProjectUpdatesBlock.tsx
@@ -1,22 +1,52 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperType } from 'swiper/types';
 import 'swiper/css';
+import {
+	ISwiperPaginationItem,
+	SwiperPagination,
+} from '@/components/SwiperPagination';
+
+const items: ISwiperPaginationItem[] = [
+	{
+		label: '1',
+		page: 0,
+	},
+	{
+		label: '2',
+		page: 1,
+	},
+	{
+		label: '3',
+		page: 2,
+	},
+	{
+		label: '4',
+		page: 3,
+	},
+	{
+		label: '5',
+		page: 4,
+	},
+	{
+		label: '6',
+		page: 5,
+	},
+];
 
 export const ProjectUpdatesBlock = () => {
-	const swiperRef = useRef<SwiperType>();
+	const [swiper, setSwiper] = useState<SwiperType>();
+
 	return (
-		<Swiper
-			slidesPerView={1}
-			onSlideChange={() => console.log('slide change')}
-			// onSwiper={swiper => (swiperRef.current = swiper)}
-			onBeforeInit={swiper => (swiperRef.current = swiper)}
-		>
-			<SwiperSlide>Slide 1</SwiperSlide>
-			<SwiperSlide>Slide 2</SwiperSlide>
-			<SwiperSlide>Slide 3</SwiperSlide>
-			<SwiperSlide>Slide 4</SwiperSlide>
-			...
-		</Swiper>
+		<>
+			<SwiperPagination swiper={swiper} items={items} />
+			<Swiper slidesPerView={1} onSwiper={setSwiper}>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				...
+			</Swiper>
+		</>
 	);
 };
