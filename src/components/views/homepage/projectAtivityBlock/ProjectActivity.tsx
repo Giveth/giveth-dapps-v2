e4b5fa9b84baa392a61d3de808eeb1e39e2ffac2
Cherik <Pourcheriki@gmail.com>
2023-01-19 14:33:49 +0330

    fix container

--- src/components/views/homepage/projectAtivityBlock/ProjectActivity.tsx
+++ src/components/views/homepage/projectAtivityBlock/ProjectActivity.tsx
@@ -1,5 +1,5 @@
-import { Container } from '@giveth/ui-design-system';
 import React from 'react';
+import { Container } from '@/components/Grid';
 import { ProjectUpdatesBlock } from './ProjectUpdatesBlock';
 
 export const ProjectActivity = () => {
