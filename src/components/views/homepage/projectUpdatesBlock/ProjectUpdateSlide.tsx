478e5b2bbd717221a58d6147206cc8b9cdbb0096
Cherik <Pourcheriki@gmail.com>
2023-01-22 18:51:27 +0330

    add StyledProjectCard

--- src/components/views/homepage/projectUpdatesBlock/ProjectUpdateSlide.tsx
+++ src/components/views/homepage/projectUpdatesBlock/ProjectUpdateSlide.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Flex } from '@/components/styled-components/Flex';
 import { IProject } from '@/apollo/types/types';
 import ProjectCard from '@/components/project-card/ProjectCard';
+import { mediaQueries } from '@/lib/constants/constants';
 
 interface IProjectUpdateSlideProps {
 	project: IProject;
@@ -14,9 +15,18 @@ export const ProjectUpdateSlide: FC<IProjectUpdateSlideProps> = ({
 	const [isVisibe, setIsVisibe] = useState(false);
 	return (
 		<ProjectUpdateSlideWrapper>
-			<ProjectCard project={project} />
+			<StyledProjectCard project={project} />
+			<ProjectUpdateCard></ProjectUpdateCard>
 		</ProjectUpdateSlideWrapper>
 	);
 };
 
 const ProjectUpdateSlideWrapper = styled(Flex)``;
+
+const StyledProjectCard = styled(ProjectCard)`
+	${mediaQueries.laptopS} {
+		width: 384px !important;
+	}
+`;
+
+const ProjectUpdateCard = styled(Flex)``;
