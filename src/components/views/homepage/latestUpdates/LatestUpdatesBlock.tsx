a091b837be562faa4e0a22cd5df6fb56a8c721f8
Cherik <Pourcheriki@gmail.com>
2023-01-23 15:52:52 +0330

    init animation

--- src/components/views/homepage/latestUpdates/LatestUpdatesBlock.tsx
+++ src/components/views/homepage/latestUpdates/LatestUpdatesBlock.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Container } from '@/components/Grid';
 import { BlockTitle } from '../common';
 import { LatestUpdateCard } from './LatestUpdateCard';
+import { Flex } from '@/components/styled-components/Flex';
 
 export const LatestUpdatesBlock = () => {
 	return (
@@ -10,11 +11,35 @@ export const LatestUpdatesBlock = () => {
 			<Container>
 				<BlockTitle>Awesome Project Updates</BlockTitle>
 			</Container>
-			<LatestUpdateCard />
+			<LatestUpdatesCardsWrapper>
+				<LatestUpdateCard />
+			</LatestUpdatesCardsWrapper>
 		</LatestUpdatesBlockWrapper>
 	);
 };
 
 const LatestUpdatesBlockWrapper = styled.div`
 	padding: 80px 0;
+`;
+
+const LatestUpdatesCardsWrapper = styled(Flex)`
+	transform: translate3d(0, 0, 0);
+	animation: moveSlideshow 10s linear infinite;
+	:hover {
+		animation-play-state: paused;
+	}
+	@keyframes moveSlideshow {
+		0% {
+			transform: translateX(0);
+		}
+		50% {
+			transform: translateX(-100%);
+		}
+		51% {
+			transform: translateX(200%);
+		}
+		100% {
+			transform: translateX(0);
+		}
+	}
 `;
