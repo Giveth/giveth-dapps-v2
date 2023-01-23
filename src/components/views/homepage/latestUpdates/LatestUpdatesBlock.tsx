cc429f89212f3d1d9459122693a54457a20e8037
Cherik <Pourcheriki@gmail.com>
2023-01-23 16:28:39 +0330

    add images

--- src/components/views/homepage/latestUpdates/LatestUpdatesBlock.tsx
+++ src/components/views/homepage/latestUpdates/LatestUpdatesBlock.tsx
@@ -28,57 +28,57 @@ const mockData = [
 	{
 		projectUpdate: {
 			content: '<p>1</p>',
-			createdAt: '1674476883589',
+			createdAt: '1674476181589',
 			id: '1',
 			projectId: '1',
 			title: '1',
 			userId: '1',
 		},
-		cover: '',
+		cover: 'https://giveth.mypinata.cloud/ipfs/QmcQFkNQ3o6f555whoRtFqJgPz6k9nb8WfNEBHk3j2i3CW',
 	},
 	{
 		projectUpdate: {
 			content: '<p>2</p>',
-			createdAt: '1674476883589',
+			createdAt: '1674476282589',
 			id: '2',
 			projectId: '2',
 			title: '2',
 			userId: '2',
 		},
-		cover: '',
+		cover: 'https://giveth.mypinata.cloud/ipfs/QmcQFkNQ3o6f555whoRtFqJgPz6k9nb8WfNEBHk3j2i3CW',
 	},
 	{
 		projectUpdate: {
 			content: '<p>3</p>',
-			createdAt: '1674476883589',
+			createdAt: '1674476383589',
 			id: '3',
 			projectId: '3',
 			title: '3',
 			userId: '3',
 		},
-		cover: '',
+		cover: 'https://giveth.mypinata.cloud/ipfs/QmcQFkNQ3o6f555whoRtFqJgPz6k9nb8WfNEBHk3j2i3CW',
 	},
 	{
 		projectUpdate: {
 			content: '<p>4</p>',
-			createdAt: '1674476883589',
+			createdAt: '1674476484589',
 			id: '4',
 			projectId: '4',
 			title: '4',
 			userId: '4',
 		},
-		cover: '',
+		cover: 'https://giveth.mypinata.cloud/ipfs/QmcQFkNQ3o6f555whoRtFqJgPz6k9nb8WfNEBHk3j2i3CW',
 	},
 	{
 		projectUpdate: {
 			content: '<p>5</p>',
-			createdAt: '1674476883589',
+			createdAt: '1674476585589',
 			id: '5',
 			projectId: '5',
 			title: '5',
 			userId: '5',
 		},
-		cover: '',
+		cover: 'https://giveth.mypinata.cloud/ipfs/QmcQFkNQ3o6f555whoRtFqJgPz6k9nb8WfNEBHk3j2i3CW',
 	},
 ];
 
@@ -86,15 +86,4 @@ const LatestUpdatesBlockWrapper = styled.div`
 	padding: 80px 0;
 `;
 
-const LatestUpdatesCardsWrapper = styled(Flex)`
-	transform: translate3d(0, 0, 0);
-	animation: moveSlideshow 10s linear infinite;
-	:hover {
-		animation-play-state: paused;
-	}
-	@keyframes moveSlideshow {
-		100% {
-			transform: translateX(-100%);
-		}
-	}
-`;
+const LatestUpdatesCardsWrapper = styled(Flex)``;
