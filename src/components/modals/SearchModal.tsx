84d3709e9e152b3a05ea6e4abfa9d29dff4c1f43
Cherik <Pourcheriki@gmail.com>
2023-01-24 16:44:00 +0330

    init search modal

--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import { useDelayedState } from '@/hooks/useDelayedState';
+import React, { FC } from 'react';
 import { Modal } from './Modal';
+import { IModal } from '@/types/common';
+import { useModalAnimation } from '@/hooks/useModalAnimation';
 
-export const SearchModal = () => {
-	const [modalAnimation, modalCondition, openModal, closeModal] =
-		useDelayedState();
+export const SearchModal: FC<IModal> = ({ setShowModal }) => {
+	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
 
 	return (
-		<Modal
-			closeModal={closeModal}
-			isAnimating={modalAnimation}
-			headerTitle='APR'
-		>
+		<Modal closeModal={closeModal} isAnimating={isAnimating} fullScreen>
 			<div>Salam</div>
 		</Modal>
 	);
