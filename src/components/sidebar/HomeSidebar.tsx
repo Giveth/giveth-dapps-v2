d97b865ec5767efebceedc6c08e932ac3fb9a1e8
Cherik <Pourcheriki@gmail.com>
2023-01-24 16:14:29 +0330

    add search button to HomeSidebar

--- src/components/sidebar/HomeSidebar.tsx
+++ src/components/sidebar/HomeSidebar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { GLink } from '@giveth/ui-design-system';
+import { GLink, IconSearch24 } from '@giveth/ui-design-system';
 import Link from 'next/link';
 import { useAppSelector } from '@/features/hooks';
 import { Flex } from '../styled-components/Flex';
-import { SidebarItem } from './SidebarItem';
+import { SidebarParentItem } from './SidebarItem';
 
 import { ProjectsItems } from '../menu/ProjectsItems';
 import { GIVeconomyItems } from '../menu/GIVeconomyItems';
@@ -13,7 +13,7 @@ import { ItemSpacer } from '../menu/common';
 import Routes from '@/lib/constants/Routes';
 
 export const HomeSidebar = () => {
-	const { theme, mainCategories } = useAppSelector(state => state.general);
+	const { theme } = useAppSelector(state => state.general);
 
 	return (
 		<HomeSidebarContainer>
@@ -22,18 +22,24 @@ export const HomeSidebar = () => {
 					<GLink size='Big'>Home</GLink>
 				</Item>
 			</Link>
-			<SidebarItem item={{ title: 'Projects', href: '/projects' }}>
+			<SidebarParentItem item={{ title: 'Projects', href: '/projects' }}>
 				<ProjectsItems inSidebar />
-			</SidebarItem>
-			<SidebarItem item={{ title: 'GIVeconomy' }}>
+			</SidebarParentItem>
+			<SidebarParentItem item={{ title: 'GIVeconomy' }}>
 				<GIVeconomyItems />
-			</SidebarItem>
+			</SidebarParentItem>
 			<ItemSpacer />
 			<Link href={Routes.NFT}>
 				<Item theme={theme}>
 					<GLink size='Big'>NFT</GLink>
 				</Item>
 			</Link>
+			<SearchButton theme={theme} isHighlighted>
+				<Flex alignItems='center' justifyContent='space-between'>
+					<GLink size='Big'>Search</GLink>
+					<IconSearch24 />
+				</Flex>
+			</SearchButton>
 		</HomeSidebarContainer>
 	);
 };
@@ -43,4 +49,8 @@ const HomeSidebarContainer = styled(Flex)`
 	gap: 16px;
 	justify-content: stretch;
 	flex-direction: column;
+`;
+
+const SearchButton = styled(Item)`
+	border-radius: 100px;
 `;
