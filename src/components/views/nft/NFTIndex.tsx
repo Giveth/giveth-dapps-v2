cbfeada1e8dd0ae95b44b5b433dbfa06abd3a492
alireza-sharifpour <ascode94@gmail.com>
2023-01-29 15:18:09 +0330

    set dark theme for nftIndex

--- src/components/views/nft/NFTIndex.tsx
+++ src/components/views/nft/NFTIndex.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { setDarkTheme, setLightTheme } from '@/features/general/general.slice';
+import { useAppDispatch } from '@/features/hooks';
 
 export const NFTIndex = () => {
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(setDarkTheme());
+		return () => {
+			dispatch(setLightTheme());
+		};
+	}, [dispatch]);
 	return <div>NFTIndex</div>;
 };
