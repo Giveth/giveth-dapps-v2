41458992973ad1a1cd6ab4bb51f18e449f538989
alireza-sharifpour <ascode94@gmail.com>
2023-01-29 15:51:08 +0330

    created overviewindex

--- src/components/views/nft/overview/index.tsx
+++ src/components/views/nft/overview/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import { OvalHorizontalGradient, OvalVerticalGradient } from '../common.styles';
 
 const OverviewIndex = () => {
-	return <div>OverviewIndex</div>;
+	return (
+		<OverviewContainer>
+			<OvalVerticalGradient />
+			<OvalHorizontalGradient />
+		</OverviewContainer>
+	);
 };
+
+const OverviewContainer = styled.div`
+	height: 1000px;
+	position: relative;
+`;
 
 export default OverviewIndex;
