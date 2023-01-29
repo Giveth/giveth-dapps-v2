20df59f4580641f19479cae303e3e615515c16c6
alireza-sharifpour <ascode94@gmail.com>
2023-01-29 15:50:21 +0330

    updated  NFTroute with theme

--- pages/nft/index.tsx
+++ pages/nft/index.tsx
@@ -1,7 +1,19 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { NFTIndex } from '@/components/views/nft/NFTIndex';
+import { setDarkTheme, setLightTheme } from '@/features/general/general.slice';
+import { useAppDispatch } from '@/features/hooks';
 
 const NFTRoute = () => {
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(setDarkTheme());
+		return () => {
+			dispatch(setLightTheme());
+		};
+	}, [dispatch]);
+
 	return (
 		<>
 			<Head>
