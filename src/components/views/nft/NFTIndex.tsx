de4f7925ab30e4754c18b67c25a84496c12fdb21
alireza-sharifpour <ascode94@gmail.com>
2023-01-29 15:50:55 +0330

    Updated NFTIndex

--- src/components/views/nft/NFTIndex.tsx
+++ src/components/views/nft/NFTIndex.tsx
@@ -1,20 +1,5 @@
-import React, { useEffect } from 'react';
-import { setDarkTheme, setLightTheme } from '@/features/general/general.slice';
-import { useAppDispatch } from '@/features/hooks';
-import OverviewIndex from './overview';
+import React from 'react';
 
 export const NFTIndex = () => {
-	const dispatch = useAppDispatch();
-
-	useEffect(() => {
-		dispatch(setDarkTheme());
-		return () => {
-			dispatch(setLightTheme());
-		};
-	}, [dispatch]);
-	return (
-		<div>
-			<OverviewIndex />
-		</div>
-	);
+	return <div>NFTIndex</div>;
 };
