566d4432da6025b9ebfc88d8065529f062260e6e
alireza-sharifpour <ascode94@gmail.com>
2023-01-30 14:01:49 +0330

    used SayHelloSection

--- src/components/views/nft/overview/index.tsx
+++ src/components/views/nft/overview/index.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-import { OvalHorizontalGradient, OvalVerticalGradient } from '../common.styles';
+import SayHelloSection from './SayHello';
 
 const OverviewIndex = () => {
-	return (
-		<OverviewContainer>
-			<OvalVerticalGradient />
-			<OvalHorizontalGradient />
-		</OverviewContainer>
-	);
+	return <SayHelloSection />;
 };
-
-const OverviewContainer = styled.div`
-	height: 1000px;
-	position: relative;
-`;
 
 export default OverviewIndex;
