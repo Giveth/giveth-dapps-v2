88f4f9cbfab38a6fcd916617fe90e42151beccb5
alireza-sharifpour <ascode94@gmail.com>
2023-01-30 17:44:42 +0330

    added second column and spacing

--- src/components/views/nft/overview/WhatElse.tsx
+++ src/components/views/nft/overview/WhatElse.tsx
@@ -10,8 +10,8 @@ const WhatElse = () => {
 			<Container>
 				<ContentContainer>
 					<H1>What else?</H1>
-					<Flex>
-						<Flex flexDirection='column'>
+					<Flex gap='64px'>
+						<Flex flexDirection='column' gap='16px'>
 							<HeaderTitle>
 								Get on the ‘$nice list’ for early access to
 								minting.
@@ -22,6 +22,19 @@ const WhatElse = () => {
 								minting of a Givers PFP. Eligible donations will
 								also earn additional rewards through the $nice
 								token program. Learn more about $nice.
+							</Lead>
+						</Flex>
+						<Flex flexDirection='column' gap='16px'>
+							<HeaderTitle>
+								Owning a Giver unlocks special benefits.
+							</HeaderTitle>
+							<Lead>
+								By holding a Giver in your wallet you will show
+								up differently on the Giveth dApp; wherever you
+								make an impact, people will notice your style.
+								You’ll also receive a discount when you register
+								your custom ‘.giveth’ Punk Domain name. Learn
+								more about the benefits.
 							</Lead>
 						</Flex>
 					</Flex>
