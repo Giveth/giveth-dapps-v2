648e2f830f3cc23e236f74d62a94848d845e5f71
alireza-sharifpour <ascode94@gmail.com>
2023-01-30 17:39:52 +0330

    Updated OverviewIndex

--- src/components/views/nft/overview/index.tsx
+++ src/components/views/nft/overview/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import SayHelloSection from './SayHello';
+import WhatElse from './WhatElse';
 
 const OverviewIndex = () => {
-	return <SayHelloSection />;
+	return (
+		<div>
+			<SayHelloSection />
+			<Separator />
+			<WhatElse />
+		</div>
+	);
 };
+
+const Separator = styled.div`
+	width: 100%;
+	height: 40px;
+`;
 
 export default OverviewIndex;
