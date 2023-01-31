20a88fe6db22544fe883fb422906781eed15ab03
alireza-sharifpour <ascode94@gmail.com>
2023-01-31 11:23:59 +0330

    added quote text

--- src/components/views/nft/overview/WhatAreGivers.tsx
+++ src/components/views/nft/overview/WhatAreGivers.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { H2, QuoteText } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+import { Flex } from '@/components/styled-components/Flex';
 
 const WhatAreGivers = () => {
-	return <div>WhatAreGivers</div>;
+	return (
+		<Flex
+			justifyContent='center'
+			alignItems='center'
+			flexDirection='column'
+			gap='24px'
+		>
+			<H2>What are the Givers</H2>
+			<QuoteContainer>
+				<QuoteText size='small'>
+					The Givers are a limited collection of 1,250 artworks
+					inspired by the Giveth Galaxy. Each NFT tells a unique story
+					of Giveth, in its own fun and vibrant style.
+				</QuoteText>
+			</QuoteContainer>
+		</Flex>
+	);
 };
+
+const QuoteContainer = styled.div`
+	max-width: 925px;
+	text-align: center;
+`;
 
 export default WhatAreGivers;
