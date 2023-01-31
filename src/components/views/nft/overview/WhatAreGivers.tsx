f69d2885a78593d5dbef199d9b2d2f3f36a645df
alireza-sharifpour <ascode94@gmail.com>
2023-01-31 13:24:03 +0330

    added images

--- src/components/views/nft/overview/WhatAreGivers.tsx
+++ src/components/views/nft/overview/WhatAreGivers.tsx
@@ -1,5 +1,6 @@
 import { H2, QuoteText } from '@giveth/ui-design-system';
 import styled from 'styled-components';
+import Image from 'next/image';
 import { Flex } from '@/components/styled-components/Flex';
 
 const WhatAreGivers = () => {
@@ -18,6 +19,47 @@ const WhatAreGivers = () => {
 					of Giveth, in its own fun and vibrant style.
 				</QuoteText>
 			</QuoteContainer>
+			<ImagesContainer>
+				<Image
+					src={'/images/nft/pfp-o-1.png'}
+					width={400}
+					height={400}
+					alt='pfp-image'
+				/>
+				<Image
+					src={'/images/nft/pfp-o-1.png'}
+					width={450}
+					height={450}
+					alt='pfp-image'
+				/>
+				<Image
+					src={'/images/nft/pfp-o-1.png'}
+					width={500}
+					height={500}
+					alt='pfp-image'
+					style={{
+						zIndex: 2,
+					}}
+				/>
+				<Image
+					src={'/images/nft/pfp-o-1.png'}
+					width={450}
+					height={450}
+					alt='pfp-image'
+					style={{
+						zIndex: 1,
+					}}
+				/>
+				<Image
+					src={'/images/nft/pfp-o-1.png'}
+					width={400}
+					height={400}
+					alt='pfp-image'
+					style={{
+						zIndex: 0,
+					}}
+				/>
+			</ImagesContainer>
 		</Flex>
 	);
 };
@@ -25,6 +67,18 @@ const WhatAreGivers = () => {
 const QuoteContainer = styled.div`
 	max-width: 925px;
 	text-align: center;
+`;
+
+const ImagesContainer = styled.div`
+	position: relative;
+	display: flex;
+	width: 100%;
+	justify-content: center;
+	align-items: center;
+	overflow: hidden;
+	> img {
+		margin: 0 -50px;
+	}
 `;
 
 export default WhatAreGivers;
