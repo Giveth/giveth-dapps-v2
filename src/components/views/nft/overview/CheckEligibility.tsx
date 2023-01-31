2046e5707b97fe111b16798056cf4fb0ce782c0d
alireza-sharifpour <ascode94@gmail.com>
2023-01-31 18:18:41 +0330

    added CheckEligibility section

--- src/components/views/nft/overview/CheckEligibility.tsx
+++ src/components/views/nft/overview/CheckEligibility.tsx
@@ -1,7 +1,17 @@
+import { Button, Container, H2 } from '@giveth/ui-design-system';
 import React from 'react';
+import Input from '@/components/Input';
 
 const CheckEligibility = () => {
-	return <div>CheckEligibility</div>;
+	return (
+		<Container>
+			<H2>Early Minting has started!</H2>
+			<br />
+			<Input label='Check here to verify your eligibility' />
+			<br />
+			<Button buttonType='primary' label='VERIFY' />
+		</Container>
+	);
 };
 
 export default CheckEligibility;
