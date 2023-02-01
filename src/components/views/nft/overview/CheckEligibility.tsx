0fb271cd87e9c95e42a0e49767dbcb029d6f0017
alireza-sharifpour <ascode94@gmail.com>
2023-02-01 15:36:38 +0330

    added CheckEligibility.tsx

--- src/components/views/nft/overview/CheckEligibility.tsx
+++ src/components/views/nft/overview/CheckEligibility.tsx
@@ -1,16 +1,27 @@
-import { Button, Container, H2 } from '@giveth/ui-design-system';
+import {
+	brandColors,
+	Button,
+	Container,
+	H2,
+	P,
+	QuoteText,
+} from '@giveth/ui-design-system';
 import React from 'react';
 import styled from 'styled-components';
-import Input from '@/components/Input';
 
 const CheckEligibility = () => {
 	return (
 		<SectionContainer>
 			<H2>Early Minting has started!</H2>
 			<br />
-			<Input label='Check here to verify your eligibility' />
-			<br />
-			<Button buttonType='primary' label='VERIFY' />
+			<CustomQuote size='small'>
+				Check here to verify your eligibility
+			</CustomQuote>
+			<StyledInput
+				as='input'
+				placeholder='Input your wallet address here'
+			/>
+			<CustomButton buttonType='primary' label='VERIFY' />
 		</SectionContainer>
 	);
 };
@@ -18,6 +29,31 @@ const CheckEligibility = () => {
 const SectionContainer = styled(Container)`
 	position: relative;
 	z-index: 1;
+`;
+
+const StyledInput = styled(P)`
+	padding: 15px 16px;
+	width: 100%;
+	color: ${brandColors.giv[200]};
+	background-color: ${brandColors.giv[700]};
+	border: 1px solid ${brandColors.giv[500]};
+	border-radius: 8px;
+	margin-bottom: 38px;
+	max-width: 440px;
+	&::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+	-moz-appearance: textfield;
+`;
+
+const CustomQuote = styled(QuoteText)`
+	margin-bottom: 14px;
+	color: ${brandColors.giv[200]};
+`;
+
+const CustomButton = styled(Button)`
+	width: 250px;
 `;
 
 export default CheckEligibility;
