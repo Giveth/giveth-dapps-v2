58eedcc090ab7aee573a6cff7abbc5eb8796b018
alireza-sharifpour <ascode94@gmail.com>
2023-02-01 14:53:12 +0330

    fixed zIndex

--- src/components/views/nft/overview/CheckEligibility.tsx
+++ src/components/views/nft/overview/CheckEligibility.tsx
@@ -1,17 +1,23 @@
 import { Button, Container, H2 } from '@giveth/ui-design-system';
 import React from 'react';
+import styled from 'styled-components';
 import Input from '@/components/Input';
 
 const CheckEligibility = () => {
 	return (
-		<Container>
+		<SectionContainer>
 			<H2>Early Minting has started!</H2>
 			<br />
 			<Input label='Check here to verify your eligibility' />
 			<br />
 			<Button buttonType='primary' label='VERIFY' />
-		</Container>
+		</SectionContainer>
 	);
 };
+
+const SectionContainer = styled(Container)`
+	position: relative;
+	z-index: 1;
+`;
 
 export default CheckEligibility;
