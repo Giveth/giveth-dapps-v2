dde64c3dace4f3e49ffb25724793e7260309d099
Cherik <Pourcheriki@gmail.com>
2023-02-06 14:08:37 +0330

    add qty to context

--- src/context/pfpmint.context.tsx
+++ src/context/pfpmint.context.tsx
@@ -10,25 +10,34 @@ export enum EPFPMinSteps {
 interface IPFPMintContext {
 	step: EPFPMinSteps;
 	setStep: Dispatch<SetStateAction<number>>;
+	qty: number;
+	setQty: Dispatch<SetStateAction<number>>;
 }
 
 const PFPMintContext = createContext<IPFPMintContext>({
-	step: EPFPMinSteps.MINT,
+	step: EPFPMinSteps.FAILURE,
 	setStep: () => {
 		console.log('setStep not initialed yet!');
+	},
+	qty: 1,
+	setQty: () => {
+		console.log('setQty not initialed yet!');
 	},
 });
 
 PFPMintContext.displayName = 'PFPMintContext';
 
 export const PFPMintProvider = ({ children }: { children: ReactNode }) => {
-	const [step, setStep] = useState(EPFPMinSteps.MINT);
+	const [step, setStep] = useState(EPFPMinSteps.FAILURE);
+	const [qty, setQty] = useState(1);
 
 	return (
 		<PFPMintContext.Provider
 			value={{
 				step,
 				setStep,
+				qty,
+				setQty,
 			}}
 		>
 			{children}
