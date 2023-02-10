4835de58d594bbc2b629f41b75184522a5a2043f
Ramin <r.ramazanpour@aut.ac.ir>
2023-02-10 21:00:09 +0330

    1. fix double click on tabs hides notifs 2. hide "Load More" and "You don't have any notifications" on loading

--- src/components/views/notification/notification.view.tsx
+++ src/components/views/notification/notification.view.tsx
@@ -75,6 +75,8 @@ function NotificationView() {
 	};
 
 	const handleChangeTab = (newTab: ENotificationTabs) => {
+		if (newTab === tab) return;
+		setLoading(true);
 		setNotifications([]);
 		setPageNumber(0);
 		setTab(newTab);
@@ -245,22 +247,24 @@ function NotificationView() {
 			</Flex>
 			<NotifsHr color={neutralColors.gray[300]} />
 			<div>
-				{notifications.length > 0 ? (
-					notifications.map(notification => (
-						<NotificationBox
-							key={notification.id}
-							notification={notification}
-							markOneNotificationRead={markOneNotificationRead}
-						/>
-					))
-				) : (
-					<FlexCenter>
-						<Lead>You don't have any notifications</Lead>
-					</FlexCenter>
-				)}
+				{notifications.length > 0
+					? notifications.map(notification => (
+							<NotificationBox
+								key={notification.id}
+								notification={notification}
+								markOneNotificationRead={
+									markOneNotificationRead
+								}
+							/>
+					  ))
+					: !loading && (
+							<FlexCenter>
+								<Lead>You don't have any notifications</Lead>
+							</FlexCenter>
+					  )}
 			</div>
 			<FlexCenter>
-				{showLoadMore && (
+				{showLoadMore && !loading && (
 					<OutlineButton
 						buttonType='primary'
 						label='Load More'
