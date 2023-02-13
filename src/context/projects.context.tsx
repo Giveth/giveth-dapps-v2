9d5de0158655b5f9454718c7f3b239dec2c35cce
Cherik <Pourcheriki@gmail.com>
2023-02-13 15:28:10 +0330

    get sort and search by query

--- src/context/projects.context.tsx
+++ src/context/projects.context.tsx
@@ -4,8 +4,10 @@ import {
 	ReactNode,
 	SetStateAction,
 	useContext,
+	useEffect,
 	useState,
 } from 'react';
+import { useRouter } from 'next/router';
 import { IMainCategory } from '@/apollo/types/types';
 import { ESortbyAllProjects } from '@/apollo/types/gqlEnums';
 
@@ -47,6 +49,40 @@ export const ProjectsProvider = (props: {
 	const [variables, setVariables] = useState<IVariables>(
 		variablesDefaultValue,
 	);
+	const router = useRouter();
+
+	useEffect(() => {
+		let sort;
+		if (router.query.sort) {
+			switch ((router.query.sort as string).toLowerCase()) {
+				case ESortbyAllProjects.MOSTFUNDED.toLowerCase():
+					sort = ESortbyAllProjects.MOSTFUNDED;
+					break;
+				case ESortbyAllProjects.MOSTLIKED.toLowerCase():
+					sort = ESortbyAllProjects.MOSTLIKED;
+					break;
+				case ESortbyAllProjects.NEWEST.toLowerCase():
+					sort = ESortbyAllProjects.NEWEST;
+					break;
+				case ESortbyAllProjects.OLDEST.toLowerCase():
+					sort = ESortbyAllProjects.OLDEST;
+					break;
+				case ESortbyAllProjects.QUALITYSCORE.toLowerCase():
+					sort = ESortbyAllProjects.QUALITYSCORE;
+					break;
+				case ESortbyAllProjects.GIVPOWER.toLowerCase():
+					sort = ESortbyAllProjects.GIVPOWER;
+					break;
+				default:
+					break;
+			}
+		}
+		let term = router.query.term as string;
+		setVariables({
+			sortingBy: sort,
+			searchTerm: term,
+		});
+	}, [router.query.sort, router.query.term]);
 
 	return (
 		<ProjectsContext.Provider
