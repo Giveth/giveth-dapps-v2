7fb2b12dfaa5f5417c3b3e8c7a8173028321425c
Cherik <Pourcheriki@gmail.com>
2023-02-13 16:49:38 +0330

    remove showing projects functionality

--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -1,7 +1,6 @@
 import { FC, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { brandColors, H6, Lead, neutralColors } from '@giveth/ui-design-system';
-import { captureException } from '@sentry/nextjs';
 import { Modal } from './Modal';
 import { IModal } from '@/types/common';
 import { useModalAnimation } from '@/hooks/useModalAnimation';
@@ -11,16 +10,6 @@ import { mediaQueries } from '@/lib/constants/constants';
 import { ETheme } from '@/features/general/general.slice';
 import { useAppSelector } from '@/features/hooks';
 import { SearchInput } from '../SearchInput';
-import { client } from '@/apollo/apolloClient';
-import { FETCH_ALL_PROJECTS } from '@/apollo/gql/gqlProjects';
-import { IFetchAllProjects } from '@/apollo/types/gqlTypes';
-import { showToastError } from '@/lib/helpers';
-import { IProject } from '@/apollo/types/types';
-import ProjectCard from '../project-card/ProjectCard';
-import {
-	ProjectsWrapper,
-	ProjectsContainer,
-} from '../views/projects/ProjectsIndex';
 
 const quickLinks = [
 	{ title: 'Top ranking projects', query: '' },
@@ -31,43 +20,10 @@ const quickLinks = [
 
 export const SearchModal: FC<IModal> = ({ setShowModal }) => {
 	const [term, setTerm] = useState<string>('');
-	const [projects, setProjects] = useState<IProject[]>([]);
-	const [loading, setLoading] = useState(false);
 	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
 	const theme = useAppSelector(state => state.general.theme);
 
-	useEffect(() => {
-		if (!term) {
-			setProjects([]);
-			return;
-		}
-		setLoading(true);
-		client
-			.query({
-				query: FETCH_ALL_PROJECTS,
-				variables: {
-					searchTerm: term,
-				},
-				fetchPolicy: 'network-only',
-			})
-			.then((res: { data: { allProjects: IFetchAllProjects } }) => {
-				const data = res.data?.allProjects?.projects;
-				const count = res.data?.allProjects?.totalCount;
-				setProjects(data);
-			})
-			.catch((err: any) => {
-				showToastError(err);
-				captureException(err, {
-					tags: {
-						section: 'fetchSearchProjects',
-					},
-				});
-			})
-			.finally(() => {
-				setLoading(false);
-			});
-		return () => {};
-	}, [term]);
+	useEffect(() => {}, [term]);
 
 	return (
 		<StyledModal
@@ -81,23 +37,6 @@ export const SearchModal: FC<IModal> = ({ setShowModal }) => {
 					<H6 weight={700}>Find awesome projects on Giveth</H6>
 					<SearchInput setTerm={setTerm} />
 				</SearchBox>
-				{loading ? (
-					<div>Loading</div>
-				) : term.length > 0 ? (
-					projects.length > 0 ? (
-						<ProjectsWrapper>
-							<ProjectsContainer>
-								{projects.map((project, idx) => (
-									<ProjectCard key={idx} project={project} />
-								))}
-							</ProjectsContainer>
-						</ProjectsWrapper>
-					) : (
-						<div>no result</div>
-					)
-				) : (
-					''
-				)}
 				<Row>
 					<Col xs={12} sm={1.5}></Col>
 					<Col xs={12} sm={3}>
