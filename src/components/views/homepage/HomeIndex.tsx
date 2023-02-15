0e71981f991ae300e7e21cbb925cb6dd35eb17b8
Cherik <Pourcheriki@gmail.com>
2023-02-15 19:35:32 +0330

    add featuredProjectsCampaigns to homepage

--- src/components/views/homepage/HomeIndex.tsx
+++ src/components/views/homepage/HomeIndex.tsx
@@ -4,28 +4,36 @@ import { neutralColors } from '@giveth/ui-design-system';
 import HomeFromBlog from './HomeFromBlog';
 import HomeGetUpdates from './HomeGetUpdates';
 import WhyGiveth from '@/components/views/homepage/whyGiveth';
-import CampaignBlock from '@/components/views/homepage/CampaignBlock';
+import ProjectsCampaignBlock from '@/components/views/homepage/ProjectsCampaignBlock';
 import IntroBlock from './introBlock';
 import VideoBlock from './videoBlock';
-import { ProjectUpdatesBlock } from './projectUpdatesBlock/ProjectUpdatesBlock';
 import { LatestUpdatesBlock } from './latestUpdates/LatestUpdatesBlock';
 import { IHomeRoute } from '../../../../pages';
 import InformationBlock from '@/components/views/homepage/InformationBlock';
 
 const HomeIndex: FC<IHomeRoute> = props => {
-	const { projects, ...rest } = props;
+	const { campaigns, latestUpdates, ...rest } = props;
+	const featuredProjectsCampaigns = campaigns.filter(
+		campaign => campaign.isFeatured && campaign.relatedProjects.length > 0,
+	);
 	return (
 		<Wrapper>
 			<IntroBlock />
 			<Separator />
-			<CampaignBlock projects={projects} />
+			{featuredProjectsCampaigns.length > 0
+				? featuredProjectsCampaigns.map(campaign => (
+						<ProjectsCampaignBlock
+							key={campaign.id}
+							campaign={campaign}
+						/>
+				  ))
+				: []}
 			<Separator />
 			<WhyGiveth {...rest} />
 			<Separator />
 			<VideoBlock />
 			<Separator />
-			<ProjectUpdatesBlock projects={projects} />
-			<LatestUpdatesBlock latestUpdates={props.latestUpdates} />
+			<LatestUpdatesBlock latestUpdates={latestUpdates} />
 			<Separator />
 			<HomeFromBlog />
 			<InformationBlock />
