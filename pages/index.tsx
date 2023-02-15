b6d53ab14da7092baf2104d53378da791b28ec42
Cherik <Pourcheriki@gmail.com>
2023-02-15 19:25:16 +0330

    remove unused from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,19 @@
-import { useEffect, useState } from 'react';
 import { GetStaticProps } from 'next/types';
 
 import HomeIndex from '@/components/views/homepage/HomeIndex';
 import { client } from '@/apollo/apolloClient';
-import { FETCH_ALL_PROJECTS } from '@/apollo/gql/gqlProjects';
 import { ESortbyAllProjects } from '@/apollo/types/gqlEnums';
 import {
 	ICampaign,
-	IProject,
 	IProjectUpdateWithProject,
 	IRecentDonation,
 } from '@/apollo/types/types';
-import { useAppSelector } from '@/features/hooks';
 import { homeMetatags } from '@/content/metatags';
 import { GeneralMetatags } from '@/components/Metatag';
 import { transformGraphQLErrorsToStatusCode } from '@/helpers/requests';
 import { HOMEPAGE_DATA } from '@/apollo/gql/gqlHomePage';
 
 export interface IHomeRoute {
-	projects: IProject[];
-	totalCount: number;
 	recentDonations: IRecentDonation[];
 	projectsPerDate: { total: number };
 	totalDonorsCountPerDate: { total: number };
@@ -28,42 +22,11 @@ export interface IHomeRoute {
 	campaigns: ICampaign[];
 }
 
-const fetchProjects = async (userId: string | undefined = undefined) => {
-	const variables: any = {
-		limit: 12,
-		sortingBy: ESortbyAllProjects.GIVPOWER,
-	};
-
-	if (userId) {
-		variables.connectedWalletUserId = Number(userId);
-	}
-	const { data } = await client.query({
-		query: FETCH_ALL_PROJECTS,
-		variables,
-		fetchPolicy: 'network-only',
-	});
-
-	return data.allProjects;
-};
-
 const HomeRoute = (props: IHomeRoute) => {
-	const { projects: _projects, totalCount: _totalCount, ...rest } = props;
-	const user = useAppSelector(state => state.user.userData);
-	const [projects, setProjects] = useState(props.projects);
-	const [totalCount, setTotalCount] = useState(props.totalCount);
-	useEffect(() => {
-		if (!user) return;
-		fetchProjects(user?.id).then(({ projects, totalCount }) => {
-			setProjects(projects);
-			setTotalCount(totalCount);
-		});
-	}, [user]);
-	console.log('props', props.campaigns);
-
 	return (
 		<>
 			<GeneralMetatags info={homeMetatags} />
-			<HomeIndex projects={projects} totalCount={totalCount} {...rest} />
+			<HomeIndex {...props} />
 		</>
 	);
 };
