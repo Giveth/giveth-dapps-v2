fa8920fc008e63690d379ae56459c6c1b568feb8
Cherik <Pourcheriki@gmail.com>
2023-02-15 20:19:16 +0330

    add FETCH_CAMPAIGN_BY_SLUG

--- src/apollo/gql/gqlCampaign.ts
+++ src/apollo/gql/gqlCampaign.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const FETCH_CAMPAIGNS = gql`
 	query FetchCampaigns($connectedWalletUserId: Int) {
-		campaigns ($connectedWalletUserId: Int) {
+		campaigns(connectedWalletUserId: $connectedWalletUserId) {
 			id
 			title
 			slug
@@ -41,6 +41,65 @@ export const FETCH_CAMPAIGNS = gql`
 			relatedProjectsCount
 			media
 			type
+			isActive
+			order
+			landingLink
+			filterFields
+			sortingField
+			createdAt
+			updatedAt
+		}
+	}
+`;
+
+export const FETCH_CAMPAIGN_BY_SLUG = gql`
+	query FetchCampaignBySlug(
+		$slug: String
+		$limit: Int
+		$skip: Int
+		$connectedWalletUserId: Int
+	) {
+		findCampaignBySlug(
+			slug: $slug
+			skip: $skip
+			limit: $limit
+			connectedWalletUserId: $connectedWalletUserId
+		) {
+			id
+			title
+			description
+			relatedProjects {
+				id
+				title
+				image
+				slug
+				description
+				verified
+				totalDonations
+				traceCampaignId
+				reaction {
+					id
+					userId
+				}
+				totalReactions
+				adminUser {
+					name
+					walletAddress
+				}
+				updatedAt
+				organization {
+					name
+					label
+					supportCustomTokens
+				}
+				projectPower {
+					powerRank
+					totalPower
+					round
+				}
+			}
+			media
+			slug
 			isActive
 			order
 			landingLink
