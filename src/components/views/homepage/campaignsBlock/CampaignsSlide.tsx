a3e19f4a3d91e7e51695c9a39a725b1811f83fc2
Cherik <Pourcheriki@gmail.com>
2023-02-16 01:32:05 +0330

    init content

--- src/components/views/homepage/campaignsBlock/CampaignsSlide.tsx
+++ src/components/views/homepage/campaignsBlock/CampaignsSlide.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 import { GLink, H2, H4 } from '@giveth/ui-design-system';
+import Image from 'next/image';
+import styled from 'styled-components';
 import { ICampaign } from '@/apollo/types/types';
 import { Col, Row } from '@/components/Grid';
 
@@ -10,11 +12,38 @@ interface ICampaignsSlideProps {
 export const CampaignsSlide: FC<ICampaignsSlideProps> = ({ campaign }) => {
 	return (
 		<Row>
-			<Col sm={12} md={4}>
+			<ContentCol sm={12} md={5}>
 				<H2>{campaign.title}</H2>
 				<H4>{campaign.description}</H4>
 				<GLink>Explore </GLink>
+			</ContentCol>
+			<Col sm={12} md={7}>
+				<ImageWrapper>
+					<Image
+						src={
+							campaign.media ||
+							'https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+						}
+						alt='campaign image'
+						fill
+						style={{ objectFit: 'cover' }}
+					/>
+				</ImageWrapper>
 			</Col>
 		</Row>
 	);
 };
+
+const ImageWrapper = styled.div`
+	position: relative;
+	width: 100%;
+	height: 314px;
+`;
+
+const ContentCol = styled(Col)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 24px;
+	flex-direction: column;
+`;
