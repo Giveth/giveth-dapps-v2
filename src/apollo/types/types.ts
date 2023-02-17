dea5adaf922a3dc1c2b09b47dba43b5698d074ab
Ramin <r.ramazanpour@aut.ac.ir>
2023-02-18 02:08:58 +0330

    #2187 remove trace donations

--- src/apollo/types/types.ts
+++ src/apollo/types/types.ts
@@ -41,9 +41,7 @@ export interface IProject {
 	}[];
 	users: IUser[];
 	totalDonations?: number;
-	totalTraceDonations?: number;
 	totalProjectUpdates?: number;
-	traceCampaignId: string | null;
 	status: {
 		id?: string;
 		name?: EProjectStatus;
