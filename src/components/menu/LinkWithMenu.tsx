dc2a89bcc05ce60421f9a4b84ef92e1fdd87cd89
Cherik <Pourcheriki@gmail.com>
2023-02-22 14:58:12 +0330

    implement menu with new approach

--- src/components/menu/LinkWithMenu.tsx
+++ src/components/menu/LinkWithMenu.tsx
@@ -6,7 +6,8 @@ import { zIndex } from '@/lib/constants/constants';
 import { useAppSelector } from '@/features/hooks';
 import { HeaderLink } from '../Header/Header.sc';
 import { useDelayedState } from '@/hooks/useDelayedState';
-import { ItemsProvider } from '@/context/Items.context';
+import { ItemsProvider, useItemsContext } from '@/context/Items.context';
+import { FlexCenter } from '../styled-components/Flex';
 
 interface ILinkWithMenu {
 	title: string;
@@ -30,24 +31,29 @@ export const LinkWithMenu: FC<ILinkWithMenu> = ({
 	}, [isHeaderShowing]);
 
 	return (
-		<LinkWithMenuContainer
-			onMouseEnter={openMenu}
-			onMouseLeave={closeMenu}
-			ref={elRef}
-			theme={theme}
-		>
-			<GLink size='Big'>{title}</GLink>
-			<ArrowContainer up={showMenu}>
-				<IconChevronDown24 />
-			</ArrowContainer>
+		<>
+			<LinkWithMenuContainer
+				ref={elRef}
+				theme={theme}
+				onMouseEnter={openMenu}
+			>
+				<GLink size='Big'>{title}</GLink>
+				<ArrowContainer up={showMenu}>
+					<IconChevronDown24 />
+				</ArrowContainer>
+			</LinkWithMenuContainer>
 			{menuCondition && (
 				<ItemsProvider close={closeMenu}>
-					<Menu isAnimating={showMenu} parentRef={elRef}>
+					<Menu
+						isAnimating={showMenu}
+						parentRef={elRef}
+						menuCondition={menuCondition}
+					>
 						{children}
 					</Menu>
 				</ItemsProvider>
 			)}
-		</LinkWithMenuContainer>
+		</>
 	);
 };
 
@@ -73,11 +79,18 @@ const ArrowContainer = styled.span<{ up: boolean }>`
 interface IMenuProps {
 	parentRef: RefObject<HTMLDivElement>;
 	isAnimating: boolean;
+	menuCondition: boolean;
 	children: ReactNode;
 }
 
-export const Menu: FC<IMenuProps> = ({ parentRef, isAnimating, children }) => {
+export const Menu: FC<IMenuProps> = ({
+	parentRef,
+	isAnimating,
+	menuCondition,
+	children,
+}) => {
 	const el = useRef(document.createElement('div'));
+	const { close } = useItemsContext();
 
 	useEffect(() => {
 		const current = el.current;
@@ -92,15 +105,43 @@ export const Menu: FC<IMenuProps> = ({ parentRef, isAnimating, children }) => {
 	}, []);
 
 	return createPortal(
-		<MenuContainer
-			isAnimating={isAnimating}
-			style={{
-				top: parentRef?.current?.getBoundingClientRect().bottom,
-				left: parentRef?.current?.getBoundingClientRect().left,
-			}}
-		>
-			{children}
-		</MenuContainer>,
+		<>
+			{menuCondition && isAnimating && (
+				<>
+					<Background
+						onMouseOver={() => {
+							console.log('enter!!!!');
+							close();
+						}}
+					/>
+					<Ajab
+						style={{
+							top: parentRef?.current?.getBoundingClientRect()
+								.top,
+							left: parentRef?.current?.getBoundingClientRect()
+								.left,
+							width: parentRef?.current?.getBoundingClientRect()
+								.width,
+							height: parentRef?.current?.getBoundingClientRect()
+								.height,
+						}}
+					/>
+				</>
+			)}
+			<MenuContainer
+				onMouseOver={e => {
+					e.preventDefault();
+					e.stopPropagation();
+				}}
+				isAnimating={isAnimating}
+				style={{
+					top: parentRef?.current?.getBoundingClientRect().bottom,
+					left: parentRef?.current?.getBoundingClientRect().left,
+				}}
+			>
+				{children}
+			</MenuContainer>
+		</>,
 		el.current,
 	);
 };
@@ -109,10 +150,24 @@ const MenuContainer = styled.div<{ isAnimating: boolean }>`
 	position: fixed;
 	color: #fff;
 	border-radius: 6px;
-	padding: 8px 0;
-	z-index: ${zIndex.MODAL};
-	top: 0;
+	padding: 10px 0;
+	z-index: ${zIndex.HEADER + 1};
+	top: -2px;
 	left: 0;
 	opacity: ${props => (props.isAnimating ? 1 : 0)};
 	transition: opacity 0.3s ease;
+`;
+
+const Ajab = styled.div`
+	position: absolute;
+	z-index: ${zIndex.HEADER + 1};
+`;
+
+const Background = styled(FlexCenter)`
+	width: 100%;
+	height: 100%;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: ${zIndex.HEADER + 1};
 `;
