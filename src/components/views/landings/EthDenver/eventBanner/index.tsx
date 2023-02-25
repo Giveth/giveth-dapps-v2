38d1baaf34aadf36f40b4e9e944bfe5873e21d49
Ramin <r.ramazanpour@aut.ac.ir>
2023-02-25 21:01:58 +0330

    add eth denver banner section

---
+++ src/components/views/landings/EthDenver/eventBanner/index.tsx
@@ -0,0 +1,57 @@
+import { brandColors, H1, H5 } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+import ETHDenverIcon from '/public/images/ETHDenver_2023.png';
+import SporkWhaleIcon from '/public/images/ETHDenver-spork-whale.png';
+import GoldSporkIcon from '/public/images/ETHDenver-gold-sporke.png';
+
+import Image from 'next/image';
+
+const EventBanner = () => {
+	return (
+		<Wrapper>
+			<TextBox>
+				<H5>Learn What We’re Up To and How to Get Involved!</H5>
+				<H1Styled weight={700}>Giveth at ETHDenver 2023</H1Styled>
+				<Image src={ETHDenverIcon} alt='ETHDenver 2023' />
+			</TextBox>
+			<SporkWhale>
+				<Image src={SporkWhaleIcon} alt='Spork Whale' />
+			</SporkWhale>
+			<GoldSpork>
+				<Image src={GoldSporkIcon} alt='Gold Spork' />
+			</GoldSpork>
+		</Wrapper>
+	);
+};
+
+const TextBox = styled.div`
+	max-width: 701px;
+`;
+
+const GoldSpork = styled.div`
+	position: absolute;
+	bottom: 0;
+	right: 155px;
+`;
+
+const SporkWhale = styled.div`
+	position: absolute;
+	top: 0;
+	left: 520px;
+`;
+
+const H1Styled = styled(H1)`
+	color: white;
+	margin: 16px 0;
+`;
+
+const Wrapper = styled.div`
+	position: relative;
+	padding: 224px 86px 177px;
+	background-image: url('/images/ETHDenver-banner.png');
+	background-repeat: no-repeat;
+	background-size: cover;
+	color: ${brandColors.mustard[500]};
+`;
+
+export default EventBanner;
