c0948228b3ec0287632e7d378d25503bc86cadd3
Cherik <Pourcheriki@gmail.com>
2023-02-26 23:43:52 +0330

    fix calendars style

--- src/components/views/landings/EthDenver/givethTalks/index.tsx
+++ src/components/views/landings/EthDenver/givethTalks/index.tsx
@@ -11,6 +11,7 @@ import CalendarWhite from '/public/images/calendar-white.svg';
 import Image from 'next/image';
 import CalendarItem from '@/components/views/landings/EthDenver/givethTalks/CalendarItem';
 import { Col, Container, Row } from '@/components/Grid';
+import { Flex } from '@/components/styled-components/Flex';
 
 const GivethTalks = () => {
 	return (
@@ -43,38 +44,40 @@ const GivethTalks = () => {
 					</a>
 				</Talks>
 				<Calendar xs={12} md={6} xl={8}>
-					<CalendarItem
-						title='Combating the Metacrisis'
-						lecturer='Griff Green'
-						date='Mar 03, 2023'
-						time='13:00 - 13:15 MST'
-						link='https://events.ethdenver.com/eden23/attendease/networking/experience/9dfae4fa-a017-47bb-837c-e2b7b8da8468/af96b57b-a734-45c0-918f-f32bb030cbcc'
-						calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MGYxZ25uNWJjc25qdmVpdWFzMHQxNmk0YnIgZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
-					/>
-					<CalendarItem
-						title='Why DAOs will change the world'
-						lecturer='Lauren Luz'
-						date='Mar 04, 2023'
-						time='10:20 - 10:40 MST'
-						link='https://events.ethdenver.com/eden23/attendease/networking/experience/3185f05e-1283-47c4-b1cd-c456c3b6af3a/dd50c09c-f707-4389-a60a-75c2d37c2aa5'
-						calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MjAwN3Nwc2dvbGhuNWh2N3B1cjdiZDg5c24gZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
-					/>
-					<CalendarItem
-						title='DAO it like you mean it'
-						lecturer='Lauren Luz'
-						date='Feb 28, 2023'
-						time='2:05 - 2:45pm MST'
-						link='https://events.ethdenver.com/eden23/attendease/networking/experience/bf427728-b151-4edf-ab90-f501a3d84970/212d0aa6-f013-45af-8b31-f98155087c88'
-						calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NzNwdHI2ZmNrMHI5ZW8xc25rODUzdDhkMTUgZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
-					/>
-					<CalendarItem
-						title='Dissecting Sustainable DAO Governance and Effective Operations'
-						lecturer='Griff Green'
-						date='Feb 28, 2023'
-						time='3:40PM MST'
-						link='https://events.ethdenver.com/eden23/attendease/networking/experience/bf427728-b151-4edf-ab90-f501a3d84970/212d0aa6-f013-45af-8b31-f98155087c88'
-						calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NzBlNG85YTUyMGFucTI3c3M2MjBmM2YxMjUgZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
-					/>
+					<Flex flexDirection='column' gap='40px'>
+						<CalendarItem
+							title='Combating the Metacrisis'
+							lecturer='Griff Green'
+							date='Mar 03, 2023'
+							time='13:00 - 13:15 MST'
+							link='https://events.ethdenver.com/eden23/attendease/networking/experience/9dfae4fa-a017-47bb-837c-e2b7b8da8468/af96b57b-a734-45c0-918f-f32bb030cbcc'
+							calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MGYxZ25uNWJjc25qdmVpdWFzMHQxNmk0YnIgZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
+						/>
+						<CalendarItem
+							title='Why DAOs will change the world'
+							lecturer='Lauren Luz'
+							date='Mar 04, 2023'
+							time='10:20 - 10:40 MST'
+							link='https://events.ethdenver.com/eden23/attendease/networking/experience/3185f05e-1283-47c4-b1cd-c456c3b6af3a/dd50c09c-f707-4389-a60a-75c2d37c2aa5'
+							calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MjAwN3Nwc2dvbGhuNWh2N3B1cjdiZDg5c24gZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
+						/>
+						<CalendarItem
+							title='DAO it like you mean it'
+							lecturer='Lauren Luz'
+							date='Feb 28, 2023'
+							time='2:05 - 2:45pm MST'
+							link='https://events.ethdenver.com/eden23/attendease/networking/experience/bf427728-b151-4edf-ab90-f501a3d84970/212d0aa6-f013-45af-8b31-f98155087c88'
+							calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NzNwdHI2ZmNrMHI5ZW8xc25rODUzdDhkMTUgZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
+						/>
+						<CalendarItem
+							title='Dissecting Sustainable DAO Governance and Effective Operations'
+							lecturer='Griff Green'
+							date='Feb 28, 2023'
+							time='3:40PM MST'
+							link='https://events.ethdenver.com/eden23/attendease/networking/experience/bf427728-b151-4edf-ab90-f501a3d84970/212d0aa6-f013-45af-8b31-f98155087c88'
+							calendarLink='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NzBlNG85YTUyMGFucTI3c3M2MjBmM2YxMjUgZ2l2ZXRoZXRoZGVudmVyQG0&tmsrc=givethethdenver%40gmail.com'
+						/>
+					</Flex>
 				</Calendar>
 			</Row>
 		</Wrapper>
@@ -82,9 +85,6 @@ const GivethTalks = () => {
 };
 
 const Calendar = styled(Col)`
-	> :first-of-type {
-		margin-bottom: 40px;
-	}
 	${mediaQueries.laptopS} {
 		padding-left: 60px;
 	}
