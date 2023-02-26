ad0974b5c41afa586bb6b3ee21dfff253d37e2d0
alireza-sharifpour <ascode94@gmail.com>
2023-02-26 15:52:30 +0330

    Fix impacttrack issues

--- src/components/views/landings/EthDenver/impactQuest/ImpactTrackMentorship.tsx
+++ src/components/views/landings/EthDenver/impactQuest/ImpactTrackMentorship.tsx
@@ -2,37 +2,41 @@ import styled from 'styled-components';
 import {
 	H3,
 	IconChevronRight32,
-	mediaQueries,
 	neutralColors,
 } from '@giveth/ui-design-system';
 import ImpactTrackImg from '/public/images/ETHDenver-Impact-mentorship.png';
 import Image from 'next/image';
 import { GhostButton } from '@/components/styled-components/Button';
+import { Col, Row } from '@/components/Grid';
 
 const ImpactTrackMentorship = () => {
 	return (
 		<Wrapper>
 			<InnerWrapper>
-				<Img src={ImpactTrackImg} alt='Impact Track Mentorship' />
-				<div>
-					<H3 weight={700}>Impact Track Mentorship</H3>
-					<Desc>
-						We love the great things that get built at ETHDenver
-						hackathons. This year, we’re working to encourage
-						hackathon participants to create real impact with their
-						projects.
-						<br />
-						<br />
-						Do you need help with your project for the Impact Track?
-						Reach out to us on the ETHDenver Discord server under
-						the "Building" section!
-					</Desc>
-					<GhostButton
-						label='Join us on Discord'
-						size='large'
-						icon={<IconChevronRight32 />}
-					/>
-				</div>
+				<Col xs={12} md={6}>
+					<Img src={ImpactTrackImg} alt='Impact Track Mentorship' />
+				</Col>
+				<Col xs={12} md={6}>
+					<div>
+						<H3 weight={700}>Impact Track Mentorship</H3>
+						<Desc>
+							We love the great things that get built at ETHDenver
+							hackathons. This year, we’re working to encourage
+							hackathon participants to create real impact with
+							their projects.
+							<br />
+							<br />
+							Do you need help with your project for the Impact
+							Track? Reach out to us on the ETHDenver Discord
+							server under the "Building" section!
+						</Desc>
+						<GhostButton
+							label='Join us on Discord'
+							size='large'
+							icon={<IconChevronRight32 />}
+						/>
+					</div>
+				</Col>
 			</InnerWrapper>
 		</Wrapper>
 	);
@@ -46,21 +50,14 @@ const Img = styled(Image)`
 	max-width: 100%;
 `;
 
-const InnerWrapper = styled.div`
-	padding: 40px;
+const InnerWrapper = styled(Row)`
 	background: ${neutralColors.gray[200]};
 	border-radius: 16px;
-	display: flex;
-	gap: 24px;
-	align-items: center;
-	flex-direction: column;
-	${mediaQueries.tablet} {
-		flex-direction: row;
-	}
+	padding: 16px;
 `;
 
 const Wrapper = styled.div`
-	padding: 0 40px;
+	padding: 40px;
 `;
 
 export default ImpactTrackMentorship;
