e266c67f7df98f718a5240336f9f4bfb89f5f6e6
alireza-sharifpour <ascode94@gmail.com>
2023-02-26 14:07:48 +0330

    fix impact bottom

--- src/components/views/landings/EthDenver/impactQuest/ImpactTrackMentorship.tsx
+++ src/components/views/landings/EthDenver/impactQuest/ImpactTrackMentorship.tsx
@@ -2,21 +2,26 @@ import styled from 'styled-components';
 import {
 	H3,
 	IconChevronRight32,
-	mediaQueries,
 	neutralColors,
 } from '@giveth/ui-design-system';
 import ImpactTrackImg from '/public/images/ETHDenver-Impact-mentorship.png';
 import Image from 'next/image';
 import { GhostButton } from '@/components/styled-components/Button';
+import { Col, Row, Container } from '@/components/Grid';
 
 const ImpactTrackMentorship = () => {
 	return (
 		<Wrapper>
 			<InnerWrapper>
-				<Img>
-					<Image src={ImpactTrackImg} alt='Impact Track Mentorship' />
-				</Img>
-				<div>
+				<Col xs={12} md={6}>
+					<Img>
+						<Image
+							src={ImpactTrackImg}
+							alt='Impact Track Mentorship'
+						/>
+					</Img>
+				</Col>
+				<Col xs={12} md={6}>
 					<H3 weight={700}>Impact Track Mentorship</H3>
 					<Desc>
 						We love the great things that get built at ETHDenver
@@ -34,7 +39,7 @@ const ImpactTrackMentorship = () => {
 						size='large'
 						icon={<IconChevronRight32 />}
 					/>
-				</div>
+				</Col>
 			</InnerWrapper>
 		</Wrapper>
 	);
@@ -44,22 +49,20 @@ const Desc = styled.div`
 	margin: 24px 0;
 `;
 
-const Img = styled.div``;
+const Img = styled.div`
+	display: flex;
+	justify-content: center;
+`;
 
-const InnerWrapper = styled.div`
+const InnerWrapper = styled(Row)`
 	padding: 40px;
 	background: ${neutralColors.gray[200]};
 	border-radius: 16px;
-	display: flex;
 	gap: 24px;
 	align-items: center;
-	flex-direction: column;
-	${mediaQueries.tablet} {
-		flex-direction: row;
-	}
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled(Container)`
 	padding: 0 40px;
 `;
 
