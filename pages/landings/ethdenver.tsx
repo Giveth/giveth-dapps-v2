dfca46c86a3051c05a7e5817968a58b191aec3db
Cherik <Pourcheriki@gmail.com>
2023-02-26 11:10:14 +0330

    get campaign

--- pages/landings/ethdenver.tsx
+++ pages/landings/ethdenver.tsx
@@ -1,15 +1,47 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { FC } from 'react';
 import EthDenverView from '@/components/views/landings/EthDenver';
+import { FETCH_CAMPAIGN_BY_SLUG } from '@/apollo/gql/gqlCampaign';
+import { client } from '@/apollo/apolloClient';
+import { ICampaign } from '@/apollo/types/types';
 
-const EthDenverRoute = () => {
+export interface IEthDenverProps {
+	campaign?: ICampaign;
+}
+
+const EthDenverRoute: FC<IEthDenverProps> = ({ campaign }) => {
 	return (
 		<>
 			<Head>
 				<title>ETHDenver</title>
 			</Head>
-			<EthDenverView />
+			<EthDenverView campaign={campaign} />
 		</>
 	);
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+	try {
+		const { data } = await client.query({
+			query: FETCH_CAMPAIGN_BY_SLUG,
+			variables: {
+				slug: 'buenos-dias',
+			},
+			fetchPolicy: 'no-cache',
+		});
+		return {
+			props: {
+				campaign: data.findCampaignBySlug,
+			},
+			revalidate: 600,
+		};
+	} catch (error) {
+		return {
+			props: {},
+			revalidate: 1,
+		};
+	}
 };
 
 export default EthDenverRoute;
