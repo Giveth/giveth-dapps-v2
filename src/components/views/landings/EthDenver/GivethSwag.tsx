9e1e17fc3786c9f0f95459eabfa8cc34e905d487
alireza-sharifpour <ascode94@gmail.com>
2023-02-26 16:44:47 +0330

    fix giveth-swag section

--- src/components/views/landings/EthDenver/GivethSwag.tsx
+++ src/components/views/landings/EthDenver/GivethSwag.tsx
@@ -3,60 +3,57 @@ import {
 	H3,
 	IconExternalLink24,
 	Lead,
-	mediaQueries,
 	neutralColors,
 } from '@giveth/ui-design-system';
 import SwagImg from '/public/images/swag.png';
 import Image from 'next/image';
 import { GhostButton } from '@/components/styled-components/Button';
+import { Col, Row } from '@/components/Grid';
 
 const GivethSwag = () => {
 	return (
 		<Wrapper>
-			<Text>
-				<H3 weight={700}>Giveth Swag</H3>
-				<Desc size='large'>
-					Want to get your hands on the latest Giveth swag? Swing by
-					our booth, and we’ll let you know how to take part in our
-					Impact Quests to claim cool prizes. Or if you prefer, order
-					yours now from our new and improved swag shop.
-				</Desc>
-				<GhostButton
-					label='Go to Swag shop'
-					size='large'
-					icon={<IconExternalLink24 />}
-				/>
-			</Text>
-			<Img>
-				<Image src={SwagImg} alt='Swag Image' />
-			</Img>
+			<Col xs={12} md={6}>
+				<Text>
+					<H3 weight={700}>Giveth Swag</H3>
+					<Desc size='large'>
+						Want to get your hands on the latest Giveth swag? Swing
+						by our booth, and we’ll let you know how to take part in
+						our Impact Quests to claim cool prizes. Or if you
+						prefer, order yours now from our new and improved swag
+						shop.
+					</Desc>
+					<GhostButton
+						label='Go to Swag shop'
+						size='large'
+						icon={<IconExternalLink24 />}
+					/>
+				</Text>
+			</Col>
+			<Col xs={12} md={6}>
+				<Img src={SwagImg} alt='Swag Image' />
+			</Col>
 		</Wrapper>
 	);
 };
 
-const Img = styled.div``;
+const Img = styled(Image)`
+	max-width: 100%;
+`;
 
 const Text = styled.div`
-	max-width: 557px;
+	/* max-width: 557px; */
 `;
 
 const Desc = styled(Lead)`
 	margin: 47px 0 31px;
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled(Row)`
 	color: ${neutralColors.gray[900]};
-	padding: 40px;
-	margin: 40px auto;
-	max-width: 1200px;
-	display: flex;
-	flex-direction: column;
 	border-radius: 16px;
 	background: ${neutralColors.gray[200]};
-	align-items: center;
-	${mediaQueries.tablet} {
-		flex-direction: row;
-	}
+	padding: 40px;
 `;
 
 export default GivethSwag;
