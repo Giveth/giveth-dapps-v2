d2329f428f044e17ab0dc72e44472c7d5677af5b
Cherik <Pourcheriki@gmail.com>
2023-03-05 19:34:05 +0330

    init showing stream cards

--- src/components/givfarm/RegenStreamSection.tsx
+++ src/components/givfarm/RegenStreamSection.tsx
@@ -2,8 +2,10 @@ import React, { FC } from 'react';
 import { H3 } from '@giveth/ui-design-system';
 import { useWeb3React } from '@web3-react/core';
 import { useIntl } from 'react-intl';
-import { Col } from '../Grid';
+import { Col, Row } from '../Grid';
 import { Subtitle, GIVfrensLink } from './RegenStreamSection.sc';
+import config from '@/configuration';
+import { RegenStreamCard } from './RegenStreamCard';
 
 interface IRegenStreamSectionProps {
 	showArchivedPools: boolean;
@@ -14,32 +16,47 @@ export const RegenStreamSection: FC<IRegenStreamSectionProps> = ({
 }) => {
 	const { chainId } = useWeb3React();
 	const { formatMessage } = useIntl();
-	// const regenFarms =
-	// 	chainId === config.XDAI_NETWORK_NUMBER
-	// 		? config.XDAI_CONFIG.regenFarms
-	// 		: config.MAINNET_CONFIG.regenFarms;
+	const regenStreams =
+		chainId === config.XDAI_NETWORK_NUMBER
+			? [
+					...config.XDAI_CONFIG.regenStreams,
+					...config.MAINNET_CONFIG.regenStreams,
+			  ]
+			: [
+					...config.MAINNET_CONFIG.regenStreams,
+					...config.XDAI_CONFIG.regenStreams,
+			  ];
 
 	return (
 		<>
 			<H3 weight={700}>RegenStreams</H3>
-			<Col md={8} lg={6}>
-				<Subtitle>
-					{formatMessage({
-						id: 'label.explore_a_multiverse_of_projects',
-					})}
-					&nbsp;
-					<GIVfrensLink
-						as='a'
-						size='Big'
-						href='https://docs.giveth.io/regenFarms'
-						target='_blank'
-						rel='noreferrer'
-					>
-						{formatMessage({ id: 'label.learn_more' })}
-					</GIVfrensLink>
-					.
-				</Subtitle>
-			</Col>
+			<Row>
+				<Col md={8} lg={6}>
+					<Subtitle>
+						{formatMessage({
+							id: 'label.explore_a_multiverse_of_projects',
+						})}
+						&nbsp;
+						<GIVfrensLink
+							as='a'
+							size='Big'
+							href='https://docs.giveth.io/regenFarms'
+							target='_blank'
+							rel='noreferrer'
+						>
+							{formatMessage({ id: 'label.learn_more' })}
+						</GIVfrensLink>
+						.
+					</Subtitle>
+				</Col>
+			</Row>
+			{regenStreams.map(regenStream => (
+				<RegenStreamCard
+					key={regenStream.type}
+					streamConfig={regenStream}
+					network={chainId || 1}
+				/>
+			))}
 			{/* {regenFarms.map((regenFarm, index) => (
 				<RegenFarm
 					key={index}
