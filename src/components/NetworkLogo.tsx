9197f51878b8a24d9f99293fbcfc5a4e1777703b
Ramin <r.ramazanpour@aut.ac.ir>
2023-03-06 20:00:07 +0330

    change backend server for testing

--- src/components/NetworkLogo.tsx
+++ src/components/NetworkLogo.tsx
@@ -5,7 +5,7 @@ import { IconPolygon } from '@/components/Icons/Polygon';
 
 const NetworkLogo = (props: { chainId?: number; logoSize?: number }) => {
 	const { chainId, logoSize } = props;
-	if (chainId === 1) {
+	if (chainId === 1 || chainId === 5) {
 		return <IconEthereum size={logoSize} />;
 	} else if (chainId === 100) {
 		return <IconGnosisChain size={logoSize} />;
