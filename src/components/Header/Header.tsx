adbceb4aa10d9f91319e67c823b6df4a9a53c593
Cherik <Pourcheriki@gmail.com>
2023-03-07 10:18:40 +0330

    revert to community

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -217,8 +217,8 @@ const Header: FC<IHeader> = () => {
 						<GIVeconomyMenu />
 					</LinkWithMenu>
 					<HeaderLink theme={theme}>
-						<Link href={Routes.NFT}>
-							<GLink size='Big'>NFT</GLink>
+						<Link href={Routes.Join}>
+							<GLink size='Big'>Community</GLink>
 						</Link>
 					</HeaderLink>
 					<SearchButton
