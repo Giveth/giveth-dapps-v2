1b2109a4278d9441a6fbb60f63c52adbcb73a111
Cherik <Pourcheriki@gmail.com>
2023-03-15 11:44:49 +0330

    update lists

--- src/components/views/userProfile/projectsTab/AddPolygonToast.tsx
+++ src/components/views/userProfile/projectsTab/AddPolygonToast.tsx
@@ -1,10 +1,10 @@
 import { FC, useState } from 'react';
 import styled from 'styled-components';
 import {
-	B,
 	brandColors,
+	Caption,
+	GLink,
 	IconChevronRight16,
-	P,
 } from '@giveth/ui-design-system';
 import { IProject } from '@/apollo/types/types';
 import { Flex } from '@/components/styled-components/Flex';
@@ -18,12 +18,16 @@ export const AddPolygonToast: FC<IAddPolygonToastProps> = ({ project }) => {
 	const [showModal, setShowModal] = useState(false);
 	return (
 		<>
-			<Wrapper onClick={() => setShowModal(true)}>
-				<P>Add polygon address for this project</P>
-				<Flex gap='8px' alignItems='center'>
-					<B>Add</B>
+			<Wrapper>
+				<Caption>Add polygon address for this project</Caption>
+				<Action
+					gap='8px'
+					alignItems='center'
+					onClick={() => setShowModal(true)}
+				>
+					<GLink size='Medium'>Add</GLink>
 					<IconChevronRight16 />
-				</Flex>
+				</Action>
 			</Wrapper>
 			{showModal && (
 				<AddPolygonAddress
@@ -40,11 +44,15 @@ const Wrapper = styled(Flex)`
 	background-color: ${brandColors.giv[50]};
 	border: 1px solid ${brandColors.giv[300]};
 	border-radius: 8px;
-	padding: 8px;
-	margin: 8px 8px 8px 0;
+	padding: 16px;
+	margin: 8px 16px 8px 0;
 	justify-content: space-between;
-	color: ${brandColors.giv[400]};
+	color: ${brandColors.giv[300]};
+`;
+
+const Action = styled(Flex)`
 	transition: color 0.3s ease;
+	color: ${brandColors.giv[500]};
 	cursor: pointer;
 	&:hover {
 		color: ${brandColors.giv[600]};
