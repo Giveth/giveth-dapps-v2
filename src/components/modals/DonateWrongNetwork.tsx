4315994f0ae603bbc3e69a80ff6b7c7b2c700f08
alireza-sharifpour <ascode94@gmail.com>
2023-03-19 12:30:20 +0330

    Added SelectNetwork Functionality and styles

--- src/components/modals/DonateWrongNetwork.tsx
+++ src/components/modals/DonateWrongNetwork.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect } from 'react';
 import styled from 'styled-components';
 import {
+	B,
 	brandColors,
 	Caption,
 	IconNetwork32,
@@ -16,6 +17,11 @@ import { useAppDispatch } from '@/features/hooks';
 import { setShowSwitchNetworkModal } from '@/features/modal/modal.slice';
 import config from '@/configuration';
 import { BasicNetworkConfig } from '@/types/config';
+import { switchNetwork } from '@/lib/metamask';
+import NetworkLogo from '../NetworkLogo';
+import { NetworkItem, SelectedNetwork } from './SwitchNetwork';
+import { useAppSelector } from '@/features/hooks';
+import { Flex } from '../styled-components/Flex';
 
 interface IDonateWrongNetwork extends IModal, ISwitchNetworkToast {}
 
@@ -29,7 +35,9 @@ export const DonateWrongNetwork: FC<IDonateWrongNetwork> = props => {
 	const { setShowModal, acceptedChains } = props;
 	const { chainId } = useWeb3React();
 	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
+	const theme = useAppSelector(state => state.general.theme);
 	const dispatch = useAppDispatch();
+
 	console.log('acceptedChains', acceptedChains, networks[0].chainId);
 
 	const eligibleNetworks: BasicNetworkConfig[] = networks.filter(network =>
@@ -59,6 +67,35 @@ export const DonateWrongNetwork: FC<IDonateWrongNetwork> = props => {
 				</Lead>
 				<br />
 				<Lead>Please switch your network</Lead>
+				<br />
+				<Flex gap='70px'>
+					{eligibleNetworks.map(network => {
+						const _chainId = parseInt(network.chainId);
+						return (
+							<NetworkItem
+								onClick={() => {
+									switchNetwork(_chainId);
+									closeModal();
+								}}
+								isSelected={_chainId === chainId}
+								key={_chainId}
+								theme={theme}
+							>
+								<NetworkLogo chainId={_chainId} logoSize={32} />
+								<B>{network.chainName}</B>
+								{_chainId === chainId && (
+									<SelectedNetwork
+										styleType='Small'
+										theme={theme}
+									>
+										Selected
+									</SelectedNetwork>
+								)}
+							</NetworkItem>
+						);
+					})}
+				</Flex>
+
 				<SwitchCaption
 					onClick={() => dispatch(setShowSwitchNetworkModal(true))}
 				>
