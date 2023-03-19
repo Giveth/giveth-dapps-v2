e5741bcabcd90b6431c35ff1c768eb8ddf5ab453
alireza-sharifpour <ascode94@gmail.com>
2023-03-19 11:53:02 +0330

    Changed modal props

--- src/components/modals/DonateWrongNetwork.tsx
+++ src/components/modals/DonateWrongNetwork.tsx
@@ -1,6 +1,12 @@
 import React, { FC, useEffect } from 'react';
 import styled from 'styled-components';
-import { P, H4, brandColors, Caption } from '@giveth/ui-design-system';
+import {
+	P,
+	H4,
+	brandColors,
+	Caption,
+	IconRocketInSpace32,
+} from '@giveth/ui-design-system';
 import { useWeb3React } from '@web3-react/core';
 
 import { mediaQueries } from '@/lib/constants/constants';
@@ -9,8 +15,6 @@ import { getNetworkNames } from '@/components/views/donate/helpers';
 import { IModal } from '@/types/common';
 import { useModalAnimation } from '@/hooks/useModalAnimation';
 import { ISwitchNetworkToast } from '@/components/views/donate/common.types';
-import NetworkLogo from '@/components/NetworkLogo';
-import { FlexCenter } from '@/components/styled-components/Flex';
 import { useAppDispatch } from '@/features/hooks';
 import { setShowSwitchNetworkModal } from '@/features/modal/modal.slice';
 
@@ -31,17 +35,15 @@ export const DonateWrongNetwork: FC<IDonateWrongNetwork> = props => {
 	const networkNames = getNetworkNames(acceptedChains!, 'or');
 
 	return (
-		<Modal closeModal={closeModal} isAnimating={isAnimating}>
+		<Modal
+			closeModal={closeModal}
+			isAnimating={isAnimating}
+			headerTitle='Switch Network'
+			headerIcon={<IconRocketInSpace32 />}
+			hiddenClose
+			headerTitlePosition='left'
+		>
 			<ModalContainer>
-				<FlexCenter gap='8px'>
-					{acceptedChains?.map(chainId => (
-						<NetworkLogo
-							logoSize={64}
-							chainId={chainId}
-							key={chainId}
-						/>
-					))}
-				</FlexCenter>
 				<Title>Switch to {networkNames}</Title>
 				<P>
 					This project doesn&apos;t accept donations in your connected
