daaf8b95e9aeea8018c0ae90474df5d11429d600
alireza-sharifpour <ascode94@gmail.com>
2023-03-21 11:09:18 +0330

    added optimism to wrong network modal

--- src/components/modals/DonateWrongNetwork.tsx
+++ src/components/modals/DonateWrongNetwork.tsx
@@ -33,6 +33,7 @@ const networks = [
 	config.MAINNET_CONFIG,
 	config.XDAI_CONFIG,
 	config.POLYGON_CONFIG,
+	config.OPTIMISM_CONFIG,
 ];
 
 export const DonateWrongNetwork: FC<IDonateWrongNetwork> = props => {
