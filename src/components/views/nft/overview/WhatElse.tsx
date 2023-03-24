04f385b027173469f532e8913d0ca84aad3debb1
Cherik <Pourcheriki@gmail.com>
2023-03-24 18:34:18 +0330

    use col for columns

--- src/components/views/nft/overview/WhatElse.tsx
+++ src/components/views/nft/overview/WhatElse.tsx
@@ -1,53 +1,54 @@
-import { brandColors, Container, H3, Lead } from '@giveth/ui-design-system';
+import {
+	brandColors,
+	Col,
+	Container,
+	H3,
+	Lead,
+	Row,
+} from '@giveth/ui-design-system';
 import React from 'react';
 import styled from 'styled-components';
-import Link from 'next/link';
-import { Flex } from '@/components/styled-components/Flex';
+import ExternalLink from '@/components/ExternalLink';
 import { OvalHorizontalGradient, OvalVerticalGradient } from '../common.styles';
-import { mediaQueries } from '@/lib/constants/constants';
 
 const WhatElse = () => {
 	return (
 		<WhatElseContainer>
-			<Container>
-				<ContentContainer>
-					<CustomFlex gap='64px'>
-						<Flex flexDirection='column' gap='16px'>
-							<HeaderTitle>Get your Giveth Name.</HeaderTitle>
-							<Lead>
-								Givers PFP Holders will be able to claim their
-								own unique <code>.giveth</code> domain name at a
-								50% discount, thanks to Punk Domains. Reserve
-								your name (e.g. <code>griff.giveth</code>) on
-								Gnosis Chain and on top of that, 80% of the
-								registration fees go to the Giveth Matching
-								Pool!
-								<p>
-									Already own a Giver?{' '}
-									<DocLink href='https://giveth.punk.domains/#/'>
-										Register your <code>.giveth</code> name.
-									</DocLink>
-								</p>
-							</Lead>
-						</Flex>
-						<Flex flexDirection='column' gap='16px'>
-							<HeaderTitle>Unlock your Giveth Flair.</HeaderTitle>
-							<Lead>
-								If you hold a Giver, you will soon be able to
-								set it as your profile picture on the Giveth.io
-								DApp. With your Giver equipped, whatever you do
-								on on Giveth, boosting with GIVpower, donating
-								or founding a project, you will be shown
-								prominently on the platform along with your
-								unique Giver NFT badge.{' '}
-								<DocLink href='https://docs.giveth.io/dapps/giverspfp/#benefits'>
-									Learn more about the benefits.
+			<StyledContainer>
+				<Row>
+					<Col sm={12} lg={6}>
+						<HeaderTitle>Get your Giveth Name.</HeaderTitle>
+						<Lead>
+							Givers PFP Holders will be able to claim their own
+							unique <code>.giveth</code> domain name at a 50%
+							discount, thanks to Punk Domains. Reserve your name
+							(e.g. <code>griff.giveth</code>) on Gnosis Chain and
+							on top of that, 80% of the registration fees go to
+							the Giveth Matching Pool!
+							<p>
+								Already own a Giver?{' '}
+								<DocLink href='https://giveth.punk.domains/#/'>
+									Register your <code>.giveth</code> name.
 								</DocLink>
-							</Lead>
-						</Flex>
-					</CustomFlex>
-				</ContentContainer>
-			</Container>
+							</p>
+						</Lead>
+					</Col>
+					<Col sm={12} lg={6}>
+						<HeaderTitle>Unlock your Giveth Flair.</HeaderTitle>
+						<Lead>
+							If you hold a Giver, you will soon be able to set it
+							as your profile picture on the Giveth.io DApp. With
+							your Giver equipped, whatever you do on on Giveth,
+							boosting with GIVpower, donating or founding a
+							project, you will be shown prominently on the
+							platform along with your unique Giver NFT badge.{' '}
+							<DocLink href='https://docs.giveth.io/dapps/giverspfp/#benefits'>
+								Learn more about the benefits.
+							</DocLink>
+						</Lead>
+					</Col>
+				</Row>
+			</StyledContainer>
 			<OvalHorizontalGradient />
 			<OvalVerticalGradient />
 		</WhatElseContainer>
@@ -70,7 +71,7 @@ const WhatElseContainer = styled.div`
 	}
 `;
 
-const DocLink = styled(Link)`
+const DocLink = styled(ExternalLink)`
 	color: ${brandColors.giv[300]};
 	font-weight: 500;
 	&:hover {
@@ -78,16 +79,9 @@ const DocLink = styled(Link)`
 	}
 `;
 
-const ContentContainer = styled.div`
+const StyledContainer = styled(Container)`
 	position: relative;
 	z-index: 1;
-`;
-
-const CustomFlex = styled(Flex)`
-	flex-direction: column;
-	${mediaQueries.tablet} {
-		flex-direction: row;
-	}
 `;
 
 const HeaderTitle = styled(H3)`
