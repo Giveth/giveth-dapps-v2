9d6406f480fca061b3e283a3bdf72421f2bbefa2
Cherik <Pourcheriki@gmail.com>
2023-03-26 20:40:46 +0330

    change imports to import type

--- src/types/contracts/IUniswapV2Pair.ts
+++ src/types/contracts/IUniswapV2Pair.ts
@@ -2,7 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import {
+import type {
   ethers,
   EventFilter,
   Signer,
@@ -14,9 +14,9 @@ import {
   Overrides,
   CallOverrides,
 } from "ethers";
-import { BytesLike } from "@ethersproject/bytes";
-import { Listener, Provider } from "@ethersproject/providers";
-import { FunctionFragment, EventFragment, Result } from "@ethersproject/abi";
+import type { BytesLike } from "@ethersproject/bytes";
+import type { Listener, Provider } from "@ethersproject/providers";
+import type { FunctionFragment, EventFragment, Result } from "@ethersproject/abi";
 import type {
   TypedEventFilter,
   TypedEvent,
