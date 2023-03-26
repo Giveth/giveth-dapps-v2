d9bb27c0c6fb00b46a2df362a92f497df4926b94
alireza-sharifpour <ascode94@gmail.com>
2023-03-26 13:28:51 +0330

    added styles to PFPItem

--- src/components/modals/UploadProfilePicModal/PfpItem.tsx
+++ src/components/modals/UploadProfilePicModal/PfpItem.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
+import { neutralColors } from '@giveth/ui-design-system';
 import { convertIPFSToHTTPS } from '@/helpers/blockchain';
+import { Shadow } from '@/components/styled-components/Shadow';
 
 interface IPfpItemProps {
 	image: string;
@@ -11,7 +13,12 @@ const PfpItem = ({ image }: IPfpItemProps) => {
 	const convertedImage = convertIPFSToHTTPS(image);
 	return (
 		<Container>
-			<Image src={convertedImage} alt='pfp' width={330} height={330} />
+			<ImageContainer
+				src={convertedImage}
+				alt='pfp'
+				width={210}
+				height={210}
+			/>
 		</Container>
 	);
 };
@@ -19,5 +26,14 @@ const PfpItem = ({ image }: IPfpItemProps) => {
 export default PfpItem;
 
 const Container = styled.div`
-	background-color: white;
+	background-color: ${neutralColors.gray[100]};
+	width: 250px;
+	height: 300px;
+	box-shadow: ${Shadow.Neutral[500]};
+`;
+
+const ImageContainer = styled(Image)`
+	box-shadow: 0px 1px 21px 1px rgba(225, 69, 141, 0.63);
+	border-radius: 8px;
+	margin-top: 24px;
 `;
