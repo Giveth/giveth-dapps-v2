0edf2c30501cc4d780679d4dcd7f193bc6e3ef11
Cherik <Pourcheriki@gmail.com>
2023-03-27 16:58:49 +0330

    fix buildUsersPfpInfoQuery

--- src/lib/subgraph/pfpQueryBuilder.ts
+++ src/lib/subgraph/pfpQueryBuilder.ts
@@ -1,21 +1,19 @@
 export function buildUsersPfpInfoQuery(users: string[]) {
 	const query = `query {
-	  ${users.map(user => {
-			const _user = user.toLowerCase();
-			return `
-		  user_${_user}: giversPFPTokens(
-			where: {user: "${_user}" }
-		  ) {
+	  ${users
+			.map(user => {
+				const _user = user.toLowerCase();
+				return `user_${_user}: giversPFPTokens(where: {user: "${_user}" }) {
 				id
 				user {
-				id
+					id
 				}
 				tokenId
 				imageIpfs
 		  }
-		}
 		`;
-		})}
+			})
+			.join('')}}
   `;
 	return query;
 }
