690af17183e51a4e00108d68c813dad1a04c4c64
Cherik <Pourcheriki@gmail.com>
2023-03-28 17:58:19 +0330

    make symbols uppercase

--- src/components/views/donate/CryptoDonation.tsx
+++ src/components/views/donate/CryptoDonation.tsx
@@ -186,17 +186,27 @@ const CryptoDonation: FC = () => {
 		}
 	};
 
+	console.log(
+		'gnosisChain.nativeCurrency.symbol',
+		gnosisChain.nativeCurrency.symbol,
+		selectedToken?.symbol,
+	);
+
 	const pollToken = useCallback(() => {
 		clearPoll();
 		if (!selectedToken) {
 			return setSelectedTokenBalance(undefined);
 		}
 		// Native token balance is provided by the Web3Provider
+		const _selectedTokenSymbol = selectedToken.symbol.toUpperCase();
 		if (
-			selectedToken.symbol === ethereumChain.nativeCurrency.symbol ||
-			selectedToken.symbol === gnosisChain.nativeCurrency.symbol ||
-			selectedToken.symbol === polygonChain.nativeCurrency.symbol
-			// TODO:Optimism || selectedToken.symbol === optimismChain.nativeCurrency.symbol
+			_selectedTokenSymbol ===
+				ethereumChain.nativeCurrency.symbol.toUpperCase() ||
+			_selectedTokenSymbol ===
+				gnosisChain.nativeCurrency.symbol.toUpperCase() ||
+			_selectedTokenSymbol ===
+				polygonChain.nativeCurrency.symbol.toUpperCase()
+			// TODO:Optimism || _selectedTokenSymbol === optimismChain.nativeCurrency.symbol
 		) {
 			return setSelectedTokenBalance(balance);
 		}
