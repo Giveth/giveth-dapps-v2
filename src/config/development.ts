dafb7bf7f88f1566a4fedd31803bf93f25dd2d8b
Cherik <Pourcheriki@gmail.com>
2023-04-05 16:27:56 +0330

    fix build issue

--- src/config/development.ts
+++ src/config/development.ts
@@ -9,7 +9,8 @@ import { networksParams } from '@/helpers/blockchain';
 
 const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 const BASE_ROUTE = 'https://impact-graph.staging.k8s.giveth.io';
-const NOTIFICATION_BASE_ROUTE = 'https://notification-center.staging.k8s.giveth.io';
+const NOTIFICATION_BASE_ROUTE =
+	'https://notification-center.staging.k8s.giveth.io';
 const SEPT_8TH_2022 = 1662595200000;
 const MAINNET_NETWORK_NUMBER = 5; // Goerli
 const XDAI_NETWORK_NUMBER = 100; // xDAI
