639891f11e04b2652a5a587ba89182d8ddbc797a
Cherik <Pourcheriki@gmail.com>
2023-04-11 03:04:32 +0330

    show network logo and name

--- src/components/modals/ManageProjectAddresses/NetworkWalletAddress.tsx
+++ src/components/modals/ManageProjectAddresses/NetworkWalletAddress.tsx
@@ -1,18 +1,34 @@
 import { FC } from 'react';
 import styled from 'styled-components';
+import { Caption } from '@giveth/ui-design-system';
 import { IWalletAddress } from '@/apollo/types/types';
-import { Flex } from '@/components/styled-components/Flex';
+import { Flex, FlexCenter } from '@/components/styled-components/Flex';
 import { Badge, EBadgeStatus } from '@/components/Badge';
+import NetworkLogo from '@/components/NetworkLogo';
+import { networksParams } from '@/helpers/blockchain';
 
 interface INetworkWalletAddress {
 	networkWallet: IWalletAddress;
 }
-export const NetworkWalletAddress: FC<INetworkWalletAddress> = () => {
+export const NetworkWalletAddress: FC<INetworkWalletAddress> = ({
+	networkWallet,
+}) => {
 	return (
-		<Wrapper flexDirection='column'>
+		<Wrapper flexDirection='column' alignItems='baseline'>
 			<Badge label='wow' status={EBadgeStatus.SUCCESS} />
+			<FlexCenter gap='8px'>
+				<NetworkLogo chainId={networkWallet.networkId} logoSize={24} />
+				{networkWallet.networkId && (
+					<Caption>
+						{networksParams[networkWallet.networkId].chainName}
+					</Caption>
+				)}
+			</FlexCenter>
 		</Wrapper>
 	);
 };
 
-const Wrapper = styled(Flex)``;
+const Wrapper = styled(Flex)`
+	margin: 24px 0;
+	gap: 10px;
+`;
