62dd21e0c888bed476de26c772e60cfce4a9151b
Cherik <Pourcheriki@gmail.com>
2023-04-17 16:04:24 +0330

    fix fetchPrice

--- src/services/token.ts
+++ src/services/token.ts
@@ -46,16 +46,23 @@ export async function __oldWay_getToken(
 
 export const fetchPrice = async (chainId: number, tokenAddress?: string) => {
 	try {
-		const chain =
-			chainId === config.OPTIMISM_NETWORK_NUMBER
-				? 'celo'
-				: chainId === config.OPTIMISM_NETWORK_NUMBER
-				? 'optimistic-ethereum'
-				: chainId === config.POLYGON_NETWORK_NUMBER
-				? 'polygon-pos'
-				: chainId === config.XDAI_NETWORK_NUMBER
-				? 'xdai'
-				: 'ethereum';
+		let chain = 'ethereum';
+		switch (chainId) {
+			case config.CELO_NETWORK_NUMBER:
+				chain = 'celo';
+				break;
+			case config.OPTIMISM_NETWORK_NUMBER:
+				chain = 'optimistic-ethereum';
+				break;
+			case config.POLYGON_NETWORK_NUMBER:
+				chain = 'polygon-pos';
+				break;
+			case config.XDAI_NETWORK_NUMBER:
+				chain = 'xdai';
+				break;
+			default:
+				break;
+		}
 		const fetchCall = await fetch(
 			`https://api.coingecko.com/api/v3/simple/token_price/${chain}?contract_addresses=${tokenAddress}&vs_currencies=usd`,
 		);
