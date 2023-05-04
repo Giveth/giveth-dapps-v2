dbecd0afc32411a9aae064cb4f3c443839fa02b8
Cherik <Pourcheriki@gmail.com>
2023-05-04 10:46:41 +0330

    add like functionality

--- src/components/views/project/projectActionCard/LikeAndShareSection.tsx
+++ src/components/views/project/projectActionCard/LikeAndShareSection.tsx
@@ -6,71 +6,93 @@ import {
 	IconHeartOutline16,
 	IconShare16,
 } from '@giveth/ui-design-system';
-import { useIntl } from 'react-intl';
 import { captureException } from '@sentry/nextjs';
+import { useIntl } from 'react-intl';
 import ShareModal from '@/components/modals/ShareModal';
 import { EContentType } from '@/lib/constants/shareContent';
 import { useProjectContext } from '@/context/project.context';
 import { Flex } from '@/components/styled-components/Flex';
-import { client } from '@/apollo/apolloClient';
-import { FETCH_PROJECT_REACTION_BY_ID } from '@/apollo/gql/gqlProjects';
 import { useAppDispatch, useAppSelector } from '@/features/hooks';
 import { isSSRMode, showToastError } from '@/lib/helpers';
-import { IReaction } from '@/apollo/types/types';
 import { useModalCallback, EModalEvents } from '@/hooks/useModalCallback';
 import {
 	incrementLikedProjectsCount,
 	decrementLikedProjectsCount,
 } from '@/features/user/user.slice';
 import { likeProject, unlikeProject } from '@/lib/reaction';
+import { FETCH_PROJECT_REACTION_BY_ID } from '@/apollo/gql/gqlProjects';
+import { client } from '@/apollo/apolloClient';
 
 export const LikeAndShareSection = () => {
 	const [showModal, setShowModal] = useState<boolean>(false);
-	const [heartedByUser, setHeartedByUser] = useState<boolean>(false);
-	const [loading, setLoading] = useState(false);
-
+	const { projectData, isActive } = useProjectContext();
+	const project = projectData!;
+	const { slug, id: projectId } = project;
+	const [reaction, setReaction] = useState(project.reaction);
+	const [totalReactions, setTotalReactions] = useState(
+		project.totalReactions,
+	);
+	const [likeLoading, setLikeLoading] = useState(false);
 	const {
 		isSignedIn,
-		isEnabled,
 		userData: user,
-		isLoading: isUserLoading,
+		isEnabled,
 	} = useAppSelector(state => state.user);
 	const dispatch = useAppDispatch();
 	const { formatMessage } = useIntl();
-	const { projectData, isActive } = useProjectContext();
-	const {
-		categories = [],
-		slug,
-		adminUser,
-		id,
-		verified,
-		verificationStatus,
-		organization,
-		verificationFormStatus,
-		projectPower,
-		projectFuturePower,
-	} = projectData || {};
-	const [reaction, setReaction] = useState<IReaction | undefined>(
-		projectData?.reaction,
-	);
+
+	useEffect(() => {
+		const fetchProjectReaction = async () => {
+			if (user?.id && project.id) {
+				try {
+					const { data } = await client.query({
+						query: FETCH_PROJECT_REACTION_BY_ID,
+						variables: {
+							id: Number(project.id),
+							connectedWalletUserId: Number(user?.id),
+						},
+						fetchPolicy: 'no-cache',
+					});
+					const _totalReactions = data?.projectById?.totalReactions;
+					const _reaction = data?.projectById?.reaction;
+					setTotalReactions(_totalReactions);
+					setReaction(_reaction);
+				} catch (e) {
+					showToastError(e);
+					captureException(e, {
+						tags: {
+							section: 'fetchProjectReaction',
+						},
+					});
+				}
+			} else {
+				setReaction(undefined);
+			}
+		};
+		fetchProjectReaction();
+	}, [project.id, user?.id]);
 
 	const likeUnlikeProject = async () => {
-		if (loading) return;
-
-		if (id) {
-			setLoading(true);
+		if (projectId) {
+			setLikeLoading(true);
 
 			try {
 				if (!reaction) {
-					const newReaction = await likeProject(id);
+					const newReaction = await likeProject(projectId);
+					setReaction(newReaction);
 					if (newReaction) {
-						setReaction(newReaction);
+						setTotalReactions(
+							_totalReactions => (_totalReactions || 0) + 1,
+						);
 						dispatch(incrementLikedProjectsCount());
 					}
 				} else if (reaction?.userId === user?.id) {
 					const successful = await unlikeProject(reaction.id);
 					if (successful) {
 						setReaction(undefined);
+						setTotalReactions(
+							_totalReactions => (_totalReactions || 1) - 1,
+						);
 						dispatch(decrementLikedProjectsCount());
 					}
 				}
@@ -78,11 +100,11 @@ export const LikeAndShareSection = () => {
 				showToastError(e);
 				captureException(e, {
 					tags: {
-						section: 'likeUnlike Project Donate Card',
+						section: 'likeUnlikeProject',
 					},
 				});
 			} finally {
-				setLoading(false);
+				setLikeLoading(false);
 			}
 		}
 	};
@@ -106,53 +128,9 @@ export const LikeAndShareSection = () => {
 		}
 	};
 
-	useEffect(() => {
-		const fetchProjectReaction = async () => {
-			if (user?.id && id) {
-				// Already fetched
-				if (user?.id === reaction?.userId) return;
-
-				try {
-					const { data } = await client.query({
-						query: FETCH_PROJECT_REACTION_BY_ID,
-						variables: {
-							id: Number(id),
-							connectedWalletUserId: Number(user?.id),
-						},
-						fetchPolicy: 'no-cache',
-					});
-					setReaction(data?.projectById?.reaction);
-				} catch (e) {
-					showToastError(e);
-					captureException(e, {
-						tags: {
-							section: 'fetchProjectReaction',
-						},
-					});
-				}
-			} else if (reaction) {
-				setReaction(undefined);
-			}
-		};
-		fetchProjectReaction();
-	}, [id, reaction, user?.id]);
-
-	useEffect(() => {
-		setHeartedByUser(!!reaction?.id && reaction?.userId === user?.id);
-	}, [projectData, reaction, user?.id]);
-
 	return (
 		<>
 			<BadgeWrapper gap='8px'>
-				{/* <ShareLikeBadge
-					type='share'
-					onClick={() => isActive && setShowModal(true)}
-				/>
-				<ShareLikeBadge
-					type='like'
-					active={heartedByUser}
-					isSimple={isMobile}
-				/> */}
 				<StyledButton
 					label={formatMessage({ id: 'label.share' })}
 					onClick={() => setShowModal(true)}
@@ -161,11 +139,11 @@ export const LikeAndShareSection = () => {
 					size='small'
 				/>
 				<StyledButton
-					label={formatMessage({ id: 'label.like' })}
+					label={totalReactions.toString()}
 					onClick={() => isActive && checkSignInThenLike()}
 					buttonType='texty-gray'
 					icon={
-						heartedByUser ? (
+						reaction?.userId && reaction?.userId === user?.id ? (
 							<IconHeartFilled16 />
 						) : (
 							<IconHeartOutline16 />
