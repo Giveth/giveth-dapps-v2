1fa99ad09fc8f1d337d66307dd8b108693be0676
Cherik <Pourcheriki@gmail.com>
2023-05-04 23:38:23 +0330

    add Boost Button

--- src/components/views/project/projectActionCard/GIVPowerSection.tsx
+++ src/components/views/project/projectActionCard/GIVPowerSection.tsx
@@ -3,19 +3,52 @@ import {
 	IconHelpFilled16,
 	Subline,
 	neutralColors,
+	Button,
+	IconRocketInSpace16,
 } from '@giveth/ui-design-system';
 import { useIntl } from 'react-intl';
 import styled from 'styled-components';
+import { useState } from 'react';
 import { IconWithTooltip } from '@/components/IconWithToolTip';
 import { Flex } from '@/components/styled-components/Flex';
 import { BoostTooltip } from '../projectDonateCard/ProjectDonateCard';
 import { CurrentRank, NextRank } from '@/components/GIVpowerRank';
 import { useProjectContext } from '@/context/project.context';
+import { useAppSelector } from '@/features/hooks';
+import { useModalCallback, EModalEvents } from '@/hooks/useModalCallback';
+import { isSSRMode } from '@/lib/helpers';
+import BoostModal from '@/components/modals/Boost/BoostModal';
 
 export const GIVPowerSection = () => {
+	const [showBoost, setShowBoost] = useState(false);
+
 	const { formatMessage } = useIntl();
 	const { projectData } = useProjectContext();
 	const { projectPower, projectFuturePower } = projectData!;
+
+	const { isSignedIn, isEnabled } = useAppSelector(state => state.user);
+
+	const showBoostModal = () => {
+		setShowBoost(true);
+	};
+
+	const { modalCallback: signInThenBoost } = useModalCallback(showBoostModal);
+
+	const { modalCallback: connectThenSign } = useModalCallback(
+		signInThenBoost,
+		EModalEvents.CONNECTED,
+	);
+
+	const handleBoostClick = () => {
+		if (isSSRMode) return;
+		if (!isEnabled) {
+			connectThenSign();
+		} else if (!isSignedIn) {
+			signInThenBoost();
+		} else {
+			showBoostModal();
+		}
+	};
 
 	return (
 		<GIVPowerSectionWrapper>
@@ -38,7 +71,7 @@ export const GIVPowerSection = () => {
 				</IconWithTooltip>
 			</Flex>
 			<CurrentRank projectPower={projectPower} />
-			<Flex justifyContent='space-between' alignItems='center'>
+			<NextRankRow justifyContent='space-between' alignItems='center'>
 				<Subline>
 					{formatMessage({
 						id: 'label.projected_rank',
@@ -48,7 +81,21 @@ export const GIVPowerSection = () => {
 					projectPower={projectPower}
 					projectFuturePower={projectFuturePower}
 				/>
-			</Flex>
+			</NextRankRow>
+			<BoostButton
+				label={formatMessage({
+					id: 'label.boost',
+				})}
+				buttonType='texty-gray'
+				icon={<IconRocketInSpace16 />}
+				onClick={handleBoostClick}
+			/>
+			{showBoost && projectData?.id && (
+				<BoostModal
+					projectId={projectData.id}
+					setShowModal={setShowBoost}
+				/>
+			)}
 		</GIVPowerSectionWrapper>
 	);
 };
@@ -56,4 +103,17 @@ export const GIVPowerSection = () => {
 const GIVPowerSectionWrapper = styled.div`
 	border-top: 1px solid ${neutralColors.gray[500]};
 	padding-top: 16px;
+`;
+
+const NextRankRow = styled(Flex)`
+	margin-bottom: 32px;
+`;
+
+const BoostButton = styled(Button)`
+	flex-direction: row-reverse;
+	gap: 8px;
+	box-shadow: 0px 3px 20px rgba(83, 38, 236, 0.13);
+	width: 100%;
+	padding: 16px;
+	color: ${neutralColors.gray[900]};
 `;
