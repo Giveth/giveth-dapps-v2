6a411ab7a6d824a5bf53a137a64a31edfe71bf19
Cherik <Pourcheriki@gmail.com>
2023-05-08 12:35:10 +0330

    fix button styles

--- src/components/views/project/projectActionCard/AdminActions.tsx
+++ src/components/views/project/projectActionCard/AdminActions.tsx
@@ -1,13 +1,27 @@
-import { Button, IconEdit16 } from '@giveth/ui-design-system';
-import React from 'react';
+import {
+	ButtonLink,
+	IconEdit16,
+	IconVerifiedBadge16,
+	OutlineButton,
+	brandColors,
+} from '@giveth/ui-design-system';
+import React, { useState } from 'react';
 import { useIntl } from 'react-intl';
 import styled from 'styled-components';
+import { Flex } from '@/components/styled-components/Flex';
+import { useProjectContext } from '@/context/project.context';
+import { VerificationModal } from '@/components/modals/VerificationModal';
+import { idToProjectEdit } from '@/lib/routeCreators';
 
 export const AdminActions = () => {
+	const [showVerificationModal, setShowVerificationModal] = useState(false);
+
+	const { projectData, isActive, isDraft } = useProjectContext();
 	const { formatMessage } = useIntl();
 	return (
-		<div>
-			{/* <FullButton
+		<>
+			<Flex flexDirection='column' gap='16px'>
+				{/* <FullButton
 				buttonType='primary'
 				label={formatMessage({ id: 'label.edit' })}
 				disabled={!isActive && !isDraft}
@@ -25,19 +39,66 @@ export const AdminActions = () => {
 					onClick={() => setShowVerificationModal(true)}
 				/>
 			)} */}
-			<EditButton
-				buttonType='texty-secondary'
-				label={formatMessage({ id: 'label.edit' })}
-				icon={<IconEdit16 />}
-			/>
-		</div>
+				{/* <ArchiveButton
+							buttonType='texty'
+							size='small'
+							label={`${
+								isActive
+									? formatMessage({
+											id: 'label.deactivate_project',
+									  })
+									: formatMessage({
+											id: 'component.button.activate_project',
+									  })
+							}`}
+							icon={<IconArchiving size={16} />}
+							onClick={() => handleProjectStatus(isActive)}
+						/> */}
+				<EditButton
+					isExternal
+					linkType='texty-secondary'
+					label={formatMessage({ id: 'label.edit' })}
+					icon={<IconEdit16 />}
+					href={idToProjectEdit(projectData?.id || '')}
+				/>
+				<VerifyButton
+					label={formatMessage({
+						id: 'label.verify_your_project',
+					})}
+					icon={<IconVerifiedBadge16 />}
+					disabled={!isActive}
+					onClick={() => setShowVerificationModal(true)}
+				/>
+			</Flex>
+			{showVerificationModal && (
+				<VerificationModal
+					onClose={() => setShowVerificationModal(false)}
+				/>
+			)}
+		</>
 	);
 };
 
-const EditButton = styled(Button)`
+const EditButton = styled(ButtonLink)`
 	width: 100%;
 	flex-direction: row-reverse;
 	gap: 8px;
 	box-shadow: 0px 3px 20px rgba(83, 38, 236, 0.13);
 	padding: 16px 24px;
+	&:hover {
+		color: ${brandColors.giv[700]};
+	}
+	transition: color 0.3s ease;
+`;
+
+const VerifyButton = styled(OutlineButton)`
+	width: 100%;
+	flex-direction: row-reverse;
+	gap: 8px;
+	padding: 16px 24px;
+	color: ${brandColors.giv[500]};
+	border: 2px solid ${brandColors.giv[500]};
+	&:hover {
+		color: ${brandColors.giv[700]};
+	}
 `;
