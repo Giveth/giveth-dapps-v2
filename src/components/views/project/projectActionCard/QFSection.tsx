ed92067fd946c28a55d9f2c62006602ad6789b07
alireza-sharifpour <ascode94@gmail.com>
2023-05-23 15:30:19 +0330

    created QF section of project action card

---
+++ src/components/views/project/projectActionCard/QFSection.tsx
@@ -0,0 +1,165 @@
+import {
+	Subline,
+	H3,
+	H4,
+	neutralColors,
+	Caption,
+	brandColors,
+	mediaQueries,
+	semanticColors,
+	IconArrowRight16,
+} from '@giveth/ui-design-system';
+import { useIntl } from 'react-intl';
+import styled from 'styled-components';
+import { useProjectContext } from '@/context/project.context';
+import { Flex } from '@/components/styled-components/Flex';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { device } from '@/lib/constants/constants';
+
+const QFSection = () => {
+	const { formatMessage, locale } = useIntl();
+	const { projectData, totalDonationsCount } = useProjectContext();
+	const { totalDonations } = projectData || {};
+	const isMobile = !useMediaQuery(device.tablet);
+
+	return (
+		<DonationSectionWrapper gap='24px'>
+			{totalDonations && totalDonations !== 0 ? (
+				<DonateInfo>
+					{isMobile && <br />}
+					<Title>
+						{formatMessage({
+							id: 'label.amount_raised',
+						})}
+					</Title>
+					<Amount weight={700}>
+						${totalDonations.toLocaleString(locale)}
+					</Amount>
+					<Description>
+						{formatMessage({
+							id: 'label.raised_from',
+						})}
+						<Caption medium>{totalDonationsCount}</Caption>
+						{formatMessage(
+							{
+								id: 'label.contributors',
+							},
+							{
+								count: totalDonationsCount,
+							},
+						)}
+					</Description>
+				</DonateInfo>
+			) : (
+				<DonateInfo>
+					<NoFund weight={700}>
+						{formatMessage({
+							id: 'label.donate_first_lead_the_way',
+						})}
+					</NoFund>
+				</DonateInfo>
+			)}
+			<Flex flexDirection='column' gap='4px'>
+				<EstimatedMatchingPrice>+ $7,200</EstimatedMatchingPrice>
+				<LightSubline> Estimated matching</LightSubline>
+			</Flex>
+			<ContributionsContainer>
+				<Flex flexDirection='column' gap='8px'>
+					<FlexSameSize justifyContent='space-between'>
+						<Subline>1 DAI</Subline>
+						<IconArrowRight16 />
+						<EndAlignedSubline>1 DAI</EndAlignedSubline>
+					</FlexSameSize>
+					<FlexSameSize justifyContent='space-between'>
+						<Subline>12312 DAI</Subline>
+						<IconArrowRight16 />
+						<EndAlignedSubline>1 DAI </EndAlignedSubline>
+					</FlexSameSize>
+					<FlexSameSize justifyContent='space-between'>
+						<Subline>1 DAI</Subline>
+						<IconArrowRight16 />
+						<EndAlignedSubline>1 DAI </EndAlignedSubline>
+					</FlexSameSize>
+				</Flex>
+			</ContributionsContainer>
+		</DonationSectionWrapper>
+	);
+};
+
+export default QFSection;
+
+const Title = styled(Subline)`
+	margin-bottom: 8px;
+`;
+
+const Amount = styled(H3)`
+	margin-bottom: 4px;
+`;
+
+const Description = styled(Caption)`
+	color: ${neutralColors.gray[700]};
+	margin-bottom: 24px;
+	& > div {
+		color: ${neutralColors.gray[900]};
+		display: inline;
+	}
+`;
+
+const DonationSectionWrapper = styled(Flex)`
+	justify-content: space-between;
+	flex-direction: column;
+	${mediaQueries.tablet} {
+		flex-direction: row;
+	}
+	${mediaQueries.laptopS} {
+		flex-direction: column;
+	}
+`;
+
+const DonateInfo = styled.div`
+	height: 90px;
+`;
+
+const NoFund = styled(H4)`
+	color: ${neutralColors.gray[800]};
+	margin-top: 16px;
+`;
+
+const EstimatedMatchingPrice = styled(H4)`
+	color: ${semanticColors.jade[500]};
+`;
+
+const LightSubline = styled(Caption)`
+	display: inline;
+	color: ${neutralColors.gray[700]};
+`;
+
+const EndAlignedSubline = styled(Subline)`
+	text-align: end;
+`;
+
+const ContributionsContainer = styled.div`
+	padding: 16px 0;
+	border-top: 1px solid ${neutralColors.gray[300]};
+	border-bottom: 1px solid ${neutralColors.gray[300]};
+`;
+
+const FlexSameSize = styled(Flex)`
+	> * {
+		flex: 1 1 0px;
+	}
+`;
+
+const DonateDescription = styled(Flex)`
+	padding: 8px 16px;
+	border: 1px solid ${neutralColors.gray[300]};
+	border-radius: 16px;
+	margin-bottom: 24px;
+`;
+
+const LearnLink = styled(Flex)`
+	color: ${brandColors.pinky[500]};
+	&:hover {
+		color: ${brandColors.pinky[700]};
+	}
+`;
