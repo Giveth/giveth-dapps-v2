e53e1b43d803eec3336a86a88a6384479ca34e57
Cherik <Pourcheriki@gmail.com>
2023-05-25 13:01:25 +0330

    add FETCH_QF_ROUNDS

---
+++ src/apollo/gql/gqlQF.ts
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const FETCH_QF_ROUNDS = gql`
+	query FetchQFRounds {
+		qfRounds {
+			id
+			name
+			isActive
+			beginDate
+			endDate
+		}
+	}
+`;
