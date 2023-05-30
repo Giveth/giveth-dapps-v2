827aa1c9971ee759c282876ce27fd8c8c73f501c
Cherik <Pourcheriki@gmail.com>
2023-05-30 16:22:22 +0330

    add title and desc

--- src/components/views/project/projectGivSavings/index.tsx
+++ src/components/views/project/projectGivSavings/index.tsx
@@ -1,7 +1,27 @@
+import { H4, Lead, brandColors } from '@giveth/ui-design-system';
 import React from 'react';
+import { useIntl } from 'react-intl';
+import styled from 'styled-components';
+import ExternalLink from '@/components/ExternalLink';
 
 const ProjectGivSavingsIndex = () => {
-	return <div>ProjectGivSavingsIndex</div>;
+	const { formatMessage } = useIntl();
+
+	return (
+		<div>
+			<H4 weight={700}>{formatMessage({ id: 'label.givsavings' })}</H4>
+			<Desc size='large'>
+				{formatMessage({ id: 'page.givsavings.desc' })}{' '}
+				<ExternalLink
+					title={formatMessage({ id: 'label.learn_more' })}
+					href='https://giveth.io/givsavings'
+					color={brandColors.pinky[500]}
+				/>
+			</Desc>
+		</div>
+	);
 };
+
+const Desc = styled(Lead)``;
 
 export default ProjectGivSavingsIndex;
