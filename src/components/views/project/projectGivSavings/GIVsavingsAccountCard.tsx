c6701b72b67d805408fff4f7d71d1ab3159552b3
Cherik <Pourcheriki@gmail.com>
2023-05-31 13:12:58 +0330

    add tabs

--- src/components/views/project/projectGivSavings/GIVsavingsAccountCard.tsx
+++ src/components/views/project/projectGivSavings/GIVsavingsAccountCard.tsx
@@ -1,6 +1,6 @@
 import { H4, H6, brandColors, neutralColors } from '@giveth/ui-design-system';
-import { FC } from 'react';
-import styled from 'styled-components';
+import { FC, useState } from 'react';
+import styled, { css } from 'styled-components';
 import { Flex } from '@/components/styled-components/Flex';
 import NetworkLogo from '@/components/NetworkLogo';
 import { OptimismNetworkConfig } from '@/types/config';
@@ -9,12 +9,20 @@ interface IGIVsavingsAccountCard {
 	network: OptimismNetworkConfig;
 }
 
+enum EAccountState {
+	DEPOSIT = 'deposit',
+	WITHDRAW = 'withdraw',
+}
+
+const tabs = [EAccountState.DEPOSIT, EAccountState.WITHDRAW];
+
 export const GIVsavingsAccountCard: FC<IGIVsavingsAccountCard> = ({
 	network,
 }) => {
+	const [state, setState] = useState(EAccountState.DEPOSIT);
 	return (
 		<Wrapper>
-			<DepositCard>
+			<ActionCard>
 				<Header justifyContent='space-between'>
 					<Flex alignItems='center' gap='8px'>
 						<NetworkLogo
@@ -31,7 +39,18 @@ export const GIVsavingsAccountCard: FC<IGIVsavingsAccountCard> = ({
 						<H6>{network.chainName}</H6>
 					</NetworkCard>
 				</Header>
-			</DepositCard>
+				<Flex gap='24px'>
+					{tabs.map(tab => (
+						<Tab
+							key={tab}
+							isActive={state === tab}
+							onClick={() => setState(tab)}
+						>
+							{tab}
+						</Tab>
+					))}
+				</Flex>
+			</ActionCard>
 		</Wrapper>
 	);
 };
@@ -41,7 +60,7 @@ const Wrapper = styled.div`
 	background-color: ${brandColors.giv['000']};
 `;
 
-const DepositCard = styled.div`
+const ActionCard = styled.div`
 	padding: 24px;
 	background-color: ${neutralColors.gray[100]};
 `;
@@ -52,4 +71,21 @@ const NetworkCard = styled(Flex)`
 	padding: 12px;
 	background-color: ${neutralColors.gray[300]};
 	border-radius: 8px;
+`;
+
+interface ITab {
+	isActive: boolean;
+}
+
+const Tab = styled(Flex)<ITab>`
+	padding: 9px 24px;
+	color: ${neutralColors.gray[800]};
+	border-radius: 50px;
+	cursor: pointer;
+	${props =>
+		props.isActive &&
+		css`
+			background-color: ${neutralColors.gray[200]};
+			color: ${brandColors.pinky['500']};
+		`};
 `;
