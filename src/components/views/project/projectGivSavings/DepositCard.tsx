324323b70c1d9e9f9f06312cda87c185de166524
Cherik <Pourcheriki@gmail.com>
2023-05-31 17:25:52 +0330

    add fetchBalance functionality

--- src/components/views/project/projectGivSavings/DepositCard.tsx
+++ src/components/views/project/projectGivSavings/DepositCard.tsx
@@ -1,12 +1,24 @@
-import { B, Caption, neutralColors } from '@giveth/ui-design-system';
-import { FC, useCallback, useState } from 'react';
+import {
+	B,
+	Caption,
+	GLink,
+	IconRefresh16,
+	brandColors,
+	neutralColors,
+} from '@giveth/ui-design-system';
+import { FC, useCallback, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { BigNumber, utils } from 'ethers';
 import { captureException } from '@sentry/nextjs';
+import { Contract } from '@ethersproject/contracts';
+import { BigNumber } from '@ethersproject/bignumber';
+import { useWeb3React } from '@web3-react/core';
+import { formatUnits, parseUnits } from '@ethersproject/units';
 import { NumericalInput } from '@/components/input';
 import { Flex } from '@/components/styled-components/Flex';
 import NetworkLogo from '@/components/NetworkLogo';
 import { GIVSavingsAccount } from '@/types/config';
+import { abi as ERC20_ABI } from '@/artifacts/ERC20.json';
+import { ERC20 } from '@/types/contracts';
 
 interface IDepositCard {
 	givsavingsAccount: GIVSavingsAccount;
@@ -15,13 +27,16 @@ interface IDepositCard {
 export const DepositCard: FC<IDepositCard> = ({ givsavingsAccount }) => {
 	const [displayAmount, setDisplayAmount] = useState('');
 	const [amount, setAmount] = useState('0');
+	const [balance, setBalance] = useState('0');
+
+	const { chainId: networkId, account, library, active } = useWeb3React();
 
 	const onUserInput = useCallback((value: string) => {
 		setDisplayAmount(value);
 		let valueBn = BigNumber.from(0);
 
 		try {
-			valueBn = utils.parseUnits(value);
+			valueBn = parseUnits(value);
 		} catch (error) {
 			console.debug(`Failed to parse input amount: "${value}"`, error);
 			captureException(error, {
@@ -30,9 +45,32 @@ export const DepositCard: FC<IDepositCard> = ({ givsavingsAccount }) => {
 				},
 			});
 		}
-
 		setAmount(valueBn.toString());
 	}, []);
+
+	const fetchBalance = useCallback(async () => {
+		if (!account) return;
+		try {
+			const tokenContract = new Contract(
+				givsavingsAccount.token.address,
+				ERC20_ABI,
+				library,
+			) as ERC20;
+			const balance = await tokenContract.balanceOf(account);
+			setBalance(formatUnits(balance));
+		} catch (error) {
+			console.debug(`Failed to fetch balance`, error);
+			captureException(error, {
+				tags: {
+					section: 'AmountInput',
+				},
+			});
+		}
+	}, [account, library, givsavingsAccount.token.address]);
+
+	useEffect(() => {
+		fetchBalance();
+	}, [fetchBalance]);
 
 	return (
 		<Wrapper>
@@ -49,6 +87,14 @@ export const DepositCard: FC<IDepositCard> = ({ givsavingsAccount }) => {
 					disabled={false}
 				/>
 			</InputWrapper>
+			<Flex justifyContent='space-between'>
+				<Flex alignItems='center' gap='6px'>
+					<GLink>Available: ${balance}</GLink>
+					<IconWrapper onClick={() => fetchBalance()}>
+						<IconRefresh16 color={brandColors.giv[500]} />
+					</IconWrapper>
+				</Flex>
+			</Flex>
 		</Wrapper>
 	);
 };
@@ -78,4 +124,8 @@ const StyledNumericalInput = styled(NumericalInput)`
 	::placeholder {
 		color: ${neutralColors.gray[800]};
 	}
+`;
+
+const IconWrapper = styled.div`
+	cursor: pointer;
 `;
