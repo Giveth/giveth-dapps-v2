5cc16520cbf195be3d44261517c2fa16dd6a8c08
Cherik <Pourcheriki@gmail.com>
2023-05-31 17:57:24 +0330

    fix NetworkLogo for different env

--- src/components/NetworkLogo.tsx
+++ src/components/NetworkLogo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { FC } from 'react';
 import config from '@/configuration';
 import { IconEthereum } from '@/components/Icons/Eth';
 import { IconGnosisChain } from '@/components/Icons/GnosisChain';
@@ -6,17 +6,21 @@ import { IconPolygon } from '@/components/Icons/Polygon';
 import { IconCelo } from './Icons/Celo';
 import { IconOptimism } from './Icons/Optimism';
 
-const NetworkLogo = (props: { chainId?: number; logoSize?: number }) => {
-	const { chainId, logoSize } = props;
-	if (chainId === config.MAINNET_NETWORK_NUMBER) {
+interface INetworkLogo {
+	chainId?: number;
+	logoSize?: number;
+}
+
+const NetworkLogo: FC<INetworkLogo> = ({ chainId, logoSize }) => {
+	if (chainId === parseInt(config.MAINNET_CONFIG.chainId)) {
 		return <IconEthereum size={logoSize} />;
-	} else if (chainId === config.XDAI_NETWORK_NUMBER) {
+	} else if (chainId === parseInt(config.XDAI_CONFIG.chainId)) {
 		return <IconGnosisChain size={logoSize} />;
-	} else if (chainId === config.POLYGON_NETWORK_NUMBER) {
+	} else if (chainId === parseInt(config.POLYGON_CONFIG.chainId)) {
 		return <IconPolygon size={logoSize} />;
-	} else if (chainId === config.OPTIMISM_NETWORK_NUMBER) {
+	} else if (chainId === parseInt(config.OPTIMISM_CONFIG.chainId)) {
 		return <IconOptimism size={logoSize} />;
-	} else if (chainId === config.CELO_NETWORK_NUMBER) {
+	} else if (chainId === parseInt(config.CELO_CONFIG.chainId)) {
 		return <IconCelo size={logoSize} />;
 	} else return null;
 };
