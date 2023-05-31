75e6f89617a483f6b2c62ca090e063c51ef485cc
alireza-sharifpour <ascode94@gmail.com>
2023-05-31 16:41:10 +0330

    added state to ProjectGivSavingsIndex

--- src/components/views/project/projectGivSavings/index.tsx
+++ src/components/views/project/projectGivSavings/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import GivSavingsAccountDetailsTabs from './GivSavingsAccountDetailsTabs';
+import GIVsavingsBalanceChart from './GivSavingsCharts/GIVsavingsBalanceChart';
 
 const ProjectGivSavingsIndex = () => {
+	const [selectedChartTab, setSelectedChartTab] = useState(0);
 	return (
 		<div>
-			<GivSavingsAccountDetailsTabs />
+			<GivSavingsAccountDetailsTabs
+				selectedChartTab={selectedChartTab}
+				setSelectedChartTab={setSelectedChartTab}
+			/>
+			<TabsContainer>
+				{selectedChartTab === 0 && <GIVsavingsBalanceChart />}
+			</TabsContainer>
 		</div>
 	);
 };
 
 export default ProjectGivSavingsIndex;
+
+const TabsContainer = styled.div`
+	margin-top: 32px;
+`;
