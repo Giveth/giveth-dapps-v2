d912d0c20fbde67440f6df36b45a68bdf4ec8d6d
Cherik <Pourcheriki@gmail.com>
2023-06-06 14:40:24 +0330

    init PassportCard

--- src/components/views/userProfile/PassportCard.tsx
+++ src/components/views/userProfile/PassportCard.tsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+import { B, H3, P, neutralColors } from '@giveth/ui-design-system';
+import { ContributeCardBox } from '@/components/ContributeCard.sc';
+import { usePassport } from '@/hooks/usePassport';
+import { Flex } from '@/components/styled-components/Flex';
 
 export const PassportCard = () => {
-	return <div>PassportCard</div>;
+	const { score } = usePassport();
+	return (
+		<StyledContributeCardBox>
+			<Flex alignItems='center' justifyContent='space-between'>
+				<B>Your Passport score</B>
+				<Score weight={700}>
+					{score?.passportScore === undefined ||
+					score?.passportScore === null
+						? '--'
+						: score?.passportScore}
+				</Score>
+			</Flex>
+			<P>
+				Verify your Gitcoin Passport to prove your donor uniqueness and
+				ensure your donations get matched in quadratic funding rounds.
+			</P>
+			<Hr></Hr>
+			<div>salam</div>
+		</StyledContributeCardBox>
+	);
 };
+
+const StyledContributeCardBox = styled(ContributeCardBox)`
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+`;
+
+const Score = styled(H3)`
+	color: ${neutralColors.gray[800]};
+`;
+
+const Hr = styled.div`
+	height: 1px;
+	background-color: ${neutralColors.gray[300]};
+	width: 100%;
+`;
