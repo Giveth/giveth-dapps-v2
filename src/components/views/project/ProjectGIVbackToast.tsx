c12ed5a939012f14046173cea75902e70d47fb85
Ramin <r.ramazanpour@aut.ac.ir>
2023-06-12 03:21:01 +0330

    add BoostModal and public and owner and verified and not verified copies

--- src/components/views/project/ProjectGIVbackToast.tsx
+++ src/components/views/project/ProjectGIVbackToast.tsx
@@ -6,56 +6,117 @@ import {
 	IconChevronRight,
 	IconGIVBack,
 	IconRocketInSpace16,
+	mediaQueries,
 	neutralColors,
 	OutlineButton,
 	P,
 	semanticColors,
 } from '@giveth/ui-design-system';
 import { useIntl } from 'react-intl';
+import { useState } from 'react';
 import { Flex } from '@/components/styled-components/Flex';
 import ExternalLink from '@/components/ExternalLink';
 import links from '@/lib/constants/links';
 import { useProjectContext } from '@/context/project.context';
+import { EModalEvents, useModalCallback } from '@/hooks/useModalCallback';
+import { isSSRMode } from '@/lib/helpers';
+import BoostModal from '@/components/modals/Boost/BoostModal';
+import { useAppSelector } from '@/features/hooks';
 
 const ProjectGIVbackToast = () => {
 	const { projectData, isAdmin } = useProjectContext();
 	const verified = projectData?.verified;
 	const isOwnerVerified = verified && isAdmin;
+	const isOwnerNotVerified = !verified && isAdmin;
+	const isPublicVerified = verified && !isAdmin;
 	const color = isOwnerVerified
 		? semanticColors.golden[600]
 		: neutralColors.gray[900];
 	const { formatMessage } = useIntl();
 
+	const title = formatMessage({
+		id: `project.givback_toast.title.${
+			isOwnerVerified
+				? 'verified_owner'
+				: isOwnerNotVerified
+				? 'non_verified_owner'
+				: isPublicVerified
+				? 'verified_public'
+				: 'non_verified_public'
+		}`,
+	});
+
+	const description = formatMessage({
+		id: `project.givback_toast.description.${
+			isOwnerVerified
+				? 'verified_owner'
+				: isOwnerNotVerified
+				? 'non_verified_owner'
+				: isPublicVerified
+				? 'verified_public'
+				: 'non_verified_public'
+		}`,
+	});
+
+	const [showBoost, setShowBoost] = useState(false);
+
+	const { isEnabled, isSignedIn } = useAppSelector(state => state.user);
+
+	const showBoostModal = () => {
+		setShowBoost(true);
+	};
+
+	const { modalCallback: signInThenBoost } = useModalCallback(showBoostModal);
+
+	const { modalCallback: connectThenSign } = useModalCallback(
+		signInThenBoost,
+		EModalEvents.CONNECTED,
+	);
+
+	const handleBoostClick = () => {
+		if (isSSRMode) return;
+		if (!isEnabled) {
+			connectThenSign();
+		} else if (!isSignedIn) {
+			signInThenBoost();
+		} else {
+			showBoostModal();
+		}
+	};
+
 	return (
-		<Wrapper>
-			<Content>
-				<IconGIVBack color={color} size={24} />
-				<div>
-					<Title color={color}>
-						Get rewarded with up to 80% of your donation value!
-					</Title>
-					<Description>
-						Donors to verified projects are rewarded with GIV. Boost
-						this project to increase its rewards percentage and make
-						it more visible on the projects page!
-					</Description>
-					<ExternalLink href={links.GIVBACK_DOC}>
-						<LearnMore>
-							{formatMessage({ id: 'label.learn_more' })}
-							<IconChevronRight size={24} />
-						</LearnMore>
-					</ExternalLink>
-				</div>
-			</Content>
-			{verified && (
-				<ButtonWrapper>
-					<OutlineButton
-						label='Boost'
-						icon={<IconRocketInSpace16 />}
-					/>
-				</ButtonWrapper>
+		<>
+			<Wrapper>
+				<Content>
+					<IconGIVBack color={color} size={24} />
+					<div>
+						<Title color={color}>{title}</Title>
+						<Description>{description}</Description>
+						<ExternalLink href={links.GIVBACK_DOC}>
+							<LearnMore>
+								{formatMessage({ id: 'label.learn_more' })}
+								<IconChevronRight size={24} />
+							</LearnMore>
+						</ExternalLink>
+					</div>
+				</Content>
+				{verified && (
+					<ButtonWrapper>
+						<OutlineButton
+							onClick={handleBoostClick}
+							label='Boost'
+							icon={<IconRocketInSpace16 />}
+						/>
+					</ButtonWrapper>
+				)}
+			</Wrapper>
+			{showBoost && (
+				<BoostModal
+					projectId={projectData?.id!}
+					setShowModal={setShowBoost}
+				/>
 			)}
-		</Wrapper>
+		</>
 	);
 };
 
@@ -102,6 +163,10 @@ const Wrapper = styled(Flex)`
 	background: #ffffff;
 	border-radius: 16px;
 	margin-top: 12px;
+	flex-direction: column;
+	${mediaQueries.tablet} {
+		flex-direction: row;
+	}
 `;
 
 export default ProjectGIVbackToast;
