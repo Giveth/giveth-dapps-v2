edcab79c8ece5ba4ba39670132fe863867e58219
alireza-sharifpour <ascode94@gmail.com>
2023-06-12 14:31:24 +0330

    added qf topsection to QF DonateCard

--- src/components/views/project/projectDonations/ProjectTotalFundCard.tsx
+++ src/components/views/project/projectDonations/ProjectTotalFundCard.tsx
@@ -1,34 +1,80 @@
 import { FC } from 'react';
 import styled from 'styled-components';
-import { H2, H4, Lead, neutralColors, P } from '@giveth/ui-design-system';
+import {
+	B,
+	H2,
+	H4,
+	H5,
+	Lead,
+	neutralColors,
+	P,
+	semanticColors,
+	SublineBold,
+} from '@giveth/ui-design-system';
 import { useIntl } from 'react-intl';
 
 import { Shadow } from '@/components/styled-components/Shadow';
 import ProjectWalletAddress from '@/components/views/project/projectDonations/ProjectWalletAddress';
 import { useProjectContext } from '@/context/project.context';
+import { hasActiveRound } from '@/helpers/qf';
+import { Flex } from '@/components/styled-components/Flex';
 
 const ProjectTotalFundCard: FC = () => {
 	const { projectData } = useProjectContext();
-	const { totalDonations, addresses } = projectData || {};
+	const { totalDonations, addresses, qfRounds } = projectData || {};
 	const { formatMessage } = useIntl();
 	const recipientAddresses = addresses?.filter(a => a.isRecipient);
-
+	const isQFActive = hasActiveRound(qfRounds);
 	return (
 		<Wrapper>
-			<UpperSection>
-				<LeadStyled>
-					{formatMessage({ id: 'label.all_time_funding' })}
-				</LeadStyled>
-				{totalDonations && totalDonations > 0 ? (
-					<TotalFund>{'$' + totalDonations.toFixed(2)}</TotalFund>
-				) : (
-					<NoDonation>
-						{formatMessage({
-							id: 'label.be_the_first_to_donate',
-						})}
-					</NoDonation>
-				)}
-			</UpperSection>
+			{!isQFActive ? (
+				<UpperSection>
+					<LeadStyled>
+						{formatMessage({ id: 'label.all_time_funding' })}
+					</LeadStyled>
+					{totalDonations && totalDonations > 0 ? (
+						<TotalFund>{'$' + totalDonations.toFixed(2)}</TotalFund>
+					) : (
+						<NoDonation>
+							{formatMessage({
+								id: 'label.be_the_first_to_donate',
+							})}
+						</NoDonation>
+					)}
+				</UpperSection>
+			) : (
+				<div>
+					<BorderedFlex>
+						<P>Round: &nbsp;</P>
+						<B>QF round 1 donations</B>
+					</BorderedFlex>
+					{totalDonations && totalDonations > 0 ? (
+						<div>
+							<TotalFund>
+								{'$' + totalDonations.toFixed(2)}
+							</TotalFund>
+							<EstimatedMatchingSection
+								justifyContent='space-between'
+								alignItems='center'
+							>
+								<EstimatedMatchingPrice>
+									+ $2000.00
+								</EstimatedMatchingPrice>
+								<EstematedMatchingText>
+									Estimated Matching
+								</EstematedMatchingText>
+							</EstimatedMatchingSection>
+						</div>
+					) : (
+						<NoDonation>
+							{formatMessage({
+								id: 'label.be_the_first_to_donate',
+							})}
+						</NoDonation>
+					)}
+				</div>
+			)}
+
 			<BottomSection>
 				<P>
 					{formatMessage({
@@ -75,6 +121,30 @@ const UpperSection = styled.div`
 
 const TotalFund = styled(H2)`
 	font-weight: 700;
+`;
+
+const BorderedFlex = styled(Flex)`
+	border-bottom: 1px solid ${neutralColors.gray[300]};
+	padding-bottom: 8px;
+	margin-bottom: 8px;
+`;
+
+const EstimatedMatchingSection = styled(Flex)`
+	background-color: ${neutralColors.gray[200]};
+	padding: 16px 8px;
+	border-radius: 8px;
+	margin-top: 8px;
+`;
+
+const EstimatedMatchingPrice = styled(H5)`
+	color: ${semanticColors.jade[600]};
+	font-weight: 700;
+`;
+
+const EstematedMatchingText = styled(SublineBold)`
+	color: ${semanticColors.jade[600]};
+	font-weight: 600;
+	max-width: 60px;
 `;
 
 export default ProjectTotalFundCard;
