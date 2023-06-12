9aa4ad7192644db742e3facdf2605a3ca7048b8f
alireza-sharifpour <ascode94@gmail.com>
2023-06-12 11:12:39 +0330

    added qf formulas

--- src/helpers/qf.ts
+++ src/helpers/qf.ts
@@ -4,3 +4,32 @@ export const hasActiveRound = (qfRounds: IQFRound[] | undefined) => {
 	if (!qfRounds) return false;
 	return qfRounds.some(round => round.isActive);
 };
+
+export const calculateTotalEstimatedMatching = (
+	projectDonationsSqrtRootSum: number,
+	allProjectsSum: number,
+	matchingPool: number,
+) => {
+	return (
+		(Math.pow(projectDonationsSqrtRootSum, 2) / allProjectsSum) *
+		matchingPool
+	);
+};
+
+export const calculateEstimatedMatchingWithDonationAmount = (
+	projectDonationsSqrtRootSum: number,
+	allProjectsSum: number,
+	matchingPool: number,
+	donationAmount: number,
+) => {
+	const beforeNewDonationPow = Math.pow(projectDonationsSqrtRootSum, 2);
+	const afterNewDonationPow = Math.pow(
+		projectDonationsSqrtRootSum + Math.sqrt(donationAmount),
+		2,
+	);
+	return (
+		(afterNewDonationPow /
+			(allProjectsSum + afterNewDonationPow - beforeNewDonationPow)) *
+		matchingPool
+	);
+};
