865fa7e7d773b6161c591489cef084bdc17a97c6
Cherik <Pourcheriki@gmail.com>
2023-06-13 12:59:44 +0330

    add change network functionality

--- src/components/NetworkSelector.tsx
+++ src/components/NetworkSelector.tsx
@@ -1,12 +1,10 @@
-import { ComponentType, ReactElement, useState } from 'react';
+import { ComponentType, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import {
 	B,
 	IconChevronDown24,
 	IconChevronUp24,
-	IconFlash16,
 	IconNetwork24,
-	IconRocketInSpace16,
 	P,
 	brandColors,
 	neutralColors,
@@ -24,16 +22,29 @@ import { switchNetwork } from '@/lib/wallet';
 import { Flex } from './styled-components/Flex';
 import { ChangeNetworkModal } from './modals/ChangeNetwork';
 import config from '../configuration';
-import { EProjectsSortBy } from '@/apollo/types/gqlEnums';
-interface ISelector {
-	isSelected: boolean;
+import { BasicNetworkConfig } from '@/types/config';
+import NetworkLogo from './NetworkLogo';
+
+export interface ISelected {
+	label: string;
+	value: number;
+	network: BasicNetworkConfig;
+	active: boolean;
 }
 
-export interface ISelectedSort {
-	icon: ReactElement;
-	label: string;
-	value: EProjectsSortBy;
-}
+const _options = [
+	{ network: config.MAINNET_CONFIG, active: true },
+	{ network: config.XDAI_CONFIG, active: true },
+	{ network: config.OPTIMISM_CONFIG, active: true },
+	{ network: config.CELO_CONFIG, active: false },
+];
+
+const options = _options.map(o => ({
+	label: o.network.chainName,
+	value: parseInt(o.network.chainId),
+	network: o.network,
+	active: o.active,
+}));
 
 const DropdownIndicator: ComponentType<DropdownIndicatorProps> = props => {
 	return props.selectProps.menuIsOpen ? (
@@ -43,16 +54,15 @@ const DropdownIndicator: ComponentType<DropdownIndicatorProps> = props => {
 	);
 };
 
-const Option: ComponentType<OptionProps<ISelectedSort>> = props => {
+const Option: ComponentType<OptionProps<ISelected>> = props => {
 	const { data } = props;
-	const { label } = data;
-	const Icon = data.icon;
+	const { value, label } = data;
 
 	return (
 		<components.Option {...props}>
 			<OptionContainer>
 				<RowContainer>
-					{Icon}
+					<NetworkLogo chainId={value} logoSize={24} />
 					<P>{label}</P>
 				</RowContainer>
 			</OptionContainer>
@@ -60,15 +70,19 @@ const Option: ComponentType<OptionProps<ISelectedSort>> = props => {
 	);
 };
 
-const Control: ComponentType<ControlProps<ISelectedSort>> = ({
+const Control: ComponentType<ControlProps<ISelected>> = ({
 	children,
 	...props
 }) => {
+	const value = props.selectProps.value;
 	return (
 		<components.Control {...props}>
-			{props.selectProps.value ? (
+			{value ? (
 				<>
-					{(props.selectProps.value as ISelectedSort).icon}
+					<NetworkLogo
+						chainId={(value as ISelected).value}
+						logoSize={24}
+					/>
 					{children}
 				</>
 			) : (
@@ -81,41 +95,42 @@ const Control: ComponentType<ControlProps<ISelectedSort>> = ({
 const selectStyles: StylesConfig = {
 	container: styles => ({
 		...styles,
+		backgroundColor: brandColors.giv[600],
+		color: neutralColors.gray[100],
 		zIndex: 3,
 		border: 'none',
-		borderRadius: '8px',
+		borderRadius: '32px',
+		minWidth: '200px',
 		'&:hover': {
 			borderColor: 'transparent',
 		},
 	}),
 	control: styles => ({
 		...styles,
+		backgroundColor: brandColors.giv[600],
+		color: neutralColors.gray[100],
 		padding: '12px 16px',
 		border: 'none',
 		boxShadow: 'none',
+		borderRadius: '0 24px 24px 0 ',
 	}),
 	indicatorSeparator: styles => ({
 		...styles,
 		display: 'none',
 	}),
+	placeholder: styles => ({
+		...styles,
+		color: neutralColors.gray[100],
+	}),
+	singleValue: (styles, { data }) => ({
+		...styles,
+		color: neutralColors.gray[100],
+	}),
 };
-
-const sortByOptions = [
-	{
-		label: 'label.givpower',
-		value: EProjectsSortBy.INSTANT_BOOSTING,
-		icon: <IconRocketInSpace16 color={brandColors.deep[900]} />,
-	},
-	{
-		label: 'label.rank',
-		value: EProjectsSortBy.GIVPOWER,
-		icon: <IconFlash16 color={brandColors.deep[900]} />,
-	},
-];
 
 export const NetworkSelector = () => {
 	const [showChangeNetworkModal, setShowChangeNetworkModal] = useState(false);
-	const [value, setValue] = useState(sortByOptions[0]);
+	const [value, setValue] = useState(options[1]);
 	const [targetNetwork, setTargetNetwork] = useState(
 		config.MAINNET_NETWORK_NUMBER,
 	);
@@ -133,6 +148,15 @@ export const NetworkSelector = () => {
 		}
 	};
 
+	useEffect(() => {
+		if (chainId) {
+			const selected = options.find(o => o.value === chainId);
+			if (selected) {
+				setValue(selected);
+			}
+		}
+	}, [chainId]);
+
 	return (
 		<>
 			{chainId ? (
@@ -148,13 +172,13 @@ export const NetworkSelector = () => {
 							Control: (props: any) => <Control {...props} />,
 						}}
 						onChange={(e: any) => {
-							console.log('e.value', e.value);
+							handleChangeNetwork(e.value);
 						}}
 						value={value}
-						options={sortByOptions}
+						options={options}
 						styles={selectStyles}
-						id='sorting'
-						name='sorting'
+						id='network-selector'
+						name='network-selector'
 						isClearable={false}
 						isSearchable={false}
 						isMulti={false}
@@ -206,6 +230,7 @@ const Title = styled(Flex)`
 	padding: 12px 16px;
 	color: ${brandColors.deep[100]};
 	background-color: ${brandColors.giv[900]};
+	border-radius: 24px 0 0 24px;
 `;
 
 const OptionContainer = styled.div`
