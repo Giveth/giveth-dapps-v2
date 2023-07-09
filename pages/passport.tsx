4dc50953afeb4c774ba7d5a2f0e3a601f4718b02
Cherik <Pourcheriki@gmail.com>
2023-07-09 11:36:00 +0330

    translate PassportRoute

--- pages/passport.tsx
+++ pages/passport.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head';
+import { useIntl } from 'react-intl';
 import { PassportView } from '@/components/views/passport/passport.view';
 
 const PassportRoute = () => {
+	const { formatMessage } = useIntl();
+
 	return (
 		<>
 			<Head>
-				<title>Passport Score | Giveth</title>
+				<title>
+					{formatMessage({ id: 'label.passport_score' })} | Giveth
+				</title>
 			</Head>
 			<PassportView />
 		</>
