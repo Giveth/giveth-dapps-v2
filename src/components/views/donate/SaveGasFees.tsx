8a531c06e4245ce2e78a09564d1a7d256c6b470a
Cherik <Pourcheriki@gmail.com>
2023-07-12 14:02:36 +0330

    show switch network modal

--- src/components/views/donate/SaveGasFees.tsx
+++ src/components/views/donate/SaveGasFees.tsx
@@ -1,17 +1,18 @@
 import { Caption, IconGasStation } from '@giveth/ui-design-system';
-import React, { FC } from 'react';
+import { FC, useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { useIntl } from 'react-intl';
 import config from '@/configuration';
-import { switchNetwork } from '@/lib/wallet';
 import { Flex } from '@/components/styled-components/Flex';
 import {
 	NetworkToast,
 	SwitchCaption,
 } from '@/components/views/donate/common.styled';
 import { ISwitchNetworkToast } from '@/components/views/donate/common.types';
+import SwitchNetwork from '@/components/modals/SwitchNetwork';
 
 const SaveGasFees: FC<ISwitchNetworkToast> = ({ acceptedChains }) => {
+	const [showModal, setShowModal] = useState(false);
 	const { chainId } = useWeb3React();
 	const { formatMessage } = useIntl();
 
@@ -33,11 +34,10 @@ const SaveGasFees: FC<ISwitchNetworkToast> = ({ acceptedChains }) => {
 					})}
 				</Caption>
 			</Flex>
-			<SwitchCaption
-				onClick={() => switchNetwork(config.XDAI_NETWORK_NUMBER)}
-			>
+			<SwitchCaption onClick={() => setShowModal(true)}>
 				{formatMessage({ id: 'label.switch_network' })}
 			</SwitchCaption>
+			{showModal && <SwitchNetwork setShowModal={setShowModal} />}
 		</NetworkToast>
 	);
 };
