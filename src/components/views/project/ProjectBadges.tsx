f8d398ab3e6016ca6e81db4ef2b233e9d584b361
alireza-sharifpour <ascode94@gmail.com>
2023-07-13 15:54:17 +0330

    added projectbadge to ActionCard

--- src/components/views/project/ProjectBadges.tsx
+++ src/components/views/project/ProjectBadges.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { IconVerifiedBadge16, semanticColors } from '@giveth/ui-design-system';
+import {
+	IconVerifiedBadge16,
+	brandColors,
+	semanticColors,
+} from '@giveth/ui-design-system';
 import styled from 'styled-components';
 import { Flex } from '@/components/styled-components/Flex';
 import { useProjectContext } from '@/context/project.context';
 import ProjectBadge from './ProjectBadge';
+import { hasActiveRound } from '@/helpers/qf';
 
 const ProjectBadges = () => {
 	const { projectData } = useProjectContext();
 
-	const { verified } = projectData || {};
+	const { verified, qfRounds } = projectData || {};
 
+	const isQF = hasActiveRound(qfRounds);
 	return (
 		<CustomFlex gap='16px'>
 			{verified && (
@@ -17,6 +23,12 @@ const ProjectBadges = () => {
 					badgeText='Verified'
 					wrapperColor={semanticColors.jade[700]}
 					BadgeIcon={<IconVerifiedBadge16 />}
+				/>
+			)}
+			{isQF && (
+				<ProjectBadge
+					badgeText='Quadratic Funding'
+					wrapperColor={brandColors.cyan[600]}
 				/>
 			)}
 		</CustomFlex>
