e2ebf76383ccbbe7c279d0b5bb2b797c9ef0d65a
Vyom Jain <vyom.j@protonmail.com>
2023-07-14 17:31:11 +0530

    fix: remove console log in UserWithPFPInCell.tsx

--- src/components/UserWithPFPInCell.tsx
+++ src/components/UserWithPFPInCell.tsx
@@ -14,16 +14,16 @@ interface IUserWithPFPInCell {
 
 export const UserWithPFPInCell: FC<IUserWithPFPInCell> = ({ user }) => {
 	const pfpToken = useGiverPFPToken(user?.walletAddress, user?.avatar);
-	console.log(addressToUserView(user?.walletAddress?.toLowerCase()));
+	const userProfileLink = addressToUserView(user?.walletAddress?.toLowerCase()) || "";
 	const name =
-		user?.name || shortenAddress(user?.walletAddress?.toLowerCase());
+		user?.name || shortenAddress(user?.walletAddress?.toLowerCase()) || '\u200C';
 	return pfpToken ? (
 		<Flex gap='12px' alignItems='center'>
 			<StyledPFP pfpToken={pfpToken} />
-			<Link href={addressToUserView(user?.walletAddress?.toLowerCase())}>{name || '\u200C'} </Link>
+			<Link href={userProfileLink}>{name} </Link>
 		</Flex>
 	) : (
-		<NoAvatar><Link href={addressToUserView(user?.walletAddress?.toLowerCase())}>{name || '\u200C'} </Link></NoAvatar>
+		<NoAvatar><Link href={userProfileLink}>{name} </Link></NoAvatar>
 	);
 };
 const StyledPFP = styled(PFP)`
