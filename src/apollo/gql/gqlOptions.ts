a24da6d49246b159d68903d1f3c40c002b19b296
Cherik <Pourcheriki@gmail.com>
2023-07-17 13:53:26 +0330

    change OPTIONS_HOME_PROJECTS sort to givpower

--- src/apollo/gql/gqlOptions.ts
+++ src/apollo/gql/gqlOptions.ts
@@ -4,7 +4,7 @@ export const OPTIONS_HOME_PROJECTS = {
 	variables: {
 		limit: 15,
 		skip: 0,
-		sortingBy: EProjectsSortBy.INSTANT_BOOSTING,
+		sortingBy: EProjectsSortBy.GIVPOWER,
 	},
 	notifyOnNetworkStatusChange: true,
 };
