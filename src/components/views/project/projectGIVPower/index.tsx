3042a0799c623ab5536c6fec304ad74a6014418d
Cherik <Pourcheriki@gmail.com>
2023-07-17 12:20:20 +0330

    update ProjectGIVPowerIndex

--- src/components/views/project/projectGIVPower/index.tsx
+++ src/components/views/project/projectGIVPower/index.tsx
@@ -1,9 +1,7 @@
 import { FC, useState } from 'react';
-import BigNumber from 'bignumber.js';
 import { Col, Row } from '@giveth/ui-design-system';
 import GIVPowerTable from './GIVPowerTable';
 import NoBoost from '@/components/views/project/projectGIVPower/NoBoost';
-import { IPowerBoosting } from '@/apollo/types/types';
 import Pagination from '@/components/Pagination';
 import { Flex } from '@/components/styled-components/Flex';
 import LoadingAnimation from '@/animations/loading_giv.json';
@@ -11,15 +9,6 @@ import LottieControl from '@/components/LottieControl';
 import { useProjectContext } from '@/context/project.context';
 import { GIVpowerCard } from './GIVpowerCard';
 
-export interface IPowerBoostingWithUserGIVpower
-	extends Omit<IPowerBoosting, 'user'> {
-	user: {
-		name: string;
-		walletAddress: string;
-		allocated: BigNumber;
-		givpowerBalance: string;
-	};
-}
 interface IProjectGIVPowerIndexProps {}
 
 const itemPerPage = 10;
@@ -28,7 +17,9 @@ const ProjectGIVPowerIndex: FC<IProjectGIVPowerIndexProps> = () => {
 	const [page, setPage] = useState(0);
 
 	const { isBoostingsLoading, boostersData } = useProjectContext();
-	const hasGivPower = boostersData ? boostersData.totalCount > 0 : false;
+	const hasGivPower = boostersData
+		? boostersData?.powerBoostings.length > 0
+		: false;
 
 	if (isBoostingsLoading)
 		return <LottieControl animationData={LoadingAnimation} size={250} />;
@@ -45,7 +36,7 @@ const ProjectGIVPowerIndex: FC<IProjectGIVPowerIndexProps> = () => {
 							) || []
 						}
 						totalPowerBoosting={
-							boostersData?.totalPowerBoosting || '0'
+							boostersData?.totalPowerBoosting || 0
 						}
 					/>
 					<Flex justifyContent='flex-end'>
