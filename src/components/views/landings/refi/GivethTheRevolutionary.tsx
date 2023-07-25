9f2743079811e28aa85144112c86e1a0d4debd39
Ramin <r.ramazanpour@aut.ac.ir>
2023-07-26 00:25:53 +0330

    develop Giveth the revolutionary section

---
+++ src/components/views/landings/refi/GivethTheRevolutionary.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { H4, neutralColors } from '@giveth/ui-design-system';
+
+const GivethTheRevolutionary = () => {
+	return (
+		<Wrapper>
+			<H4>
+				<b>Giveth</b>, the revolutionary crypto fundraising platform,
+				embodies
+				<b> ReFi</b> by leveraging blockchain technology to empower
+				changemakers to contribute to a more equitable and sustainable
+				financial system.
+			</H4>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.div`
+	max-width: 1180px;
+	padding: 120px 30px 80px;
+	margin: 0 auto;
+  color: ${neutralColors.gray[900]}};
+`;
+
+export default GivethTheRevolutionary;
