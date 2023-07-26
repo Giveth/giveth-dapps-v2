07afd6d99e8da68d5d0e484bc50164f4d7ac3988
Ramin <r.ramazanpour@aut.ac.ir>
2023-07-26 17:05:10 +0330

    add particles to GivethTheRevolutionary.tsx

--- src/components/views/landings/refi/GivethTheRevolutionary.tsx
+++ src/components/views/landings/refi/GivethTheRevolutionary.tsx
@@ -1,19 +1,57 @@
 import styled from 'styled-components';
-import { H4, neutralColors } from '@giveth/ui-design-system';
+import { brandColors, H4, neutralColors } from '@giveth/ui-design-system';
+import Wave from '@/components/particles/Wave';
+import QuarterCircle from '@/components/particles/QuarterCircle';
+import Plus from '@/components/particles/Plus';
 
 const GivethTheRevolutionary = () => {
 	return (
-		<Wrapper>
-			<H4>
-				<b>Giveth</b>, the revolutionary crypto fundraising platform,
-				embodies
-				<b> ReFi</b> by leveraging blockchain technology to empower
-				changemakers to contribute to a more equitable and sustainable
-				financial system.
-			</H4>
-		</Wrapper>
+		<OuterWrapper>
+			<Wrapper>
+				<H4>
+					<b>Giveth</b>, the revolutionary crypto fundraising
+					platform, embodies
+					<b> ReFi</b> by leveraging blockchain technology to empower
+					changemakers to contribute to a more equitable and
+					sustainable financial system.
+				</H4>
+			</Wrapper>
+			<WaveWrapper>
+				<Wave color={brandColors.pinky[500]} />
+			</WaveWrapper>
+			<ArcWrapper>
+				<QuarterCircle color={brandColors.ocean[600]} />
+			</ArcWrapper>
+			<PlusWrapper>
+				<Plus color={brandColors.mustard[500]} />
+			</PlusWrapper>
+		</OuterWrapper>
 	);
 };
+
+const PlusWrapper = styled.div`
+	position: absolute;
+	left: 50px;
+	bottom: 60px;
+	display: none;
+`;
+
+const ArcWrapper = styled.div`
+	position: absolute;
+	top: 45px;
+	left: 250px;
+`;
+
+const WaveWrapper = styled.div`
+	position: absolute;
+	right: 0;
+	bottom: 120px;
+	display: none;
+`;
+
+const OuterWrapper = styled.div`
+	position: relative;
+`;
 
 const Wrapper = styled.div`
 	max-width: 1180px;
