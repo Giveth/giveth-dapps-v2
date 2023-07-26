e11eac9270ec3c980b0a040e39f80c91ff9f05de
Cherik <Pourcheriki@gmail.com>
2023-07-26 12:38:47 +0330

    add color and size feature

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -5,10 +5,19 @@ import { FC } from 'react';
 interface IColor {
 	color?: string;
 }
+interface ISize {
+	size?: number;
+}
 
-export const Spinner: FC<IColor> = ({ color = brandColors.giv[500] }) => {
+interface ISpinner extends ISize, IColor {}
+interface IWrappedSpinner extends ISize, IColor {}
+
+export const Spinner: FC<ISpinner> = ({
+	color = brandColors.giv[500],
+	size = 80,
+}) => {
 	return (
-		<Ring color={color}>
+		<Ring color={color} size={size}>
 			<div></div>
 			<div></div>
 			<div></div>
@@ -16,12 +25,6 @@ export const Spinner: FC<IColor> = ({ color = brandColors.giv[500] }) => {
 		</Ring>
 	);
 };
-
-interface ISize {
-	size?: number;
-}
-
-interface IWrappedSpinner extends ISize, IColor {}
 
 export const WrappedSpinner: FC<IWrappedSpinner> = ({
 	size = 700,
@@ -39,23 +42,23 @@ export const SpinnerContainer = styled.div<ISize>`
 	/* min-width: ${({ size }) => size}px; */
 `;
 
-const Ring = styled.div`
+const Ring = styled.div<ISpinner>`
 	display: inline-block;
 	position: relative;
-	width: 80px;
-	height: 80px;
+	width: ${props => props.size}px;
+	height: ${props => props.size}px;
 
 	& div {
 		box-sizing: border-box;
 		display: block;
 		position: absolute;
-		width: 64px;
-		height: 64px;
-		margin: 8px;
-		border: 8px solid ${brandColors.giv[500]};
+		width: ${props => props.size! - props.size! / 5}px;
+		height: ${props => props.size! - props.size! / 5}px;
+		margin: ${props => props.size! / 10}px;
+		border: ${props => props.size! / 10}px solid ${props => props.color};
 		border-radius: 50%;
 		animation: ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-		border-color: ${brandColors.giv[500]} transparent transparent
+		border-color: ${props => props.color} transparent transparent
 			transparent;
 	}
 	& div:nth-child(1) {
