1916a339499bc62e778b861dc15f50d5119adfa4
alireza-sharifpour <ascode94@gmail.com>
2023-07-26 13:01:18 +0330

    Added walletAddressInput to CreateProjectAddAddressModal

--- src/components/views/create/CreateProjectAddAddressModal.tsx
+++ src/components/views/create/CreateProjectAddAddressModal.tsx
@@ -2,13 +2,22 @@ import { IconWalletOutline24 } from '@giveth/ui-design-system';
 import React from 'react';
 import { Modal } from '@/components/modals/Modal';
 import { useModalAnimation } from '@/hooks/useModalAnimation';
+import WalletAddressInput from './WalletAddressInput';
 
 interface ICreateProjectAddAddressModal {
 	setShowModal: (show: boolean) => void;
+	networkId: number;
+	isActive: boolean;
+	setIsActive: (active: boolean) => void;
+	userAddresses: string[];
 }
 
 const CreateProjectAddAddressModal = ({
 	setShowModal,
+	networkId,
+	isActive,
+	setIsActive,
+	userAddresses,
 }: ICreateProjectAddAddressModal) => {
 	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
 
@@ -20,7 +29,13 @@ const CreateProjectAddAddressModal = ({
 			headerTitle='Add an Address'
 			headerIcon={<IconWalletOutline24 />}
 		>
-			CreateProjectAddAddressModal
+			<WalletAddressInput
+				networkId={networkId}
+				isActive={isActive}
+				setIsActive={setIsActive}
+				userAddresses={userAddresses}
+				setResolvedENS={() => {}}
+			/>
 		</Modal>
 	);
 };
