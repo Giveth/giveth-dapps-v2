e29f8dde9920e291806558769fb5188fedbc66f0
Ramin <r.ramazanpour@aut.ac.ir>
2023-07-31 18:02:37 +0330

    add header to index

--- src/components/views/landings/publicGoods/index.tsx
+++ src/components/views/landings/publicGoods/index.tsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+import { neutralColors } from '@giveth/ui-design-system';
+import PublicGoodsHeader from '@/components/views/landings/publicGoods/header';
+
 const PublicGoods = () => {
-	return <div>Public Goods</div>;
+	return (
+		<Wrapper>
+			<PublicGoodsHeader />
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	background: ${neutralColors.gray[200]};
+	color: ${neutralColors.gray[900]};
+`;
 
 export default PublicGoods;
