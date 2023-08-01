304e92c65e9e973e5c917c61ae1a043c7b8ed4e0
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-01 16:31:20 +0330

    add common styles

---
+++ src/components/views/landings/publicGoods/common.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const PublicGoodsOuterWrapper = styled.div`
+	background: white;
+	position: relative;
+`;
+
+export const PublicGoodsWrapper = styled.div`
+	max-width: 1200px;
+	padding: 40px;
+	margin: 0 auto;
+`;
