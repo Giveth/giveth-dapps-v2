0b4214d388ddaa441c8d390e89ee2230db912b16
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-01 16:16:07 +0330

    add GivethIsTheOnly to index

--- src/components/views/landings/publicGoods/index.tsx
+++ src/components/views/landings/publicGoods/index.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { neutralColors } from '@giveth/ui-design-system';
 import PublicGoodsHeader from '@/components/views/landings/publicGoods/header';
+import GivethIsTheOnly from '@/components/views/landings/publicGoods/GivethIsTheOnly';
 
 const PublicGoods = () => {
 	return (
 		<Wrapper>
 			<PublicGoodsHeader />
+			<GivethIsTheOnly />
 		</Wrapper>
 	);
 };
