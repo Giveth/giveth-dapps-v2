b2a6473c31fc2dfe879783c59b7351923461ce4e
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-10 01:24:02 +0330

    add mouse hover state and styles

--- src/components/views/userProfile/projectsTab/ProjectActions.tsx
+++ src/components/views/userProfile/projectsTab/ProjectActions.tsx
@@ -1,10 +1,12 @@
 import {
 	brandColors,
 	GLink,
+	IconChevronDown24,
 	IconChevronUp24,
 	neutralColors,
 } from '@giveth/ui-design-system';
 import styled from 'styled-components';
+import { useState } from 'react';
 import { IProject } from '@/apollo/types/types';
 import { EProjectStatus } from '@/apollo/types/gqlEnums';
 
@@ -12,10 +14,19 @@ const ProjectActions = (props: { project: IProject }) => {
 	const { project } = props;
 	const status = project.status.name;
 	const isCancelled = status === EProjectStatus.CANCEL;
+
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
-		<Actions size='Big' isCancelled={isCancelled}>
+		<Actions
+			onMouseEnter={() => setIsOpen(true)}
+			onMouseLeave={() => setIsOpen(false)}
+			isOpen={isOpen}
+			size='Big'
+			isCancelled={isCancelled}
+		>
 			<div>Actions</div>
-			<IconChevronUp24 />
+			{isOpen ? <IconChevronUp24 /> : <IconChevronDown24 />}
 			{/*<InternalLink*/}
 			{/*	href={idToProjectEdit(project.id)}*/}
 			{/*	title={formatMessage({*/}
@@ -48,12 +59,15 @@ const CustomGlink = styled(GLink)`
 	padding-top: 2px;
 `;
 
-const Actions = styled(GLink)<{ isCancelled: boolean }>`
+const Actions = styled(GLink)<{ isCancelled: boolean; isOpen: boolean }>`
+	padding: 4px 16px;
+	border-radius: 8px;
 	display: flex;
 	gap: 4px;
 	color: ${props =>
 		props.isCancelled ? brandColors.pinky[200] : neutralColors.gray[900]};
 	cursor: ${props => (props.isCancelled ? 'default' : 'pointer')};
+	${props => props.isOpen && 'background: white;'}
 `;
 
 export default ProjectActions;
