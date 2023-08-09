e7d5150804d906743dbb5e1d3843f197db4dc47f
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-09 12:24:16 +0330

    Remove Gnosis Safe option from Sign in modal

--- src/lib/wallet/walletTypes.ts
+++ src/lib/wallet/walletTypes.ts
@@ -1,13 +1,13 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
-import { SafeAppConnector } from '@gnosis.pm/safe-apps-web3-react';
+// import { SafeAppConnector } from '@gnosis.pm/safe-apps-web3-react';
 // import { PortisConnector } from '@web3-react/portis-connector';
 // import { FortmaticConnector } from '@web3-react/fortmatic-connector';
 // import { AuthereumConnector } from '@web3-react/authereum-connector';
 import metamaskIcon from '/public//images/wallets/metamask.svg';
 // import walletConnectIcon from '/public//images/wallets/walletconnect.svg';
 import torusIcon from '/public//images/wallets/torus.svg';
-import gnosisIcon from '/public//images/wallets/gnosis.svg';
+// import gnosisIcon from '/public//images/wallets/gnosis.svg';
 // import authereumIcon from '/public//images/wallets/authereum.svg';
 // import portisIcon from '/public//images/wallets/portis.svg';
 // import fortmaticIcon from '/public//images/wallets/fortmatic.svg';
@@ -29,7 +29,7 @@ export const walletconnectConnector = new WalletConnectConnector({
 		[config.XDAI_NETWORK_NUMBER]: config.XDAI_CONFIG.nodeUrl,
 	},
 });
-export const gnosisSafeConnector = new SafeAppConnector();
+// export const gnosisSafeConnector = new SafeAppConnector();
 // export const portisConnector = new PortisConnector({
 // 	dAppId: process.env.PORTIS_DAPP_ID as string,
 // 	networks: [1, 3, 100],
@@ -48,19 +48,17 @@ export type TWalletConnector =
 	// | PortisConnector
 	// | FortmaticConnector
 	// | AuthereumConnector,
-	| InjectedConnector
-	| WalletConnectConnector
-	| TorusConnector
-	| SafeAppConnector;
+	// | SafeAppConnector
+	InjectedConnector | WalletConnectConnector | TorusConnector;
 
 export enum EWallets {
 	METAMASK = 'metamask',
 	WALLETCONNECT = 'wallet connect',
-	PORTIS = 'portis',
-	FORTMATIC = 'fortmatic',
+	// PORTIS = 'portis',
+	// FORTMATIC = 'fortmatic',
 	TORUS = 'torus',
-	AUTHEREUM = 'authereum',
-	GNOSISSAFE = 'gnosis safe',
+	// AUTHEREUM = 'authereum',
+	// GNOSISSAFE = 'gnosis safe',
 }
 
 export interface IWallet {
@@ -92,12 +90,12 @@ export const walletsArray: IWallet[] = [
 	// 	connector: walletconnectConnector,
 	// },
 	torusWallet,
-	{
-		name: 'GnosisSafe',
-		value: EWallets.GNOSISSAFE,
-		image: gnosisIcon,
-		connector: gnosisSafeConnector,
-	},
+	// {
+	// 	name: 'GnosisSafe',
+	// 	value: EWallets.GNOSISSAFE,
+	// 	image: gnosisIcon,
+	// 	connector: gnosisSafeConnector,
+	// },
 	// {
 	// 	name: 'Portis',
 	// 	value: EWallets.PORTIS,
@@ -123,7 +121,7 @@ export const useWalletName = (Web3ReactContext: Web3ReactContextInterface) => {
 	if (connector instanceof WalletConnectConnector)
 		return EWallets.WALLETCONNECT;
 	if (connector instanceof TorusConnector) return EWallets.TORUS;
-	if (connector instanceof SafeAppConnector) return EWallets.GNOSISSAFE;
+	// if (connector instanceof SafeAppConnector) return EWallets.GNOSISSAFE;
 	// if (connector instanceof PortisConnector) return EWallets.PORTIS;
 	// if (connector instanceof FortmaticConnector) return EWallets.FORTMATIC;
 	// if (connector instanceof AuthereumConnector) return EWallets.AUTHEREUM;
