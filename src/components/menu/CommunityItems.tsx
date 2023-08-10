1ed660425b6d065749cfff8312be221db797b1dc
Cherik <Pourcheriki@gmail.com>
2023-08-10 13:49:27 +0330

    update CommunityItems

--- src/components/menu/CommunityItems.tsx
+++ src/components/menu/CommunityItems.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Link from 'next/link';
 import { GLink } from '@giveth/ui-design-system';
 import { useIntl } from 'react-intl';
@@ -8,53 +8,69 @@ import Routes from '@/lib/constants/Routes';
 import { Item } from './Item';
 import links from '@/lib/constants/links';
 
+const communityItems = [
+	{
+		title: 'label.get_a',
+		label: 'label.givers_nft',
+		href: Routes.NFT,
+	},
+	{
+		title: 'label.learn_how_to',
+		label: 'label.join_us',
+		href: Routes.Join,
+	},
+	{
+		title: 'label.discover_our',
+		label: 'label.mission',
+		href: links.OUR_MISSION,
+		isExternal: true,
+	},
+];
+
 export const CommunityItems = () => {
+	return (
+		<>
+			{communityItems.map((item, idx) =>
+				item.isExternal ? (
+					<a
+						key={idx}
+						href={item.href}
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<CommunityItem item={item} />
+					</a>
+				) : (
+					<Link key={idx} href={item.href}>
+						<CommunityItem item={item} />
+					</Link>
+				),
+			)}
+		</>
+	);
+};
+
+interface ICommunityItemProps {
+	item: {
+		title: string;
+		label: string;
+		href: string;
+		isExternal?: boolean;
+	};
+}
+
+export const CommunityItem: FC<ICommunityItemProps> = ({ item }) => {
 	const theme = useAppSelector(state => state.general.theme);
 	const { formatMessage } = useIntl();
 
-	const handleClick = (e: React.MouseEvent, outsideLink?: boolean) => {
-		if (outsideLink) {
-			e.stopPropagation();
-		}
-	};
-
-	const communityItems = [
-		{
-			title: formatMessage({ id: 'label.get_a' }),
-			label: formatMessage({ id: 'label.givers_nft' }),
-			href: Routes.NFT,
-		},
-		{
-			title: formatMessage({ id: 'label.learn_how_to' }),
-			label: formatMessage({ id: 'label.join_us' }),
-			href: Routes.Join,
-		},
-		{
-			title: formatMessage({ id: 'label.discover_our' }),
-			label: formatMessage({ id: 'label.mission' }),
-			href: links.OUR_MISSION,
-			outsideLink: true,
-		},
-	];
-
 	return (
-		<>
-			{communityItems.map((item, idx) => (
-				<Link
-					key={idx}
-					href={item.href}
-					target={item.outsideLink ? '_blank' : '_self'}
-					rel={item.outsideLink ? 'noopener noreferrer' : ''} //For security reasons
-					onClick={e => handleClick(e, item.outsideLink)}
-				>
-					<Item theme={theme}>
-						<ItemTitle theme={theme}>{item.title}</ItemTitle>
-						<ItemRow>
-							<GLink>{item.label}</GLink>
-						</ItemRow>
-					</Item>
-				</Link>
-			))}
-		</>
+		<Item theme={theme}>
+			<ItemTitle theme={theme}>
+				{formatMessage({ id: item.title })}
+			</ItemTitle>
+			<ItemRow>
+				<GLink>{formatMessage({ id: item.label })}</GLink>
+			</ItemRow>
+		</Item>
 	);
 };
