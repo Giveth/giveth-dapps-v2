84b87bc77f21f104660caeda9bcd751c34c040d0
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-17 16:07:37 +0330

    remove chain hard coding from fetchPrice

--- src/services/token.ts
+++ src/services/token.ts
@@ -1,28 +1,9 @@
 import { captureException } from '@sentry/nextjs';
-import { initializeApollo } from '@/apollo/apolloClient';
 import config from '@/configuration';
-
-const apolloClient = initializeApollo();
 
 export const fetchPrice = async (chainId: number, tokenAddress?: string) => {
 	try {
-		let chain = 'ethereum';
-		switch (chainId) {
-			case config.CELO_NETWORK_NUMBER:
-				chain = 'celo';
-				break;
-			case config.OPTIMISM_NETWORK_NUMBER:
-				chain = 'optimistic-ethereum';
-				break;
-			case config.POLYGON_NETWORK_NUMBER:
-				chain = 'polygon-pos';
-				break;
-			case config.XDAI_NETWORK_NUMBER:
-				chain = 'xdai';
-				break;
-			default:
-				break;
-		}
+		const chain = config.NETWORKS_CONFIG[chainId || 1].coingeckoChainName;
 		const fetchCall = await fetch(
 			`https://api.coingecko.com/api/v3/simple/token_price/${chain}?contract_addresses=${tokenAddress}&vs_currencies=usd`,
 		);
