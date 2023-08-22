8c18b4f5c58e0bbc357ade53c12478b09a4b477b
Kilter <mateodaza@gmail.com>
2023-08-22 13:00:34 -0500

    Update RewardItems.tsx

--- src/components/menu/RewardItems.tsx
+++ src/components/menu/RewardItems.tsx
@@ -115,7 +115,7 @@ export const RewardItems: FC<IRewardItemsProps> = ({
 			);
 		}
 	}, [currentValues, chainId, givTokenDistroHelper]);
-	console.log({ farmsLiquidPart });
+
 	return (
 		<>
 			<Item theme={theme}>
