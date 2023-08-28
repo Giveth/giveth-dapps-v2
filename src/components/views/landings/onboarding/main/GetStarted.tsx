c06c0b5035d45dc808d175b15de2af951bbaffa1
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-28 20:02:00 +0330

    Add GetStarted

---
+++ src/components/views/landings/onboarding/main/GetStarted.tsx
@@ -0,0 +1,23 @@
+import { Button, H5 } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+import { OnboardingHeaderWrapper } from '@/components/views/landings/onboarding/common.styled';
+import Input from '@/components/styled-components/Input';
+
+const GetStarted = () => {
+	return (
+		<OnboardingHeaderWrapperStyled>
+			<H5 weight={700}>Get started with our Giveth onboarding guide</H5>
+			<Input />
+			<Button size='small' label='Discover the Future of Giving' />
+		</OnboardingHeaderWrapperStyled>
+	);
+};
+
+const OnboardingHeaderWrapperStyled = styled(OnboardingHeaderWrapper)`
+	margin: 64px auto 74px;
+	> * {
+		margin-bottom: 16px;
+	}
+`;
+
+export default GetStarted;
