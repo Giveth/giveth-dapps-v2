3675a05e574ed3232920e8308e8324979d4100b5
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-29 17:56:45 +0330

    Add VerticalNumberedSteps to StepByStep

--- src/components/views/landings/onboarding/main/StepByStep.tsx
+++ src/components/views/landings/onboarding/main/StepByStep.tsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 import { H3, H5 } from '@giveth/ui-design-system';
 import { OnboardingWrapper } from '@/components/views/landings/onboarding/common.styled';
+import VerticalNumberedSteps from '@/components/VerticalNumberedSteps';
+import mainSteps from '@/components/views/landings/onboarding/main/steps';
 
 const StepByStep = () => {
 	return (
 		<Wrapper>
-			<H5 weight={700}>A step-by-step approach</H5>
-			<H3 weight={700}>Ready to give Giveth a whirl?</H3>
+			<div>
+				<H5 weight={700}>A step-by-step approach</H5>
+				<H3 weight={700}>Ready to give Giveth a whirl?</H3>
+			</div>
+			<VerticalNumberedSteps inputArray={mainSteps} />
 		</Wrapper>
 	);
 };
@@ -14,6 +19,9 @@ const StepByStep = () => {
 const Wrapper = styled(OnboardingWrapper)`
 	margin: 75px auto 130px;
 	text-align: center;
+	> div:first-child {
+		margin-bottom: 32px;
+	}
 `;
 
 export default StepByStep;
