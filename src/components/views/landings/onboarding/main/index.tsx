2f8ed630ef2ad32ea6643a60109014cee569718e
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-30 14:27:45 +0330

    fix OnboardingMainIndex styles

--- src/components/views/landings/onboarding/main/index.tsx
+++ src/components/views/landings/onboarding/main/index.tsx
@@ -1,19 +1,31 @@
+import { neutralColors } from '@giveth/ui-design-system';
+import styled from 'styled-components';
 import OnboardingMainHeader from '@/components/views/landings/onboarding/main/Header';
 import GetStarted from '@/components/views/landings/onboarding/main/GetStarted';
 import WhatIsGiveth from '@/components/views/landings/onboarding/main/WhatIsGiveth';
 import WhatIsGIV from '@/components/views/landings/onboarding/main/WhatIsGIV';
 import StepByStep from '@/components/views/landings/onboarding/main/StepByStep';
+import Divider from '@/components/Divider';
+import ExploringMore from '@/components/views/landings/onboarding/main/ExploringMore';
 
 const OnboardingMainIndex = () => {
 	return (
-		<>
+		<Wrapper>
 			<OnboardingMainHeader />
 			<GetStarted />
 			<WhatIsGiveth />
 			<WhatIsGIV />
 			<StepByStep />
-		</>
+			<Divider height='29px' color={neutralColors.gray[200]} />
+			<ExploringMore />
+			<Divider height='29px' color={neutralColors.gray[200]} />
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled.div`
+	background: white;
+	padding-top: 40px;
+`;
 
 export default OnboardingMainIndex;
