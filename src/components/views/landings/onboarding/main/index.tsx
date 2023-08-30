10c869eec63d4be35b3de70603f52a61b37d33e9
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-30 16:16:25 +0330

    add new components to OnboardingMainIndex

--- src/components/views/landings/onboarding/main/index.tsx
+++ src/components/views/landings/onboarding/main/index.tsx
@@ -7,6 +7,9 @@ import WhatIsGIV from '@/components/views/landings/onboarding/main/WhatIsGIV';
 import StepByStep from '@/components/views/landings/onboarding/main/StepByStep';
 import Divider from '@/components/Divider';
 import ExploringMore from '@/components/views/landings/onboarding/main/ExploringMore';
+import GivethIsHelping from '@/components/views/landings/onboarding/main/GivethIsHelping';
+import WhatElse from '@/components/views/landings/onboarding/main/WhatElse';
+import JoinOurCommunity from '@/components/views/landings/onboarding/main/JoinOurCommunity';
 
 const OnboardingMainIndex = () => {
 	return (
@@ -19,6 +22,10 @@ const OnboardingMainIndex = () => {
 			<Divider height='29px' color={neutralColors.gray[200]} />
 			<ExploringMore />
 			<Divider height='29px' color={neutralColors.gray[200]} />
+			<GivethIsHelping />
+			<Divider height='29px' color={neutralColors.gray[200]} />
+			<WhatElse />
+			<JoinOurCommunity />
 		</Wrapper>
 	);
 };
@@ -26,6 +33,7 @@ const OnboardingMainIndex = () => {
 const Wrapper = styled.div`
 	background: white;
 	padding-top: 40px;
+	padding-bottom: 90px;
 `;
 
 export default OnboardingMainIndex;
