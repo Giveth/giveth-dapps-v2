14f8594bfb66af37661c00fe2e4f6327540de78e
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-30 21:50:41 +0330

    add QuarterCircle to WhatIsGIV

--- src/components/views/landings/onboarding/main/WhatIsGIV.tsx
+++ src/components/views/landings/onboarding/main/WhatIsGIV.tsx
@@ -11,40 +11,53 @@ import { OnboardingWrapper } from '@/components/views/landings/onboarding/common
 import Routes from '@/lib/constants/Routes';
 import InternalLink from '@/components/InternalLink';
 import GivPic from 'public/images/onboard/two-giv.svg';
+import { Relative } from '@/components/styled-components/Position';
+import QuarterCircle from '@/components/particles/QuarterCircle';
 
 const WhatIsGIV = () => {
 	return (
-		<OnboardingWrapperStyled>
-			<Image src={GivPic} alt='Giv' />
-			<Content>
-				<H3 weight={700}>What is GIV?</H3>
-				<LeadStyled size='large'>
-					Giveth is powered by our native cryptocurrency token - GIV.
-					You can buy give on the open market, or you can get GIV as a
-					reward for donating to{' '}
+		<Relative>
+			<OnboardingWrapperStyled>
+				<Image src={GivPic} alt='Giv' />
+				<Content>
+					<H3 weight={700}>What is GIV?</H3>
+					<LeadStyled size='large'>
+						Giveth is powered by our native cryptocurrency token -
+						GIV. You can buy give on the open market, or you can get
+						GIV as a reward for donating to{' '}
+						<InternalLink
+							href={Routes.Projects}
+							title='verified projects'
+							color={brandColors.pinky[500]}
+						/>{' '}
+						(through our{' '}
+						<InternalLink
+							href={Routes.GIVbacks}
+							title='GIVbacks'
+							color={brandColors.pinky[500]}
+						/>{' '}
+						program)
+					</LeadStyled>
 					<InternalLink
-						href={Routes.Projects}
-						title='verified projects'
 						color={brandColors.pinky[500]}
-					/>{' '}
-					(through our{' '}
-					<InternalLink
-						href={Routes.GIVbacks}
-						title='GIVbacks'
-						color={brandColors.pinky[500]}
-					/>{' '}
-					program)
-				</LeadStyled>
-				<InternalLink
-					color={brandColors.pinky[500]}
-					href='https://docs.giveth.io/blog/welcomeGIVeconomy/#the-giv-token'
-				>
-					<ButtonText>Read MORE ABOUT GIV HERE</ButtonText>
-				</InternalLink>
-			</Content>
-		</OnboardingWrapperStyled>
+						href='https://docs.giveth.io/blog/welcomeGIVeconomy/#the-giv-token'
+					>
+						<ButtonText>Read MORE ABOUT GIV HERE</ButtonText>
+					</InternalLink>
+				</Content>
+			</OnboardingWrapperStyled>
+			<ParticleWrapper>
+				<QuarterCircle color={brandColors.pinky[500]} />
+			</ParticleWrapper>
+		</Relative>
 	);
 };
+
+const ParticleWrapper = styled.div`
+	position: absolute;
+	top: 0;
+	right: 39px;
+`;
 
 const Content = styled.div`
 	max-width: 750px;
