743cbd41186918d33c9afc8c4f7cc0281023e208
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-30 16:17:23 +0330

    fix OnboardingCard styles

--- src/components/OnboardingCard.tsx
+++ src/components/OnboardingCard.tsx
@@ -5,11 +5,13 @@ import {
 	H3,
 	IconArrowRight32,
 	Lead,
+	mediaQueries,
 	neutralColors,
 } from '@giveth/ui-design-system';
 import { Shadow } from '@/components/styled-components/Shadow';
 import Divider from '@/components/Divider';
 import ExternalLink from '@/components/ExternalLink';
+import { FlexCenter } from '@/components/styled-components/Flex';
 
 interface IOnboardingCard {
 	icon: JSX.Element;
@@ -28,7 +30,7 @@ const OnboardingCard = (props: IOnboardingCard) => {
 			<Divider color={neutralColors.gray[300]} height='1px' />
 			<Lead size='medium'>{description}</Lead>
 			<ExternalLink href={buttonLink} color={brandColors.pinky[500]}>
-				<Button
+				<ButtonStyled
 					label={buttonText}
 					buttonType='texty-primary'
 					icon={<IconArrowRight32 />}
@@ -38,18 +40,24 @@ const OnboardingCard = (props: IOnboardingCard) => {
 	);
 };
 
-const Wrapper = styled.div`
+const ButtonStyled = styled(Button)`
+	padding-left: 0;
+	padding-right: 0;
+`;
+
+const Wrapper = styled(FlexCenter)`
 	box-shadow: ${Shadow.Neutral[400]};
 	border-radius: 12px;
 	background: ${neutralColors.gray[100]};
-	padding: 32px;
-	display: flex;
+	padding: 32px 16px;
 	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+	text-align: center;
 	gap: 16px;
 	max-width: 450px;
 	flex: 1 1 0px;
+	${mediaQueries.tablet} {
+		padding: 32px;
+	}
 `;
 
 export default OnboardingCard;
