0659902cb7e56f066d3149583904ca660fcb22d9
Cherik <Pourcheriki@gmail.com>
2023-08-31 14:26:38 +0330

    fix StakingPoolInfoAndActions

--- src/components/cards/StakingCards/BaseStakingCard/StakingPoolInfoAndActions.tsx
+++ src/components/cards/StakingCards/BaseStakingCard/StakingPoolInfoAndActions.tsx
@@ -18,7 +18,6 @@ import { avgAPR } from '@/helpers/givpower';
 import { BN, formatEthHelper, formatWeiHelper } from '@/helpers/number';
 import {
 	PoolStakingConfig,
-	RegenNetworkConfig,
 	RegenPoolStakingConfig,
 	SimplePoolStakingConfig,
 	StakingType,
@@ -118,9 +117,7 @@ export const StakingPoolInfoAndActions: FC<IStakingPoolInfoAndActionsProps> = ({
 		provideLiquidityLink,
 	} = poolStakingConfig;
 	const regenStreamConfig = regenStreamType
-		? (
-				config.NETWORKS_CONFIG[poolNetwork] as RegenNetworkConfig
-		  ).regenStreams.find(
+		? config.NETWORKS_CONFIG[poolNetwork].regenStreams?.find(
 				regenStream => regenStream.type === regenStreamType,
 		  )
 		: undefined;
