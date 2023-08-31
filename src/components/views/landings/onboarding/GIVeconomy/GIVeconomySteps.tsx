0f6475fc5776f27908445b3c0b001e0768d93f74
Ramin <r.ramazanpour@aut.ac.ir>
2023-09-01 00:26:03 +0330

    add GIVeconomySteps

---
+++ src/components/views/landings/onboarding/GIVeconomy/GIVeconomySteps.tsx
@@ -0,0 +1,55 @@
+const GIVeconomySteps = [
+	{
+		title: 'Explore the GIVeconomy',
+		description:
+			'Giveth is more than just a platform, because of crypto and web3, we have our own economy! Explore the ways in which you can interact with the Giveth community and join the Future of Giving.',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/_ULHxC5jwrg?si=XhF5UlvPK-Un5otu'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+	{
+		title: 'GIVBacks: Get Rewarded for Donating',
+		description:
+			'Not only does Giveth take zero fees for all donations, but we actually reward donors for donating! Learn about our revolutionary GIVbacks program, and how you can earn GIV tokens when you donate to a verified project on Giveth.',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/X8_UDMmJZyE?si=0kSEqeFvBX5TsUvk'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+	{
+		title: 'Get a GIVstream',
+		description:
+			"Wouldn't it be cool if you could get constant rewards for donating? That's exactly what our GIVstream does. When you donate to a verified project on Giveth, you'll get a constant stream of GIV tokens until December 23, 2026!",
+		buttonText: 'VIEW OUR ENTIRE GIVeconomy SERIES',
+		buttonLink:
+			'https://www.youtube.com/watch?v=-JXwwIGJGQQ&list=PL4Artm1rmCWH4Q5XnrQWf8fm0xob3hbdZ',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/2ZpYkTg3huc?si=nSkBIBzwHU-cMZAT'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+];
+
+export default GIVeconomySteps;
