c080ed57ee72b97ffb82059dfa8c0dafef52106f
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-31 23:51:36 +0330

    update OnboardingOwnerIndex

--- src/components/views/landings/onboarding/project-owner/index.tsx
+++ src/components/views/landings/onboarding/project-owner/index.tsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 import { neutralColors } from '@giveth/ui-design-system';
 import OnboardingHeader from '@/components/views/landings/onboarding/common/Header';
-import BecomeProjectOwner from '@/components/views/landings/onboarding/project-owner/BecomeProjectOwner';
+import MainContentCreator from '@/components/views/landings/onboarding/common/MainContentCreator';
 import Divider from '@/components/Divider';
 import WhatElse from '@/components/views/landings/onboarding/common/WhatElse';
 import {
 	donorCard,
 	GIVeconomyCard,
 } from '@/components/views/landings/onboarding/common/cards';
+import projectOwnerSteps from '@/components/views/landings/onboarding/project-owner/projectOwnerSteps';
+import Routes from '@/lib/constants/Routes';
 
 const OnboardingOwnerIndex = () => {
 	return (
@@ -16,11 +18,20 @@ const OnboardingOwnerIndex = () => {
 				title='Get Started as a Project owner'
 				subtitle='Create project, promote it and raise donations'
 			/>
-			<BecomeProjectOwner />
+			<MainContentCreator {...content} />
 			<Divider height='29px' color={neutralColors.gray[200]} />
 			<WhatElse cards={[donorCard, GIVeconomyCard]} />
 		</Wrapper>
 	);
+};
+
+const content = {
+	title: 'Become a project Owner on Giveth',
+	description:
+		'We may be biased, but we really believe that Giveth is the best donation platform in the world. When you raise funds for your project on Giveth, you receive 100% of every cent that was donated to you since Giveth takes zero fees. Not only that, but you can also earn rewards when you refer others to donate on Giveth! Check out our resources below to learn how to quickly and easily launch your project on Giveth and join us in the Future of Giving',
+	steps: projectOwnerSteps,
+	buttonLink: Routes.CreateProject,
+	buttonText: 'CREATE PROJECT',
 };
 
 const Wrapper = styled.div`
