4f6563c70d831b814cc8466ef8cf71fc26d95ad5
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-31 23:50:16 +0330

    add donorSteps

---
+++ src/components/views/landings/onboarding/donor/donorSteps.tsx
@@ -0,0 +1,55 @@
+const donorSteps = [
+	{
+		title: 'Connect a Web3 Wallet',
+		description:
+			"If you already have a web3 wallet, all you have to do is connect on our site. If you're new to web3, the easiest way to get started is to create an Ethereum web3 wallet (like MetaMask).",
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/TLlebSlRDyo?si=nDHLLPSgcdHoq_Ql'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+	{
+		title: 'Fund Your Wallet',
+		description:
+			'Next up, try donating to a project on Giveth! We’ve got over 1,800 to choose from in categories ranging from Disaster Relief, Art & Culture, Education, Health & Wellness and more!',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/O-Vq7i4FqOw?si=AlndqmD49bUQtkyY'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+	{
+		title: 'Donate to a Project',
+		description:
+			'We have thousands of projects to choose from in dozens of categories. Find a project or cause you believe in from our projects page and donate. Remember 100% of everything you donate goes directly to the project!',
+		buttonText: 'VIEW OUR ENTIRE DONOR ONBOARDING SERIES',
+		buttonLink:
+			'https://www.youtube.com/watch?v=-JXwwIGJGQQ&list=PL4Artm1rmCWH4Q5XnrQWf8fm0xob3hbdZ&ab_channel=Giveth',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/UlaXjiamHZk?si=i5JehwWrxC8Mt3RL'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+];
+
+export default donorSteps;
