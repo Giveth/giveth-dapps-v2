3de0e37139bf995d933360b353330ed169f7ae19
alireza-sharifpour <ascode94@gmail.com>
2023-08-31 11:42:47 +0330

    updated SwitchToAcceptedChain to use wagmi

--- src/components/views/donate/SwitchToAcceptedChain.tsx
+++ src/components/views/donate/SwitchToAcceptedChain.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
-import { useWeb3React } from '@web3-react/core';
 import { useIntl } from 'react-intl';
 import { Caption } from '@giveth/ui-design-system';
+import { useChainId } from 'wagmi';
 import { getNetworkNames } from '@/components/views/donate/helpers';
 import { switchNetwork } from '@/lib/wallet';
 import {
@@ -11,8 +11,8 @@ import {
 import { ISwitchNetworkToast } from '@/components/views/donate/common.types';
 
 const SwitchToAcceptedChain: FC<ISwitchNetworkToast> = ({ acceptedChains }) => {
-	const { chainId } = useWeb3React();
 	const { formatMessage } = useIntl();
+	const chainId = useChainId();
 
 	if (!chainId || !acceptedChains || acceptedChains?.includes(chainId)) {
 		return null;
