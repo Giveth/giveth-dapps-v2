411dcd4a4bff5fd2ae6ef74a3e08944bb75e9d6a
Ramin <r.ramazanpour@aut.ac.ir>
2023-08-31 16:02:33 +0330

    add projectOwnerSteps

---
+++ src/components/views/landings/onboarding/project-owner/projectOwnerSteps.tsx
@@ -0,0 +1,55 @@
+const projectOwnerSteps = [
+	{
+		title: 'Set up Your Giveth Project',
+		description:
+			'Learn how to set up your own project on Giveth, so you can start earning funds straight away with zero fees and join of the Future of Giving.',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/sOtvDCAqV88?si=XgPE98l3M1BnzSp2'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+	{
+		title: 'Get Your Project Verified',
+		description:
+			'It is possible to have a project on Giveth without being verified. However, there are some pretty good benefits to getting verified, including greater legitimacy and more rewards for your donors. Our verification process is simply, straightforward and human.',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/8myBgJCKXiU?si=kTxDYJBD969pMAXC'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+	{
+		title: 'Promote Your Giveth Project',
+		description:
+			'Learn how to make your project stand out so you can get as many donors and supporters to your project as possible both from your network as well as the Giveth community.',
+		buttonText: 'VIEW OUR ENTIRE PROJECT ONBOARDING SERIES',
+		buttonLink:
+			'https://www.youtube.com/watch?v=QgC05uGTaZo&list=PL4Artm1rmCWFi-qEkOtjl9nL4tojSIIKm&ab_channel=Giveth',
+		element: (
+			<iframe
+				width='100%'
+				height='100%'
+				src='https://www.youtube.com/embed/S6B9bf0GMpk?si=Nk9h1u28XBx5A3Rk'
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			/>
+		),
+	},
+];
+
+export default projectOwnerSteps;
