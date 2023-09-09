356e19813df769287d6b0995758071e69e0f21d2
Ramin <r.ramazanpour@aut.ac.ir>
2023-09-09 19:54:04 +0330

    add YouKnowHow to GIVBacksIndex

--- src/components/views/landings/GIVBacks/index.tsx
+++ src/components/views/landings/GIVBacks/index.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import GetUpdates from '@/components/GetUpdates';
 import GIVBacksHeader from '@/components/views/landings/GIVBacks/GIVBacksHeader';
+import YouKnowHow from '@/components/views/landings/GIVBacks/YouKnowHow';
 
 const GIVBacksIndex = () => {
 	return (
 		<Wrapper>
 			<GIVBacksHeader />
+			<YouKnowHow />
 			<GetUpdates />
 		</Wrapper>
 	);
