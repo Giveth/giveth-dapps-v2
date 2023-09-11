7c2085f94ef1468a335dc9b028d2b7a6c1ec053e
Ramin <r.ramazanpour@aut.ac.ir>
2023-09-11 15:28:47 +0330

    update GIVBacksIndex

--- src/components/views/landings/GIVBacks/index.tsx
+++ src/components/views/landings/GIVBacks/index.tsx
@@ -7,6 +7,10 @@ import Divider from '@/components/Divider';
 import WhatAreGIVbacks from '@/components/views/landings/GIVBacks/WhatAreGIVbacks';
 import OfTheDonors from '@/components/views/landings/GIVBacks/OfTheDonors';
 import WhatMakesGIVbacks from '@/components/views/landings/GIVBacks/WhatMakesGIVbacks';
+import HowGIVbacksWork from '@/components/views/landings/GIVBacks/HowGIVbacksWork';
+import WhatCanYouDo from '@/components/views/landings/GIVBacks/WhatCanYouDo';
+import JoinTheRevolution from '@/components/views/landings/GIVBacks/JoinTheRevolution';
+import GiveAndGetGIVback from '@/components/views/landings/GIVBacks/GiveAndGetGIVback';
 
 const GIVBacksIndex = () => {
 	return (
@@ -19,6 +23,15 @@ const GIVBacksIndex = () => {
 			<OfTheDonors />
 			<Divider color={neutralColors.gray[200]} height='40px' />
 			<WhatMakesGIVbacks />
+			<Divider color={neutralColors.gray[200]} height='40px' />
+			<HowGIVbacksWork />
+			<Divider color={neutralColors.gray[200]} height='40px' />
+			<WhatCanYouDo />
+			<Divider color={neutralColors.gray[200]} height='40px' />
+			<JoinTheRevolution />
+			<Divider color={neutralColors.gray[200]} height='40px' />
+			<GiveAndGetGIVback />
+			<Divider color={neutralColors.gray[200]} height='100px' />
 			<GetUpdates />
 		</Wrapper>
 	);
