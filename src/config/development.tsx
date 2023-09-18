721b5e0baaf5338b68ea89fde82a941658ee71b7
Cherik <Pourcheriki@gmail.com>
2023-09-18 15:14:29 +0330

    update development

--- src/config/development.tsx
+++ src/config/development.tsx
@@ -1,4 +1,3 @@
-import { parseUnits } from '@ethersproject/units';
 import React from 'react';
 import {
 	EnvConfig,
@@ -122,8 +121,8 @@ const config: EnvConfig = {
 		nodeUrl: networksParams[100]?.rpcUrls[0],
 		...networksParams[100],
 		gasPreference: {
-			maxFeePerGas: parseUnits('2', 'gwei').toString(),
-			maxPriorityFeePerGas: parseUnits('1', 'gwei').toString(),
+			maxFeePerGas: (2e9).toString(),
+			maxPriorityFeePerGas: (1e9).toString(),
 		},
 
 		blockExplorerName: ['Blockscout'],
