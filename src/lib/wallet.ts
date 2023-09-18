ed028c93f438a32ff285966f3821f2b643c5dead
Cherik <Pourcheriki@gmail.com>
2023-09-18 20:36:41 +0330

    remove walletconnect

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -1,32 +1,4 @@
-import { captureException } from '@sentry/nextjs';
 import { fetchEnsAddress } from '@wagmi/core';
-import {
-	EWallets,
-	torusConnector,
-	walletconnectConnector,
-} from '@/lib/wallet/walletTypes';
-import { switchNetwork as metamaskSwitchNetwork } from '@/lib/metamask';
-import StorageLabel from '@/lib/localStorage';
-import { showToastError } from './helpers';
-
-// @DEV it's not tested yet! didn't have a multi-chain wallet to test
-const switchWalletConnectNetwork = async (chainId: number) => {
-	try {
-		await walletconnectConnector?.walletConnectProvider?.connector?.updateSession(
-			{
-				chainId,
-				accounts: [],
-			},
-		);
-	} catch (switchError: any) {
-		console.error(switchError);
-		captureException(switchError, {
-			tags: {
-				section: 'switchWalletConnectNetwork',
-			},
-		});
-	}
-};
 
 export function isAddressENS(ens: string | undefined) {
 	if (!ens) return false;
