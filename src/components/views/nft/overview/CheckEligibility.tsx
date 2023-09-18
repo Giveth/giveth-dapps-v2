efeba375d98caeb69c1651f21579a3dbf461b8a4
Cherik <Pourcheriki@gmail.com>
2023-09-18 14:28:17 +0330

    use wagmi on checkAddress

--- src/components/views/nft/overview/CheckEligibility.tsx
+++ src/components/views/nft/overview/CheckEligibility.tsx
@@ -7,53 +7,41 @@ import {
 	QuoteText,
 	semanticColors,
 } from '@giveth/ui-design-system';
-import { Contract } from 'ethers';
 import React, { ChangeEvent, useState } from 'react';
 import styled from 'styled-components';
-import { useWeb3React } from '@web3-react/core';
-import { utils } from 'ethers';
-import { JsonRpcProvider } from '@ethersproject/providers';
 import { abi as PFP_ABI } from '@/artifacts/pfpGiver.json';
 import config from '@/configuration';
 import { getAddressFromENS, isAddressENS, switchNetwork } from '@/lib/wallet';
 import { Flex } from '@/components/styled-components/Flex';
 import EligibilityModal from './EligibilityModal';
 import { GiversPFP } from '@/types/contracts';
+import { useChainId } from 'wagmi';
+import { getContract } from 'wagmi/dist/actions';
 
 const CheckEligibility = () => {
-	const { library, chainId } = useWeb3React();
 	const [walletAddress, setWalletAddress] = useState('');
 	const [error, setError] = useState('');
 	const [showModal, setShowModal] = useState(false);
 	const [status, setStatus] = useState<boolean | undefined>();
+
+	const chainId = useChainId();
+
 	const onAddressChange = (event: ChangeEvent<HTMLInputElement>) => {
 		setWalletAddress(event.target.value);
 	};
 
 	const checkAddress = async (address: string) => {
-		if (utils.isAddress(address)) {
-			try {
-				const _provider =
-					chainId === config.MAINNET_NETWORK_NUMBER
-						? library
-						: new JsonRpcProvider(config.MAINNET_CONFIG.nodeUrl);
-
-				const PFPContract = new Contract(
-					config.MAINNET_CONFIG.PFP_CONTRACT_ADDRESS ?? '',
-					PFP_ABI,
-					_provider,
-				) as GiversPFP;
-				const res = await PFPContract.allowList(address);
-				console.log(res);
-				res === true ? setStatus(true) : setStatus(false);
-				setShowModal(true);
-			} catch (error) {
-				setError('Cannot get data');
-				console.log('ErrorRRR', error);
-			}
-		} else {
-			setError('Address is not valid');
-			console.log('Address is Not valid');
+		try {
+			const pfpContract = getContract({
+				address: config.MAINNET_CONFIG.PFP_CONTRACT_ADDRESS,
+				abi: PFP_ABI,
+			});
+			const res = await pfpContract.read.allowList([address]);
+			res === true ? setStatus(true) : setStatus(false);
+			setShowModal(true);
+		} catch (error) {
+			setError('Cannot get data');
+			console.log('ErrorRRR', error);
 		}
 	};
 
