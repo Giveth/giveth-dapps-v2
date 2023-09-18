bbc1a4bb17a2498a20efda3bc5c327381463885b
Cherik <Pourcheriki@gmail.com>
2023-09-18 14:32:20 +0330

    use wagmi on usePurpleList

--- src/hooks/usePurpleList.ts
+++ src/hooks/usePurpleList.ts
@@ -1,11 +1,11 @@
-import { useWeb3React } from '@web3-react/core';
 import { useEffect, useState } from 'react';
 import { CHECK_PURPLE_LIST } from '@/apollo/gql/gqlPurpleList';
 import { ICheckPurpleListGQL } from '@/apollo/types/gqlTypes';
 import { backendGQLRequest } from '@/helpers/requests';
+import { useAccount } from 'wagmi';
 
 export default function usePurpleList() {
-	const { account: address } = useWeb3React();
+	const { address } = useAccount();
 
 	const [isPurpleList, setIsPurpleList] = useState(false);
 
