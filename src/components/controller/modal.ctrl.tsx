d95c8fa3daaf6ef36f091e26d09a5dff73630947
Cherik <Pourcheriki@gmail.com>
2023-09-21 16:17:44 +0330

    remove showWalletModal from ModalController

--- src/components/controller/modal.ctrl.tsx
+++ src/components/controller/modal.ctrl.tsx
@@ -22,7 +22,6 @@ const ModalController = () => {
 		showCompleteProfile,
 		showFirstWelcomeModal,
 		showSignWithWallet,
-		showWalletModal,
 		showWelcomeModal,
 		showSearchModal,
 		showSwitchNetwork,
