f12b80af16c8e7aef4536c558be1fd2e8c32e240
Cherik <Pourcheriki@gmail.com>
2023-09-29 15:00:12 +0330

    add donate button

--- src/components/views/project/projectDonations/NoDonation.tsx
+++ src/components/views/project/projectDonations/NoDonation.tsx
@@ -1,14 +1,22 @@
-import { H1, brandColors } from '@giveth/ui-design-system';
+import { ButtonLink, H1, brandColors } from '@giveth/ui-design-system';
+import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
+import { slugToProjectDonate } from '@/lib/routeCreators';
+import { useProjectContext } from '@/context/project.context';
 
 export const NoDonation = () => {
+	const { projectData } = useProjectContext();
+	const { slug } = projectData || {};
 	return (
 		<Wrapper>
 			<H1>Be the</H1>
 			<H1 weight={700}>First</H1>
 			<H1>to</H1>
 			<H1 weight={700}>Donate</H1>
+			<Link href={slugToProjectDonate(slug || '')}>
+				<DonateButton linkType='primary' label='donate' />
+			</Link>
 		</Wrapper>
 	);
 };
@@ -18,4 +26,10 @@ const Wrapper = styled.div`
 	background-image: url('/images/backgrounds/giv-background.svg');
 	color: ${brandColors.giv[500]};
 	text-align: center;
+`;
+
+const DonateButton = styled(ButtonLink)`
+	margin-top: 40px;
+	display: inline-block;
+	min-width: 260px;
 `;
