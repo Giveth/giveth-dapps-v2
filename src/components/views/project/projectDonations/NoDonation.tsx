2046fc06c31594461a0d965feb939218661979fd
Cherik <Pourcheriki@gmail.com>
2023-09-29 14:53:11 +0330

    add content to NoDonation

--- src/components/views/project/projectDonations/NoDonation.tsx
+++ src/components/views/project/projectDonations/NoDonation.tsx
@@ -1,5 +1,21 @@
+import { H1, brandColors } from '@giveth/ui-design-system';
 import React from 'react';
+import styled from 'styled-components';
 
 export const NoDonation = () => {
-	return <div>NoDonation</div>;
+	return (
+		<Wrapper>
+			<H1>Be the</H1>
+			<H1 weight={700}>First</H1>
+			<H1>to</H1>
+			<H1 weight={700}>Donate</H1>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	padding: 60px;
+	background-image: url('/images/backgrounds/giv-background.svg');
+	color: ${brandColors.giv[500]};
+	text-align: center;
+`;
