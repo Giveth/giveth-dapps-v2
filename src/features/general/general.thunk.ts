cbededad6ea83ec80e96758e27bdd24b82f9ff02
Cherik <Pourcheriki@gmail.com>
2023-10-01 10:07:24 +0330

    add fetchQFRounds

--- src/features/general/general.thunk.ts
+++ src/features/general/general.thunk.ts
@@ -1,10 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { print } from 'graphql';
 import { backendGQLRequest } from '@/helpers/requests';
 import { FETCH_MAIN_CATEGORIES } from './genera.queries';
+import { FETCH_QF_ROUNDS } from '@/apollo/gql/gqlQF';
 
 export const fetchMainCategories = createAsyncThunk(
 	'general/fetchMainCategories',
 	async () => {
 		return backendGQLRequest(FETCH_MAIN_CATEGORIES);
+	},
+);
+
+export const fetchQFRounds = createAsyncThunk(
+	'general/fetchQFRounds',
+	async () => {
+		return backendGQLRequest(print(FETCH_QF_ROUNDS));
 	},
 );
