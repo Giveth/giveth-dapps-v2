7f1376b32a62e9d8edef892ec5d3fe7c0b31a05b
alireza-sharifpour <ascode94@gmail.com>
2023-10-01 13:56:53 +0330

    fix chainNameById function

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -3,5 +3,5 @@ import config from '@/configuration';
 export const chainNameById = (chainId?: number) => {
 	const unknown = 'Unknown';
 	if (!chainId) return unknown;
-	return config.NETWORKS_CONFIG[chainId]?.name || unknown;
+	return config.NETWORKS_CONFIG[chainId]?.chainName || unknown;
 };
