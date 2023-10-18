0c8b43c703e2f4bb56aba902ad09493280bf7a33
Cherik <Pourcheriki@gmail.com>
2023-10-18 16:37:31 +0330

    show complete amount number

--- src/components/AmountInput.tsx
+++ src/components/AmountInput.tsx
@@ -24,6 +24,7 @@ export const AmountInput: FC<IAmountInput> = ({
 }) => {
 	const { formatMessage } = useIntl();
 	const [displayAmount, setDisplayAmount] = useState('');
+	const [test, setTest] = useState(0n);
 	const [activeStep, setActiveStep] = useState(0);
 
 	const setAmountPercentage = useCallback(
@@ -33,6 +34,7 @@ export const AmountInput: FC<IAmountInput> = ({
 				.div(100)
 				.toFixed(0);
 			setAmount(BigInt(newAmount));
+			setTest(BigInt(newAmount));
 			setDisplayAmount(formatWeiHelper(newAmount, undefined, false));
 		},
 		[maxAmount, setAmount],
@@ -136,6 +138,9 @@ export const AmountInput: FC<IAmountInput> = ({
 					100%
 				</Step>
 			</FiltersRow>
+			<div>
+				{test.toString()}
+			</div>
 		</>
 	);
 };
