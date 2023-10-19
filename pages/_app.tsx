89b3c4a45a654350817569d21f7a7d971cd4525e
Mitch Oz <divine_comedian@protonmail.com>
2023-10-19 12:44:40 -0600

    testing torus &rainbow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -102,9 +102,12 @@ createWeb3Modal({
 	chains,
 	featuredWalletIds: [
 		'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+		'3fecad5e2f0a30aba97edea69ebf015884a9b8a9aec93e66d4b4b695fee1f010',
 	],
 	includeWalletIds: [
 		'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+		'3fecad5e2f0a30aba97edea69ebf015884a9b8a9aec93e66d4b4b695fee1f010',
+		'1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369',
 	],
 });
 
