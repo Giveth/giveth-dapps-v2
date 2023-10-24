9cf37ad973e4ca51559c4ca65a12a7860270d5b4
Cherik <Pourcheriki@gmail.com>
2023-10-24 22:54:49 +0330

    init RecurringSection

--- src/components/views/donate/RecurringDonationCard.tsx
+++ src/components/views/donate/RecurringDonationCard.tsx
@@ -1,4 +1,5 @@
 import {
+	B,
 	Caption,
 	H6,
 	brandColors,
@@ -6,6 +7,7 @@ import {
 } from '@giveth/ui-design-system';
 import React from 'react';
 import styled from 'styled-components';
+import { Flex } from '@/components/styled-components/Flex';
 
 export const RecurringDonationCard = () => {
 	return (
@@ -20,6 +22,11 @@ export const RecurringDonationCard = () => {
 				Provide continuous funding by streaming your donations over
 				time.
 			</Desc>
+			<RecurringSection>
+				<RecurringSectionTitle>
+					Creating a Monthly recurring donation
+				</RecurringSectionTitle>
+			</RecurringSection>
 		</>
 	);
 };
@@ -34,5 +41,21 @@ const Desc = styled(Caption)`
 	background-color: ${neutralColors.gray[200]};
 	padding: 8px;
 	width: 100%;
+	text-align: left;
+`;
+
+const RecurringSection = styled(Flex)`
+	flex-direction: column;
+	gap: 24px;
+	padding: 16px;
+	border-radius: 12px;
+	border: 1px solid ${neutralColors.gray[300]};
+	width: 100%;
+`;
+
+const RecurringSectionTitle = styled(B)`
+	width: 100%;
+	padding-bottom: 8px;
+	border-bottom: 1px solid ${neutralColors.gray[300]};
 	text-align: left;
 `;
