18194059cf264ef61cfcd40007b9ccd0f0c5c962
Cherik <Pourcheriki@gmail.com>
2023-10-24 19:02:13 +0330

    add tabs

--- src/components/views/donate/DonationCard.tsx
+++ src/components/views/donate/DonationCard.tsx
@@ -1,17 +1,34 @@
-import { B, neutralColors } from '@giveth/ui-design-system';
-import React from 'react';
-import styled from 'styled-components';
+import { B, P, neutralColors } from '@giveth/ui-design-system';
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
 import { Shadow } from '@/components/styled-components/Shadow';
+import { Flex } from '@/components/styled-components/Flex';
+
+const tabs = ['One-Time Donation', 'Recurring Donation'];
 
 export const DonationCard = () => {
+	const [tab, setTab] = useState(1);
 	return (
 		<DonationCardWrapper>
 			<Title>How do you want to donate?</Title>
+			<Flex>
+				{tabs.map((_tab, idx) => (
+					<Tab
+						key={idx}
+						selected={idx === tab}
+						onClick={() => setTab(idx)}
+					>
+						{_tab}
+					</Tab>
+				))}
+			</Flex>
 		</DonationCardWrapper>
 	);
 };
 
-const DonationCardWrapper = styled.div`
+const DonationCardWrapper = styled(Flex)`
+	flex-direction: column;
+	gap: 16px;
 	padding: 24px;
 	border-radius: 16px;
 	background: ${neutralColors.gray[100]};
@@ -21,4 +38,26 @@ const DonationCardWrapper = styled.div`
 const Title = styled(B)`
 	color: ${neutralColors.gray[800]};
 	text-align: left;
+`;
+
+interface ITab {
+	selected?: boolean;
+}
+
+const Tab = styled(P)<ITab>`
+	padding: 8px 12px;
+	border-bottom: 1px solid;
+	cursor: pointer;
+	${props =>
+		props.selected
+			? css`
+					font-weight: 500;
+					color: ${neutralColors.gray[900]};
+					border-bottom-color: ${neutralColors.gray[900]};
+			  `
+			: css`
+					font-weight: 400;
+					color: ${neutralColors.gray[700]};
+					border-bottom-color: ${neutralColors.gray[300]};
+			  `}
 `;
