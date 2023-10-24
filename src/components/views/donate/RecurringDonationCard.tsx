b57c04c3b8495539ececd43dce028a1515ef1727
Cherik <Pourcheriki@gmail.com>
2023-10-24 22:25:54 +0330

    init RecurringDonationCard

---
+++ src/components/views/donate/RecurringDonationCard.tsx
@@ -0,0 +1,36 @@
+import {
+	Caption,
+	H6,
+	brandColors,
+	neutralColors,
+} from '@giveth/ui-design-system';
+import React from 'react';
+import styled from 'styled-components';
+
+export const RecurringDonationCard = () => {
+	return (
+		<>
+			<Title weight={700}>
+				Make a recurring donation with{' '}
+				<a href='https://www.superfluid.finance/' target='_blank'>
+					SuperFluid
+				</a>
+			</Title>
+			<Desc>
+				Provide continuous funding by streaming your donations over
+				time.
+			</Desc>
+		</>
+	);
+};
+
+const Title = styled(H6)`
+	& > a {
+		color: ${brandColors.pinky[500]};
+	}
+`;
+
+const Desc = styled(Caption)`
+	background-color: ${neutralColors.gray[200]};
+	padding: 8px;
+`;
