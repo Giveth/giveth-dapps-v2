0f72165d3cf582a6646554083f4670fe671c7db4
alireza-sharifpour <ascode94@gmail.com>
2023-10-26 17:28:03 +0800

    Added some parts to ProjectItem

--- src/components/views/userProfile/projectsTab/ProjectItem.tsx
+++ src/components/views/userProfile/projectsTab/ProjectItem.tsx
@@ -1,7 +1,16 @@
-import { neutralColors } from '@giveth/ui-design-system';
+import {
+	H1,
+	IconPublish16,
+	Subline,
+	neutralColors,
+} from '@giveth/ui-design-system';
 import React from 'react';
 import styled from 'styled-components';
+import { useIntl } from 'react-intl';
 import { IProject } from '@/apollo/types/types';
+import { Flex } from '@/components/styled-components/Flex';
+import StatusBadge from './StatusBadge';
+import { smallFormatDate } from '@/lib/helpers';
 
 interface IProjectItem {
 	project: IProject;
@@ -9,7 +18,27 @@ interface IProjectItem {
 
 const ProjectItem = ({ project }: IProjectItem) => {
 	console.log('project', project);
-	return <ProjectContainer>{project?.title}</ProjectContainer>;
+	const { formatMessage, locale } = useIntl();
+
+	return (
+		<ProjectContainer>
+			<Flex gap='16px' alignItems='center'>
+				<StatusBadge status={project.status.name!}></StatusBadge>
+				<Subline>
+					<Flex alignItems='center'>
+						<IconPublish16 />
+						&nbsp;
+						{formatMessage({ id: 'label.created_at' })} &nbsp;
+						{smallFormatDate(
+							new Date(project.creationDate!),
+							locale,
+						)}
+					</Flex>
+				</Subline>
+			</Flex>
+			<H1>{project.title}</H1>
+		</ProjectContainer>
+	);
 };
 
 const ProjectContainer = styled.div`
