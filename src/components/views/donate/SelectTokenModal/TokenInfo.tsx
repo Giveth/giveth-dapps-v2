bcf3306c8ebb0366a519180771234323d55c989b
Cherik <Pourcheriki@gmail.com>
2023-10-26 15:21:05 +0330

    update token info styles

--- src/components/views/donate/SelectTokenModal/TokenInfo.tsx
+++ src/components/views/donate/SelectTokenModal/TokenInfo.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
+import { Caption, neutralColors } from '@giveth/ui-design-system';
+import styled from 'styled-components';
 import { IToken } from '@/types/config';
 import { Flex } from '@/components/styled-components/Flex';
+import { TokenIcon } from '../TokenIcon';
 
 interface ITokenInfoProps {
 	token: IToken;
@@ -9,8 +12,34 @@ interface ITokenInfoProps {
 
 export const TokenInfo: FC<ITokenInfoProps> = ({ token, balance }) => {
 	return (
-		<Flex>
-			<Flex></Flex>
-		</Flex>
+		<Wrapper gap='16px' alignItems='center'>
+			<TokenIcon symbol={token.symbol} size={32} />
+			<InfoWrapper flexDirection='column' alignItems='flex-start'>
+				<TopRow justifyContent='space-between'>
+					<Caption medium>{token.symbol}</Caption>
+					<Flex gap='4px'>
+						<Caption medium>{balance.toString()}</Caption>
+						<GrayCaption>{token.symbol}</GrayCaption>
+					</Flex>
+				</TopRow>
+				<GrayCaption>{token.name}</GrayCaption>
+			</InfoWrapper>
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled(Flex)`
+	padding: 4px 8px;
+`;
+
+const InfoWrapper = styled(Flex)`
+	flex: 1;
+`;
+
+const TopRow = styled(Flex)`
+	width: 100%;
+`;
+
+const GrayCaption = styled(Caption)`
+	color: ${neutralColors.gray[700]};
+`;
