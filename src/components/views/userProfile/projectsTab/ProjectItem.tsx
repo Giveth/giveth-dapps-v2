1d6f807e4f2f3a5c04e8ea4a0f72aa6dc37ad712
alireza-sharifpour <ascode94@gmail.com>
2023-11-01 19:04:33 +0800

    Added new parts to ProjectItem

--- src/components/views/userProfile/projectsTab/ProjectItem.tsx
+++ src/components/views/userProfile/projectsTab/ProjectItem.tsx
@@ -1,6 +1,7 @@
 import {
 	H1,
 	IconPublish16,
+	P,
 	Subline,
 	neutralColors,
 } from '@giveth/ui-design-system';
@@ -11,6 +12,7 @@ import { IProject } from '@/apollo/types/types';
 import { Flex } from '@/components/styled-components/Flex';
 import StatusBadge from './StatusBadge';
 import { smallFormatDate } from '@/lib/helpers';
+import { VerifiedBadge } from '@/components/badges/VerifiedBadge';
 
 interface IProjectItem {
 	project: IProject;
@@ -37,6 +39,36 @@ const ProjectItem = ({ project }: IProjectItem) => {
 				</Subline>
 			</Flex>
 			<H1>{project.title}</H1>
+			<HorizontalDivider />
+			<Flex>
+				<Flex flexDirection='column' gap='8px'>
+					<P>
+						{formatMessage({
+							id: 'label.verification_status',
+						})}
+					</P>
+					{/* Should change after new design is ready */}
+					<VerifiedBadge />
+				</Flex>
+				<VerticalDivider />
+				<Flex flexDirection='column' gap='8px'>
+					<P>Listed on public site</P>
+					{/* Should change after new design is ready */}
+					<VerifiedBadge />
+				</Flex>
+				<VerticalDivider />
+
+				<Flex flexDirection='column' gap='8px'>
+					<P>
+						{formatMessage({
+							id: 'label.quadratic_funding',
+						})}
+					</P>
+					{/* Should change after new design is ready */}
+					<VerifiedBadge />
+					<VerticalDivider />
+				</Flex>
+			</Flex>
 		</ProjectContainer>
 	);
 };
@@ -44,6 +76,23 @@ const ProjectItem = ({ project }: IProjectItem) => {
 const ProjectContainer = styled.div`
 	padding: 24px;
 	background-color: ${neutralColors.gray[100]};
+`;
+
+const HorizontalDivider = styled.hr`
+	border: 1px solid ${neutralColors.gray[300]};
+	margin: 24px 0;
+`;
+
+const VerticalDivider = styled.div`
+	width: 1px;
+
+	background-color: ${neutralColors.gray[300]};
+	flex-shrink: 0;
+	margin: 10px 20px;
+`;
+
+const FlexContainer = styled.div`
+	flex: 1;
 `;
 
 export default ProjectItem;
