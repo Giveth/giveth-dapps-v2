f0acd81915eba8cf4e60008a9d45b13187d5038d
alireza-sharifpour <ascode94@gmail.com>
2023-11-02 18:44:26 +0800

    Created bottom part of ProjectItem

--- src/components/views/userProfile/projectsTab/ProjectItem.tsx
+++ src/components/views/userProfile/projectsTab/ProjectItem.tsx
@@ -1,5 +1,7 @@
 import {
 	H1,
+	IconFund24,
+	IconHeartOutline24,
 	IconPublish16,
 	P,
 	Subline,
@@ -10,9 +12,7 @@ import styled from 'styled-components';
 import { useIntl } from 'react-intl';
 import { IProject } from '@/apollo/types/types';
 import { Flex } from '@/components/styled-components/Flex';
-import StatusBadge from './StatusBadge';
 import { smallFormatDate } from '@/lib/helpers';
-import { VerifiedBadge } from '@/components/badges/VerifiedBadge';
 
 interface IProjectItem {
 	project: IProject;
@@ -25,7 +25,6 @@ const ProjectItem = ({ project }: IProjectItem) => {
 	return (
 		<ProjectContainer>
 			<Flex gap='16px' alignItems='center'>
-				<StatusBadge status={project.status.name!}></StatusBadge>
 				<Subline>
 					<Flex alignItems='center'>
 						<IconPublish16 />
@@ -40,34 +39,61 @@ const ProjectItem = ({ project }: IProjectItem) => {
 			</Flex>
 			<H1>{project.title}</H1>
 			<HorizontalDivider />
-			<Flex>
-				<Flex flexDirection='column' gap='8px'>
-					<P>
-						{formatMessage({
-							id: 'label.verification_status',
-						})}
-					</P>
-					{/* Should change after new design is ready */}
-					<VerifiedBadge />
-				</Flex>
-				<VerticalDivider />
-				<Flex flexDirection='column' gap='8px'>
-					<P>Listed on public site</P>
-					{/* Should change after new design is ready */}
-					<VerifiedBadge />
-				</Flex>
-				<VerticalDivider />
-
-				<Flex flexDirection='column' gap='8px'>
-					<P>
-						{formatMessage({
-							id: 'label.quadratic_funding',
-						})}
-					</P>
-					{/* Should change after new design is ready */}
-					<VerifiedBadge />
-					<VerticalDivider />
-				</Flex>
+			<Flex justifyContent='space-between'>
+				<ProjectStatusesContainer>
+					<Flex justifyContent='space-between'>
+						<P>{formatMessage({ id: 'label.project_status' })}</P>
+						<div>456</div>
+					</Flex>
+					<Flex justifyContent='space-between'>
+						<P>Listed on public site</P>
+						<div>124312312412</div>
+					</Flex>
+					<Flex justifyContent='space-between'>
+						<P>
+							{formatMessage({
+								id: 'label.verification_status',
+							})}
+						</P>
+						<div>123123123</div>
+					</Flex>
+					<Flex justifyContent='space-between'>
+						<div>
+							{formatMessage({
+								id: 'label.quadratic_funding',
+							})}
+						</div>
+						<div>123123123</div>
+					</Flex>
+				</ProjectStatusesContainer>
+				<ProjectStatusesContainer>
+					<Flex justifyContent='space-between'>
+						<P>
+							<Flex alignItems='center' gap='6px'>
+								<IconHeartOutline24 />
+								{formatMessage({ id: 'label.likes' })}
+							</Flex>
+						</P>
+						<div>{project.totalReactions}</div>
+					</Flex>
+					<Flex justifyContent='space-between'>
+						<P>
+							<Flex alignItems='center' gap='6px'>
+								<IconFund24 />
+								{formatMessage({ id: 'label.total_raised' })}
+							</Flex>
+						</P>
+						<div>{project.sumDonationValueUsd}</div>
+					</Flex>
+					<Flex justifyContent='space-between'>
+						<P>
+							<Flex alignItems='center' gap='6px'>
+								Claim Recurring Donations
+							</Flex>
+						</P>
+						<div>{project.sumDonationValueUsd}</div>
+					</Flex>
+				</ProjectStatusesContainer>
 			</Flex>
 		</ProjectContainer>
 	);
@@ -85,14 +111,13 @@ const HorizontalDivider = styled.hr`
 
 const VerticalDivider = styled.div`
 	width: 1px;
-
 	background-color: ${neutralColors.gray[300]};
 	flex-shrink: 0;
 	margin: 10px 20px;
 `;
 
-const FlexContainer = styled.div`
-	flex: 1;
+const ProjectStatusesContainer = styled.div`
+	width: 330px;
 `;
 
 export default ProjectItem;
