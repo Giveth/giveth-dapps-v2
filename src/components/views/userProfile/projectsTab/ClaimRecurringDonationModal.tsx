b5cd44259f2c846eee9654ac9335b98b9715a9bc
alireza-sharifpour <ascode94@gmail.com>
2023-11-04 16:01:01 +0800

    Created ClaimRecurringDonationModal

---
+++ src/components/views/userProfile/projectsTab/ClaimRecurringDonationModal.tsx
@@ -0,0 +1,25 @@
+import { IProject } from '@/apollo/types/types';
+import { Modal } from '@/components/modals/Modal';
+import { useModalAnimation } from '@/hooks/useModalAnimation';
+import { IModal } from '@/types/common';
+
+interface IClaimRecurringDonationModal extends IModal {
+	project: IProject;
+}
+
+const ClaimRecurringDonationModal = ({
+	setShowModal,
+	project,
+}: IClaimRecurringDonationModal) => {
+	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
+
+	return (
+		<Modal
+			closeModal={closeModal}
+			isAnimating={isAnimating}
+			headerTitle='Claimable Donations'
+		></Modal>
+	);
+};
+
+export default ClaimRecurringDonationModal;
