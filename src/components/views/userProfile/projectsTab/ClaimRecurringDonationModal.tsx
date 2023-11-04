d54996b08b33061007a46ab9f429b9b9decc33bf
alireza-sharifpour <ascode94@gmail.com>
2023-11-04 20:47:56 +0800

    Added superfluid logo

--- src/components/views/userProfile/projectsTab/ClaimRecurringDonationModal.tsx
+++ src/components/views/userProfile/projectsTab/ClaimRecurringDonationModal.tsx
@@ -1,5 +1,12 @@
-import { B, P, brandColors, neutralColors } from '@giveth/ui-design-system';
+import {
+	B,
+	Button,
+	P,
+	brandColors,
+	neutralColors,
+} from '@giveth/ui-design-system';
 import styled from 'styled-components';
+import Image from 'next/image';
 import { IProject } from '@/apollo/types/types';
 import { IconEthereum } from '@/components/Icons/Eth';
 import { IconPolygon } from '@/components/Icons/Polygon';
@@ -14,12 +21,12 @@ interface IClaimRecurringDonationModal extends IModal {
 
 const recurringDonationItems = [
 	{
-		icon: <IconEthereum size={32} />,
+		icon: <IconEthereum size={24} />,
 		value: '0.5 ETH',
 		usdValue: '$1,000',
 	},
 	{
-		icon: <IconPolygon size={32} />,
+		icon: <IconPolygon size={24} />,
 		value: '0.5 MATIC',
 		usdValue: '$1,000',
 	},
@@ -57,7 +64,28 @@ const ClaimRecurringDonationModal = ({
 							<ClaimButton>Claim tokens</ClaimButton>
 						</ItemContainer>
 					))}
+					<TotalAmountContainer>
+						<Flex justifyContent='space-between'>
+							<B>Total amount claimable </B>
+							<B>~945 USD</B>
+						</Flex>
+					</TotalAmountContainer>
+					<Button label='Claim All Tokens' />
+					<Button
+						label='Cancel'
+						buttonType='texty-gray'
+						onClick={() => setShowModal(false)}
+					/>
 				</Flex>
+				<SuperfluidLogoContainer gap='15px'>
+					<P>Streams powered by </P>{' '}
+					<Image
+						src='/images/logo/superfluid-logo.png'
+						width={120}
+						height={30}
+						alt='Superfluid logo'
+					/>
+				</SuperfluidLogoContainer>
 			</ModalContainer>
 		</Modal>
 	);
@@ -79,6 +107,16 @@ const ItemContainer = styled(Flex)`
 const ClaimButton = styled(P)`
 	color: ${brandColors.pinky[500]};
 	cursor: pointer;
+`;
+
+const TotalAmountContainer = styled.div`
+	padding: 8px;
+	border-radius: 8px;
+	background-color: ${neutralColors.gray[300]};
+`;
+
+const SuperfluidLogoContainer = styled(Flex)`
+	margin-top: 32px;
 `;
 
 export default ClaimRecurringDonationModal;
