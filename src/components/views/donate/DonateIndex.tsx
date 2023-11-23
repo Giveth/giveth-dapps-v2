a25a19a3f082649291b08e9ac39d4fbf20c4cdf8
Cherik <Pourcheriki@gmail.com>
2023-11-23 11:41:50 +0330

    fix styles

--- src/components/views/donate/DonateIndex.tsx
+++ src/components/views/donate/DonateIndex.tsx
@@ -18,7 +18,6 @@ import { useIntl } from 'react-intl';
 import dynamic from 'next/dynamic';
 import { useNetwork } from 'wagmi';
 import { BigArc } from '@/components/styled-components/Arc';
-import { mediaQueries } from '@/lib/constants/constants';
 import SocialBox from '../../DonateSocialBox';
 import NiceBanner from './NiceBanner';
 // import PurchaseXDAI from './PurchaseXDAIBanner';
@@ -181,34 +180,9 @@ const ProjectsButton = styled(ButtonLink)`
 
 const DonateContainer = styled(Container)`
 	text-align: center;
-	padding-top: 64px;
+	padding-top: 128px;
 	padding-bottom: 64px;
 	position: relative;
-`;
-
-const Sections = styled.div`
-	height: 100%;
-	${mediaQueries.tablet} {
-		display: grid;
-		grid-template-columns: repeat(2, minmax(500px, 1fr));
-		grid-auto-rows: minmax(100px, auto);
-	}
-	${mediaQueries.mobileL} {
-		grid-template-columns: repeat(2, minmax(100px, 1fr));
-		padding: 0 40px;
-	}
-`;
-
-const Right = styled.div`
-	z-index: 1;
-	background: white;
-	text-align: left;
-	padding: 32px;
-	min-height: 620px;
-	border-radius: 16px;
-	${mediaQueries.tablet} {
-		border-radius: 0 16px 16px 0;
-	}
 `;
 
 export default DonateIndex;
