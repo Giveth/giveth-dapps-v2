e455b2eb86de6b80a35c9ecc2ab70d0fafa87f0b
Cherik <Pourcheriki@gmail.com>
2023-11-27 15:28:21 +0330

    add getEthersSigner

--- src/helpers/ethers.ts
+++ src/helpers/ethers.ts
@@ -1,4 +1,9 @@
-import { type PublicClient, getPublicClient } from '@wagmi/core';
+import {
+	type PublicClient,
+	type WalletClient,
+	getPublicClient,
+	getWalletClient,
+} from '@wagmi/core';
 import { providers } from 'ethers';
 import { type HttpTransport } from 'viem';
 
@@ -23,4 +28,23 @@ export function publicClientToProvider(publicClient: PublicClient) {
 export function getEthersProvider({ chainId }: { chainId?: number } = {}) {
 	const publicClient = getPublicClient({ chainId });
 	return publicClientToProvider(publicClient);
+}
+
+export function walletClientToSigner(walletClient: WalletClient) {
+	const { account, chain, transport } = walletClient;
+	const network = {
+		chainId: chain.id,
+		name: chain.name,
+		ensAddress: chain.contracts?.ensRegistry?.address,
+	};
+	const provider = new providers.Web3Provider(transport, network);
+	const signer = provider.getSigner(account.address);
+	return signer;
+}
+
+/** Action to convert a viem Wallet Client to an ethers.js Signer. */
+export async function getEthersSigner({ chainId }: { chainId?: number } = {}) {
+	const walletClient = await getWalletClient({ chainId });
+	if (!walletClient) return undefined;
+	return walletClientToSigner(walletClient);
 }
