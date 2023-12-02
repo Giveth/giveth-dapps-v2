fe6f4301c7ce2820cb1481f219ed18dcce5d00ca
Cherik <Pourcheriki@gmail.com>
2023-12-02 23:16:17 +0330

    add title

--- src/components/views/donate/RecurringDonationModal/RecurringDonationModal.tsx
+++ src/components/views/donate/RecurringDonationModal/RecurringDonationModal.tsx
@@ -1,21 +1,47 @@
-import React, { FC } from 'react';
+import { FC, useState } from 'react';
+import { IconDonation32, mediaQueries } from '@giveth/ui-design-system';
+import styled from 'styled-components';
 import { Modal } from '@/components/modals/Modal';
 import { useModalAnimation } from '@/hooks/useModalAnimation';
 import { IModal } from '@/types/common';
+import { Flex } from '@/components/styled-components/Flex';
 
 interface IRecurringDonationModalProps extends IModal {}
+
+enum EDonationSteps {
+	APPROVE,
+	APPROVING,
+	DONATE,
+	DONATING,
+	SUBMITTED,
+}
+
+const headerTitleGenerator = (step: EDonationSteps) => {
+	switch (step) {
+		case EDonationSteps.APPROVE:
+		case EDonationSteps.APPROVING:
+		case EDonationSteps.DONATE:
+			return 'Confirm your donation';
+		case EDonationSteps.DONATING:
+			return 'Donating';
+		case EDonationSteps.SUBMITTED:
+			return 'Donation Submitted';
+	}
+};
 
 export const RecurringDonationModal: FC<IRecurringDonationModalProps> = ({
 	setShowModal,
 }) => {
+	const [step, setStep] = useState(EDonationSteps.APPROVE);
 	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
 
 	return (
 		<Modal
 			closeModal={closeModal}
 			isAnimating={isAnimating}
-			headerTitle='Select a Token'
+			headerTitle={headerTitleGenerator(step)}
 			headerTitlePosition='left'
+			headerIcon={<IconDonation32 />}
 		>
 			<RecurringDonationInnerModal setShowModal={setShowModal} />
 		</Modal>
@@ -23,5 +49,16 @@ export const RecurringDonationModal: FC<IRecurringDonationModalProps> = ({
 };
 
 const RecurringDonationInnerModal: FC<IRecurringDonationModalProps> = () => {
-	return <div></div>;
+	return <Wrapper>hi</Wrapper>;
 };
+
+const Wrapper = styled(Flex)`
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 16px;
+	width: 100%;
+	padding: 16px 24px 24px 24px;
+	${mediaQueries.tablet} {
+		width: 430px;
+	}
+`;
