206382d2f2f040395d4b4fc6f7a298e254e8a319
Cherik <Pourcheriki@gmail.com>
2023-12-04 11:30:16 +0330

    init item

---
+++ src/components/views/donate/RecurringDonationModal/Item.tsx
@@ -0,0 +1,37 @@
+import { type FC } from 'react';
+import styled from 'styled-components';
+import { P, B, neutralColors } from '@giveth/ui-design-system';
+import { Flex } from '@/components/styled-components/Flex';
+import { IToken } from '@/types/superFluid';
+
+interface IItemProps {
+	title: string;
+	amount: bigint;
+	price: bigint;
+	token: IToken;
+}
+
+export const Item: FC<IItemProps> = ({ title, amount, price, token }) => {
+	return (
+		<Wrapper gap='4px'>
+			<IconWrapper></IconWrapper>
+			<Flex flexDirection='column' gap='4px'>
+				<P>{title}</P>
+				<Flex gap='4px'>
+					<B>
+						{amount.toString()}&nbsp;{token.symbol}
+					</B>
+					<B>~</B>
+					<P>{(amount * price).toString()}&nbsp;USD</P>
+				</Flex>
+			</Flex>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled(Flex)`
+	padding: 8px;
+	background: ${neutralColors.gray[200]};
+`;
+
+const IconWrapper = styled.div``;
