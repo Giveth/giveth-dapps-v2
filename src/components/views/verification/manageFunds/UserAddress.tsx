dec150fd317ceac9cc55094b879addcbe45757ff
alireza-sharifpour <ascode94@gmail.com>
2023-12-14 13:23:28 +0330

    changed IAddress address of

--- src/components/views/verification/manageFunds/UserAddress.tsx
+++ src/components/views/verification/manageFunds/UserAddress.tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { FC } from 'react';
 import { FlexCenter } from '@/components/styled-components/Flex';
 import Input from '@/components/Input';
-import { IAddress } from '@/components/views/verification/manageFunds/ManageFundsIndex';
 import { RemoveIcon } from '@/components/views/verification/Common';
 import { chainNameById } from '@/lib/network';
+import { IAddress } from '@/apollo/types/types';
 
 interface IProps {
 	address: IAddress;
