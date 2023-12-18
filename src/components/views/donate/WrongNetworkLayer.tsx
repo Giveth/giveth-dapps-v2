fdacd0e4ee4444c48d6edbc71522fc1bcc592b2c
Cherik <Pourcheriki@gmail.com>
2023-12-18 11:19:54 +0330

    init toast

--- src/components/views/donate/WrongNetworkLayer.tsx
+++ src/components/views/donate/WrongNetworkLayer.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FlexCenter } from '@/components/styled-components/Flex';
+import {
+	Caption,
+	IconInfoFilled16,
+	brandColors,
+	neutralColors,
+} from '@giveth/ui-design-system';
+import { Flex, FlexCenter } from '@/components/styled-components/Flex';
 
 export const WrongNetworkLayer = () => {
-	return <Wrapper></Wrapper>;
+	return (
+		<Overlay>
+			<Toast>
+				<Header>
+					<Title gap='4px' alignItems='center'>
+						<IconInfoFilled16 />
+						<Caption>
+							Recurring donations are currently only available on
+							Optimism
+						</Caption>
+					</Title>
+				</Header>
+			</Toast>
+		</Overlay>
+	);
 };
 
-const Wrapper = styled(FlexCenter)`
+const Overlay = styled(FlexCenter)`
 	position: absolute;
 	z-index: 100;
 	width: 100%;
@@ -17,4 +37,20 @@ const Wrapper = styled(FlexCenter)`
 	right: 0;
 	bottom: 0;
 	backdrop-filter: blur(2px);
+`;
+
+const Toast = styled.div`
+	padding: 16px;
+	border-radius: 8px;
+	border: 1px solid ${brandColors.giv[500]};
+	background: #fff;
+`;
+
+const Header = styled(Flex)`
+	padding-bottom: 4px;
+	border-bottom: 1px solid ${neutralColors.gray[400]};
+`;
+
+const Title = styled(Flex)`
+	color: ${brandColors.giv[500]};
 `;
