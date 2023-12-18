9eebc9450d2445630fc0d86fe2ad9335fb2ed5b1
Cherik <Pourcheriki@gmail.com>
2023-12-18 10:48:05 +0330

    add wrapper

--- src/components/views/donate/WrongNetworkLayer.tsx
+++ src/components/views/donate/WrongNetworkLayer.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FlexCenter } from '@/components/styled-components/Flex';
 
 export const WrongNetworkLayer = () => {
-	return <div>WrongNetworkLayer</div>;
+	return <Wrapper></Wrapper>;
 };
+
+const Wrapper = styled(FlexCenter)`
+	position: absolute;
+	z-index: 100;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(255, 255, 255, 0.7);
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	backdrop-filter: blur(2px);
+`;
