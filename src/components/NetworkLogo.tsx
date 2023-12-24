b14b354df64f88d5837b32172ab73785213b27ad
Ramin <raminramazanpour@gmail.com>
2023-12-24 19:31:26 +0330

    add solana logo

--- src/components/NetworkLogo.tsx
+++ src/components/NetworkLogo.tsx
@@ -1,12 +1,23 @@
 import config from '@/configuration';
+import { ChainType } from '@/types/config';
 
 const networkIds = Object.keys(config.NETWORKS_CONFIG).map(Number);
 
-const NetworkLogo = (props: { chainId?: number; logoSize?: number }) => {
-	const { chainId, logoSize } = props;
+interface INetworkLogoProps {
+	chainId?: number;
+	logoSize?: number;
+	chainType?: ChainType;
+}
+
+const NetworkLogo = (props: INetworkLogoProps) => {
+	const { chainId, logoSize, chainType } = props;
+	if (chainType && chainType !== ChainType.EVM) {
+		return config.NETWORKS_CONFIG[chainType].chainLogo(logoSize);
+	}
 	if (chainId && networkIds.includes(chainId)) {
 		return config.NETWORKS_CONFIG[chainId].chainLogo(logoSize);
-	} else return null;
+	}
+	return null;
 };
 
 export default NetworkLogo;
