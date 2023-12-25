e77c8528833cf421b4830804c6c58aa19d1f0c85
Cherik <Pourcheriki@gmail.com>
2023-12-25 17:05:21 +0330

    set donation tx

--- src/components/views/donate/RecurringDonationModal/RecurringDonationModal.tsx
+++ src/components/views/donate/RecurringDonationModal/RecurringDonationModal.tsx
@@ -100,7 +100,8 @@ const RecurringDonationInnerModal: FC<IRecurringDonationInnerModalProps> = ({
 	setShowModal,
 	isUpdating,
 }) => {
-	const { project, selectedToken, tokenStreams } = useDonateData();
+	const { project, selectedToken, tokenStreams, setSuccessDonation } =
+		useDonateData();
 	const { address } = useAccount();
 	const tokenPrice = useTokenPrice(selectedToken?.token);
 
@@ -263,6 +264,9 @@ const RecurringDonationInnerModal: FC<IRecurringDonationInnerModalProps> = ({
 			}
 			const batchOp = sf.batchCall(operations);
 			const tx = await batchOp.exec(signer);
+			if (tx.hash) {
+				setSuccessDonation({ txHash: [tx.hash] });
+			}
 			const res = await tx.wait();
 			if (!res.status) {
 				throw new Error('Transaction failed');
