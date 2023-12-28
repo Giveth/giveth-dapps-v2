f9fc4ba6bfd33152f0c2897ec6b8641b7a1d167b
Cherik <Pourcheriki@gmail.com>
2023-12-28 14:52:06 +0330

    Fix errors (#3544)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -248,7 +248,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 												}}
 											/>
 										)}
-										{process.env.NEXT_PUBLIC_ENV !==
+										{/* {process.env.NEXT_PUBLIC_ENV !==
 											'production' && (
 											<Script
 												id='console-script'
@@ -257,7 +257,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 													__html: `javascript:(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();`,
 												}}
 											/>
-										)}
+										)} */}
 
 										<FooterWrapper />
 										<ModalController />
