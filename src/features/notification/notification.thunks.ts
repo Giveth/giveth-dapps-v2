66d5cc55ce576af9a7abd331c4ad491ac78ab0d4
Kilter <mateodaza@gmail.com>
2024-01-16 11:20:03 -0500

    Update notification.thunks.ts

--- src/features/notification/notification.thunks.ts
+++ src/features/notification/notification.thunks.ts
@@ -1,13 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { fetchNotificationCount } from './notification.services';
+// import { fetchNotificationCount } from './notification.services';
 
 export const fetchNotificationCountAsync = createAsyncThunk(
 	'notification/fetchNotificationCount',
 	async (address: string) => {
-		// TODO: SOLUTION FOR NOW BUT WE SHOULD BRING IT BACK LATER
+		// SENDING ZERO FOR NOW ON ALL VALUES
+		return {
+			general: 0,
+			givEconomyRelated: 0,
+			projectsRelated: 0,
+			supportedProjects: 0,
+			lastNotificationId: 0,
+			total: 0,
+		};
+
 		// const response = await fetchNotificationCount(address);
 		// The value we return becomes the `fulfilled` action payload
 		// return response;
-		return 0;
 	},
 );
