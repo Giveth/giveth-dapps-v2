01849cabf9378316b5b6818a1282de08623756f5
Cherik <Pourcheriki@gmail.com>
2024-01-18 02:06:05 +0330

    add withdraw functionality

--- src/components/views/donate/ModifySuperToken/WithDrawSuperToken.tsx
+++ src/components/views/donate/ModifySuperToken/WithDrawSuperToken.tsx
@@ -2,6 +2,7 @@ import { useState, type FC } from 'react';
 import { useAccount, useBalance } from 'wagmi';
 import { Button } from '@giveth/ui-design-system';
 import { useIntl } from 'react-intl';
+import { Framework } from '@superfluid-finance/sdk-core';
 import { ModifyInfoToast } from './ModifyInfoToast';
 import { ModifySection } from './ModifySection';
 import { StreamInfo } from './StreamInfo';
@@ -10,6 +11,9 @@ import { ITokenStreams } from '@/context/donate.context';
 import { ISuperToken, IToken } from '@/types/superFluid';
 import { actionButtonLabel, EModifySuperTokenSteps } from './common';
 import { ModifyWrapper, Wrapper } from './common.sc';
+import { getEthersProvider, getEthersSigner } from '@/helpers/ethers';
+import config from '@/configuration';
+import { showToastError } from '@/lib/helpers';
 
 interface IWithDrawSuperTokenProps extends IModifySuperTokenInnerModalProps {
 	tokenStreams: ITokenStreams;
@@ -45,7 +49,51 @@ export const WithDrawSuperToken: FC<IWithDrawSuperTokenProps> = ({
 		) || 0n;
 	const minRemainingBalance = totalStreamPerSec * BigInt(6 * 60 * 60);
 
-	const onWithDraw = async () => {};
+	const onWithDraw = async () => {
+		setStep(EModifySuperTokenSteps.WITHDRAWING);
+		try {
+			const _provider = getEthersProvider({
+				chainId: config.OPTIMISM_CONFIG.id,
+			});
+
+			const signer = await getEthersSigner({
+				chainId: config.OPTIMISM_CONFIG.id,
+			});
+
+			if (!_provider || !signer || !address || !superToken)
+				throw new Error('Provider or signer not found');
+
+			const sf = await Framework.create({
+				chainId: config.OPTIMISM_CONFIG.id,
+				provider: _provider,
+			});
+
+			// EThx is not a Wrapper Super Token and should load separately
+			let superTokenAsset;
+			if (superToken.symbol === 'ETHx') {
+				superTokenAsset = await sf.loadNativeAssetSuperToken(
+					superToken.id,
+				);
+			} else {
+				superTokenAsset = await sf.loadWrapperSuperToken(superToken.id);
+			}
+
+			const downgradeOperation = await superTokenAsset.downgrade({
+				amount: amount.toString(),
+			});
+
+			const tx = await downgradeOperation.exec(signer);
+			const res = await tx.wait();
+			if (!res.status) {
+				throw new Error('Withdraw failed');
+			}
+			setStep(EModifySuperTokenSteps.SUBMITTED);
+		} catch (error) {
+			setStep(EModifySuperTokenSteps.WITHDRAWING);
+			showToastError(error);
+			console.log('error', error);
+		}
+	};
 
 	return (
 		<Wrapper>
