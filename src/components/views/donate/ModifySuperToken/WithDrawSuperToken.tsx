370a596df5a6f4a7ded6b8a87a239ea24b050f01
Cherik <Pourcheriki@gmail.com>
2024-01-17 18:42:31 +0330

    init WithDrawSuperToken

--- src/components/views/donate/ModifySuperToken/WithDrawSuperToken.tsx
+++ src/components/views/donate/ModifySuperToken/WithDrawSuperToken.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Flex } from '@/components/styled-components/Flex';
+import { useState, type FC } from 'react';
+import { useAccount, useBalance } from 'wagmi';
+import { ModifyInfoToast } from './ModifyInfoToast';
+import { ModifySection } from './ModifySection';
+import { StreamInfo } from './StreamInfo';
+import { IModifySuperTokenInnerModalProps } from './ModifySuperTokenModal';
+import { ITokenStreams } from '@/context/donate.context';
+import { ISuperToken, IToken } from '@/types/superFluid';
+import { AddressZero } from '@/lib/constants/constants';
 
-export const WithDrawSuperToken = () => {
-	return <Wrapper></Wrapper>;
+interface IWithDrawSuperTokenProps extends IModifySuperTokenInnerModalProps {
+	tokenStreams: ITokenStreams;
+	token?: IToken;
+	superToken?: ISuperToken;
+}
+
+export const WithDrawSuperToken: FC<IWithDrawSuperTokenProps> = ({
+	token,
+	superToken,
+	tokenStreams,
+}) => {
+	const [amount, setAmount] = useState(0n);
+	const { address } = useAccount();
+
+	const {
+		data: balance,
+		refetch,
+		isRefetching,
+	} = useBalance({
+		token: token?.id === AddressZero ? undefined : token?.id,
+		address: address,
+	});
+
+	const { data: SuperTokenBalance } = useBalance({
+		token: superToken?.id,
+		address: address,
+	});
+
+	return (
+		<>
+			<ModifySection
+				setAmount={setAmount}
+				token={token}
+				balance={balance}
+				refetch={refetch}
+				isRefetching={isRefetching}
+			/>
+			<StreamInfo
+				tokenStreams={tokenStreams}
+				superToken={superToken}
+				SuperTokenBalance={SuperTokenBalance}
+			/>
+			<ModifyInfoToast />
+		</>
+	);
 };
-
-const Wrapper = styled(Flex)`
-	flex-direction: column;
-	gap: 24px;
-`;
