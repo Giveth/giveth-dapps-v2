b3149a20ebe1a6a86ff2823d84b975e914df2498
Cherik <Pourcheriki@gmail.com>
2024-01-23 15:15:00 +0330

    show info

--- src/components/views/userProfile/donationsTab/recurringTab/StreamRow.tsx
+++ src/components/views/userProfile/donationsTab/recurringTab/StreamRow.tsx
@@ -1,8 +1,9 @@
 import { type FC } from 'react';
 import styled from 'styled-components';
-import { P } from '@giveth/ui-design-system';
+import { P, semanticColors } from '@giveth/ui-design-system';
 import { formatUnits } from 'viem';
 import { useAccount, useBalance } from 'wagmi';
+import { useIntl } from 'react-intl';
 import { TokenIcon } from '@/components/views/donate/TokenIcon/TokenIcon';
 import { TableCell } from './ActiveStreamsSection';
 import { ISuperfluidStream } from '@/types/superFluid';
@@ -15,6 +16,7 @@ interface IStreamRowProps {
 
 export const StreamRow: FC<IStreamRowProps> = ({ tokenStream }) => {
 	const { address } = useAccount();
+	const { formatMessage } = useIntl();
 
 	const {
 		data: balance,
@@ -27,30 +29,41 @@ export const StreamRow: FC<IStreamRowProps> = ({ tokenStream }) => {
 		// cacheTime: 5_000,
 	});
 
-	const symbol = tokenStream[0].token.underlyingToken?.symbol || 'ETH';
+	const underlyingSymbol =
+		tokenStream[0].token.underlyingToken?.symbol || 'ETH';
 	const totalFlowRate = tokenStream.reduce(
 		(acc, curr) => acc + BigInt(curr.currentFlowRate),
 		0n,
 	);
 	const monthlyFlowRate = totalFlowRate * ONE_MONTH_SECONDS;
+	const { symbol, decimals } = tokenStream[0].token;
+	const runOutMonth = balance?.value ? balance?.value / monthlyFlowRate : 0n;
 
 	return (
 		<RowWrapper>
 			<TableCell>
-				<TokenIcon symbol={symbol} />
+				<TokenIcon symbol={underlyingSymbol} />
 				<P>{limitFraction(balance?.formatted || '0')}</P>
-				<P>{tokenStream[0].token.symbol}</P>
+				<P>{symbol}</P>
 			</TableCell>
 			<TableCell>
-				{limitFraction(
-					formatUnits(
-						monthlyFlowRate || 0n,
-						tokenStream[0].token.decimals,
-					),
-				)}
+				<P color={semanticColors.jade[500]}>
+					{limitFraction(
+						formatUnits(monthlyFlowRate || 0n, decimals),
+					)}
+					&nbsp;
+					{underlyingSymbol}
+					&nbsp;monthly
+				</P>
 			</TableCell>
 			<TableCell>{tokenStream.length}</TableCell>
-			<TableCell></TableCell>
+			<TableCell>
+				{runOutMonth.toString()}
+				{formatMessage(
+					{ id: 'label.months' },
+					{ count: runOutMonth.toString() },
+				)}
+			</TableCell>
 			<TableCell>Modify stream balance</TableCell>
 		</RowWrapper>
 	);
