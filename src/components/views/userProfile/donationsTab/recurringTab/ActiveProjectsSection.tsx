7da10bbb4a07119c16c4dd16f94e1953617c3dc8
Cherik <Pourcheriki@gmail.com>
2024-02-01 16:38:12 +0330

    add header

--- src/components/views/userProfile/donationsTab/recurringTab/ActiveProjectsSection.tsx
+++ src/components/views/userProfile/donationsTab/recurringTab/ActiveProjectsSection.tsx
@@ -1,15 +1,21 @@
-import { neutralColors } from '@giveth/ui-design-system';
+import { H5, neutralColors } from '@giveth/ui-design-system';
 import styled from 'styled-components';
 import { Flex } from '@/components/styled-components/Flex';
 
 export const ActiveProjectsSection = () => {
-	return <Wrapper>ActiveProjectsSection</Wrapper>;
+	return (
+		<Wrapper>
+			<Flex>
+				<H5 weight={900}>Active projects</H5>
+			</Flex>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled(Flex)`
 	flex-direction: column;
 	gap: 12px;
 	background-color: ${neutralColors.gray[100]};
-	padding: 26px 24px;
+	padding: 24px;
 	border-radius: 12px;
 `;
