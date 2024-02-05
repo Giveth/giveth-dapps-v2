98dbd00fe939acdb82e8fe0e9c75f59cd33bd72a
Cherik <Pourcheriki@gmail.com>
2024-02-05 11:54:45 +0330

    add calculateERC20TransferTxHash

--- src/helpers/donate.ts
+++ src/helpers/donate.ts
@@ -1,4 +1,7 @@
-import { Address } from 'viem';
+import { Address, encodeFunctionData } from 'viem';
+import { getWalletClient } from '@wagmi/core';
+import { erc20ABI } from 'wagmi';
+import { utils } from 'ethers/lib/ethers';
 import config from '@/configuration';
 import { IProject } from '@/apollo/types/types';
 import {
@@ -38,3 +41,40 @@ export const findUserStreamOnSelectedToken = (
 			stream.receiver.id.toLowerCase() === projectOpAddress.toLowerCase(),
 	);
 };
+
+export async function calculateERC20TransferTxHash(
+	tokenAddress: Address,
+	toAddress: Address,
+	amount: bigint,
+	chainId: number,
+) {
+	const walletClient = await getWalletClient({ chainId });
+	if (!walletClient) return;
+	console.log('filter| walletClient', walletClient);
+
+	// Encode the transaction data
+	const data = encodeFunctionData({
+		abi: erc20ABI,
+		functionName: 'transferFrom',
+		args: [walletClient.account.address, toAddress, amount],
+	});
+	console.log('filter| data', data);
+
+	// Create the transaction object
+	const tx = {
+		to: tokenAddress,
+		value: 0n,
+		data: data,
+	};
+	console.log('filter| tx', tx);
+
+	// Sign the transaction
+	const signedTx = await walletClient.signTransaction(tx);
+	console.log('filter| signedTx', signedTx);
+
+	// Calculate the transaction hash
+	const txHash = utils.keccak256(signedTx);
+	console.log('filter| txHash', txHash);
+
+	return txHash;
+}
