fea25a39f532cdb0020074e0c06e28fa0c4e3433
alireza-sharifpour <ascode94@gmail.com>
2024-02-08 11:24:52 +0330

    created ClaimRecurringItem

---
+++ src/components/views/userProfile/projectsTab/ClaimRecurringItem.tsx
@@ -0,0 +1,82 @@
+import { B, P, brandColors, neutralColors } from '@giveth/ui-design-system';
+import { utils } from 'ethers';
+import styled from 'styled-components';
+import { useEffect } from 'react';
+import { Flex } from '@/components/styled-components/Flex';
+import { limitFraction } from '@/helpers/number';
+import { TokenIcon } from '../../donate/TokenIcon/TokenIcon';
+import { ITokenWithBalance } from '@/hooks/useProjectClaimableDonations';
+import { useTokenPrice } from '@/hooks/useTokenPrice';
+import { IAllTokensUsd } from './ClaimRecurringDonationModal';
+
+interface IClaimRecurringItem {
+	tokenWithBalance: ITokenWithBalance;
+	onSelectStream: (item: ITokenWithBalance) => void;
+	setAllTokensUsd: (allTokensUsd: IAllTokensUsd) => void;
+	allTokensUsd: IAllTokensUsd;
+}
+
+export const ClaimRecurringItem = ({
+	tokenWithBalance,
+	onSelectStream,
+	setAllTokensUsd,
+	allTokensUsd,
+}: IClaimRecurringItem) => {
+	const symbol = tokenWithBalance.token.underlyingToken?.symbol!;
+	const price = useTokenPrice(tokenWithBalance.token);
+
+	useEffect(() => {
+		if (price !== undefined) {
+			setAllTokensUsd({
+				...allTokensUsd,
+				[symbol]: price,
+			});
+		}
+	}, [price]);
+
+	console.log('Price', price);
+	return (
+		<ItemContainer
+			justifyContent='space-between'
+			alignItems='center'
+			key={tokenWithBalance.token.symbol}
+		>
+			<Flex alignItems='center'>
+				<TokenIcon
+					symbol={tokenWithBalance.token.underlyingToken?.symbol!}
+				/>
+				&nbsp; &nbsp;
+				<B>
+					{`
+                    ${limitFraction(
+						utils.formatUnits(
+							tokenWithBalance.balance,
+							tokenWithBalance.token.decimals,
+						),
+						6,
+					)} 
+                    ${tokenWithBalance.token.underlyingToken?.symbol} ~
+                    ${price}
+                    USD
+                `}
+				</B>
+			</Flex>
+			<ClaimButton onClick={() => onSelectStream(tokenWithBalance)}>
+				Claim tokens
+			</ClaimButton>
+		</ItemContainer>
+	);
+};
+
+const ItemContainer = styled(Flex)`
+	padding: 8px;
+	border-radius: 8px;
+	:hover {
+		background-color: ${neutralColors.gray[300]};
+	}
+`;
+
+const ClaimButton = styled(P)`
+	color: ${brandColors.pinky[500]};
+	cursor: pointer;
+`;
