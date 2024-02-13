41f1a53164961c57df66af1cdb866fce926d88a6
alireza-sharifpour <ascode94@gmail.com>
2024-02-13 12:06:11 +0330

    Add new design for editproject

--- src/components/views/create/AddressInterface.tsx
+++ src/components/views/create/AddressInterface.tsx
@@ -6,9 +6,12 @@ import {
 	Button,
 	GLink,
 	IconArrowRight16,
+	IconCheck16,
 	IconTrash24,
 	P,
+	brandColors,
 	neutralColors,
+	semanticColors,
 } from '@giveth/ui-design-system';
 import { EInputs } from '@/components/views/create/CreateProject';
 import NetworkLogo from '@/components/NetworkLogo';
@@ -34,6 +37,7 @@ const AddressInterface = ({
 	onButtonClick,
 	chainType,
 	anchorContractData,
+	isEditMode,
 }: IAddressInterfaceProps) => {
 	const { setValue, watch } = useFormContext();
 	const { formatMessage } = useIntl();
@@ -50,6 +54,7 @@ const AddressInterface = ({
 	const walletAddress = addressObj?.address;
 
 	const hasAddress = !!walletAddress;
+	const hasAnchorContract = !!anchorContractData?.isActive;
 
 	return (
 		<Container>
@@ -114,26 +119,35 @@ const AddressInterface = ({
 						</IconContainer>
 					)}
 				</Flex>
-				{isOptimism &&
-					isRecurringActive &&
-					isOnEVM &&
-					!anchorContractData?.isActive && (
-						// Render this section only on Optimism
-						<AlloProtocolContainer>
-							<Flex>
-								<div>
-									<B>
-										Set up Profile on the Allo Protocol
-										Registry
-									</B>
-									<P>
-										Your project will be included in a
-										shared registry of public goods projects
-										with Gitcoin and others. You will also
-										set up your project to receive recurring
-										donations.
-									</P>
-								</div>
+				{isOptimism && isRecurringActive && isOnEVM && (
+					// Render this section only on Optimism
+					<AlloProtocolContainer>
+						<Flex>
+							<div>
+								<B>
+									{hasAnchorContract && isEditMode
+										? formatMessage({
+												id: 'label.allo_protocol_registry_set_up',
+											})
+										: formatMessage({
+												id: 'label.set_up_profile_on_the_allo_protocol_registry',
+											})}
+								</B>
+								<P>
+									{hasAnchorContract && isEditMode
+										? formatMessage({
+												id: 'label.your_project_is_set_up_to_receive_recurring_donations',
+											})
+										: formatMessage({
+												id: 'label.do_you_want_this_project_to_be_setup_to_receive_recurring_donations',
+											})}
+								</P>
+							</div>
+							{hasAnchorContract && isEditMode ? (
+								<IconCheckContainer>
+									<IconCheck16 color={brandColors.giv[100]} />
+								</IconCheckContainer>
+							) : (
 								<ToggleSwitch
 									isOn={alloProtocolRegistry}
 									toggleOnOff={() =>
@@ -144,9 +158,10 @@ const AddressInterface = ({
 									}
 									caption=''
 								/>
-							</Flex>
-						</AlloProtocolContainer>
-					)}
+							)}
+						</Flex>
+					</AlloProtocolContainer>
+				)}
 			</MiddleContainer>
 		</Container>
 	);
@@ -199,6 +214,14 @@ const IconContainer = styled(FlexCenter)`
 
 const AlloProtocolContainer = styled.div`
 	margin-top: 24px;
+`;
+
+const IconCheckContainer = styled(FlexCenter)`
+	width: 24px;
+	height: 24px;
+	border-radius: 50px;
+	background-color: ${semanticColors.jade[500]};
+	padding: 5px;
 `;
 
 export default AddressInterface;
