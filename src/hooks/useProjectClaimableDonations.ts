c6eede9b4468080612898917b283a9c46b7fe8f2
alireza-sharifpour <ascode94@gmail.com>
2024-02-14 12:10:17 +0330

    use contractWrite instead of usePrepareContract

--- src/hooks/useProjectClaimableDonations.ts
+++ src/hooks/useProjectClaimableDonations.ts
@@ -22,46 +22,49 @@ export const useProjectClaimableDonations = (
 	const [isLoading, setIsLoading] = useState(false); // Add isLoading state
 	const [balances, setBalances] = useState<ITokenWithBalance[]>([]);
 
-	const fetchTokenBalance = async (
-		token: IToken,
-	): Promise<ITokenWithBalance | null> => {
-		if (!anchorContractAddress) return null;
-		try {
-			const balance = await fetchBalance(token.id, anchorContractAddress);
-			if (balance) {
-				return {
-					token,
-					balance: balance, // Convert balance to BigNumber
-				};
-			}
-			return null;
-		} catch (error) {
-			return null;
-		}
-	};
-
-	// Initiate all balance fetches concurrently
-	const fetchAllBalances = async () => {
-		try {
-			setIsLoading(true);
-			const _allTokensWithBalance = allTokens.map(token => {
-				return fetchTokenBalance(token);
-			});
-			const results = await Promise.all(_allTokensWithBalance);
-			// Filter out null values
-			const filteredResults = results.filter(
-				result => result !== null && result.balance !== 0n,
-			) as ITokenWithBalance[];
-			setBalances(filteredResults);
-		} catch (error) {
-		} finally {
-			setIsLoading(false);
-		}
-	};
-
 	useEffect(() => {
+		const fetchTokenBalance = async (
+			token: IToken,
+		): Promise<ITokenWithBalance | null> => {
+			if (!anchorContractAddress) return null;
+			try {
+				const balance = await fetchBalance(
+					token.id,
+					anchorContractAddress,
+				);
+				if (balance) {
+					return {
+						token,
+						balance: balance, // Convert balance to BigNumber
+					};
+				}
+				return null;
+			} catch (error) {
+				return null;
+			}
+		};
+
+		// Initiate all balance fetches concurrently
+		const fetchAllTokensBalances = async () => {
+			try {
+				setIsLoading(true);
+				const _allTokensWithBalance = allTokens.map(token => {
+					return fetchTokenBalance(token);
+				});
+				const results = await Promise.all(_allTokensWithBalance);
+				// Filter out null values
+				const filteredResults = results.filter(
+					result => result !== null && result.balance !== 0n,
+				) as ITokenWithBalance[];
+				setBalances(filteredResults);
+			} catch (error) {
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
 		if (!anchorContractAddress) return;
-		fetchAllBalances();
+		fetchAllTokensBalances();
 	}, []);
 
 	return { isLoading, balances };
