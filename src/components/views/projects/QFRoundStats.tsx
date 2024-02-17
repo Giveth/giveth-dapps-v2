ae674bb876e9f6965f90dbf7a16274930de0e6b1
Cherik <Pourcheriki@gmail.com>
2024-02-17 15:55:58 +0330

    add info section

--- src/components/views/projects/QFRoundStats.tsx
+++ src/components/views/projects/QFRoundStats.tsx
@@ -1,11 +1,13 @@
 import { H3, brandColors, neutralColors } from '@giveth/ui-design-system';
 import React from 'react';
 import styled from 'styled-components';
+import { Flex } from '@/components/styled-components/Flex';
 
 export const QFRoundStats = () => {
 	return (
 		<Wrapper>
 			<H3 weight={700}>Round Stats</H3>
+			<InfoSection></InfoSection>
 		</Wrapper>
 	);
 };
@@ -17,4 +19,11 @@ const Wrapper = styled.div`
 	border-radius: 16px;
 	color: ${brandColors.giv[500]};
 	text-align: center;
+`;
+
+const InfoSection = styled(Flex)`
+	margin-top: 40px;
+	padding: 40px;
+	background-color: ${brandColors.giv[100]};
+	border-radius: 16px;
 `;
