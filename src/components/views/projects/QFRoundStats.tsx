e08eb5c3b4698bff1656731ec31f5eb6181cef40
Cherik <Pourcheriki@gmail.com>
2024-02-17 15:52:50 +0330

    add title

--- src/components/views/projects/QFRoundStats.tsx
+++ src/components/views/projects/QFRoundStats.tsx
@@ -1,5 +1,20 @@
+import { H3, brandColors, neutralColors } from '@giveth/ui-design-system';
 import React from 'react';
+import styled from 'styled-components';
 
 export const QFRoundStats = () => {
-	return <div>QFRoundStats</div>;
+	return (
+		<Wrapper>
+			<H3 weight={700}>Round Stats</H3>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	margin-top: 40px;
+	padding: 40px;
+	background-color: ${neutralColors.gray[100]};
+	border-radius: 16px;
+	color: ${brandColors.giv[500]};
+	text-align: center;
+`;
