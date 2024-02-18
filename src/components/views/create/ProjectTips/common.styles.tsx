da154fa85cf42ad3c3d8a98f53a73783891a8f48
alireza-sharifpour <ascode94@gmail.com>
2024-02-18 15:54:26 +0330

    added common styles for project tips

---
+++ src/components/views/create/ProjectTips/common.styles.tsx
@@ -0,0 +1,26 @@
+import { brandColors } from '@giveth/ui-design-system';
+import styled from 'styled-components';
+
+export const TipListItem = styled.div`
+	padding: 10px;
+	padding-left: 30px; // Include the space for the custom bullet
+	font-size: 18px;
+	line-height: 1.5;
+	position: relative;
+	margin-bottom: 10px; // Space between items
+	box-sizing: border-box; // Includes padding in the width calculation
+	word-wrap: break-word; // Ensures text wraps and doesn't overflow
+
+	&::before {
+		content: '';
+		position: absolute;
+		left: 10px;
+		top: 22px;
+		height: 5px;
+		width: 5px;
+		max-width: 100%;
+		background-color: ${brandColors.giv[500]};
+		border-radius: 50%;
+		box-shadow: 0 0 0 3px ${brandColors.giv[100]};
+	}
+`;
