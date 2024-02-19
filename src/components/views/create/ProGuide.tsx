1e3e8618c8bc32d40a097115f2bd84261d176b70
Cherik <Pourcheriki@gmail.com>
2024-02-20 01:54:45 +0330

    add wrapper

--- src/components/views/create/ProGuide.tsx
+++ src/components/views/create/ProGuide.tsx
@@ -1,15 +1,23 @@
 import { type FC } from 'react';
 import { deviceSize } from '@giveth/ui-design-system';
+import styled from 'styled-components';
 import ProjectTip, { IProjectTipProps } from './ProjectTips/ProjectTip';
 import useMediaQuery from '@/hooks/useMediaQuery';
+import { Flex } from '@/components/styled-components/Flex';
 
 interface IProGuideProps extends IProjectTipProps {}
 
 export const ProGuide: FC<IProGuideProps> = ({ activeSection }) => {
 	const isLaptopL = useMediaQuery(`(min-width: ${deviceSize.laptopL}px)`);
 	return isLaptopL ? (
-		<div>
+		<Wrapper>
 			<ProjectTip activeSection={activeSection} />
-		</div>
+		</Wrapper>
 	) : null;
 };
+
+const Wrapper = styled(Flex)`
+	position: sticky;
+	top: 100px;
+	margin-top: 80px;
+`;
--- src/components/views/create/ProjectTips/ProjectTip.tsx
+++ src/components/views/create/ProjectTips/ProjectTip.tsx
@@ -65,11 +65,7 @@ const ProjectTip = ({ activeSection }: IProjectTipProps) => {
 };
 
 const Container = styled.div`
-	position: sticky;
-	top: 100px;
-	width: 400px;
 	padding: 24px;
-	margin-top: 80px;
 	background-color: ${neutralColors.gray[100]};
 	border-radius: 16px;
 `;
