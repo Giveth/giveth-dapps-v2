dc01674b9c095f0c3ff96a44669beca12bcd27a1
alireza-sharifpour <ascode94@gmail.com>
2024-02-19 16:54:51 +0330

    update links

--- src/components/views/create/ProjectTips/DescriptionTip.tsx
+++ src/components/views/create/ProjectTips/DescriptionTip.tsx
@@ -1,8 +1,12 @@
-import { P } from '@giveth/ui-design-system';
+import { P, brandColors } from '@giveth/ui-design-system';
 import React from 'react';
 import { useIntl } from 'react-intl';
+import Link from 'next/link';
+import styled from 'styled-components';
 import { TipLine, TipListItem } from './common.styles';
 import { Flex } from '@/components/styled-components/Flex';
+import links from '@/lib/constants/links';
+import Routes from '@/lib/constants/Routes';
 
 const DescriptionTip = () => {
 	const { formatMessage } = useIntl();
@@ -44,11 +48,31 @@ const DescriptionTip = () => {
 						{formatMessage({
 							id: 'label.make_sure_your_description_aligns_with_our',
 						})}
+						<CustomLink href={links.COVENANT_DOC} target='_blank'>
+							{formatMessage({
+								id: 'label.covenant',
+							})}
+						</CustomLink>
+						&nbsp;
+						{formatMessage({
+							id: 'label.or',
+						})}
+						&nbsp;
+						<CustomLink href={Routes.Terms} target='_blank'>
+							{formatMessage({
+								id: 'component.title.tos',
+							})}
+							.
+						</CustomLink>
 					</P>
 				</TipListItem>
 			</Flex>
 		</div>
 	);
 };
+
+const CustomLink = styled(Link)`
+	color: ${brandColors.pinky[500]};
+`;
 
 export default DescriptionTip;
