75082ccdc391eb34392c93014e78b384b2bd58e2
Cherik <Pourcheriki@gmail.com>
2024-02-20 02:25:52 +0330

    add content

--- src/components/views/create/score/ProjectScoreCard.tsx
+++ src/components/views/create/score/ProjectScoreCard.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { H6 } from '@giveth/ui-design-system';
 import Image from 'next/image';
 import { Card } from '../ProGuide.sc';
 import { Flex } from '@/components/styled-components/Flex';
 
+enum EScoreState {
+	LOW = 'LOW',
+	MEDIUM = 'MEDIUM',
+	HIGH = 'HIGH',
+}
+
+const contentMap = {
+	[EScoreState.LOW]: {
+		mainTip:
+			'Your project score is too low to publish, you need at least a score of 50 to proceed.',
+		title: 'Why is it low?',
+	},
+	[EScoreState.MEDIUM]: {
+		mainTip:
+			'You can still publish your project but it might prevent you to receive the donation your are looking for.',
+		title: 'Why is it low?',
+	},
+	[EScoreState.HIGH]: {
+		mainTip:
+			'Just keep in mind to regularly update your project to keep donation coming your way.',
+		title: 'What else you can do?',
+	},
+};
+
 export const ProjectScoreCard = () => {
+	const [Score, setScore] = useState(0);
 	return (
 		<Card>
 			<Flex gap='16px'>
