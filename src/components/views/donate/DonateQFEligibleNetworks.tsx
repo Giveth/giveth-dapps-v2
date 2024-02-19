5190a1c736d79c504eee4015197a3329f45b699d
Cherik <Pourcheriki@gmail.com>
2024-02-19 17:11:22 +0330

    add bridge tokens

--- src/components/views/donate/DonateQFEligibleNetworks.tsx
+++ src/components/views/donate/DonateQFEligibleNetworks.tsx
@@ -1,5 +1,6 @@
 import {
 	Caption,
+	IconExternalLink16,
 	IconInfoFilled16,
 	brandColors,
 	neutralColors,
@@ -11,7 +12,6 @@ import { Flex } from '@/components/styled-components/Flex';
 import SwitchNetwork from '@/components/modals/SwitchNetwork';
 import { useDonateData } from '@/context/donate.context';
 import { getActiveRound } from '@/helpers/qf';
-import { SwitchCaption } from './common.styled';
 import { getChainName } from '@/lib/network';
 import { ChainType } from '@/types/config';
 
@@ -50,10 +50,16 @@ const DonateQFEligibleNetworks = () => {
 				&nbsp;<BoldCaption>{chainsString}</BoldCaption>&nbsp;
 				{formatMessage({ id: 'label.are_eligible_to_be_matched' })}
 			</MakeDonationDescription>
-			<ActionsRow justifyContent='space-between' alignItems='center'>
+			<ActionsRow justifyContent='flex-start' alignItems='center'>
 				<CustomSwitchCaption onClick={() => setShowModal(true)}>
 					{formatMessage({ id: 'label.switch_network' })}
 				</CustomSwitchCaption>
+				<ExternalLink href='/'>
+					<CustomSwitchCaption onClick={() => setShowModal(true)}>
+						{formatMessage({ id: 'label.bridge_tokens' })}
+					</CustomSwitchCaption>
+					<IconExternalLink16 />
+				</ExternalLink>
 			</ActionsRow>
 			{showModal && (
 				<SwitchNetwork
@@ -90,10 +96,21 @@ const BoldCaption = styled(Caption)`
 	display: inline;
 `;
 
-const CustomSwitchCaption = styled(SwitchCaption)``;
+const CustomSwitchCaption = styled(Caption)`
+	margin: 0;
+`;
 
 const ActionsRow = styled(Flex)`
 	margin-top: 8px;
+	gap: 16px;
+	color: ${brandColors.pinky[500]};
+`;
+
+const ExternalLink = styled.a`
+	text-decoration: none;
+	display: flex;
+	align-items: center;
+	gap: 4px;
 `;
 
 export default DonateQFEligibleNetworks;
