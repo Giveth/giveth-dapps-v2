ff1b3e9ae2883d65bbb565d88f8d154fca303c7b
Cherik <Pourcheriki@gmail.com>
2024-02-20 16:47:35 +0330

    add improvement tips

--- src/components/views/create/proGuide/score/ImprovementTips.tsx
+++ src/components/views/create/proGuide/score/ImprovementTips.tsx
@@ -1,17 +1,31 @@
 import { type FC } from 'react';
 import styled from 'styled-components';
 import {
-	H5,
+	H6,
 	IconChevronDown32,
 	IconChevronUp32,
+	P,
 } from '@giveth/ui-design-system';
 import useAnimatedHeight from '@/hooks/useAnimatedHeight';
 import { Flex } from '@/components/styled-components/Flex';
-import { infoMap, getScoreState, ScoreState } from './scoreHelpers';
+import { infoMap, getScoreState, ScoreState, EScoreType } from './scoreHelpers';
 
 interface IImprovementTipsProps {
 	fieldsScores: ScoreState;
 }
+
+const tipMap = {
+	[EScoreType.DESCRIPTION]:
+		'The project description is way too short, add at least 1200 characters to your description.',
+	[EScoreType.DESC_IMAGE]:
+		'Add an image to your description to make it more appealing.',
+	[EScoreType.CATEGORIES]:
+		'You need to select at least one category to proceed.',
+	[EScoreType.LOCATION]:
+		'Add location to your project to make it more appealing.',
+	[EScoreType.IMAGE]:
+		'Add a custom banner to your project to make it more appealing.',
+};
 
 export const ImprovementTips: FC<IImprovementTipsProps> = ({
 	fieldsScores,
@@ -25,17 +39,26 @@ export const ImprovementTips: FC<IImprovementTipsProps> = ({
 				onClick={toggleOpen}
 				alignItems='center'
 			>
-				<H5>{info.title}</H5>
+				<H6 weight={700}>{info.title}</H6>
 				{isOpen ? <IconChevronUp32 /> : <IconChevronDown32 />}
 			</TitleRow>
 			<AnimatedDiv
 				maxHeight={isOpen ? maxHeight : '0px'}
 				ref={contentRef}
 			>
-				{/* Dynamic content goes here */}
-				<p>Your content here...</p>
-				<p>More content...</p>
-				{/* ... */}
+				{fieldsScores.DESCRIPTION === 0 && (
+					<P>{tipMap[EScoreType.DESCRIPTION]}</P>
+				)}
+				{fieldsScores.DESC_IMAGE === 0 && (
+					<P>{tipMap[EScoreType.DESC_IMAGE]}</P>
+				)}
+				{fieldsScores.CATEGORIES === 0 && (
+					<P>{tipMap[EScoreType.CATEGORIES]}</P>
+				)}
+				{fieldsScores.LOCATION === 0 && (
+					<P>{tipMap[EScoreType.LOCATION]}</P>
+				)}
+				{fieldsScores.IMAGE === 0 && <P>{tipMap[EScoreType.IMAGE]}</P>}
 			</AnimatedDiv>
 		</div>
 	);
