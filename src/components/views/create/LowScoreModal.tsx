b29dfb095324467ba0c18cbef701b7650ee95e0a
Cherik <Pourcheriki@gmail.com>
2024-02-21 20:33:04 +0330

    update modal

--- src/components/views/create/LowScoreModal.tsx
+++ src/components/views/create/LowScoreModal.tsx
@@ -1,20 +1,19 @@
 import React, { FC } from 'react';
-import {
-	brandColors,
-	IconNetwork32,
-	neutralColors,
-	Overline,
-	P,
-} from '@giveth/ui-design-system';
+import { Button, IconNetwork32, P } from '@giveth/ui-design-system';
 import styled from 'styled-components';
 import { useModalAnimation } from '@/hooks/useModalAnimation';
 import { Modal } from '@/components/modals/Modal';
 import { IModal } from '@/types/common';
-import { ETheme } from '@/features/general/general.slice';
+import { Flex } from '@/components/styled-components/Flex';
 
-interface ISwitchNetworkModal extends IModal {}
+interface ISwitchNetworkModal extends IModal {
+	onSubmit: any;
+}
 
-export const LowScoreModal: FC<ISwitchNetworkModal> = ({ setShowModal }) => {
+export const LowScoreModal: FC<ISwitchNetworkModal> = ({
+	onSubmit,
+	setShowModal,
+}) => {
 	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
 
 	return (
@@ -25,28 +24,46 @@ export const LowScoreModal: FC<ISwitchNetworkModal> = ({ setShowModal }) => {
 			isAnimating={isAnimating}
 			headerTitlePosition='left'
 		>
-			<P>
-				Your project can be published with its{' '}
-				<b>current score below 80</b>, but achieving an optimal score
-				might better motivate donors to support your cause.
-			</P>
-			<P>Are you sure you want to continue publishing your project?</P>
+			<Wrapper>
+				<P>
+					Your project can be published with its{' '}
+					<b>current score below 80</b>, but achieving an optimal
+					score might better motivate donors to support your cause.
+				</P>
+				<P>
+					Are you sure you want to continue publishing your project?
+				</P>
+				<Flex flexDirection='column' gap='8px'>
+					<StyledButton
+						label='Yes, Publish My Project'
+						buttonType='primary'
+						onClick={() => {
+							closeModal();
+							onSubmit();
+						}}
+					/>
+					<StyledButton
+						label='No, let me improve my project score'
+						onClick={closeModal}
+						buttonType='texty-gray'
+					/>
+				</Flex>
+			</Wrapper>
 		</Modal>
 	);
 };
 
-export const SelectedNetwork = styled(Overline)`
-	color: ${props =>
-		props.themeState === ETheme.Dark
-			? brandColors.giv[100]
-			: brandColors.giv[500]};
-	position: absolute;
-	top: -8px;
-	left: 10px;
-	background: ${props =>
-		props.themeState === ETheme.Dark
-			? brandColors.giv[600]
-			: neutralColors.gray[100]};
-	padding: 0 3px;
-	border-radius: 4px;
+const Wrapper = styled(Flex)`
+	flex-direction: column;
+	padding: 40px 24px;
+	gap: 24px;
+	flex-wrap: wrap;
+	max-width: 600px;
+	text-align: left;
+`;
+
+const StyledButton = styled(Button)`
+	width: fit-content;
+	min-width: 300px;
+	margin: 0 auto;
 `;
