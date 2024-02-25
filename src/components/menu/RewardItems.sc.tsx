7ba23b8e702f48554a734c35ed0e7d8f13630576
Cherik <Pourcheriki@gmail.com>
2024-02-25 15:31:47 +0330

    fix RewardItems.sc.tsx

--- src/components/menu/RewardItems.sc.tsx
+++ src/components/menu/RewardItems.sc.tsx
@@ -1,6 +1,6 @@
 import { brandColors, B, P, Caption } from '@giveth/ui-design-system';
 import styled from 'styled-components';
-import { Flex } from '../styled-components/Flex';
+import { Flex } from '@giveth/ui-design-system';
 
 export const FlowrateRow = styled(Flex)`
 	align-items: center;
