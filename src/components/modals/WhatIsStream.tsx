37813acc9dcc80a062f44f134580ac775a1e8f07
Cherik <Pourcheriki@gmail.com>
2024-02-25 15:31:48 +0330

    fix WhatIsStream.tsx

--- src/components/modals/WhatIsStream.tsx
+++ src/components/modals/WhatIsStream.tsx
@@ -12,7 +12,7 @@ import {
 import styled from 'styled-components';
 import { FC } from 'react';
 import Link from 'next/link';
-import { Flex, FlexCenter } from '../styled-components/Flex';
+import { Flex, FlexCenter } from '@giveth/ui-design-system';
 import { Modal } from './Modal';
 import Routes from '@/lib/constants/Routes';
 import { IModal } from '@/types/common';
