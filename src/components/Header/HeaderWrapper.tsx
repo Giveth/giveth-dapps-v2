2d61e98615490713c00b9c21dc286d91d0c69c19
Cherik <Pourcheriki@gmail.com>
2024-02-25 15:31:46 +0330

    fix HeaderWrapper.tsx

--- src/components/Header/HeaderWrapper.tsx
+++ src/components/Header/HeaderWrapper.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Flex } from '@giveth/ui-design-system';
 import { useAppSelector } from '@/features/hooks';
 import {
 	HeaderLink,
@@ -10,7 +11,6 @@ import {
 	StyledHeader,
 } from './Header.sc';
 import Routes from '@/lib/constants/Routes';
-import { Flex } from '../styled-components/Flex';
 
 const Header = dynamic(() => import('./Header'), {
 	loading: () => <HeaderLoading />,
@@ -20,7 +20,7 @@ const Header = dynamic(() => import('./Header'), {
 const HeaderLoading = () => {
 	const theme = useAppSelector(state => state.general.theme);
 	return (
-		<StyledHeader $alignItems='center' $baseTheme={theme} show={true}>
+		<StyledHeader $alignItems='center' $baseTheme={theme} $show={true}>
 			<Flex gap='24px' $alignItems='center'>
 				<Link href={Routes.Home}>
 					<Logo>
