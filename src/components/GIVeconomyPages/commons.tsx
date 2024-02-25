a82ef75150b9f968fc844eaecc532079d42c25df
Cherik <Pourcheriki@gmail.com>
2024-02-25 15:31:46 +0330

    fix commons.tsx

--- src/components/GIVeconomyPages/commons.tsx
+++ src/components/GIVeconomyPages/commons.tsx
@@ -1,7 +1,7 @@
 import { brandColors, D1, GLink } from '@giveth/ui-design-system';
 import styled from 'styled-components';
 import { Container } from '@giveth/ui-design-system';
-import { Flex } from '../styled-components/Flex';
+import { Flex } from '@giveth/ui-design-system';
 import { mediaQueries } from '@/lib/constants/constants';
 import { RewardCard } from '../RewardCard';
 
