8af68ffd080cd5b1087f0681c97282a98670c817
Cherik <Pourcheriki@gmail.com>
2024-02-25 15:31:49 +0330

    fix InlineToast.tsx

--- src/components/toasts/InlineToast.tsx
+++ src/components/toasts/InlineToast.tsx
@@ -10,9 +10,8 @@ import {
 import React, { FC } from 'react';
 import styled from 'styled-components';
 import { useIntl } from 'react-intl';
+import { FlexCenter } from '@giveth/ui-design-system';
 import ExternalLink from '@/components/ExternalLink';
-import { FlexCenter } from '@/components/styled-components/Flex';
-
 export enum EToastType {
 	Error = 'error',
 	Warning = 'warning',
@@ -73,12 +72,12 @@ const InlineToast: FC<IProps> = props => {
 			{!noIcon && <IconContainer>{colorType.icon}</IconContainer>}
 			<Text>
 				<div>
-					{title && <Title medium>{title}</Title>}
+					{title && <Title $medium>{title}</Title>}
 					<Caption>{message}</Caption>
 				</div>
 				{link && (
 					<ExternalLink color={colorType.color} href={link}>
-						<Caption medium>
+						<Caption $medium>
 							{linkText ||
 								formatMessage({ id: 'label.learn_more' })}
 						</Caption>
