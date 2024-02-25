b8ec3dff134918e4d44a0f6ac38720809835e1fa
Cherik <Pourcheriki@gmail.com>
2024-02-25 15:31:49 +0330

    fix common.tsx

--- src/components/views/claim/cards/common.tsx
+++ src/components/views/claim/cards/common.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Flex } from '@/components/styled-components/Flex';
+import { Flex } from '@giveth/ui-design-system';
 
 export interface ICardProps {
 	activeIndex: number;
