216bc35dc9c8e3688904936f939138461a3e7ed5
Cherik <Pourcheriki@gmail.com>
2024-02-26 11:10:32 +0330

    fix ImpactTrackMentorship.tsx

--- src/components/views/landings/EthDenver/impactQuest/ImpactTrackMentorship.tsx
+++ src/components/views/landings/EthDenver/impactQuest/ImpactTrackMentorship.tsx
@@ -4,10 +4,11 @@ import {
 	H3,
 	IconChevronRight32,
 	neutralColors,
+	Col,
+	Row,
 } from '@giveth/ui-design-system';
 import ImpactTrackImg from '/public/images/ETHDenver-Impact-mentorship.png';
 import Image from 'next/image';
-import { Col, Row } from '@giveth/ui-design-system';
 import links from '@/lib/constants/links';
 import ExternalLink from '@/components/ExternalLink';
 
