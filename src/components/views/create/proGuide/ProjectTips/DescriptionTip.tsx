8f8a706776af9db6f01d4e9ba7e857136e6533ab
Cherik <Pourcheriki@gmail.com>
2024-02-26 11:10:30 +0330

    fix DescriptionTip.tsx

--- src/components/views/create/proGuide/ProjectTips/DescriptionTip.tsx
+++ src/components/views/create/proGuide/ProjectTips/DescriptionTip.tsx
@@ -1,9 +1,8 @@
-import { P, brandColors } from '@giveth/ui-design-system';
+import { P, brandColors, Flex } from '@giveth/ui-design-system';
 import React from 'react';
 import { useIntl } from 'react-intl';
 import Link from 'next/link';
 import styled from 'styled-components';
-import { Flex } from '@giveth/ui-design-system';
 import { TipLine, TipListItem } from './common.styles';
 import links from '@/lib/constants/links';
 import Routes from '@/lib/constants/Routes';
