de0545c4172a3f57b4192518c8b0fa82126fd302
Cherik <Pourcheriki@gmail.com>
2024-02-26 18:39:36 +0330

    fix ProfileContributes.tsx

--- src/components/views/userProfile/ProfileContributes.tsx
+++ src/components/views/userProfile/ProfileContributes.tsx
@@ -31,7 +31,7 @@ enum EProfile {
 }
 
 interface ITab {
-	active: boolean;
+	$active: boolean;
 }
 
 const ProfileContributes: FC<IUserProfileView> = () => {
@@ -68,40 +68,40 @@ const ProfileContributes: FC<IUserProfileView> = () => {
 		<Container>
 			<ProfileTabsContainer>
 				<Link href={pathname + profileTabs.overview}>
-					<ProfileTab active={tab === EProfile.OVERVIEW}>
+					<ProfileTab $active={tab === EProfile.OVERVIEW}>
 						{formatMessage({ id: 'label.overview' })}
 					</ProfileTab>
 				</Link>
 				<Link href={pathname + profileTabs.givpower}>
-					<ProfileTab active={tab === EProfile.GIVPOWER}>
+					<ProfileTab $active={tab === EProfile.GIVPOWER}>
 						{`${
 							myAccount
 								? formatMessage({ id: 'label.my_givpower' })
 								: 'GIVpower'
 						}`}
 						{myAccount && user?.boostedProjectsCount !== 0 && (
-							<Count active={tab === EProfile.GIVPOWER}>
+							<Count $active={tab === EProfile.GIVPOWER}>
 								{user?.boostedProjectsCount}
 							</Count>
 						)}
 					</ProfileTab>
 				</Link>
 				<Link href={pathname + profileTabs.projects}>
-					<ProfileTab active={tab === EProfile.PROJECTS}>
+					<ProfileTab $active={tab === EProfile.PROJECTS}>
 						{`${
 							myAccount
 								? formatMessage({ id: 'label.my_projects' })
 								: formatMessage({ id: 'label.projects' })
 						}`}
 						{myAccount && user?.projectsCount != 0 && (
-							<Count active={tab === EProfile.PROJECTS}>
+							<Count $active={tab === EProfile.PROJECTS}>
 								{user?.projectsCount}
 							</Count>
 						)}
 					</ProfileTab>
 				</Link>
 				<Link href={pathname + profileTabs.donations}>
-					<ProfileTab active={tab === EProfile.DONATIONS}>
+					<ProfileTab $active={tab === EProfile.DONATIONS}>
 						{`${
 							myAccount
 								? formatMessage({
@@ -110,7 +110,7 @@ const ProfileContributes: FC<IUserProfileView> = () => {
 								: formatMessage({ id: 'label.donations' })
 						}`}
 						{myAccount && user?.donationsCount != 0 && (
-							<Count active={tab === EProfile.DONATIONS}>
+							<Count $active={tab === EProfile.DONATIONS}>
 								{user?.donationsCount}
 							</Count>
 						)}
@@ -118,12 +118,12 @@ const ProfileContributes: FC<IUserProfileView> = () => {
 				</Link>
 				<Link href={pathname + profileTabs.likedProjects}>
 					<ProfileTab
-						active={tab === EProfile.LIKED}
+						$active={tab === EProfile.LIKED}
 						onClick={() => setTab(EProfile.LIKED)}
 					>
 						{formatMessage({ id: 'label.liked_projects' })}
 						{myAccount && !!user?.likedProjectsCount && (
-							<Count active={tab === EProfile.LIKED}>
+							<Count $active={tab === EProfile.LIKED}>
 								{user?.likedProjectsCount}
 							</Count>
 						)}
@@ -158,9 +158,9 @@ const ProfileTab = styled(P)<ITab>`
 	white-space: nowrap;
 	cursor: pointer;
 	color: ${(props: ITab) =>
-		props.active ? brandColors.deep[600] : brandColors.pinky[500]};
+		props.$active ? brandColors.deep[600] : brandColors.pinky[500]};
 	${props =>
-		props.active &&
+		props.$active &&
 		`
 		background: ${neutralColors.gray[100]};
 		box-shadow: 0 3px 20px rgba(212, 218, 238, 0.4);
@@ -170,7 +170,7 @@ const ProfileTab = styled(P)<ITab>`
 
 const Count = styled.div<ITab>`
 	background-color: ${(props: ITab) =>
-		props.active ? neutralColors.gray[500] : brandColors.pinky[500]};
+		props.$active ? neutralColors.gray[500] : brandColors.pinky[500]};
 	color: white;
 	width: 24px;
 	height: 24px;
