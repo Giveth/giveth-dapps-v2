5bf16b8b8f48e1f99135de40537ca4629ab0f1f5
Cherik <Pourcheriki@gmail.com>
2024-02-26 11:10:29 +0330

    fix common.tsx

--- src/components/menu/common.tsx
+++ src/components/menu/common.tsx
@@ -4,9 +4,9 @@ import {
 	GLink,
 	neutralColors,
 	Overline,
+	Flex,
 } from '@giveth/ui-design-system';
 import styled from 'styled-components';
-import { Flex } from '@giveth/ui-design-system';
 import { ETheme } from '@/features/general/general.slice';
 import { Shadow } from '../styled-components/Shadow';
 
