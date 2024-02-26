4927223f95c2861bd1a75925e21329faad88eb0f
Cherik <Pourcheriki@gmail.com>
2024-02-26 11:10:31 +0330

    fix ProjectsCampaignBlock.tsx

--- src/components/views/homepage/ProjectsCampaignBlock.tsx
+++ src/components/views/homepage/ProjectsCampaignBlock.tsx
@@ -5,6 +5,10 @@ import {
 	IconPointerLeft,
 	IconPointerRight,
 	mediaQueries,
+	Container,
+	OneSideContainer,
+	Flex,
+	FlexCenter,
 } from '@giveth/ui-design-system';
 import styled from 'styled-components';
 import { useIntl } from 'react-intl';
@@ -13,8 +17,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { FC, useRef, useState } from 'react';
 import { Swiper as SwiperClass } from 'swiper/types';
 import Link from 'next/link';
-import { Container, OneSideContainer } from '@giveth/ui-design-system';
-import { Flex, FlexCenter } from '@giveth/ui-design-system';
 import { ICampaign } from '@/apollo/types/types';
 import ProjectCard from '@/components/project-card/ProjectCard';
 import 'swiper/css';
