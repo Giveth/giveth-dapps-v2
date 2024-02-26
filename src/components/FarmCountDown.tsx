21e0e50d32a4ce0b68b7845352169f7059087f91
Cherik <Pourcheriki@gmail.com>
2024-02-26 11:10:27 +0330

    fix FarmCountDown.tsx

--- src/components/FarmCountDown.tsx
+++ src/components/FarmCountDown.tsx
@@ -1,7 +1,6 @@
-import { Lead, Subline } from '@giveth/ui-design-system';
+import { Lead, Subline, Flex } from '@giveth/ui-design-system';
 import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Flex } from '@giveth/ui-design-system';
 import { getNowUnixMS } from '@/helpers/time';
 import { durationToString } from '@/lib/helpers';
 import type { Dispatch, SetStateAction } from 'react';
