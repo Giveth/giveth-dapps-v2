748a176de138d4cd08e1cbb9fdfa0e1962d4e1a4
Cherik <Pourcheriki@gmail.com>
2024-03-18 11:22:45 +0330

    init modify stream

---
+++ src/components/views/userProfile/donationsTab/recurringTab/ModifyStreamModal.tsx
@@ -0,0 +1,34 @@
+import { IconDonation32 } from '@giveth/ui-design-system';
+import { FC } from 'react';
+import { useIntl } from 'react-intl';
+import { Modal } from '@/components/modals/Modal';
+import { useModalAnimation } from '@/hooks/useModalAnimation';
+import { IModal } from '@/types/common';
+
+interface IModifyStreamModalProps extends IModal {}
+
+export const ModifyStreamModal: FC<IModifyStreamModalProps> = ({
+	...props
+}) => {
+	const { isAnimating, closeModal } = useModalAnimation(props.setShowModal);
+
+	const { formatMessage } = useIntl();
+
+	return (
+		<Modal
+			closeModal={closeModal}
+			isAnimating={isAnimating}
+			headerTitle={formatMessage({
+				id: 'label.modify_recurring_donation_amount',
+			})}
+			headerTitlePosition='left'
+			headerIcon={<IconDonation32 />}
+		>
+			<ModifyStreamInnerModal {...props} />
+		</Modal>
+	);
+};
+
+const ModifyStreamInnerModal: FC<IModifyStreamModalProps> = () => {
+	return <div>ModifyStreamInnerModal</div>;
+};
