042dbf303dded5acbf22e0848a756669049fa211
Cherik <Pourcheriki@gmail.com>
2024-03-18 12:21:27 +0330

    update styles

--- src/components/views/userProfile/donationsTab/recurringTab/ModifyStreamModal.tsx
+++ src/components/views/userProfile/donationsTab/recurringTab/ModifyStreamModal.tsx
@@ -2,12 +2,16 @@ import {
 	B,
 	Caption,
 	Flex,
-	IconCaretDown16,
 	IconDonation32,
 	IconHelpFilled16,
+	mediaQueries,
+	neutralColors,
 } from '@giveth/ui-design-system';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useIntl } from 'react-intl';
+import styled from 'styled-components';
+import { useAccount, useBalance } from 'wagmi';
+import { formatUnits } from 'viem';
 import { Modal } from '@/components/modals/Modal';
 import { useModalAnimation } from '@/hooks/useModalAnimation';
 import { IModal } from '@/types/common';
@@ -15,11 +19,8 @@ import { IWalletRecurringDonation } from '@/apollo/types/types';
 import { FlowRateTooltip } from '@/components/GIVeconomyPages/GIVstream.sc';
 import { IconWithTooltip } from '@/components/IconWithToolTip';
 import { TokenIcon } from '@/components/views/donate/TokenIcon/TokenIcon';
-import {
-	SelectTokenWrapper,
-	InputWrapper,
-} from '@/components/views/donate/RecurringDonationCard';
 import config from '@/configuration';
+import { limitFraction } from '@/helpers/number';
 
 interface IModifyStreamModalProps extends IModal {
 	donation: IWalletRecurringDonation;
@@ -49,73 +50,91 @@ export const ModifyStreamModal: FC<IModifyStreamModalProps> = ({
 
 const ModifyStreamInnerModal: FC<IModifyStreamModalProps> = ({ donation }) => {
 	const { formatMessage } = useIntl();
+	const { address } = useAccount();
 
 	console.log('donation', donation);
-	const superToken = config.OPTIMISM_CONFIG.SUPER_FLUID_TOKENS.find(
-		s => s.underlyingToken.symbol === donation.currency,
+	const superToken = useMemo(
+		() =>
+			config.OPTIMISM_CONFIG.SUPER_FLUID_TOKENS.find(
+				s => s.underlyingToken.symbol === donation.currency,
+			),
+		[donation.currency],
 	);
-	console.log('superToken', superToken);
+	const { data: balance } = useBalance({
+		token: superToken?.id,
+		address,
+	});
+
+	console.log('superToken', superToken, balance);
 	return (
-		<Flex $flexDirection='column' gap='8px'>
-			<Flex gap='8px' $alignItems='center'>
-				<Caption $medium>
-					{formatMessage({ id: 'label.stream_balance' })}
-				</Caption>
-				<IconWithTooltip
-					icon={<IconHelpFilled16 />}
-					direction='right'
-					align='bottom'
-				>
-					<FlowRateTooltip>
-						{formatMessage({
-							id: 'tooltip.flowrate',
-						})}
-					</FlowRateTooltip>
-				</IconWithTooltip>
-			</Flex>
-			<InputWrapper>
-				<SelectTokenWrapper
-					$alignItems='center'
-					$justifyContent='space-between'
-					// onClick={() => setShowSelectTokenModal(true)}
-				>
-					<Flex gap='8px' $alignItems='center'>
+		<Wrapper>
+			<Flex $flexDirection='column' gap='8px'>
+				<Flex gap='8px' $alignItems='center'>
+					<Caption $medium>
+						{formatMessage({ id: 'label.stream_balance' })}
+					</Caption>
+					<IconWithTooltip
+						icon={<IconHelpFilled16 />}
+						direction='right'
+						align='bottom'
+					>
+						<FlowRateTooltip>
+							{formatMessage({
+								id: 'tooltip.flowrate',
+							})}
+						</FlowRateTooltip>
+					</IconWithTooltip>
+				</Flex>
+				<TokenInfoWrapper>
+					<TokenSymbol gap='8px' $alignItems='center'>
 						<TokenIcon
 							symbol={superToken?.underlyingToken.symbol}
 							size={24}
 						/>
-						<B>{superToken?.symbol}</B>
-					</Flex>
-					<IconCaretDown16 />
-				</SelectTokenWrapper>
-				<p>
-					{/* {limitFraction(
-						formatUnits(
-							balance?.value || 0n,
-							selectedToken.token.decimals,
-						),
-					)} */}
-				</p>
-			</InputWrapper>
-			{/* {!selectedToken?.token.isSuperToken &&
-				selectedToken !== undefined &&
-				balance !== undefined && (
-					<Flex gap='4px'>
-						<GLink size='Small'>
-							{formatMessage({
-								id: 'label.available',
-							})}
-							: {limitFraction(balance?.formatted)}
-						</GLink>
-						<IconWrapper onClick={() => !isRefetching && refetch()}>
-							{isRefetching ? (
-								<Spinner size={16} />
-							) : (
-								<IconRefresh16 />
-							)}
-						</IconWrapper>
-					</Flex>
-				)} */}
-		</Flex>
+						<B>{superToken?.underlyingToken.symbol}</B>
+					</TokenSymbol>
+					<TokenBalance>
+						{limitFraction(
+							formatUnits(
+								balance?.value || 0n,
+								balance?.decimals || 18,
+							),
+						)}
+						&nbsp;
+						{superToken?.symbol}
+					</TokenBalance>
+				</TokenInfoWrapper>
+			</Flex>
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled(Flex)`
+	flex-direction: column;
+	align-items: stretch;
+	justify-content: stretch;
+	gap: 16px;
+	width: 100%;
+	padding: 16px 24px 24px 24px;
+	${mediaQueries.tablet} {
+		width: 630px;
+	}
+`;
+
+const TokenInfoWrapper = styled(Flex)`
+	border: 2px solid ${neutralColors.gray[300]};
+	border-radius: 8px;
+	overflow: hidden;
+	& > * {
+		padding: 13px 16px;
+	}
+	align-items: center;
+`;
+
+const TokenSymbol = styled(Flex)`
+	min-width: 140px;
+`;
+
+const TokenBalance = styled(B)`
+	border-left: 2px solid ${neutralColors.gray[300]};
+`;
