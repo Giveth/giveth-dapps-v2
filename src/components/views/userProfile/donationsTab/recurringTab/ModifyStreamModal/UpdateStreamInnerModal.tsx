70fb8b756ff1a6916bfa296ca877f8fd101b9f84
Cherik <Pourcheriki@gmail.com>
2024-03-20 14:27:19 +0330

    init UpdateStreamInnerModal

--- src/components/views/userProfile/donationsTab/recurringTab/ModifyStreamModal/UpdateStreamInnerModal.tsx
+++ src/components/views/userProfile/donationsTab/recurringTab/ModifyStreamModal/UpdateStreamInnerModal.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { EDonationSteps, IModifyStreamModalProps } from './ModifyStreamModal';
+import { Wrapper } from './ModifyStreamInnerModal';
+import { Item } from '@/components/views/donate/RecurringDonationModal/Item';
+import { IToken } from '@/types/superFluid';
+import { RunOutInfo } from '@/components/views/donate/RunOutInfo';
+import { useTokenPrice } from '@/hooks/useTokenPrice';
 
-export const UpdateStreamInnerModal = () => {
-	return <div>UpdateStreamInnerModal</div>;
+interface IModifyStreamInnerModalProps extends IModifyStreamModalProps {
+	step: EDonationSteps;
+	setStep: (step: EDonationSteps) => void;
+	token: IToken;
+	amount: bigint;
+	totalPerMonth: bigint;
+}
+
+export const UpdateStreamInnerModal: FC<IModifyStreamInnerModalProps> = ({
+	donation,
+	step,
+	setStep,
+	token,
+	amount,
+	totalPerMonth,
+}) => {
+	const tokenPrice = useTokenPrice(token);
+	return (
+		<Wrapper>
+			<Item
+				title={`Donate to ${donation.project.title}`}
+				subtext={'per month'}
+				amount={amount}
+				price={tokenPrice}
+				token={token}
+			/>
+			<RunOutInfo
+				amount={amount}
+				totalPerMonth={totalPerMonth}
+				symbol={token.symbol || ''}
+			/>
+		</Wrapper>
+	);
 };
