2c19f6eb697ba1712f013f278453c3dfa9e44d1a
Cherik <Pourcheriki@gmail.com>
2024-03-25 16:08:23 +0330

    init ArchiveStreamModal

---
+++ src/components/views/userProfile/donationsTab/recurringTab/ArchiveStreamModal.tsx
@@ -0,0 +1,119 @@
+import {
+	Flex,
+	H3,
+	IconAlertTriangleOutline32,
+	Lead,
+	mediaQueries,
+} from '@giveth/ui-design-system';
+import { useIntl } from 'react-intl';
+import { useState, type FC } from 'react';
+import styled from 'styled-components';
+import { useAccount } from 'wagmi';
+import { Modal } from '@/components/modals/Modal';
+import { useModalAnimation } from '@/hooks/useModalAnimation';
+import { IModal } from '@/types/common';
+import { ActionButton } from './ModifyStreamModal/ModifyStreamInnerModal';
+import { IWalletRecurringDonation } from '@/apollo/types/types';
+
+enum EArchiveStreamSteps {
+	CONFIRM,
+	ARCHIVING,
+	SUCCESS,
+}
+
+export interface IArchiveStreamModalProps extends IModal {
+	donation: IWalletRecurringDonation;
+	refetch: () => void;
+}
+
+export const ArchiveStreamModal: FC<IArchiveStreamModalProps> = ({
+	...props
+}) => {
+	const { isAnimating, closeModal } = useModalAnimation(props.setShowModal);
+	const { formatMessage } = useIntl();
+
+	return (
+		<Modal
+			closeModal={closeModal}
+			isAnimating={isAnimating}
+			headerTitle={formatMessage({
+				id: 'label.archive_stream',
+			})}
+			headerTitlePosition='left'
+			headerIcon={<IconAlertTriangleOutline32 />}
+		>
+			<ArchiveStreamInnerModal {...props} />
+		</Modal>
+	);
+};
+
+interface IArchiveStreamInnerModalProps extends IArchiveStreamModalProps {}
+
+const ArchiveStreamInnerModal: FC<IArchiveStreamInnerModalProps> = ({
+	setShowModal,
+	donation,
+	refetch,
+}) => {
+	const [step, setStep] = useState(EArchiveStreamSteps.CONFIRM);
+	const { formatMessage } = useIntl();
+	const { address } = useAccount();
+
+	const onArchive = () => {
+		setStep(EArchiveStreamSteps.ARCHIVING);
+		setStep(EArchiveStreamSteps.SUCCESS);
+	};
+
+	return step === EArchiveStreamSteps.CONFIRM ||
+		step === EArchiveStreamSteps.ARCHIVING ? (
+		<Wrapper>
+			<Lead>
+				{formatMessage({
+					id: 'component.archive_stream_modal.confirm_question',
+				})}
+			</Lead>
+			<Flex gap='16px'>
+				<ActionButton
+					label={formatMessage({ id: 'label.cancel' })}
+					onClick={() => setShowModal(false)}
+					buttonType='texty-gray'
+					disabled={step === EArchiveStreamSteps.ARCHIVING}
+				/>
+				<ActionButton
+					label={formatMessage({
+						id: 'component.archive_stream_modal.confirm_button',
+					})}
+					onClick={() => onArchive()}
+					disabled={step === EArchiveStreamSteps.ARCHIVING}
+					loading={step === EArchiveStreamSteps.ARCHIVING}
+				/>
+			</Flex>
+		</Wrapper>
+	) : (
+		<Wrapper>
+			<H3 weight={700}>
+				{formatMessage({
+					id: 'component.archive_stream_modal.archived_title',
+				})}
+			</H3>
+			<ActionButton
+				label={formatMessage({ id: 'label.done' })}
+				onClick={() => {
+					setShowModal(false);
+				}}
+			/>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled(Flex)`
+	text-align: left;
+	flex-direction: column;
+	align-items: stretch;
+	justify-content: stretch;
+	gap: 16px;
+	width: 100%;
+	padding: 16px 24px 24px 24px;
+	${mediaQueries.tablet} {
+		width: 530px;
+	}
+`;
