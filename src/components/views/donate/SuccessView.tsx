bbb63e4b3ee0fa2f18efa03d52c0df6d11fd4352
alireza-sharifpour <ascode94@gmail.com>
2024-03-25 18:00:53 +0330

    remove comment

--- src/components/views/donate/SuccessView.tsx
+++ src/components/views/donate/SuccessView.tsx
@@ -130,7 +130,6 @@ export const SuccessView: FC = () => {
 								contentType={EContentType.justDonated}
 							/>
 						</SocialBoxWrapper>
-						{/* {isRecurringActive && <DonationInfo />} */}
 					</RightSectionWrapper>
 				</Col>
 			</Row>
