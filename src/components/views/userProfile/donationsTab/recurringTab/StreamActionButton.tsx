f5e5a356acf56f5accad5422aa595d777036629a
Cherik <Pourcheriki@gmail.com>
2024-03-26 13:21:44 +0330

    move to recurring tab

--- src/components/views/userProfile/donationsTab/recurringTab/StreamActionButton.tsx
+++ src/components/views/userProfile/donationsTab/recurringTab/StreamActionButton.tsx
@@ -63,6 +63,7 @@ export const StreamActionButton: FC<IStreamActionButtonProps> = ({
 							cb: () =>
 								router.push(
 									slugToProjectDonate(donation.project.slug),
+									{ query: { tab: 'recurring' } },
 								),
 						},
 						{
