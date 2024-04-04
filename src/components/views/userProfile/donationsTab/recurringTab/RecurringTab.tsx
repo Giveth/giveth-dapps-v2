f476a845e75b529c39a8bc2b2605890ef118b821
Cherik <Pourcheriki@gmail.com>
2024-04-04 15:56:01 +0330

    show ActiveStreamsSection for my account

--- src/components/views/userProfile/donationsTab/recurringTab/RecurringTab.tsx
+++ src/components/views/userProfile/donationsTab/recurringTab/RecurringTab.tsx
@@ -1,11 +1,13 @@
 import { Flex } from '@giveth/ui-design-system';
 import { ActiveProjectsSection } from './ActiveProjectsSection';
 import { ActiveStreamsSection } from './ActiveStreamsSection';
+import { useProfileContext } from '@/context/profile.context';
 
 export const RecurringTab = () => {
+	const { myAccount } = useProfileContext();
 	return (
 		<Flex $flexDirection='column' gap='24px'>
-			<ActiveStreamsSection />
+			{myAccount && <ActiveStreamsSection />}
 			<ActiveProjectsSection />
 		</Flex>
 	);
