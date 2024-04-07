f7a0a048e7fa10bc0f47af97f1b42a09e378dff7
Cherik <Pourcheriki@gmail.com>
2024-04-07 15:28:19 +0330

    add new scores

--- src/components/views/create/proGuide/score/scoreHelpers.ts
+++ src/components/views/create/proGuide/score/scoreHelpers.ts
@@ -6,6 +6,7 @@ export enum EScoreType {
 	LOCATION = 'LOCATION',
 	IMAGE = 'IMAGE',
 	DESC_MEDIA = 'DESC_MEDIA',
+	SOCIAL_MEDIA = 'SOCIAL_MEDIA',
 }
 
 export enum EQualityState {
@@ -33,6 +34,7 @@ export const initialState: ScoreState = {
 	[EScoreType.LOCATION]: 0,
 	[EScoreType.IMAGE]: 0,
 	[EScoreType.DESC_MEDIA]: 0,
+	[EScoreType.SOCIAL_MEDIA]: 0,
 	totalScore: 0,
 	quality: EQualityState.LOW,
 };
@@ -83,17 +85,20 @@ export function calculateScore(
 			break;
 		case EScoreType.CATEGORIES:
 			newState[EScoreType.CATEGORIES] =
-				action.payload && action.payload.length > 0 ? 9 : 0;
+				action.payload && action.payload.length > 0 ? 7 : 0;
 			break;
 		case EScoreType.LOCATION:
-			newState[EScoreType.LOCATION] = action.payload ? 9 : 0;
+			newState[EScoreType.LOCATION] = action.payload ? 7 : 0;
 			break;
 		case EScoreType.IMAGE:
 			newState[EScoreType.IMAGE] =
-				action.payload && !action.payload.startsWith('/') ? 19 : 0;
+				action.payload && !action.payload.startsWith('/') ? 15 : 0;
 			break;
 		case EScoreType.DESC_MEDIA:
-			newState[EScoreType.DESC_MEDIA] = action.payload ? 12 : 0;
+			newState[EScoreType.DESC_MEDIA] = action.payload ? 8 : 0;
+			break;
+		case EScoreType.SOCIAL_MEDIA:
+			newState[EScoreType.SOCIAL_MEDIA] = action.payload ? 12 : 0;
 			break;
 		default:
 			return state;
