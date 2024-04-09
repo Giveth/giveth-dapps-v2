d2dfadef68de64756f59cb8fb8c9e910444b558b
Cherik <Pourcheriki@gmail.com>
2024-04-09 12:16:27 +0330

    add space

--- src/components/views/archivedQF/ArchivedQF.view.tsx
+++ src/components/views/archivedQF/ArchivedQF.view.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Flex } from '@giveth/ui-design-system';
 import { ArchivedQFBanner } from './ArchivedQFBanner';
+import { EQFPageStatus, QFHeader } from './QFHeader';
 
 export const ArchivedQFView = () => {
 	return (
-		<div>
+		<Wrapper>
 			<ArchivedQFBanner />
-		</div>
+			<QFHeader status={EQFPageStatus.ARCHIVED} />
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled(Flex)`
+	flex-direction: column;
+	gap: 40px;
+`;
