a758eaf9b55bc4fff539b3896b5b9f7de6894eea
Cherik <Pourcheriki@gmail.com>
2024-04-09 11:45:20 +0330

    init route

---
+++ pages/qf-archive/index.tsx
@@ -0,0 +1,10 @@
+import { ArchivedQFView } from '@/components/views/archivedQF/ArchivedQF.view';
+
+const ArchivedQFPageRoute = () => {
+	return (
+		<div>
+			<ArchivedQFView />
+		</div>
+	);
+};
+export default ArchivedQFPageRoute;
