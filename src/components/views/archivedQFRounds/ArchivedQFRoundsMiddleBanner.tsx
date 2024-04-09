693daf7ea8e4e9c4bff1a5cf916902ff393cfd4f
Cherik <Pourcheriki@gmail.com>
2024-04-09 15:20:05 +0330

    add ArchivedQFRoundsMiddleBanner

---
+++ src/components/views/archivedQFRounds/ArchivedQFRoundsMiddleBanner.tsx
@@ -0,0 +1,32 @@
+import { semanticColors } from '@giveth/ui-design-system';
+import { useIntl } from 'react-intl';
+import styled from 'styled-components';
+import {
+	BigArc,
+	Box,
+	Title,
+	Caption,
+} from '../projects/MiddleBanners/common.sc';
+
+export const ArchivedQFRoundsMiddleBanner = () => {
+	const { formatMessage } = useIntl();
+	return (
+		<Box $flexDirection='column' gap='23px'>
+			<StyledBigArc color={semanticColors.jade[200]} />
+			<Title weight={700} color={semanticColors.jade[700]}>
+				{formatMessage({
+					id: 'component.qf_middle_banner.title',
+				})}
+			</Title>
+			<Caption>
+				{formatMessage({
+					id: 'component.qf_middle_banner.desc',
+				})}
+			</Caption>
+		</Box>
+	);
+};
+
+const StyledBigArc = styled(BigArc)`
+	left: -125%;
+`;
