ecd5465f5799161874320caa1612270ca6bf509e
Cherik <Pourcheriki@gmail.com>
2024-04-10 00:08:48 +0330

    add wrapper

--- src/components/cards/StakingCards/BaseStakingCard/StakeTogetherCard.tsx
+++ src/components/cards/StakingCards/BaseStakingCard/StakeTogetherCard.tsx
@@ -1,5 +1,16 @@
+import { Flex, brandColors } from '@giveth/ui-design-system';
 import React from 'react';
+import styled from 'styled-components';
 
 export const StakeTogetherCard = () => {
-	return <div>StakeTogether</div>;
+	return <Wrapper>StakeTogether</Wrapper>;
 };
+
+const Wrapper = styled(Flex)`
+	flex-direction: column;
+	padding: 24px;
+	align-items: flex-start;
+	gap: 16px;
+	border-radius: 8px;
+	background-color: ${brandColors.giv[800]};
+`;
