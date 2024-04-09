454dc2ba4cc89337bf92bb23f83bd9d3a0508893
Cherik <Pourcheriki@gmail.com>
2024-04-09 11:45:26 +0330

    init view

---
+++ src/components/views/archivedQF/ArchivedQF.view.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ArchivedQFBanner } from './ArchivedQFBanner';
+
+export const ArchivedQFView = () => {
+	return (
+		<div>
+			<ArchivedQFBanner />
+		</div>
+	);
+};
