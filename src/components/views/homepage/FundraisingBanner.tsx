595bb116750f4d12ac08b4857add6550bf0d8858
Mitch Oz <divine_comedian@protonmail.com>
2024-04-18 15:12:48 -0600

    reinsert octant banner

--- src/components/views/homepage/FundraisingBanner.tsx
+++ src/components/views/homepage/FundraisingBanner.tsx
@@ -1,26 +1,27 @@
 import styled from 'styled-components';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { brandColors, P } from '@giveth/ui-design-system';
 import { FlexCenter } from '@/components/styled-components/Flex';
 import ExternalLink from '@/components/ExternalLink';
-import GitcoinLogo from 'public/images/gitcoin-grants.png';
+// import GitcoinLogo from 'public/images/gitcoin-grants.png';
 
 const FundraisingBanner = () => {
 	return (
 		<Wrapper>
 			<P>
-				Support Giveth's growth by donating to us on the Glo Dollar x
-				Arbitrum DAO round on Gitcoin.
+				<b>Got $GLM?</b> The allocation window is open for Octant Epoch
+				4, allocate your staking rewards to Giveth to support our
+				growth!
 			</P>
-			<Image
+			{/* <Image
 				src={GitcoinLogo}
 				alt='Gitcoin Grants'
 				width={24}
 				height={24}
-			/>
+			/> */}
 			<PStyled>
-				<ExternalLink href='https://explorer.gitcoin.co/#/round/42161/0xc5262fc451a72f68f83563e30126d9c1d5d77659/0xc5262fc451a72f68f83563e30126d9c1d5d77659-12'>
-					<Purple> Click here to view our grant. </Purple>
+				<ExternalLink href='https://octant.app/project/3/0x6e8873085530406995170Da467010565968C7C62'>
+					<Purple>View our project. </Purple>
 				</ExternalLink>
 			</PStyled>
 		</Wrapper>
