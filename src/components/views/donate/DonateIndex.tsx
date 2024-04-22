c285c8c7a77ecb7465105f5776b2df858a148c74
Cherik <Pourcheriki@gmail.com>
2024-04-22 16:25:50 +0330

    fix condition

--- src/components/views/donate/DonateIndex.tsx
+++ src/components/views/donate/DonateIndex.tsx
@@ -74,7 +74,7 @@ const DonateIndex: FC = () => {
 				)}
 				<DonateContainer>
 					{/* <PurchaseXDAI /> */}
-					{alreadyDonated && isRecurringTab && (
+					{alreadyDonated && !isRecurringTab && (
 						<AlreadyDonatedWrapper>
 							<IconDonation24 />
 							<SublineBold>
