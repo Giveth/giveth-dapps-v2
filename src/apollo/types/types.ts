37a0bd44e2e8b0351e122da6091591cfcf8231f5
Cherik <Pourcheriki@gmail.com>
2024-05-11 23:33:15 +0330

    remove unused params

--- src/apollo/types/types.ts
+++ src/apollo/types/types.ts
@@ -478,8 +478,7 @@ export interface IQFRound {
 }
 
 export interface IArchivedQFRound extends IQFRound {
-	matchingFund: number;
-	raisedFundInUsd: number;
+	totalDonations: number;
 	uniqueDonors: number;
 }
 
