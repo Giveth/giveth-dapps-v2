a9350c56cfc4a00403acacaaeb265a8cac92569d
Cherik <Pourcheriki@gmail.com>
2024-05-12 10:22:34 +0330

    add arc

--- src/components/views/archivedQFRounds/ArchivedQFRoundsMiddleBanner.tsx
+++ src/components/views/archivedQFRounds/ArchivedQFRoundsMiddleBanner.tsx
@@ -1,18 +1,13 @@
 import { Container, semanticColors } from '@giveth/ui-design-system';
 import { useIntl } from 'react-intl';
-import styled from 'styled-components';
-import {
-	BigArc,
-	Box,
-	Title,
-	Caption,
-} from '../projects/MiddleBanners/common.sc';
+import Image from 'next/image';
+import { Box, Title, Caption } from '../projects/MiddleBanners/common.sc';
 
 export const ArchivedQFRoundsMiddleBanner = () => {
 	const { formatMessage } = useIntl();
 	return (
 		<Box $flexDirection='column' gap='23px'>
-			<StyledBigArc color={semanticColors.jade[200]} />
+			<Image src='/images/arcs/arc1.svg' alt='arc' fill />
 			<Container>
 				<Title weight={700} color={semanticColors.jade[700]}>
 					{formatMessage({
@@ -28,7 +23,3 @@ export const ArchivedQFRoundsMiddleBanner = () => {
 		</Box>
 	);
 };
-
-const StyledBigArc = styled(BigArc)`
-	left: -125%;
-`;
