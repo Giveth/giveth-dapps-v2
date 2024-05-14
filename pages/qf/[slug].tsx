81ba0d2c63a7162a78b7b7c51165abe132c0f11e
Cherik <Pourcheriki@gmail.com>
2024-05-14 14:49:21 +0330

    make qf pages ssg

--- pages/qf/[slug].tsx
+++ pages/qf/[slug].tsx
@@ -1,8 +1,6 @@
-import { GetServerSideProps } from 'next/types';
+import { GetStaticProps } from 'next/types';
 import { EProjectsFilter, IMainCategory } from '@/apollo/types/types';
-import { transformGraphQLErrorsToStatusCode } from '@/helpers/requests';
 import { initializeApollo } from '@/apollo/apolloClient';
-import { OPTIONS_HOME_PROJECTS } from '@/apollo/gql/gqlOptions';
 import {
 	FETCH_ALL_PROJECTS,
 	FETCH_MAIN_CATEGORIES,
@@ -14,8 +12,7 @@ import { ProjectsProvider } from '@/context/projects.context';
 import { FETCH_QF_ROUNDS } from '@/apollo/gql/gqlQF';
 import { useReferral } from '@/hooks/useReferral';
 import { IProjectsRouteProps, allCategoriesItem } from 'pages/projects/[slug]';
-import { getMainCategorySlug } from '@/helpers/projects';
-import { EProjectsSortBy } from '@/apollo/types/gqlEnums';
+import { OPTIONS_HOME_PROJECTS } from '@/apollo/gql/gqlOptions';
 
 interface IProjectsCategoriesRouteProps extends IProjectsRouteProps {
 	selectedMainCategory: IMainCategory;
@@ -45,89 +42,72 @@ const QFProjectsCategoriesRoute = (props: IProjectsCategoriesRouteProps) => {
 	);
 };
 
-export const getServerSideProps: GetServerSideProps = async context => {
+// This function gets called at build time
+export async function getStaticPaths() {
 	const apolloClient = initializeApollo();
-	const { variables, notifyOnNetworkStatusChange } = OPTIONS_HOME_PROJECTS;
+
+	// Call an external API endpoint to get posts
+	const { data } = await apolloClient.query({
+		query: FETCH_MAIN_CATEGORIES,
+		fetchPolicy: 'no-cache', // Adjust based on your caching policy needs
+	});
+
+	const mainCategories = data.mainCategories as IMainCategory[];
+
+	// Get the paths we want to pre-render based on posts
+	const _paths = mainCategories.map(category => ({
+		params: { slug: category.slug },
+	}));
+
+	const paths = [{ params: { slug: 'all' } }, ..._paths];
+
+	// We'll pre-render only these paths at build time.
+	// { fallback: false } means other routes should 404.
+	return { paths, fallback: false };
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const apolloClient = initializeApollo();
+	const { variables } = OPTIONS_HOME_PROJECTS;
 	try {
-		const { query } = context;
-		const slug = query.slug;
-
-		const {
-			data: { mainCategories },
-		}: {
-			data: { mainCategories: IMainCategory[] };
-		} = await apolloClient.query({
+		// Fetch main categories
+		const { data: mainCategoriesData } = await apolloClient.query({
 			query: FETCH_MAIN_CATEGORIES,
-			fetchPolicy: 'network-only',
+			fetchPolicy: 'no-cache', // Adjust based on your caching policy needs
 		});
 
-		const updatedMainCategory = [allCategoriesItem, ...mainCategories];
-		const selectedMainCategory = updatedMainCategory.find(mainCategory => {
-			return mainCategory.slug === slug;
+		// Fetch projects with a predefined sorting
+		const { data: projectsData } = await apolloClient.query({
+			query: FETCH_ALL_PROJECTS,
+			variables: {
+				...variables,
+				mainCategory: params?.slug === 'all' ? null : params?.slug,
+				filters: [EProjectsFilter.ACTIVE_QF_ROUND],
+			},
+			fetchPolicy: 'no-cache',
 		});
 
-		if (selectedMainCategory) {
-			const updatedSelectedMainCategory = {
-				...selectedMainCategory,
-				selected: true,
-			};
-			const apolloClient = initializeApollo();
+		// Fetch QF rounds
+		const { data: qfRoundsData } = await apolloClient.query({
+			query: FETCH_QF_ROUNDS,
+			fetchPolicy: 'no-cache',
+		});
 
-			let _filters = query.filter
-				? Array.isArray(query.filter)
-					? query.filter
-					: [query.filter]
-				: undefined;
-
-			_filters
-				? _filters.push(EProjectsFilter.ACTIVE_QF_ROUND)
-				: (_filters = [EProjectsFilter.ACTIVE_QF_ROUND]);
-
-			const { data } = await apolloClient.query({
-				query: FETCH_ALL_PROJECTS,
-				variables: {
-					...variables,
-					sortingBy: query.sort || EProjectsSortBy.INSTANT_BOOSTING,
-					searchTerm: query.searchTerm,
-					filters: _filters,
-					campaignSlug: query.campaignSlug,
-					category: query.category,
-					mainCategory: getMainCategorySlug(
-						updatedSelectedMainCategory,
-					),
-					notifyOnNetworkStatusChange,
-				},
-				fetchPolicy: 'network-only',
-			});
-			const { projects, totalCount } = data.allProjects;
-			const {
-				data: { qfRounds },
-			} = await apolloClient.query({
-				query: FETCH_QF_ROUNDS,
-				fetchPolicy: 'network-only',
-			});
-			return {
-				props: {
-					projects,
-					mainCategories: updatedMainCategory,
-					selectedMainCategory: updatedSelectedMainCategory,
-					totalCount,
-					qfRounds,
-				},
-			};
-		}
-		return {
-			notFound: true,
-		};
-	} catch (error: any) {
-		const statusCode = transformGraphQLErrorsToStatusCode(
-			error?.graphQLErrors,
-		);
 		return {
 			props: {
-				errorStatus: statusCode,
+				projects: projectsData.allProjects.projects,
+				totalCount: projectsData.allProjects.totalCount,
+				mainCategories: [
+					allCategoriesItem,
+					...mainCategoriesData.mainCategories,
+				],
+				qfRounds: qfRoundsData.qfRounds,
 			},
+			revalidate: 300, // Optionally, revalidate at most once per hour
 		};
+	} catch (error) {
+		console.error('Failed to fetch API:', error);
+		return { props: { hasError: true } };
 	}
 };
 
