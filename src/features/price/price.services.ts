6cef703969bbbd9ad7fa45b1c169de8419ed3ccf
Ramin <raminramazanpour@gmail.com>
2024-05-20 19:43:38 +0330

    fix giveth tag for subgraph queries

--- src/features/price/price.services.ts
+++ src/features/price/price.services.ts
@@ -1,6 +1,5 @@
 import BigNumber from 'bignumber.js';
 import config from '@/configuration';
-import { gqlRequest } from '@/helpers/requests';
 import {
 	FETCH_MAINNET_TOKEN_PRICE,
 	FETCH_GNOSIS_TOKEN_PRICE,
@@ -14,14 +13,17 @@ export const fetchMainnetTokenPrice = async (
 		tokenId: tokenId.toLowerCase(),
 		daiId: '0x6b175474e89094c44da98b954eedeac495271d0f'.toLowerCase(),
 	};
-	const { data } = await gqlRequest(
-		config.MAINNET_CONFIG.uniswapV2Subgraph,
-		false,
+	const body = {
 		query,
 		variables,
-	);
-	const tokenEthPrice = new BigNumber(data.token.derivedETH);
-	const daiEthPrice = new BigNumber(data.daitoken.derivedETH);
+	};
+	const res = await fetch(config.MAINNET_CONFIG.uniswapV2Subgraph, {
+		method: 'POST',
+		body: JSON.stringify(body),
+	});
+	const data = await res.json();
+	const tokenEthPrice = new BigNumber(data?.data?.token.derivedETH);
+	const daiEthPrice = new BigNumber(data?.data?.daitoken.derivedETH);
 	return tokenEthPrice.div(daiEthPrice).toString();
 };
 
@@ -37,6 +39,14 @@ export const fetchGnosisTokenPrice = async (
 		console.log('Subgraph is not defined');
 		return '0';
 	}
-	const { data } = await gqlRequest(subgraph, false, query, variables);
-	return data?.token?.derivedETH || '0';
+	const body = {
+		query,
+		variables,
+	};
+	const res = await fetch(subgraph, {
+		method: 'POST',
+		body: JSON.stringify(body),
+	});
+	const data = await res.json();
+	return data?.data?.token?.derivedETH || '0';
 };
