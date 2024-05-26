4c3e7fb1527f22643f71a217062ece65830bb2c3
Cherik <Pourcheriki@gmail.com>
2024-05-26 14:58:29 +0330

    add load more functionality

--- src/components/views/archivedQFRounds/ArchivedQFRounds.view.tsx
+++ src/components/views/archivedQFRounds/ArchivedQFRounds.view.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Container, Flex } from '@giveth/ui-design-system';
+import { Container, Flex, OutlineButton } from '@giveth/ui-design-system';
 import { ArchivedQFBanner } from './ArchivedQFBanner';
 import { EQFPageStatus, QFHeader } from './QFHeader';
 import { client } from '@/apollo/apolloClient';
@@ -10,6 +10,7 @@ import { ArchivedQFRoundsMiddleBanner } from './ArchivedQFRoundsMiddleBanner';
 import { FETCH_ARCHIVED_QF_ROUNDS } from '@/apollo/gql/gqlQF';
 import { useArchivedQFRounds } from './archivedQfRounds.context';
 import { EQFRoundsSortBy } from '@/apollo/types/gqlEnums';
+import { showToastError } from '@/lib/helpers';
 
 enum EQfArchivedRoundsSort {
 	allocatedFund = 'allocatedFund',
@@ -23,53 +24,80 @@ enum EOrderDirection {
 	DESC = 'DESC',
 }
 
+const ITEMS_PER_PAGE = 10;
+
 export const ArchivedQFRoundsView = () => {
 	const [archivedQFRounds, setArchivedQFRounds] = useState<
 		IArchivedQFRound[]
 	>([]);
+	const [hasMore, setHasMore] = useState(true);
+	const [loading, setLoading] = useState(false);
 	const { orderBy } = useArchivedQFRounds();
 
-	useEffect(() => {
-		let field = EQfArchivedRoundsSort.beginDate;
-		let direction = EOrderDirection.DESC;
-		switch (orderBy) {
-			case EQFRoundsSortBy.MATCHING_POOL:
-				field = EQfArchivedRoundsSort.allocatedFund;
-				direction = EOrderDirection.DESC;
-				break;
-			case EQFRoundsSortBy.UNIQUE_DONORS:
-				field = EQfArchivedRoundsSort.uniqueDonors;
-				direction = EOrderDirection.DESC;
-				break;
-			case EQFRoundsSortBy.NEWEST:
-				field = EQfArchivedRoundsSort.beginDate;
-				direction = EOrderDirection.DESC;
-				break;
-			case EQFRoundsSortBy.OLDEST:
-				field = EQfArchivedRoundsSort.beginDate;
-				direction = EOrderDirection.ASC;
-				break;
-			default:
-				break;
-		}
-
-		const fetchQFRounds = async () => {
-			const {
-				data: { qfArchivedRounds },
-			} = await client.query({
-				query: FETCH_ARCHIVED_QF_ROUNDS,
-				fetchPolicy: 'network-only',
-				variables: {
-					orderBy: {
-						field,
-						direction,
+	const fetchQFRounds = useCallback(
+		async (isLoadMore: boolean = false, skip: number) => {
+			setLoading(true);
+			let field = EQfArchivedRoundsSort.beginDate;
+			let direction = EOrderDirection.DESC;
+			switch (orderBy) {
+				case EQFRoundsSortBy.MATCHING_POOL:
+					field = EQfArchivedRoundsSort.allocatedFund;
+					direction = EOrderDirection.DESC;
+					break;
+				case EQFRoundsSortBy.UNIQUE_DONORS:
+					field = EQfArchivedRoundsSort.uniqueDonors;
+					direction = EOrderDirection.DESC;
+					break;
+				case EQFRoundsSortBy.NEWEST:
+					field = EQfArchivedRoundsSort.beginDate;
+					direction = EOrderDirection.DESC;
+					break;
+				case EQFRoundsSortBy.OLDEST:
+					field = EQfArchivedRoundsSort.beginDate;
+					direction = EOrderDirection.ASC;
+					break;
+				default:
+					break;
+			}
+			let _qfArchivedRounds: IArchivedQFRound[] = [];
+			try {
+				const {
+					data: { qfArchivedRounds },
+				} = await client.query({
+					query: FETCH_ARCHIVED_QF_ROUNDS,
+					fetchPolicy: 'network-only',
+					variables: {
+						limit: ITEMS_PER_PAGE,
+						skip,
+						orderBy: {
+							field,
+							direction,
+						},
 					},
-				},
-			});
-			setArchivedQFRounds(qfArchivedRounds);
-		};
-		fetchQFRounds();
-	}, [orderBy]);
+				});
+				setArchivedQFRounds(prev => {
+					return isLoadMore
+						? [...prev, ...qfArchivedRounds]
+						: qfArchivedRounds;
+				});
+			} catch (error) {
+				showToastError(error);
+			} finally {
+				setLoading(false);
+			}
+		},
+		[orderBy],
+	);
+
+	useEffect(() => {
+		if (loading) return;
+		fetchQFRounds(false, 0);
+	}, [fetchQFRounds]);
+
+	const loadMore = () => {
+		fetchQFRounds(true, archivedQFRounds.length);
+	};
+
 	return (
 		<Wrapper>
 			<ArchivedQFBanner />
@@ -88,6 +116,13 @@ export const ArchivedQFRoundsView = () => {
 					)}
 				/>
 			</Container>
+			{hasMore && (
+				<LoadMoreButton
+					buttonType='texty-primary'
+					onClick={loadMore}
+					label='Load More'
+				/>
+			)}
 		</Wrapper>
 	);
 };
@@ -95,4 +130,8 @@ export const ArchivedQFRoundsView = () => {
 const Wrapper = styled(Flex)`
 	flex-direction: column;
 	gap: 40px;
+`;
+
+const LoadMoreButton = styled(OutlineButton)`
+	margin: 20px auto;
 `;
