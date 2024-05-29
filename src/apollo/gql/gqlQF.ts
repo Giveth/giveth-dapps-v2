313bad5338a4f6c20e3717ecb5213f6f0e84a39f
Ramin <raminramazanpour@gmail.com>
2024-05-29 19:26:29 +0330

    remove FETCH_QF_ROUNDS

--- src/apollo/gql/gqlQF.ts
+++ src/apollo/gql/gqlQF.ts
@@ -25,10 +25,6 @@ export const FETCH_QF_ROUNDS_QUERY = `
 	}
 `;
 
-export const FETCH_QF_ROUNDS = gql`
-	${FETCH_QF_ROUNDS_QUERY}
-`;
-
 export const FETCH_DOES_DONATED_PROJECT_IN_ROUND = gql`
 	query ($projectId: Int!, $qfRoundId: Int!, $userId: Int!) {
 		doesDonatedToProjectInQfRound(
