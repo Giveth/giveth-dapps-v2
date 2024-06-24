1da0d720b0b361045c981abbd922e4f6420887c5
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-06-24 13:05:00 +0330

    Fix lint errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -98,8 +98,7 @@ createWeb3Modal({
 	chainImages: {
 		[configuration.CLASSIC_NETWORK_NUMBER]:
 			'/images/currencies/classic/32.svg',
-		[configuration.ZKEVM_NETWORK_NUMBER]:
-			'/images/currencies/zkevm/16.svg',
+		[configuration.ZKEVM_NETWORK_NUMBER]: '/images/currencies/zkevm/16.svg',
 		[configuration.POLYGON_NETWORK_NUMBER]:
 			'/images/currencies/polygon/16.svg',
 	},
