46231fe79f6a4f73b85fef997acb525d346b7e10
Cherik <Pourcheriki@gmail.com>
2024-07-22 15:03:06 +0330

    update useGIVTokenDistroHelper to use useQuery

--- src/hooks/useGIVTokenDistroHelper.ts
+++ src/hooks/useGIVTokenDistroHelper.ts
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { AddressZero } from '@ethersproject/constants';
-import { useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { useAccount } from 'wagmi';
 import { TokenDistroHelper } from '@/lib/contractHelper/TokenDistroHelper';
 import { SubgraphDataHelper } from '@/lib/subgraph/subgraphDataHelper';
+import { fetchSubgraphData } from '@/components/controller/subgraph.ctrl';
+import config from '@/configuration';
 
 export const defaultTokenDistroHelper = new TokenDistroHelper({
 	contractAddress: AddressZero,
@@ -20,17 +22,18 @@ const useGIVTokenDistroHelper = (hold = false) => {
 		useState<TokenDistroHelper>(defaultTokenDistroHelper);
 	const [isLoaded, setIsLoaded] = useState(false);
 	const { chain, address } = useAccount();
-	const queryClient = useQueryClient();
-	const currentValue = queryClient.getQueryData([
-		'subgraph',
-		chain?.id,
-		address,
-	]);
+	const currentValues = useQuery({
+		queryKey: ['subgraph', chain?.id, address],
+		queryFn: async () => await fetchSubgraphData(chain?.id, address),
+		enabled: !hold,
+		staleTime: config.SUBGRAPH_POLLING_INTERVAL,
+	});
+	console.log('rerendered');
 	useEffect(() => {
-		const sdh = new SubgraphDataHelper(currentValue);
+		const sdh = new SubgraphDataHelper(currentValues.data);
 		setGIVTokenDistroHelper(new TokenDistroHelper(sdh.getGIVTokenDistro()));
-		// setIsLoaded(currentValue.isLoaded as boolean); //TODO:
-	}, [currentValue]);
+		setIsLoaded(currentValues.isFetched as boolean); //TODO:
+	}, [currentValues.data]);
 	return { givTokenDistroHelper, isLoaded };
 };
 
