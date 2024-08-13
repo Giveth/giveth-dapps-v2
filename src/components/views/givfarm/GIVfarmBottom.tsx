76f404e24ceeb7bfec3b42b4b56e5548e35cb528
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-08-13 17:39:31 +0330

    Fix lint errors

--- src/components/views/givfarm/GIVfarmBottom.tsx
+++ src/components/views/givfarm/GIVfarmBottom.tsx
@@ -9,7 +9,12 @@ import {
 import { useIntl } from 'react-intl';
 import { useAccount } from 'wagmi';
 import config from '@/configuration';
-import { NetworkConfig, SimplePoolStakingConfig } from '@/types/config';
+import {
+	BalancerPoolStakingConfig,
+	ICHIPoolStakingConfig,
+	NetworkConfig,
+	SimplePoolStakingConfig
+} from "@/types/config";
 import {
 	PoolRow,
 	ContractRow,
@@ -36,7 +41,11 @@ const renderPool = (pool: SimplePoolStakingConfig, id: number) => (
 	</Col>
 );
 
-const getPoolsInfoByChainID = (chainId?: number): NetworkConfig[] => {
+const getPoolsInfoByChainID = (chainId?: number): Array<
+	| SimplePoolStakingConfig
+	| BalancerPoolStakingConfig
+	| ICHIPoolStakingConfig
+	> => {
 	switch (chainId) {
 		case config.GNOSIS_NETWORK_NUMBER:
 			return [
