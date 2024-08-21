ebae2c71727a6894c4ea6a6be484840db1c02655
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-08-21 10:35:57 +0100

    fix: error while clicking on next

--- src/components/views/donate/OnTime/SelectTokenModal/QRCodeDonation/QRDonationCard.tsx
+++ src/components/views/donate/OnTime/SelectTokenModal/QRCodeDonation/QRDonationCard.tsx
@@ -108,12 +108,14 @@ export const QRDonationCard: FC<QRDonationCardProps> = ({
 		);
 
 		const parsedLocalStorageItem = JSON.parse(draftDonations!);
-		
+
 		const projectAddress: IWalletAddress | undefined =
 			project.addresses?.find(
 				address => address.chainType === ChainType.STELLAR,
 			);
-		let draftDonationId = parsedLocalStorageItem ? parsedLocalStorageItem[projectAddress?.address!] : null;
+		let draftDonationId = parsedLocalStorageItem
+			? parsedLocalStorageItem[projectAddress?.address!]
+			: null;
 
 		const retDraftDonation = !!draftDonationId
 			? await retrieveDraftDonation(Number(draftDonationId))
