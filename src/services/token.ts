65f75ddf4bdd85f62919664b76fd005cb3e7bca3
Cherik <Pourcheriki@gmail.com>
2024-08-25 13:56:16 +0330

    add price services

--- src/services/token.ts
+++ src/services/token.ts
@@ -3,10 +3,15 @@ import { type Address } from 'viem';
 import { getPublicClient } from 'wagmi/actions';
 import { erc20Abi } from 'viem';
 import { readContract } from '@wagmi/core';
+import BigNumber from 'bignumber.js';
 import config from '@/configuration';
 import { AddressZero } from '@/lib/constants/constants';
 import { ChainType } from '@/types/config';
 import { wagmiConfig } from '@/wagmiConfigs';
+import {
+	FETCH_GNOSIS_TOKEN_PRICE,
+	FETCH_MAINNET_TOKEN_PRICE,
+} from '@/apollo/gql/gqlPrice';
 
 export const fetchPrice = async (
 	chainId: number | ChainType,
@@ -64,4 +69,63 @@ export const fetchPriceWithCoingeckoId = async (coingeckoId: string) => {
 			},
 		});
 	}
+};
+
+export const fetchMainnetTokenPrice = async (
+	tokenId: string,
+): Promise<string> => {
+	const query = FETCH_MAINNET_TOKEN_PRICE;
+	const variables = {
+		tokenId: tokenId.toLowerCase(),
+		daiId: '0x6b175474e89094c44da98b954eedeac495271d0f'.toLowerCase(),
+	};
+	const body = {
+		query,
+		variables,
+	};
+	const myHeaders = new Headers();
+	myHeaders.append('content-type', 'application/json');
+	const res = await fetch(config.MAINNET_CONFIG.uniswapV2Subgraph, {
+		method: 'POST',
+		body: JSON.stringify(body),
+		headers: myHeaders,
+	});
+	const data = await res.json();
+	const tokenEthPrice = new BigNumber(data?.data?.token.derivedETH);
+	const daiEthPrice = new BigNumber(data?.data?.daitoken.derivedETH);
+	return tokenEthPrice.div(daiEthPrice).toString();
+};
+
+export const fetchGnosisTokenPrice = async (
+	tokenId?: string,
+): Promise<string> => {
+	if (!tokenId) return '0';
+	const query = FETCH_GNOSIS_TOKEN_PRICE;
+	const variables = {
+		id: tokenId.toLowerCase(),
+	};
+	const subgraph = config.GNOSIS_CONFIG.uniswapV2Subgraph;
+	if (!subgraph) {
+		console.log('Subgraph is not defined');
+		return '0';
+	}
+	const body = {
+		query,
+		variables,
+	};
+
+	const myHeaders = new Headers();
+	myHeaders.append('content-type', 'application/json');
+
+	const requestOptions: RequestInit = {
+		method: 'POST',
+		body: JSON.stringify(body),
+		headers: myHeaders,
+	};
+	const res = await fetch(
+		config.GNOSIS_CONFIG.uniswapV2Subgraph!,
+		requestOptions,
+	);
+	const data = await res.json();
+	return data?.data?.token?.derivedNativeCurrency || '0';
 };
