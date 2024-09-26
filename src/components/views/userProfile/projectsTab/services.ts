3122236f23362f0a34afc0addbf083ec8eea1a49
Cherik <Pourcheriki@gmail.com>
2024-09-26 13:38:22 +0330

    separate fetchUserProjects as a service

---
+++ src/components/views/userProfile/projectsTab/services.ts
@@ -0,0 +1,26 @@
+import { client } from '@/apollo/apolloClient';
+import { IOrder } from '../UserProfile.view';
+import { FETCH_USER_PROJECTS } from '@/apollo/gql/gqlUser';
+import { userProjectsPerPage } from './constants';
+import { IProject } from '@/apollo/types/types';
+
+export const fetchUserProjects = async (
+	userId: string,
+	page: number,
+	order: IOrder,
+) => {
+	const { data } = await client.query({
+		query: FETCH_USER_PROJECTS,
+		variables: {
+			userId: parseFloat(userId || '') || -1,
+			take: userProjectsPerPage,
+			skip: page * userProjectsPerPage,
+			orderBy: order.by,
+			direction: order.direction,
+		},
+	});
+	return data.projectsByUserId as {
+		projects: IProject[];
+		totalCount: number;
+	};
+};
