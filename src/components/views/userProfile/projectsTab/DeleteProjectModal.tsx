ccd12557ec1d831563000f891a35a99624801b39
Cherik <Pourcheriki@gmail.com>
2024-09-26 13:41:42 +0330

    delete project

--- src/components/views/userProfile/projectsTab/DeleteProjectModal.tsx
+++ src/components/views/userProfile/projectsTab/DeleteProjectModal.tsx
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 import { useCallback, type FC } from 'react';
 import { Button, Flex, IconTrash32, P } from '@giveth/ui-design-system';
 import { useIntl } from 'react-intl';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { IProject } from '@/apollo/types/types';
 import { Modal } from '@/components/modals/Modal';
 import { IModal } from '@/types/common';
 import { useModalAnimation } from '@/hooks/useModalAnimation';
 import { client } from '@/apollo/apolloClient';
 import { DELETE_DRAFT_PROJECT } from '@/apollo/gql/gqlProjects';
+import { useProfileContext } from '@/context/profile.context';
 
 interface IDeleteProjectModal extends IModal {
 	project: IProject;
@@ -19,15 +21,26 @@ const DeleteProjectModal: FC<IDeleteProjectModal> = ({
 }) => {
 	const { formatMessage } = useIntl();
 	const { isAnimating, closeModal } = useModalAnimation(setShowModal);
+	const queryClient = useQueryClient();
+	const { user } = useProfileContext();
+
+	const deleteProjectMutation = useMutation({
+		mutationFn: (projectId: string) =>
+			client.mutate({
+				mutation: DELETE_DRAFT_PROJECT,
+				variables: { id: projectId },
+			}),
+		// On success, refetch the user's projects
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: [user.id, 'projects'],
+			});
+		},
+	});
 
 	const handleRemoveProject = useCallback(async () => {
-		client.mutate({
-			mutation: DELETE_DRAFT_PROJECT,
-			variables: {
-				id: project.id,
-			},
-		});
-	}, [project.id]);
+		deleteProjectMutation.mutate(project.id);
+	}, [deleteProjectMutation, project.id]);
 
 	return (
 		<Modal
