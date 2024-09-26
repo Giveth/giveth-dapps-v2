69cf94e30517f150239b0cc502ab581517de5075
Cherik <Pourcheriki@gmail.com>
2024-09-26 16:48:58 +0330

    move projectsOrder to constants

--- src/components/views/userProfile/projectsTab/constants.ts
+++ src/components/views/userProfile/projectsTab/constants.ts
@@ -1,1 +1,9 @@
+import { EDirection } from '@/apollo/types/gqlEnums';
+import { EOrderBy } from './type';
+
 export const userProjectsPerPage = 12;
+
+export const projectsOrder = {
+	by: EOrderBy.CreationDate,
+	direction: EDirection.DESC,
+};
