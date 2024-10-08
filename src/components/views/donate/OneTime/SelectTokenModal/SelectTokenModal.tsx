b48c07afe64a0efcd08c07463fdedd120f4ef9ac
Kilter <mateodaza@gmail.com>
2024-10-08 10:02:02 -0500

    Update SelectTokenModal.tsx

--- src/components/views/donate/OneTime/SelectTokenModal/SelectTokenModal.tsx
+++ src/components/views/donate/OneTime/SelectTokenModal/SelectTokenModal.tsx
@@ -148,14 +148,13 @@ const SelectTokenInnerModal: FC<ISelectTokenModalProps> = ({
 				}
 			} else {
 				setCustomToken(undefined);
+				const sQuery: string = searchQuery;
 				const filtered = tokens.filter(
 					token =>
 						token.symbol
 							.toLowerCase()
-							.includes(searchQuery.toLowerCase()) ||
-						token.name
-							.toLowerCase()
-							.includes(searchQuery.toLowerCase()),
+							.includes(sQuery.toLowerCase()) ||
+						token.name.toLowerCase().includes(sQuery.toLowerCase()),
 				);
 				setFilteredTokens(filtered);
 			}
