cae6ba6f3c97834c1d7535dd8db3c1f835f17d8d
kkatusic <kkatusic@gmail.com>
2025-01-23 13:50:18 +0100

    removing tab

--- src/components/views/donate/OneTime/EstimatedMatchingToast.tsx
+++ src/components/views/donate/OneTime/EstimatedMatchingToast.tsx
@@ -46,7 +46,7 @@ const EstimatedMatchingToast: FC<IEstimatedMatchingToast> = ({
 		estimatedMatching || {};
 
 	const { activeStartedRound } = getActiveRound(qfRounds);
-	
+
 	const {
 		allocatedFundUSDPreferred,
 		allocatedFundUSD,
