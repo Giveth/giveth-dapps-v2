c0d12063b4db1e55b71e0cc23487ca0857deedb2
kkatusic <kkatusic@gmail.com>
2025-03-17 12:43:42 +0100

    HOTFIX: Update social text for new QF Round

--- src/components/SocialBox.tsx
+++ src/components/SocialBox.tsx
@@ -30,16 +30,32 @@ const SocialBox: FC<ISocialBox> = props => {
 	const { descriptionSummary, slug } = project;
 	const { formatMessage } = useIntl();
 
-	const shareTitleTwitter = shareContentCreator(
-		contentType,
-		ESocialType.twitter,
-	);
+	const projectUrl = fullPath(slugToProjectView(slug));
+
+	// Remove after round ends
+	const activeQFRound = project.qfRounds?.find(round => round.isActive);
+	const now = new Date();
+
+	// Remove after round ends
+	const isRoundActive =
+		activeQFRound &&
+		new Date(activeQFRound.beginDate) <= now &&
+		new Date(activeQFRound.endDate) >= now;
+
+	// Remove after round ends
+	const shareTitleTwitter = isRoundActive
+		? `I just donated in the @OctantApp @ENS_DAO QF round on @Giveth! 💜 Check out the project I supported: `
+		: shareContentCreator(contentType, ESocialType.twitter);
+
+	// Remove after round ends
+	const shareTitleFarcaster = isRoundActive
+		? `I just donated in the @octant @ensdao QF round on @Giveth! 💜 Check out the project I supported: ${projectUrl}`
+		: shareContentCreator(contentType, ESocialType.twitter);
+
 	const shareTitleFacebookAndLinkedin = shareContentCreator(
 		contentType,
 		ESocialType.facebook,
 	);
-
-	const projectUrl = fullPath(slugToProjectView(slug));
 
 	return (
 		<Social $isDonateFooter={isDonateFooter}>
@@ -87,7 +103,7 @@ const SocialBox: FC<ISocialBox> = props => {
 				</SocialItem>
 				<SocialItem $isDonateFooter={isDonateFooter}>
 					<Link
-						href={`https://warpcast.com/~/compose?embeds[]=${projectUrl}&text=${shareTitleTwitter}`}
+						href={`https://warpcast.com/~/compose?embeds[]=${projectUrl}&text=${shareTitleFarcaster}`}
 						target='_blank'
 					>
 						<Image
