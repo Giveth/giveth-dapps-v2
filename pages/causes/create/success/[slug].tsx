4921dfca35b1cc354b26e133fa2eda81be16e4e1
kkatusic <kkatusic@gmail.com>
2025-07-01 16:02:22 +0200

    starting fixing

--- pages/causes/create/success/[slug].tsx
+++ pages/causes/create/success/[slug].tsx
@@ -1,16 +1,18 @@
 import { useRouter } from 'next/router';
 import { useQuery } from '@apollo/client';
-import { GET_CAUSE_BY_SLUG } from '@/apollo/gql/gqlCauses';
+import { FETCH_PROJECT_BY_SLUG_SUCCESS } from '@/apollo/gql/gqlProjects';
 import SuccessfulCauseCreation from '@/components/views/causes/create/SuccessfulCauseCreation';
 
 export default function CauseSuccessPage() {
 	const { query } = useRouter();
 	const slug = query.slug as string;
 
-	const { data, loading } = useQuery(GET_CAUSE_BY_SLUG, {
+	const { data, loading } = useQuery(FETCH_PROJECT_BY_SLUG_SUCCESS, {
 		variables: { slug },
 		skip: !slug,
 	});
 
-	return <SuccessfulCauseCreation cause={data?.cause} isLoading={loading} />;
+	return (
+		<SuccessfulCauseCreation cause={data?.project} isLoading={loading} />
+	);
 }
