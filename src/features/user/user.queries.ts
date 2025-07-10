a33763ac22c958364d118219e5415d27ecb504a3
kkatusic <kkatusic@gmail.com>
2025-07-10 13:54:28 +0200

    fix causes number and causes project number

--- src/features/user/user.queries.ts
+++ src/features/user/user.queries.ts
@@ -23,6 +23,7 @@ export const GET_USER_BY_ADDRESS = `query UserByAddress($address: String!) {
 		wasReferred
         activeQFMBDScore
         isEmailVerified
+        ownedCausesCount
     }
 }`;
 
