1326f63fbff7d399a34c67f402b5928dea8630cf
kkatusic <kkatusic@gmail.com>
2025-07-16 14:30:19 +0200

    fixing cause profile card

--- src/components/ContributeCard.sc.tsx
+++ src/components/ContributeCard.sc.tsx
@@ -1,13 +1,15 @@
 import { brandColors, Subline } from '@giveth/ui-design-system';
 import styled from 'styled-components';
 
-export const ContributeCardBox = styled.div`
+export const ContributeCardBox = styled.div<{
+	$gridTemplateColumns?: string;
+}>`
 	background: ${brandColors.giv['000']};
 	box-shadow: 0 3px 20px rgba(212, 218, 238, 0.4);
 	border-radius: 12px;
 	display: grid;
 	padding: 24px;
-	grid-template-columns: 1fr 1fr;
+	grid-template-columns: ${props => props.$gridTemplateColumns || '1fr 1fr'};
 	width: 100%;
 `;
 
