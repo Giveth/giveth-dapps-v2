cd9e5ba28190064b4788369f956a4752206851a5
kkatusic <kkatusic@gmail.com>
2025-08-11 11:14:54 +0200

    remove unused function

--- src/components/views/project/projectActionCard/DonationSection.tsx
+++ src/components/views/project/projectActionCard/DonationSection.tsx
@@ -16,7 +16,7 @@ import styled from 'styled-components';
 import { type FC } from 'react';
 import useMediaQuery from '@/hooks/useMediaQuery';
 import { device } from '@/lib/constants/constants';
-import { formatDonation, limitFraction } from '@/helpers/number';
+import { formatDonation } from '@/helpers/number';
 import { IProject } from '@/apollo/types/types';
 import { useDonateData } from '@/context/donate.context';
 import { ORGANIZATION } from '@/lib/constants/organizations';
