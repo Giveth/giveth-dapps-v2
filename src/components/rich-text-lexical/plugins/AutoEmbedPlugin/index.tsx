957557e00b0a8d410b1ddc930ade9e0ae178b7b8
kkatusic <kkatusic@gmail.com>
2025-09-09 14:27:10 +0200

    finished embedded stuff

--- src/components/rich-text-lexical/plugins/AutoEmbedPlugin/index.tsx
+++ src/components/rich-text-lexical/plugins/AutoEmbedPlugin/index.tsx
@@ -6,353 +6,402 @@
  *
  */
 
-import type {LexicalEditor} from 'lexical';
-import type {JSX} from 'react';
-
 import {
-  AutoEmbedOption,
-  EmbedConfig,
-  EmbedMatchResult,
-  LexicalAutoEmbedPlugin,
-  URL_MATCHER,
+	AutoEmbedOption,
+	EmbedConfig,
+	EmbedMatchResult,
+	LexicalAutoEmbedPlugin,
+	URL_MATCHER,
 } from '@lexical/react/LexicalAutoEmbedPlugin';
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {useMemo, useState} from 'react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { useMemo, useState } from 'react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import styled from 'styled-components';
 import useModal from '../../hooks/useModal';
 import Button from '../../ui/Button';
-import {DialogActions} from '../../ui/Dialog';
-import {INSERT_FIGMA_COMMAND} from '../FigmaPlugin';
-import {INSERT_TWEET_COMMAND} from '../TwitterPlugin';
-import {INSERT_YOUTUBE_COMMAND} from '../YouTubePlugin';
+import { DialogActions } from '../../ui/Dialog';
+import { INSERT_FIGMA_COMMAND } from '../FigmaPlugin';
+import { INSERT_TWEET_COMMAND } from '../TwitterPlugin';
+import { INSERT_YOUTUBE_COMMAND } from '../YouTubePlugin';
+import type { LexicalEditor } from 'lexical';
+import type { JSX } from 'react';
 
 interface PlaygroundEmbedConfig extends EmbedConfig {
-  // Human readable name of the embedded content e.g. Tweet or Google Map.
-  contentName: string;
+	// Human readable name of the embedded content e.g. Tweet or Google Map.
+	contentName: string;
 
-  // Icon for display.
-  icon?: JSX.Element;
+	// Icon for display.
+	icon?: JSX.Element;
 
-  // An example of a matching url https://twitter.com/jack/status/20
-  exampleUrl: string;
+	// An example of a matching url https://twitter.com/jack/status/20
+	exampleUrl: string;
 
-  // For extra searching.
-  keywords: Array<string>;
+	// For extra searching.
+	keywords: Array<string>;
 
-  // Embed a Figma Project.
-  description?: string;
+	// Embed a Figma Project.
+	description?: string;
 }
 
 export const YoutubeEmbedConfig: PlaygroundEmbedConfig = {
-  contentName: 'Youtube Video',
+	contentName: 'Youtube Video',
 
-  exampleUrl: 'https://www.youtube.com/watch?v=jNQXAC9IVRw',
+	exampleUrl: 'https://www.youtube.com/watch?v=jNQXAC9IVRw',
 
-  // Icon for display.
-  icon: <i className="icon youtube" />,
+	// Icon for display.
+	icon: <i className='icon youtube' />,
 
-  insertNode: (editor: LexicalEditor, result: EmbedMatchResult) => {
-    editor.dispatchCommand(INSERT_YOUTUBE_COMMAND, result.id);
-  },
+	insertNode: (editor: LexicalEditor, result: EmbedMatchResult) => {
+		editor.dispatchCommand(INSERT_YOUTUBE_COMMAND, result.id);
+	},
 
-  keywords: ['youtube', 'video'],
+	keywords: ['youtube', 'video'],
 
-  // Determine if a given URL is a match and return url data.
-  parseUrl: async (url: string) => {
-    const match =
-      /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/.exec(url);
+	// Determine if a given URL is a match and return url data.
+	parseUrl: async (url: string) => {
+		const match =
+			/^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/.exec(
+				url,
+			);
 
-    const id = match ? (match?.[2].length === 11 ? match[2] : null) : null;
+		const id = match ? (match?.[2].length === 11 ? match[2] : null) : null;
 
-    if (id != null) {
-      return {
-        id,
-        url,
-      };
-    }
+		if (id != null) {
+			return {
+				id,
+				url,
+			};
+		}
 
-    return null;
-  },
+		return null;
+	},
 
-  type: 'youtube-video',
+	type: 'youtube-video',
 };
 
 export const TwitterEmbedConfig: PlaygroundEmbedConfig = {
-  // e.g. Tweet or Google Map.
-  contentName: 'X(Tweet)',
+	// e.g. Tweet or Google Map.
+	contentName: 'X(Tweet)',
 
-  exampleUrl: 'https://x.com/jack/status/20',
+	exampleUrl: 'https://x.com/jack/status/20',
 
-  // Icon for display.
-  icon: <i className="icon x" />,
+	// Icon for display.
+	icon: <i className='icon x' />,
 
-  // Create the Lexical embed node from the url data.
-  insertNode: (editor: LexicalEditor, result: EmbedMatchResult) => {
-    editor.dispatchCommand(INSERT_TWEET_COMMAND, result.id);
-  },
+	// Create the Lexical embed node from the url data.
+	insertNode: (editor: LexicalEditor, result: EmbedMatchResult) => {
+		editor.dispatchCommand(INSERT_TWEET_COMMAND, result.id);
+	},
 
-  // For extra searching.
-  keywords: ['tweet', 'twitter', 'x'],
+	// For extra searching.
+	keywords: ['tweet', 'twitter', 'x'],
 
-  // Determine if a given URL is a match and return url data.
-  parseUrl: (text: string) => {
-    const match =
-      /^https:\/\/(twitter|x)\.com\/(#!\/)?(\w+)\/status(es)*\/(\d+)/.exec(
-        text,
-      );
+	// Determine if a given URL is a match and return url data.
+	parseUrl: (text: string) => {
+		const match =
+			/^https:\/\/(twitter|x)\.com\/(#!\/)?(\w+)\/status(es)*\/(\d+)/.exec(
+				text,
+			);
 
-    if (match != null) {
-      return {
-        id: match[5],
-        url: match[1],
-      };
-    }
+		if (match != null) {
+			return {
+				id: match[5],
+				url: match[1],
+			};
+		}
 
-    return null;
-  },
+		return null;
+	},
 
-  type: 'tweet',
+	type: 'tweet',
 };
 
 export const FigmaEmbedConfig: PlaygroundEmbedConfig = {
-  contentName: 'Figma Document',
+	contentName: 'Figma Document',
 
-  exampleUrl: 'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File',
+	exampleUrl: 'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File',
 
-  icon: <i className="icon figma" />,
+	icon: <i className='icon figma' />,
 
-  insertNode: (editor: LexicalEditor, result: EmbedMatchResult) => {
-    editor.dispatchCommand(INSERT_FIGMA_COMMAND, result.id);
-  },
+	insertNode: (editor: LexicalEditor, result: EmbedMatchResult) => {
+		editor.dispatchCommand(INSERT_FIGMA_COMMAND, result.id);
+	},
 
-  keywords: ['figma', 'figma.com', 'mock-up'],
+	keywords: ['figma', 'figma.com', 'mock-up'],
 
-  // Determine if a given URL is a match and return url data.
-  parseUrl: (text: string) => {
-    const match =
-      /https:\/\/([\w.-]+\.)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/.exec(
-        text,
-      );
+	// Determine if a given URL is a match and return url data.
+	parseUrl: (text: string) => {
+		const match =
+			/https:\/\/([\w.-]+\.)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/.exec(
+				text,
+			);
 
-    if (match != null) {
-      return {
-        id: match[3],
-        url: match[0],
-      };
-    }
+		if (match != null) {
+			return {
+				id: match[3],
+				url: match[0],
+			};
+		}
 
-    return null;
-  },
+		return null;
+	},
 
-  type: 'figma',
+	type: 'figma',
 };
 
 export const EmbedConfigs = [
-  TwitterEmbedConfig,
-  YoutubeEmbedConfig,
-  FigmaEmbedConfig,
+	TwitterEmbedConfig,
+	YoutubeEmbedConfig,
+	FigmaEmbedConfig,
 ];
 
 function AutoEmbedMenuItem({
-  index,
-  isSelected,
-  onClick,
-  onMouseEnter,
-  option,
+	index,
+	isSelected,
+	onClick,
+	onMouseEnter,
+	option,
 }: {
-  index: number;
-  isSelected: boolean;
-  onClick: () => void;
-  onMouseEnter: () => void;
-  option: AutoEmbedOption;
+	index: number;
+	isSelected: boolean;
+	onClick: () => void;
+	onMouseEnter: () => void;
+	option: AutoEmbedOption;
 }) {
-  let className = 'item';
-  if (isSelected) {
-    className += ' selected';
-  }
-  return (
-    <li
-      key={option.key}
-      tabIndex={-1}
-      className={className}
-      ref={option.setRefElement}
-      role="option"
-      aria-selected={isSelected}
-      id={'typeahead-item-' + index}
-      onMouseEnter={onMouseEnter}
-      onClick={onClick}>
-      <span className="text">{option.title}</span>
-    </li>
-  );
+	let className = 'item';
+	if (isSelected) {
+		className += ' selected';
+	}
+	return (
+		<li
+			key={option.key}
+			tabIndex={-1}
+			className={className}
+			ref={option.setRefElement}
+			role='option'
+			aria-selected={isSelected}
+			id={'typeahead-item-' + index}
+			onMouseEnter={onMouseEnter}
+			onClick={onClick}
+		>
+			<span className='text'>{option.title}</span>
+		</li>
+	);
 }
 
 function AutoEmbedMenu({
-  options,
-  selectedItemIndex,
-  onOptionClick,
-  onOptionMouseEnter,
+	options,
+	selectedItemIndex,
+	onOptionClick,
+	onOptionMouseEnter,
 }: {
-  selectedItemIndex: number | null;
-  onOptionClick: (option: AutoEmbedOption, index: number) => void;
-  onOptionMouseEnter: (index: number) => void;
-  options: Array<AutoEmbedOption>;
+	selectedItemIndex: number | null;
+	onOptionClick: (option: AutoEmbedOption, index: number) => void;
+	onOptionMouseEnter: (index: number) => void;
+	options: Array<AutoEmbedOption>;
 }) {
-  return (
-    <div className="typeahead-popover">
-      <ul>
-        {options.map((option: AutoEmbedOption, i: number) => (
-          <AutoEmbedMenuItem
-            index={i}
-            isSelected={selectedItemIndex === i}
-            onClick={() => onOptionClick(option, i)}
-            onMouseEnter={() => onOptionMouseEnter(i)}
-            key={option.key}
-            option={option}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='typeahead-popover'>
+			<ul>
+				{options.map((option: AutoEmbedOption, i: number) => (
+					<AutoEmbedMenuItem
+						index={i}
+						isSelected={selectedItemIndex === i}
+						onClick={() => onOptionClick(option, i)}
+						onMouseEnter={() => onOptionMouseEnter(i)}
+						key={option.key}
+						option={option}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 const debounce = (callback: (text: string) => void, delay: number) => {
-  let timeoutId: number;
-  return (text: string) => {
-    window.clearTimeout(timeoutId);
-    timeoutId = window.setTimeout(() => {
-      callback(text);
-    }, delay);
-  };
+	let timeoutId: number;
+	return (text: string) => {
+		window.clearTimeout(timeoutId);
+		timeoutId = window.setTimeout(() => {
+			callback(text);
+		}, delay);
+	};
 };
 
 export function AutoEmbedDialog({
-  embedConfig,
-  onClose,
+	embedConfig,
+	onClose,
 }: {
-  embedConfig: PlaygroundEmbedConfig;
-  onClose: () => void;
+	embedConfig: PlaygroundEmbedConfig;
+	onClose: () => void;
 }): JSX.Element {
-  const [text, setText] = useState('');
-  const [editor] = useLexicalComposerContext();
-  const [embedResult, setEmbedResult] = useState<EmbedMatchResult | null>(null);
+	const [text, setText] = useState('');
+	const [editor] = useLexicalComposerContext();
+	const [embedResult, setEmbedResult] = useState<EmbedMatchResult | null>(
+		null,
+	);
 
-  const validateText = useMemo(
-    () =>
-      debounce((inputText: string) => {
-        const urlMatch = URL_MATCHER.exec(inputText);
-        if (embedConfig != null && inputText != null && urlMatch != null) {
-          Promise.resolve(embedConfig.parseUrl(inputText)).then(
-            (parseResult) => {
-              setEmbedResult(parseResult);
-            },
-          );
-        } else if (embedResult != null) {
-          setEmbedResult(null);
-        }
-      }, 200),
-    [embedConfig, embedResult],
-  );
+	const validateText = useMemo(
+		() =>
+			debounce((inputText: string) => {
+				const urlMatch = URL_MATCHER.exec(inputText);
+				if (
+					embedConfig != null &&
+					inputText != null &&
+					urlMatch != null
+				) {
+					Promise.resolve(embedConfig.parseUrl(inputText)).then(
+						parseResult => {
+							setEmbedResult(parseResult);
+						},
+					);
+				} else if (embedResult != null) {
+					setEmbedResult(null);
+				}
+			}, 200),
+		[embedConfig, embedResult],
+	);
 
-  const onClick = () => {
-    if (embedResult != null) {
-      embedConfig.insertNode(editor, embedResult);
-      onClose();
-    }
-  };
+	const onClick = () => {
+		if (embedResult != null) {
+			embedConfig.insertNode(editor, embedResult);
+			onClose();
+		}
+	};
 
-  return (
-    <div style={{width: '600px'}}>
-      <div className="Input__wrapper">
-        <input
-          type="text"
-          className="Input__input"
-          placeholder={embedConfig.exampleUrl}
-          value={text}
-          data-test-id={`${embedConfig.type}-embed-modal-url`}
-          onChange={(e) => {
-            const {value} = e.target;
-            setText(value);
-            validateText(value);
-          }}
-        />
-      </div>
-      <DialogActions>
-        <Button
-          disabled={!embedResult}
-          onClick={onClick}
-          data-test-id={`${embedConfig.type}-embed-modal-submit-btn`}>
-          Embed
-        </Button>
-      </DialogActions>
-    </div>
-  );
+	return (
+		<AutoEmbededWrapper style={{ width: '600px' }}>
+			<div className='Input__wrapper'>
+				<input
+					type='text'
+					className='Input__input'
+					placeholder={embedConfig.exampleUrl}
+					value={text}
+					data-test-id={`${embedConfig.type}-embed-modal-url`}
+					onChange={e => {
+						const { value } = e.target;
+						setText(value);
+						validateText(value);
+					}}
+				/>
+			</div>
+			<DialogActions>
+				<Button
+					disabled={!embedResult}
+					onClick={onClick}
+					data-test-id={`${embedConfig.type}-embed-modal-submit-btn`}
+				>
+					Embed
+				</Button>
+			</DialogActions>
+		</AutoEmbededWrapper>
+	);
 }
 
 export default function AutoEmbedPlugin(): JSX.Element {
-  const [modal, showModal] = useModal();
+	const [modal, showModal] = useModal();
 
-  const openEmbedModal = (embedConfig: PlaygroundEmbedConfig) => {
-    showModal(`Embed ${embedConfig.contentName}`, (onClose) => (
-      <AutoEmbedDialog embedConfig={embedConfig} onClose={onClose} />
-    ));
-  };
+	const openEmbedModal = (embedConfig: PlaygroundEmbedConfig) => {
+		showModal(`Embed ${embedConfig.contentName}`, onClose => (
+			<AutoEmbedDialog embedConfig={embedConfig} onClose={onClose} />
+		));
+	};
 
-  const getMenuOptions = (
-    activeEmbedConfig: PlaygroundEmbedConfig,
-    embedFn: () => void,
-    dismissFn: () => void,
-  ) => {
-    return [
-      new AutoEmbedOption('Dismiss', {
-        onSelect: dismissFn,
-      }),
-      new AutoEmbedOption(`Embed ${activeEmbedConfig.contentName}`, {
-        onSelect: embedFn,
-      }),
-    ];
-  };
+	const getMenuOptions = (
+		activeEmbedConfig: PlaygroundEmbedConfig,
+		embedFn: () => void,
+		dismissFn: () => void,
+	) => {
+		return [
+			new AutoEmbedOption('Dismiss', {
+				onSelect: dismissFn,
+			}),
+			new AutoEmbedOption(`Embed ${activeEmbedConfig.contentName}`, {
+				onSelect: embedFn,
+			}),
+		];
+	};
 
-  return (
-    <>
-      {modal}
-      <LexicalAutoEmbedPlugin<PlaygroundEmbedConfig>
-        embedConfigs={EmbedConfigs}
-        onOpenEmbedModalForConfig={openEmbedModal}
-        getMenuOptions={getMenuOptions}
-        menuRenderFn={(
-          anchorElementRef,
-          {selectedIndex, options, selectOptionAndCleanUp, setHighlightedIndex},
-        ) =>
-          anchorElementRef.current
-            ? ReactDOM.createPortal(
-                <div
-                  className="typeahead-popover auto-embed-menu"
-                  style={{
-                    marginLeft: `${Math.max(
-                      parseFloat(anchorElementRef.current.style.width) - 200,
-                      0,
-                    )}px`,
-                    width: 200,
-                  }}>
-                  <AutoEmbedMenu
-                    options={options}
-                    selectedItemIndex={selectedIndex}
-                    onOptionClick={(option: AutoEmbedOption, index: number) => {
-                      setHighlightedIndex(index);
-                      selectOptionAndCleanUp(option);
-                    }}
-                    onOptionMouseEnter={(index: number) => {
-                      setHighlightedIndex(index);
-                    }}
-                  />
-                </div>,
-                anchorElementRef.current,
-              )
-            : null
-        }
-      />
-    </>
-  );
+	return (
+		<>
+			{modal}
+			<LexicalAutoEmbedPlugin<PlaygroundEmbedConfig>
+				embedConfigs={EmbedConfigs}
+				onOpenEmbedModalForConfig={openEmbedModal}
+				getMenuOptions={getMenuOptions}
+				menuRenderFn={(
+					anchorElementRef,
+					{
+						selectedIndex,
+						options,
+						selectOptionAndCleanUp,
+						setHighlightedIndex,
+					},
+				) =>
+					anchorElementRef.current
+						? ReactDOM.createPortal(
+								<div
+									className='typeahead-popover auto-embed-menu'
+									style={{
+										marginLeft: `${Math.max(
+											parseFloat(
+												anchorElementRef.current.style
+													.width,
+											) - 200,
+											0,
+										)}px`,
+										width: 200,
+									}}
+								>
+									<AutoEmbedMenu
+										options={options}
+										selectedItemIndex={selectedIndex}
+										onOptionClick={(
+											option: AutoEmbedOption,
+											index: number,
+										) => {
+											setHighlightedIndex(index);
+											selectOptionAndCleanUp(option);
+										}}
+										onOptionMouseEnter={(index: number) => {
+											setHighlightedIndex(index);
+										}}
+									/>
+								</div>,
+								anchorElementRef.current,
+							)
+						: null
+				}
+			/>
+		</>
+	);
 }
+
+const AutoEmbededWrapper = styled.div`
+	width: 600px;
+
+	.Input__wrapper {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		margin-bottom: 10px;
+	}
+
+	.Input__label {
+		display: flex;
+		flex: 1;
+		color: #666;
+	}
+
+	.Input__input {
+		display: flex;
+		flex: 2;
+		border: 1px solid #999;
+		padding: 7px 10px;
+		font-size: 16px;
+		border-radius: 5px;
+		min-width: 0;
+	}
+`;
