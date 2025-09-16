2e1a13d0c338a6e96ca6c4bcad3c378988c48d7f
kkatusic <kkatusic@gmail.com>
2025-09-16 15:23:48 +0200

    fix import order

--- pages/qf/index.tsx
+++ pages/qf/index.tsx
@@ -1,10 +1,10 @@
 // pages/qf/index.tsx
 
 import React from 'react';
+import { projectsQFRoundMetatags } from '@/content/metatags';
 import { GeneralMetatags } from '@/components/Metatag';
 import { QFRoundsProvider } from '@/context/qfrounds.context';
 import QFRoundsIndex from '@/components/views/QFRounds/QFRoundsIndex';
-import { projectsQFRoundMetatags } from '@/content/metatags';
 
 export default function QfLanding() {
 	return (
