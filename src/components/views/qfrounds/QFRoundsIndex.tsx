ae431dfb1b9e9559d5667369cac51b2f54e9d2b2
kkatusic <kkatusic@gmail.com>
2025-09-16 13:51:29 +0200

    fixing routes

--- src/components/views/qfrounds/QFRoundsIndex.tsx
+++ src/components/views/qfrounds/QFRoundsIndex.tsx
@@ -15,8 +15,8 @@ import { useEffect } from 'react';
 import { useQFRoundsContext } from '@/context/qfrounds.context';
 import { PassportBanner } from '@/components/PassportBanner';
 import { Spinner } from '@/components/Spinner';
-import { QFRoundsBanner } from '@/components/views/qfrounds/QFRoundsBanner';
-import QFRoundCard from '@/components/views/qfrounds/QFRoundCard';
+import { QFRoundsBanner } from '@/components/views/QFRounds/QFRoundsBanner';
+import QFRoundCard from '@/components/views/QFRounds/QFRoundCard';
 import useDetectDevice from '@/hooks/useDetectDevice';
 import Routes from '@/lib/constants/Routes';
 import {
