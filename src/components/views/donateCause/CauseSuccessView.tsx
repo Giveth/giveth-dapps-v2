42b16b33d3251a88abafcb3bc6141fd6ad0e063a
kkatusic <kkatusic@gmail.com>
2025-10-06 17:12:33 +0200

    Fix eligible success donation QF Toast

--- src/components/views/donateCause/CauseSuccessView.tsx
+++ src/components/views/donateCause/CauseSuccessView.tsx
@@ -27,7 +27,6 @@ import {
 	usePassport,
 	EQFElegibilityState,
 } from '@/hooks/usePassport';
-import { getActiveRound } from '@/helpers/qf';
 import ProjectCardImage from '@/components/project-card/ProjectCardImage';
 import { DonatePageProjectDescription } from '@/components/views/donate/DonatePageProjectDescription';
 import SocialBox from '@/components/SocialBox';
@@ -105,9 +104,7 @@ export const CauseSuccessView: FC<ISuccessView> = ({
 		)
 	);
 
-	const { activeStartedRound } = getActiveRound(project.qfRounds);
-
-	const isOnEligibleNetworks = activeStartedRound?.eligibleNetworks?.includes(
+	const isOnEligibleNetworks = selectedQFRound?.eligibleNetworks?.includes(
 		(isStellar ? config.STELLAR_NETWORK_NUMBER : chainId) || 0,
 	);
 
