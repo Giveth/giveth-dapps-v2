7a9479edc08882f54d5e9f3134aadc76b43079a9
kkatusic <kkatusic@gmail.com>
2025-11-25 11:16:57 +0100

    fixing excalidraw-types

---
+++ src/components/rich-text-lexical/plugins/ExcalidrawPlugin/excalidraw-types.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Type declaration for @excalidraw/excalidraw/types module
+ * This resolves the module path to the actual type definitions
+ */
+declare module '@excalidraw/excalidraw/types' {
+	export * from '@excalidraw/excalidraw/dist/types/excalidraw/types';
+}
