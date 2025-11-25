9cc83aa41eacb3834aaf89272775196c5b5d7c02
kkatusic <kkatusic@gmail.com>
2025-11-25 15:20:01 +0100

    fixing lint errors

--- src/components/rich-text-lexical/commenting/index.ts
+++ src/components/rich-text-lexical/commenting/index.ts
@@ -131,7 +131,6 @@ export class CommentStore {
 	): void {
 		const nextComments = Array.from(this._comments);
 		// The YJS types explicitly use `any` as well.
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		const sharedCommentsArray: YArray<any> | null =
 			this._getCollabComments();
 
@@ -184,7 +183,6 @@ export class CommentStore {
 	): { markedComment: Comment; index: number } | null {
 		const nextComments = Array.from(this._comments);
 		// The YJS types explicitly use `any` as well.
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		const sharedCommentsArray: YArray<any> | null =
 			this._getCollabComments();
 		let commentIndex: number | null = null;
@@ -266,19 +264,16 @@ export class CommentStore {
 		}
 	}
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	_getCollabComments(): null | YArray<any> {
 		const provider = this._collabProvider;
 		if (provider !== null) {
 			// @ts-expect-error doc does exist
 			const doc = provider.doc;
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			return doc.get('comments', YArray) as YArray<any>;
 		}
 		return null;
 	}
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	_createCollabSharedMap(commentOrThread: Comment | Thread): YMap<any> {
 		const sharedMap = new YMap();
 		const type = commentOrThread.type;
@@ -342,7 +337,6 @@ export class CommentStore {
 
 		const onSharedCommentChanges = (
 			// The YJS types explicitly use `any` as well.
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			events: Array<YEvent<any>>,
 			transaction: Transaction,
 		) => {
@@ -373,7 +367,6 @@ export class CommentStore {
 								insert
 									.slice()
 									.reverse()
-									// eslint-disable-next-line @typescript-eslint/no-explicit-any
 									.forEach((map: YMap<any>) => {
 										const id = map.get('id');
 										const type = map.get('type');
