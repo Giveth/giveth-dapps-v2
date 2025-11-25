0dfcea0eaaa367388c72d7d191bdbd719f6e654b
kkatusic <kkatusic@gmail.com>
2025-11-25 14:27:43 +0100

    fix eslint errors

--- src/components/rich-text-lexical/collaboration.ts
+++ src/components/rich-text-lexical/collaboration.ts
@@ -6,38 +6,38 @@
  *
  */
 
-import {Provider} from '@lexical/yjs';
-import {WebsocketProvider} from 'y-websocket';
-import {Doc} from 'yjs';
+import { Provider } from '@lexical/yjs';
+import { WebsocketProvider } from 'y-websocket';
+import { Doc } from 'yjs';
 
 const url = new URL(window.location.href);
 const params = new URLSearchParams(url.search);
 const WEBSOCKET_ENDPOINT =
-  params.get('collabEndpoint') || 'ws://localhost:1234';
+	params.get('collabEndpoint') || 'ws://localhost:1234';
 const WEBSOCKET_SLUG = 'playground';
 const WEBSOCKET_ID = params.get('collabId') || '0';
 
 // parent dom -> child doc
 export function createWebsocketProvider(
-  id: string,
-  yjsDocMap: Map<string, Doc>,
+	id: string,
+	yjsDocMap: Map<string, Doc>,
 ): Provider {
-  let doc = yjsDocMap.get(id);
+	let doc = yjsDocMap.get(id);
 
-  if (doc === undefined) {
-    doc = new Doc();
-    yjsDocMap.set(id, doc);
-  } else {
-    doc.load();
-  }
+	if (doc === undefined) {
+		doc = new Doc();
+		yjsDocMap.set(id, doc);
+	} else {
+		doc.load();
+	}
 
-  // @ts-expect-error
-  return new WebsocketProvider(
-    WEBSOCKET_ENDPOINT,
-    WEBSOCKET_SLUG + '/' + WEBSOCKET_ID + '/' + id,
-    doc,
-    {
-      connect: false,
-    },
-  );
+	// @ts-expect-error
+	return new WebsocketProvider(
+		WEBSOCKET_ENDPOINT,
+		WEBSOCKET_SLUG + '/' + WEBSOCKET_ID + '/' + id,
+		doc,
+		{
+			connect: false,
+		},
+	);
 }
