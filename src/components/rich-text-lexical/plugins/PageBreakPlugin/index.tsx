1523cdf9234a998da60accba232f5306661043b3
kkatusic <kkatusic@gmail.com>
2025-11-26 10:54:28 +0100

    fix lint errors

--- src/components/rich-text-lexical/plugins/PageBreakPlugin/index.tsx
+++ src/components/rich-text-lexical/plugins/PageBreakPlugin/index.tsx
@@ -6,55 +6,54 @@
  *
  */
 
-import type {JSX} from 'react';
-
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {$insertNodeToNearestRoot, mergeRegister} from '@lexical/utils';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { $insertNodeToNearestRoot, mergeRegister } from '@lexical/utils';
 import {
-  $getSelection,
-  $isRangeSelection,
-  COMMAND_PRIORITY_EDITOR,
-  createCommand,
-  LexicalCommand,
+	$getSelection,
+	$isRangeSelection,
+	COMMAND_PRIORITY_EDITOR,
+	createCommand,
+	LexicalCommand,
 } from 'lexical';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
-import {$createPageBreakNode, PageBreakNode} from '../../nodes/PageBreakNode';
+import { $createPageBreakNode, PageBreakNode } from '../../nodes/PageBreakNode';
+import type { JSX } from 'react';
 
 export const INSERT_PAGE_BREAK: LexicalCommand<undefined> = createCommand();
 
 export default function PageBreakPlugin(): JSX.Element | null {
-  const [editor] = useLexicalComposerContext();
+	const [editor] = useLexicalComposerContext();
 
-  useEffect(() => {
-    if (!editor.hasNodes([PageBreakNode])) {
-      throw new Error(
-        'PageBreakPlugin: PageBreakNode is not registered on editor',
-      );
-    }
+	useEffect(() => {
+		if (!editor.hasNodes([PageBreakNode])) {
+			throw new Error(
+				'PageBreakPlugin: PageBreakNode is not registered on editor',
+			);
+		}
 
-    return mergeRegister(
-      editor.registerCommand(
-        INSERT_PAGE_BREAK,
-        () => {
-          const selection = $getSelection();
+		return mergeRegister(
+			editor.registerCommand(
+				INSERT_PAGE_BREAK,
+				() => {
+					const selection = $getSelection();
 
-          if (!$isRangeSelection(selection)) {
-            return false;
-          }
+					if (!$isRangeSelection(selection)) {
+						return false;
+					}
 
-          const focusNode = selection.focus.getNode();
-          if (focusNode !== null) {
-            const pgBreak = $createPageBreakNode();
-            $insertNodeToNearestRoot(pgBreak);
-          }
+					const focusNode = selection.focus.getNode();
+					if (focusNode !== null) {
+						const pgBreak = $createPageBreakNode();
+						$insertNodeToNearestRoot(pgBreak);
+					}
 
-          return true;
-        },
-        COMMAND_PRIORITY_EDITOR,
-      ),
-    );
-  }, [editor]);
+					return true;
+				},
+				COMMAND_PRIORITY_EDITOR,
+			),
+		);
+	}, [editor]);
 
-  return null;
+	return null;
 }
