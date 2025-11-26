f86bf32de09661e10b1df2b672b2099e9f1cc25e
kkatusic <kkatusic@gmail.com>
2025-11-26 11:04:14 +0100

    fix lint errors

--- src/components/rich-text-lexical/utils/getDOMRangeRect.ts
+++ src/components/rich-text-lexical/utils/getDOMRangeRect.ts
@@ -6,22 +6,22 @@
  *
  */
 export function getDOMRangeRect(
-  nativeSelection: Selection,
-  rootElement: HTMLElement,
+	nativeSelection: Selection,
+	rootElement: HTMLElement,
 ): DOMRect {
-  const domRange = nativeSelection.getRangeAt(0);
+	const domRange = nativeSelection.getRangeAt(0);
 
-  let rect;
+	let rect;
 
-  if (nativeSelection.anchorNode === rootElement) {
-    let inner = rootElement;
-    while (inner.firstElementChild != null) {
-      inner = inner.firstElementChild as HTMLElement;
-    }
-    rect = inner.getBoundingClientRect();
-  } else {
-    rect = domRange.getBoundingClientRect();
-  }
+	if (nativeSelection.anchorNode === rootElement) {
+		let inner = rootElement;
+		while (inner.firstElementChild != null) {
+			inner = inner.firstElementChild as HTMLElement;
+		}
+		rect = inner.getBoundingClientRect();
+	} else {
+		rect = domRange.getBoundingClientRect();
+	}
 
-  return rect;
+	return rect;
 }
