3c33f5b544ff8d0552bcff8dbad6b81542638aca
kkatusic <kkatusic@gmail.com>
2025-11-26 09:35:44 +0100

    fix lint errors

--- src/components/rich-text-lexical/plugins/StickyPlugin/index.ts
+++ src/components/rich-text-lexical/plugins/StickyPlugin/index.ts
@@ -6,19 +6,20 @@
  *
  */
 
-import type {JSX} from 'react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { useEffect } from 'react';
 
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {useEffect} from 'react';
-
-import {StickyNode} from '../../nodes/StickyNode';
+import { StickyNode } from '../../nodes/StickyNode';
+import type { JSX } from 'react';
 
 export default function StickyPlugin(): JSX.Element | null {
-  const [editor] = useLexicalComposerContext();
-  useEffect(() => {
-    if (!editor.hasNodes([StickyNode])) {
-      throw new Error('StickyPlugin: StickyNode not registered on editor');
-    }
-  }, [editor]);
-  return null;
+	const [editor] = useLexicalComposerContext();
+	useEffect(() => {
+		if (!editor.hasNodes([StickyNode])) {
+			throw new Error(
+				'StickyPlugin: StickyNode not registered on editor',
+			);
+		}
+	}, [editor]);
+	return null;
 }
--- src/components/rich-text-lexical/plugins/TabFocusPlugin/index.tsx
+++ src/components/rich-text-lexical/plugins/TabFocusPlugin/index.tsx
@@ -6,15 +6,15 @@
  *
  */
 
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import {
-  $getSelection,
-  $isRangeSelection,
-  $setSelection,
-  COMMAND_PRIORITY_LOW,
-  FOCUS_COMMAND,
+	$getSelection,
+	$isRangeSelection,
+	$setSelection,
+	COMMAND_PRIORITY_LOW,
+	FOCUS_COMMAND,
 } from 'lexical';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 const TAB_TO_FOCUS_INTERVAL = 100;
 
@@ -22,44 +22,44 @@ let lastTabKeyDownTimestamp = 0;
 let hasRegisteredKeyDownListener = false;
 
 function registerKeyTimeStampTracker() {
-  window.addEventListener(
-    'keydown',
-    (event: KeyboardEvent) => {
-      // Tab
-      if (event.key === 'Tab') {
-        lastTabKeyDownTimestamp = event.timeStamp;
-      }
-    },
-    true,
-  );
+	window.addEventListener(
+		'keydown',
+		(event: KeyboardEvent) => {
+			// Tab
+			if (event.key === 'Tab') {
+				lastTabKeyDownTimestamp = event.timeStamp;
+			}
+		},
+		true,
+	);
 }
 
 export default function TabFocusPlugin(): null {
-  const [editor] = useLexicalComposerContext();
+	const [editor] = useLexicalComposerContext();
 
-  useEffect(() => {
-    if (!hasRegisteredKeyDownListener) {
-      registerKeyTimeStampTracker();
-      hasRegisteredKeyDownListener = true;
-    }
+	useEffect(() => {
+		if (!hasRegisteredKeyDownListener) {
+			registerKeyTimeStampTracker();
+			hasRegisteredKeyDownListener = true;
+		}
 
-    return editor.registerCommand(
-      FOCUS_COMMAND,
-      (event: FocusEvent) => {
-        const selection = $getSelection();
-        if ($isRangeSelection(selection)) {
-          if (
-            lastTabKeyDownTimestamp + TAB_TO_FOCUS_INTERVAL >
-            event.timeStamp
-          ) {
-            $setSelection(selection.clone());
-          }
-        }
-        return false;
-      },
-      COMMAND_PRIORITY_LOW,
-    );
-  }, [editor]);
+		return editor.registerCommand(
+			FOCUS_COMMAND,
+			(event: FocusEvent) => {
+				const selection = $getSelection();
+				if ($isRangeSelection(selection)) {
+					if (
+						lastTabKeyDownTimestamp + TAB_TO_FOCUS_INTERVAL >
+						event.timeStamp
+					) {
+						$setSelection(selection.clone());
+					}
+				}
+				return false;
+			},
+			COMMAND_PRIORITY_LOW,
+		);
+	}, [editor]);
 
-  return null;
+	return null;
 }
