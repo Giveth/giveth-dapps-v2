db6f063f17c881a53bf1ad62382f6d7a463ffe17
kkatusic <kkatusic@gmail.com>
2025-11-26 09:28:45 +0100

    fix lint errors

--- src/components/rich-text-lexical/plugins/SpecialTextPlugin/index.ts
+++ src/components/rich-text-lexical/plugins/SpecialTextPlugin/index.ts
@@ -5,69 +5,72 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-import type {LexicalEditor} from 'lexical';
-import type {JSX} from 'react';
 
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {TextNode} from 'lexical';
-import {useEffect} from 'react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { TextNode } from 'lexical';
+import { useEffect } from 'react';
 
 import {
-  $createSpecialTextNode,
-  SpecialTextNode,
+	$createSpecialTextNode,
+	SpecialTextNode,
 } from '../../nodes/SpecialTextNode';
+import type { JSX } from 'react';
+import type { LexicalEditor } from 'lexical';
 
 const BRACKETED_TEXT_REGEX = /\[([^\[\]]+)\]/; // eslint-disable-line
 
 function $findAndTransformText(node: TextNode): null | TextNode {
-  const text = node.getTextContent();
+	const text = node.getTextContent();
 
-  const match = BRACKETED_TEXT_REGEX.exec(text);
-  if (match) {
-    const matchedText = match[1];
-    const startIndex = match.index;
+	const match = BRACKETED_TEXT_REGEX.exec(text);
+	if (match) {
+		const matchedText = match[1];
+		const startIndex = match.index;
 
-    let targetNode;
-    if (startIndex === 0) {
-      [targetNode] = node.splitText(startIndex + match[0].length);
-    } else {
-      [, targetNode] = node.splitText(startIndex, startIndex + match[0].length);
-    }
+		let targetNode;
+		if (startIndex === 0) {
+			[targetNode] = node.splitText(startIndex + match[0].length);
+		} else {
+			[, targetNode] = node.splitText(
+				startIndex,
+				startIndex + match[0].length,
+			);
+		}
 
-    const specialTextNode = $createSpecialTextNode(matchedText);
-    targetNode.replace(specialTextNode);
-    return specialTextNode;
-  }
+		const specialTextNode = $createSpecialTextNode(matchedText);
+		targetNode.replace(specialTextNode);
+		return specialTextNode;
+	}
 
-  return null;
+	return null;
 }
 
 function $textNodeTransform(node: TextNode): void {
-  let targetNode: TextNode | null = node;
+	let targetNode: TextNode | null = node;
 
-  while (targetNode !== null) {
-    if (!targetNode.isSimpleText()) {
-      return;
-    }
+	while (targetNode !== null) {
+		if (!targetNode.isSimpleText()) {
+			return;
+		}
 
-    targetNode = $findAndTransformText(targetNode);
-  }
+		targetNode = $findAndTransformText(targetNode);
+	}
 }
 
 function useTextTransformation(editor: LexicalEditor): void {
-  useEffect(() => {
-    if (!editor.hasNodes([SpecialTextNode])) {
-      throw new Error(
-        'SpecialTextPlugin: SpecialTextNode not registered on editor',
-      );
-    }
+	useEffect(() => {
+		if (!editor.hasNodes([SpecialTextNode])) {
+			throw new Error(
+				'SpecialTextPlugin: SpecialTextNode not registered on editor',
+			);
+		}
 
-    return editor.registerNodeTransform(TextNode, $textNodeTransform);
-  }, [editor]);
+		return editor.registerNodeTransform(TextNode, $textNodeTransform);
+	}, [editor]);
 }
 
 export default function SpecialTextPlugin(): JSX.Element | null {
-  const [editor] = useLexicalComposerContext();
-  useTextTransformation(editor);
-  return null;
+	const [editor] = useLexicalComposerContext();
+	useTextTransformation(editor);
+	return null;
 }
